faa72d2219936e9275898eae78d27cc0519e2509
Minna Win <minnawin@eyewall.rap.ucar.edu>
2016-09-07 23:22:01 +0000

    not quite working, but can run the run series analysis app.  In the process of generating support for plotting the animated plots.

--- ush/run_series_analysis.py
+++ ush/run_series_analysis.py
@@ -39,14 +39,11 @@ def run_series_analysis(type):
     out_dir = p.opt["OUT_DIR"]
     proj_dir = p.opt["PROJ_DIR"]
 
-    series_analysis_exe = p.opt["SERIES_ANALYSIS"]
     plot_data_plane_exe = p.opt["PLOT_DATA_PLANE"]
     var_list = p.opt["VAR_LIST"]
+    stat_list = p.opt["STAT_LIST"]
     init_time_list = p.opt["INIT_LIST"]
-    log_dir = p.opt["LOG_DIR"]
-    log_ = p.opt["LOG_FILENAME"]
     logger = util.get_logger(p)
-   
     
     cur_ = sys._getframe().f_code.co_filename
     cur_function = sys._getframe().f_code.co_name 
@@ -60,7 +57,7 @@ def run_series_analysis(type):
     if series_method_type == 'LEAD':
         # Series analysis based on lead (forecast hour)
         try:
-            analysis_by_lead_time(proj_dir, fhr_beg, fhr_end, out_dir, var_list, logger, p)
+            analysis_by_lead_time(proj_dir, fhr_beg, fhr_end, out_dir, var_list, stat_list, logger, p)
                 
         except OSError as e:
             # Since Python 2.6 doesn't have FileNotFoundError until Python 3.3 
@@ -80,7 +77,7 @@ def run_series_analysis(type):
           
 
     
-def analysis_by_lead_time(proj_dir, fhr_beg, fhr_end, out_dir, var_list,logger, p):
+def analysis_by_lead_time(proj_dir, fhr_beg, fhr_end, out_dir, var_list, stat_list, logger, p):
     '''Invoke the series_analysis script based on lead time (forecast hour) 
        Create the command:
          series_analysis -fcst <OUT_DIR>/FCST_FILES_F<CUR_FHR>
@@ -100,6 +97,8 @@ def analysis_by_lead_time(proj_dir, fhr_beg, fhr_end, out_dir, var_list,logger, 
 
         var_list:   A list of variables to process.
  
+        stat_list:  A list of statistics of interest.
+
         logger  :   The logging object to which logging will be saved.
 
         p:          The handle to the config master.  
@@ -113,7 +112,6 @@ def analysis_by_lead_time(proj_dir, fhr_beg, fhr_end, out_dir, var_list,logger, 
 
 
     '''
-    logger.info("Creating series analysis command for init time")
     
     # Check for the existence of the storm track tiles and raise an error if these are missing.
     # Get a list of the grb2 forecast tiles in <proj_dir>/series_analysis/*/*/FCST_TILE_F<cur_fhr>.grb2
@@ -125,6 +123,7 @@ def analysis_by_lead_time(proj_dir, fhr_beg, fhr_end, out_dir, var_list,logger, 
     fcst_tiles = get_files(tile_dir,fcst_regex)
     num_anly_tiles = len(anly_tiles)
     num_fcst_tiles = len(fcst_tiles)
+    series_analysis_exe = p.opt["SERIES_ANALYSIS"]
 
     # Preliminary check for mismatched fcst and analysis
     # regridded tiles.
@@ -132,23 +131,20 @@ def analysis_by_lead_time(proj_dir, fhr_beg, fhr_end, out_dir, var_list,logger, 
         # Something is wrong, we are missing
         # either an ANLY tile file or a FCST tile
         # file, this presages an issue.
-        print("WARNING: There are a different number of anly and fcst tiles, there should be the same number...")
         logger.warn("WARNING: There are a different number of anly and fcst tiles, there should be the same number...")
          
     # Check that there are analysis and forecast tiles (which were, or should have been created earlier by extract_tiles).
     if num_anly_tiles == 0 :
         # Cannot proceed, either the necessary 30x30 degree analysis tiles are missing
         logger.error("ERROR: No anly tile files were found  "+ tile_dir)
-        print("ERROR: no analysis tiles found")
         raise OSError("No anlysis tiles were found")
     elif num_fcst_tiles == 0 :
         # Cannot proceed, either the necessary 30x30 degree fcst tiles are missing
         logger.error("ERROR: No fcst tile files were found  "+ tile_dir)
-        print("ERROR: no fcst tiles found")
         raise OSError("No fcst tiles were found")
     else:
         # Create the command for running the series_analysis application
-        logger.info("Creating the command for series_analysis")    
+        logger.info("Creating the command for series_analysis...")    
     
         # Gather all the regridded 30x30 tiles that correspond to the lead time range and
         # save these to an ASCII file, one for analysis tiles and another for forecast tiles.
@@ -157,8 +153,9 @@ def analysis_by_lead_time(proj_dir, fhr_beg, fhr_end, out_dir, var_list,logger, 
         anly_file_regexp = ".*/([0-9]{8}_[0-9]{2}/.*/ANLY_TILE_F([0-9]{3}).*_([0-9]{8}_[0-9]{4}_[0-9]{3})).grb2"
         fcst_file_regexp = ".*/([0-9]{8}_[0-9]{2}/.*/FCST_TILE_F([0-9]{3}).*_([0-9]{8}_[0-9]{4}_[0-9]{3})).grb2"
     
-        # Get the -config param file name
-        config_param = p.opt["CONFIG_BY_LEAD"]
+        # Get the series analysis config file based on the 
+        # type/methodology of  series analysis: by lead or by init time
+        series_anly_configuration_file = p.opt["CONFIG_BY_LEAD"]
    
         # Create the -fcst param and -obs param, these are just the
         # filenames of the ASCII files that contain a list of the
@@ -190,30 +187,85 @@ def analysis_by_lead_time(proj_dir, fhr_beg, fhr_end, out_dir, var_list,logger, 
 
 
             # For each FCST_TILE_F<cur_fhr>*.grb2 file, check if there is a corresponding
-            # ANLY_TILE_F<cur_fhr>*.grb2 file.  If it doesn't exist, log a warning and 
-            # proceed to the next FCST_TILE_F file
+            # ANLY_TILE_F<cur_fhr>*.grb2 file.  If it doesn't exist, log this info and 
+            # proceed to the next FCST_TILE_F file, since there isn't always a one-to-one
+            # match between all FCST and ANLY tile files.  The ANLY Tile files only encompass 
+            # the 000 lead time (fcst hour), whereas the FCST Tile files encompass 003, 006, 012,
+            # 018, 024, 030, ...etc. hour lead times (forecast hours). Expect only one ANLY file
+            # to match for every yyyymmdd_HH (init time) grouping.
             anly_tile_file = find_matching_tile(fcst)
-
-           # Create two ASCII files, one which lists all the ANLY tile files and another for 
-           # all the FCST tile files
-            if anly_tile_file == None:
-                print("WARNING, no corresponding analysis file for fcst file{}".format(fcst_param))
-                logger.warn("WARNING: No corresponding analysis file found for forecast file." +  fcst_param)
-            else:
+            if anly_tile_file not in anly_tiles:
+                logger.info("No analysis tile file "+ anly_tile_file +" for this forecast hour and fcst file " + fcst "  continuing...")
+            
+            else: 
+                # Create two ASCII files, one which lists all the ANLY tile files and another for 
+                # all the FCST tile files
                 # Proceed with saving/appending the analysis and forecast filenames to their corresponding ASCII files.
                 # ASCII file containing all FCST files
-                fcst_ascii = create_ascii_file('FCST', ascii_fcst_tiles_filename, fcst, logger)  
-                anly_ascii = create_ascii_file('ANLY', ascii_anly_tiles_filename, anly_tile_file, logger)  
+                create_ascii_file('FCST', ascii_fcst_tiles_filename, fcst, logger)  
+                create_ascii_file('ANLY', ascii_anly_tiles_filename, anly_tile_file, logger)  
+    
+                # Create the -fcst and -obs params
+                fcst_param_list = ['-fcst ', ascii_fcst_tiles_filename]
+                fcst_param = ''.join(fcst_param_list)
+                obs_param_list = ['-obs ', ascii_anly_tiles_filename]
+                obs_param = ''.join(obs_param_list)
+                logger.info("fcst param: "+fcst_param)
+                logger.info("obs param: "+ obs_param)
 
-            # Create the -out param and invoke the series_analysis script
-#            for cur_var in var_list:
-                # Get the name and level which are needed to create the -out param
-#                match = re.match(r'(.*)/(.*)',cur_var)
-#                name = match.group(1)
-#                level = match.group(2)
-#                out_param_list = [out_dir,'/series_F',fcst_hr,'/', 'series_F',fcst_hr,'_', name, '_', level, '.nc']
-#                out_param = ''.join(out_param_list)  
-                    
+                # Create the series analysis command
+                config_param_list = ['-config  ', series_anly_configuration_file]
+                config_param = ''.join(config_param_list)
+    
+
+                # Create the -out param and invoke the series_analysis script
+                for cur_var in var_list:
+                    # Get the name and level which are needed to create the -out param
+                    # and set the NAME and LEVEL environment variables which are required by the 
+                    # series_analysis application
+                    match = re.match(r'(.*)/(.*)',cur_var)
+                    name = match.group(1)
+                    level = match.group(2)
+                    os.environ['NAME'] = name
+                    os.environ['LEVEL'] = level  
+                    out_param_list = ['-out ', out_dir,'/series_F',fcst_hr,'/', 'series_F',fcst_hr,'_', name, '_', level, '.nc']
+                    out_param = ''.join(out_param_list)  
+                    logger.info("out param: "+ out_param)
+    
+                # Create the full series analysis command
+                series_analysis_cmd_list = [series_analysis_exe,' ', fcst_param, ' ', obs_param, ' ', config_param, ' ', out_param]
+                series_analysis_cmd = ''.join(series_analysis_cmd_list)
+                logger.info("series analysis cmd: "+series_analysis_cmd)
+                os.system(series_analysis_cmd)                    
+
+    # Create the output directory for the animation plots
+    animation_dir = os.path.join(proj_dir, '/series_analysis/series_animat')
+    logger.info("Creating animation directory " + animation_dir
+
+    # Generate a plot for each variable of interest
+    for var in var_list:
+        match = re.match(r'(.*)/(.*)',cur_var)
+        name = match.group(1)
+        level = match.group(2)
+        os.environ['NAME'] = name
+        os.environ['LEVEL'] = level  
+            
+        for stat in stat_list:
+            # Set the CUR_STAT environment variable required by the plot data plane binary  
+            os.environ['CUR_STAT'] = stat
+
+            # Retrieve all the netCDF files that were created by the run series analysis step above
+            anly_regex = "ANLY_TILE_F[0-9]{3}.*.grb2"
+            anly_tiles = get_files(tile_dir,anly_regex)
+            nc_regex = "*.nc"
+            # TODO finish this section, figure out how to get the dir where the netCDF files are saved 
+            # as a result of running the run series analysis step 
+            nc_dir_parts = [proj_dir,'/series_analysis/series_F]
+            nc_dir = ''.join(nc_dir_parts)
+            
+
+             
+
 
 def create_ascii_file(type, ascii_tiles_filename, tiles_filename, logger):
     ''' Create an ASCII file containing a list of the 30 x 30 tile files that
@@ -233,17 +285,15 @@ def create_ascii_file(type, ascii_tiles_filename, tiles_filename, logger):
 
     '''
 
-    print("inside create ascii,  tiles filename: {}".format(tiles_filename))
+    
     try:
         with open(ascii_tiles_filename, 'a') as ascii_output_file:
-            print("!!WRITING to ASCII FILE {}".format(tiles_filename))
             ascii_output_file.write(tiles_filename)   
+            ascii_output_file.write("\n")
             
     except IOError as e:
         logger.error("Could not create requested ASCII file ") 
 
-    else:
-        return ascii_output_file
 
 
 def find_matching_tile(fcst_file):
@@ -303,6 +353,6 @@ def get_files(filedir, filename_regex):
 
 
 if __name__ == "__main__":
-    run_series_analysis("lead" )
+    run_series_analysis("lead")
 #    run_series_analysis("init")
 
