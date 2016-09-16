12b3d763ddabe04f3eda017c10d937a7b69c8f3f
Julie Prestopnik <jpresto@eyewall.rap.ucar.edu>
2016-09-16 17:19:00 +0000

    Modified working area paths

--- parm/feature_relative_config_jpresto.py
+++ parm/feature_relative_config_jpresto.py
@@ -11,23 +11,23 @@ import datetime
 #
 
 #Levels: DEBUG, INFO, WARNING, ERROR, CRITICAL
-LOG_DIR = "/d1/jpresto/sbu_wip/MySBU_WIP/MySBU_WIP/SBU_util/logs"
+LOG_DIR = "/d1/jpresto/my_sbu_wip/logs"
 LOG_LEVEL = "DEBUG"
 LOG_FILENAME = os.path.join(LOG_DIR, "feature_relative_config_jpresto." + datetime.datetime.now().strftime("%Y%m%d") + ".log")
 
 #
 # Output Directories
 # 
-OUT_DIR = "/d1/jpresto/sbu_wip/MySBU_WIP/MySBU_WIP/SBU_util/out/series_analysis"
+OUT_DIR = ""
 
 #
 # Project Directories
 #
 
-PROJ_DIR = "/d1/jpresto/sbu_wip/MySBU_WIP/MySBU_WIP/SBU_util/data"
+PROJ_DIR = "/d1/jpresto/my_sbu_wip/data"
 GFS_DIR = os.path.join(PROJ_DIR, "model_data")
 TRACK_DATA_DIR = os.path.join(PROJ_DIR, "track_data")
 TC_PAIRS_DIR = os.path.join(PROJ_DIR, "tc_pairs")
 
 # For tc pairs
-TC_PAIRS_CONFIG_PATH = "/d1/jpresto/sbu_wip/MySBU_WIP/MySBU_WIP/SBU_util/parm/TCPairsETCConfig"
+TC_PAIRS_CONFIG_PATH = "/d1/jpresto/my_sbu_wip/parm/TCPairsETCConfig"
