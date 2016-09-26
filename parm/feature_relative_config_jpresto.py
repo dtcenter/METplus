f1485aa935e2a2f11cb2abe19e0bd726420ffbe2
Julie Prestopnik <jpresto@eyewall.rap.ucar.edu>
2016-09-26 20:23:27 +0000

    Updated paths

--- parm/feature_relative_config_jpresto.py
+++ parm/feature_relative_config_jpresto.py
@@ -16,17 +16,26 @@ LOG_LEVEL = "DEBUG"
 LOG_FILENAME = os.path.join(LOG_DIR, "feature_relative_config_jpresto." + datetime.datetime.now().strftime("%Y%m%d") + ".log")
 
 #
+# Lists
+#
+
+# Used for performing series analysis both for lead time and init time
+INIT_LIST = ["20150126_00", "20150126_12", "20150127_12"]
+
+
+#
 # Output Directories
 # 
-OUT_DIR = ""
+OUT_DIR = "/d1/jpresto/my_sbu_wip/data/series_analysis"
 
 #
 # Project Directories
 #
 
 PROJ_DIR = "/d1/jpresto/my_sbu_wip/data"
-GFS_DIR = os.path.join(PROJ_DIR, "model_data")
-TRACK_DATA_DIR = os.path.join(PROJ_DIR, "track_data")
+#GFS_DIR = os.path.join(PROJ_DIR, "model_data")
+GFS_DIR = "/d1/SBU/GFS/model_data"
+TRACK_DATA_DIR = "/d1/SBU/GFS/track_data"
 TC_PAIRS_DIR = os.path.join(PROJ_DIR, "tc_pairs")
 
 # For tc pairs
