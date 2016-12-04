6f3fb9bff32d6cd35a79ac615b425d2b2a1659af
Minna Win <minnawin@eyewall.rap.ucar.edu>
2016-12-04 21:59:19 +0000

    Change invocation of tc_stat due to change in function signature change.
    Also clean up long lines.

--- ush/extract_tiles.py
+++ ush/extract_tiles.py
@@ -47,7 +47,7 @@ def main():
     output_dir = p.opt["OUT_DIR"]
     project_dir = p.opt["PROJ_DIR"]
     overwrite_flag = p.opt["OVERWRITE_TRACK"]
-    filter_opts = p.opt["EXTRACT_TILES_FILTER_OPTS"]
+    addl_filter_opts = p.opt["EXTRACT_TILES_FILTER_OPTS"]
     filtered_out_dir = p.opt["EXTRACT_OUT_DIR"]
     tc_stat_exe = p.opt["TC_STAT"]
 
@@ -57,43 +57,62 @@ def main():
     cur_pid = str(os.getpid())
     tmp_dir = os.path.join(p.opt["TMP_DIR"], cur_pid)
    
-    # Logging output: TIME UTC |TYPE (DEBUG, INFO, WARNING, etc.) | [File : function]| Message
-    logger.info("INFO |  [" + cur_filename +  ":" + "cur_function] |" + "BEGIN extract_tiles")
+    # Logging output: TIME UTC |TYPE (DEBUG, INFO, WARNING, etc.) |
+    # [File : function]| Message logger.info("INFO |  [" + 
+    # cur_filename +  ":" + "cur_function] |" + "BEGIN extract_tiles")
     
     # Process TC pairs by initialization time
     for cur_init in init_times:
-        logger.info("INFO| [" + cur_filename + ":" + cur_function +  " ] |Begin processing for initialization time: " + cur_init)
+        msg = ("INFO| [" + cur_filename + ":" + cur_function +  
+               " ] |Begin processing for initialization time: " + cur_init)
+        logger.info(msg)
         year_month = util.extract_year_month(cur_init, logger)
         
-       # Create the name of the filter file we need to find.  If the file doesn't exist, then run TC_STAT 
+       # Create the name of the filter file we need to find.  If 
+       # the file doesn't exist, then run TC_STAT 
         filter_filename = "filter_" + cur_init + ".tcst"
         filter_name = os.path.join(filtered_out_dir, cur_init, filter_filename)
 
         if util.file_exists(filter_name) and overwrite_flag == False:
-            logger.info("INFO| [" + cur_filename + ":" + cur_function +  " ] | Filter file exists, using Track data file: " + filter_name)
+            msg = ("INFO| [" + cur_filename + ":" + cur_function +  
+                   " ] | Filter file exists, using Track data file: " + 
+                   filter_name)
+            logger.info(msg)
         else:
            # Create the storm track
             filter_path = os.path.join(filtered_out_dir, cur_init)
             util.mkdir_p(filter_path)
-            tc_cmd_list = [tc_stat_exe, " -job filter -lookin ", project_dir,"/tc_pairs/", year_month, " -init_inc ", cur_init, " -match_points true -dump_row ", filter_name]
+            tc_cmd_list = [tc_stat_exe, " -job filter -lookin ", 
+                           project_dir,"/tc_pairs/", year_month, 
+                           " -init_inc ", cur_init, 
+                           " -match_points true -dump_row ", 
+                           filter_name, " ", addl_filter_opts]
 
             # Perform any filtering by calling run_tc_stat
             tc_cmd = ''.join(tc_cmd_list)
-            tcs.tc_stat(p, logger, filter_opts, cur_init, tc_cmd, filtered_out_dir)
-            logger.info("INFO| [" + cur_filename + ":" + cur_function +  " ] | tc command: " + tc_cmd)
+            tcs.tc_stat(p, logger, tc_cmd, 
+                        filtered_out_dir)
+            msg = ("INFO| [" + cur_filename + ":" + cur_function +  
+                   " ] | tc command: " + tc_cmd)
+            logger.info(msg)
             
-        # Now get unique storm ids from the filter file, filter_yyyymmdd_hh.tcst
+        # Now get unique storm ids from the filter file, 
+        # filter_yyyymmdd_hh.tcst
         sorted_storm_ids = get_storm_ids(filter_name, logger)
        
         # Process each storm in the sorted_storm_ids list
-        # Iterate over each filter file in the output directory and search for the
-        # presence of the storm id.  Store this corresponding row of data into
-        # a temporary file in the /tmp/<pid> directory.
+        # Iterate over each filter file in the output directory and 
+        # search for the presence of the storm id.  Store this 
+        # corresponding row of data into a temporary file in the 
+        # /tmp/<pid> directory.
        
         storm_match_list = [] 
         for cur_storm in sorted_storm_ids:
-            logger.info("INFO| [" + cur_filename + ":" + cur_function +  " ] | Processing storm: " + cur_storm)
-            storm_output_dir = os.path.join(filtered_out_dir,cur_init, cur_storm)
+            msg = ("INFO| [" + cur_filename + ":" + cur_function +  
+                   " ] | Processing storm: " + cur_storm)
+            logger.info(msg)
+            storm_output_dir = os.path.join(filtered_out_dir,cur_init, 
+                                            cur_storm)
             util.mkdir_p(storm_output_dir)
             util.mkdir_p(tmp_dir)
             tmp_file = "filter_" + cur_init + "_" + cur_storm
@@ -104,8 +123,8 @@ def main():
                for storm_match in storm_match_list:
                    tmp_file.write(storm_match)
                
-            # Peform regridding of the forecast and analysis files to a 30 x 30 degree tile
-            # centered on the storm
+            # Peform regridding of the forecast and analysis files 
+            # to a 30 x 30 degree tile centered on the storm
             regrid_fcst_anly(tmp_filename, cur_init, cur_storm, logger, p)
 
         # end of for cur_storm 
@@ -123,8 +142,9 @@ def regrid_fcst_anly(tmp_filename, cur_init, cur_storm, logger, p):
         regridding 
    
         Args:
-        tmp_filename:      Filename of the temporary filter file in the /tmp directory
-                           which contains rows of data corresponding to a storm id of varying
+        tmp_filename:      Filename of the temporary filter file in 
+                           the /tmp directory which contains rows 
+                           of data corresponding to a storm id of varying
                            lead times.
 
         cur_init:          The current init time
@@ -135,16 +155,20 @@ def regrid_fcst_anly(tmp_filename, cur_init, cur_storm, logger, p):
         p          :       ConfigMaster constants file
      
         Returns:
-        None:              Performs regridding via invoking wgrib2 on the forecast and 
-                           analysis files to a 30 x 30 degree tile centered on the storm:
+        None:              Performs regridding via invoking wgrib2 
+                           on the forecast and analysis files to 
+                           a 30 x 30 degree tile centered on the storm:
                            latlon lon0:nlon:dlon lat0:nlat:dlat
-                           NOTE:  the values for nlon, dlon and lat and lon adjustment values
-                           are stored in the extract_tiles_parm parameter/config file.
+                           NOTE:  the values for nlon, dlon and lat and 
+                                  lon adjustment values are stored in 
+                                  the extract_tiles_parm parameter/config 
+                                  file.
 
     '''
 
-    # Extract the columns of interest: init time, lead time, valid time lat and lon of both
-    # tropical cyclone tracks, etc. Then calculate the forecast hour and other things.
+    # Extract the columns of interest: init time, lead time, 
+    # valid time lat and lon of both  tropical cyclone tracks, etc. 
+    # Then calculate the forecast hour and other things.
    
     cur_filename = sys._getframe().f_code.co_filename
     cur_function = sys._getframe().f_code.co_name
@@ -160,10 +184,11 @@ def regrid_fcst_anly(tmp_filename, cur_init, cur_storm, logger, p):
         for line in tf:
             col = line.split()
             # Columns of interest are 8, 9, 10, 19, 20, 21, and 22
-            # for init time, lead time, valid time, alat, alon, blat, and blon (positions of the
-            # two extra-tropical cyclone tracks)
-            # but Python is zero-based so indices differ by 1.
-            init, lead, valid, alat, alon, blat, blon = col[7], col[8], col[9], col[18], col[19], col[20], col[21]
+            # for init time, lead time, valid time, alat, alon, 
+            # blat, and blon (positions of the two extra-tropical
+            # cyclone tracks)  but Python is zero-based so indices 
+            # differ by 1.
+            init, lead, valid, alat, alon, blat, blon = col[7], col[8], col[9],                                                         col[18], col[19],                                                               col[20], col[21]
 
             # integer division for both Python 2 and 3
             lead_time = int(lead)
@@ -204,34 +229,77 @@ def regrid_fcst_anly(tmp_filename, cur_init, cur_storm, logger, p):
             fcst_dir = os.path.join(gfs_dir, init_ymd)
             init_ymdh_split = init_ymdh.split("_")
             init_YYYYmmddHH = "".join(init_ymdh_split)
-            fcstSTS = sts.StringTemplateSubstitution(logger, p.opt["GFS_FCST_FILE_TMPL"], init=init_YYYYmmddHH, lead=lead_str)
+            fcstSTS = sts.StringTemplateSubstitution(logger,
+                                                     p.opt["GFS_FCST_FILE_TMPL"]                                                     , init=init_YYYYmmddHH, 
+                                                     lead=lead_str)
+
             fcst_file = fcstSTS.doStringSub()
             fcst_filename = os.path.join(fcst_dir, fcst_file)
 
             anly_dir =  os.path.join(gfs_dir, valid_ymd)
             valid_ymdh_split = valid_ymdh.split("_")
             valid_YYYYmmddHH = "".join(valid_ymdh_split)
-            anlySTS = sts.StringTemplateSubstitution(logger, p.opt["GFS_ANLY_FILE_TMPL"], valid=valid_YYYYmmddHH, lead=lead_str)
+            anlySTS = sts.StringTemplateSubstitution(logger, 
+                                                     p.opt["GFS_ANLY_FILE_TMPL"]                                                     , valid=valid_YYYYmmddHH,
+                                                     lead=lead_str)
             anly_file = anlySTS.doStringSub()
             anly_filename = os.path.join(anly_dir, anly_file)
             
+            # Create the tmp file to be used for troubleshooting 
+            # and verification.  The file will contain all the 
+            # fcst and analysis files that will be used as input 
+            # for another script.
+            tmp_fcst_filename = os.path.join(filtered_out_dir, 
+                                             "tmp_fcst_regridded.txt")
+            tmp_anly_filename = os.path.join(filtered_out_dir, 
+                                             "tmp_anly_regridded.txt")
 
-            # Check if the forecast file exists. If it doesn't exist, just log it
+            # Check if the forecast file exists. If it doesn't 
+            # exist, just log it
             if util.file_exists(fcst_filename):
-                logger.info("INFO| [" + cur_filename + ":" + cur_function +  " ] | Forecast file: " + fcst_filename)
+                msg = ("INFO| [" + cur_filename + ":" + cur_function +  
+                       " ] | Forecast file: " + fcst_filename)
+                logger.info(msg)
+                # Write this to the tmp file (to be used for 
+                # troubleshooting and validation) which will be saved
+                # in the EXTRACT_OUT_DIR
+                with open(tmp_fcst_filename, "a+") as tmpfile:
+                    tmpfile.write(fcst_filename+"\n")
+                
             else:
-                logger.warn("WARNING| [" + cur_filename + ":" + cur_function +  " ] | Can't find forecast file, continuing anyway: " + fcst_filename)
+                msg = ("WARNING| [" + cur_filename + ":" + 
+                       cur_function +  " ] | " +
+                       "Can't find forecast file, continuing anyway: " + 
+                       fcst_filename)
+                logger.warn(msg)
                 continue
 
-            # Check if the analysis file exists. If it doesn't exist, just log it.
+            # Check if the analysis file exists. If it doesn't 
+            # exist, just log it.
             if util.file_exists(anly_filename):
-                    logger.info("INFO| [" + cur_filename + ":" + cur_function +  " ] | Analysis file: " + anly_filename)
+                    msg = ("INFO| [" + cur_filename + ":" +
+                           cur_function +  " ] | Analysis file: " + 
+                           anly_filename)
+                    logger.info(msg)
+                    # Write this to the tmp file (to be used for 
+                    # troubleshooting and validation). This will
+                    # be stored in the EXTRACT_OUT_DIR
+                    with open(tmp_anly_filename, "a+") as tmpfile:
+                        tmpfile.write(fcst_filename+"\n")
             else:
-                logger.warn("WARNING| [" + cur_filename + ":" + cur_function +  " ] | Can't find analysis file, continuing anyway: " + anly_filename)
+                msg = ("WARNING| [" + cur_filename + ":" + 
+                       cur_function +  " ] | " + 
+                       "Can't find analysis file, continuing anyway: " +
+                       anly_filename)
+                logger.warn(msg)
                 continue
 
-            # Regrid the forecast and analysis files to a 30 degree X 30 degree tile centered on
+            # Regrid the forecast and analysis files to an n degree X 
+            # m degree tile centered on
             # latlon lon0:nlon:dlon lat0:nlat:dlat
+            # The size of the grid is defined in the constants_pdef.py
+            # param/config file, under the LON_ADJ and LAT_ADJ 
+            # variables.
         
             fcst_base = os.path.basename(fcst_filename)
             anly_base = os.path.basename(anly_filename)
@@ -241,35 +309,55 @@ def regrid_fcst_anly(tmp_filename, cur_init, cur_storm, logger, p):
             tile_dir = os.path.join(filtered_out_dir, cur_init, cur_storm)
             fcst_hr_str = str(fcst_hr).zfill(3)
             
-            fcst_tile_filename = p.opt["FCST_TILE_PREFIX"] + fcst_hr_str + "_" + fcst_base
+            fcst_tile_filename = p.opt["FCST_TILE_PREFIX"] + fcst_hr_str                                         + "_" + fcst_base
             fcst_tile_file = os.path.join(tile_dir, fcst_tile_filename)
-            anly_tile_filename = p.opt["ANLY_TILE_PREFIX"] + fcst_hr_str + "_" + anly_base
+            anly_tile_filename =  p.opt["ANLY_TILE_PREFIX"] + fcst_hr_str                                         + "_" + anly_base
             anly_tile_file = os.path.join(tile_dir, anly_tile_filename)
-
             
-            # Invoke wgrib2 on the fcst file only if a fcst tile file does NOT already exist.
+            # Regrid via wgrib2 on the fcst file only if a fcst tile 
+            # file does NOT already exist.
 
             # Create new gridded file for fcst tile
             if util.file_exists(fcst_tile_file):
-                logger.info("INFO| [" + cur_filename + ":" + cur_function +  " ] | Forecast tile file " + fcst_tile_file + " exists, skip wgrib2 regridding")
+                msg = ("INFO| [" + cur_filename + ":" + 
+                       cur_function +  " ] | Forecast tile file " + 
+                       fcst_tile_file + " exists, skip wgrib2 regridding")
+                logger.info(msg)
             else:
                 # Invoke wgrb2 to perform regridding on the records of interest
-                fcst_cmd_list= [wgrib2_exe, ' ' , fcst_filename, ' | ', egrep_exe, ' "',requested_records, '"|', wgrib2_exe, ' -i ', fcst_filename, ' -new_grid ', fcst_tile_grid, ' ', fcst_tile_file]
+                fcst_cmd_list= [wgrib2_exe, ' ' , fcst_filename, ' | ', 
+                                egrep_exe, ' "',requested_records, '"|', 
+                                wgrib2_exe, ' -i ', fcst_filename,
+                                ' -new_grid ', fcst_tile_grid, ' ', 
+                                fcst_tile_file]
                 wgrb_cmd_fcst = ''.join(fcst_cmd_list)
-                wgrb_fcst_out = subprocess.check_output(wgrb_cmd_fcst, stderr=subprocess.STDOUT, shell=True)
+                msg = ("INFO|[wgrib2]| Regridding via wgrib2:" + 
+                       wgrb_cmd_fcst)
+                logger.info(msg)
+                wgrb_fcst_out = subprocess.check_output(wgrb_cmd_fcst, 
+                                                        stderr=
+                                                        subprocess.STDOUT, 
+                                                        shell=True)
                 logger.info("INFO|[wgrib2]| on fcst file:" + wgrb_fcst_out)
             
             # Create new gridded file for anly tile
             if util.file_exists(anly_tile_file):
-                logger.info("INFO| [" + cur_filename + ":" + cur_function +  " ] | Analysis tile file: " + anly_tile_file + " exists, skip wgrib2 regridding")
+                logger.info("INFO| [" + cur_filename + ":" + 
+                            cur_function +  " ] | Analysis tile file: " + 
+                            anly_tile_file + " exists, skip wgrib2 regridding")
             else:
                 # Invoke wgrb2 to perform regridding on the records of interest
-                anly_cmd_list= [wgrib2_exe, ' ' , anly_filename, ' | ', egrep_exe, ' "', requested_records, '"|',wgrib2_exe, ' -i ', anly_filename, ' -new_grid ', anly_tile_grid, ' ', anly_tile_file, '>/dev/null']
+                anly_cmd_list= [wgrib2_exe, ' ' , anly_filename, ' | ', 
+                                egrep_exe, ' "', requested_records, '"|',
+                                wgrib2_exe, ' -i ', anly_filename, 
+                                ' -new_grid ', anly_tile_grid, ' ', 
+                                anly_tile_file, '>/dev/null']
                 wgrb_cmd_anly = ''.join(anly_cmd_list)
-                wgrb_anly_out = subprocess.check_output(wgrb_cmd_anly, stderr=subprocess.STDOUT, shell=True)
+                wgrb_anly_out = subprocess.check_output(wgrb_cmd_anly, 
+                                                        stderr=
+                                                        subprocess.STDOUT, 
+                                                        shell=True)
                 logger.info("INFO|[wgrib2]| on analysis file:" + wgrb_anly_out)
-        # end of 'for line in tf:'
-    # end of 'with open(tmp_filename, "r") as tf:'
 
 
 def get_storm_ids(filter_filename, logger):
@@ -287,7 +375,7 @@ def get_storm_ids(filter_filename, logger):
     cur_filename = sys._getframe().f_code.co_filename
     cur_function = sys._getframe().f_code.co_name
     storm_id_list = set()
-    if os.path.isfile(filter_filename) and os.path.getsize(filter_filename) > 0:
+    if os.path.isfile(filter_filename) and                                             os.path.getsize(filter_filename) > 0:
         with open(filter_filename) as fileobj:
              # skip the first line as it contains the header
              try:
@@ -334,9 +422,9 @@ def create_tile_grid_string(lat,lon,logger,p):
      # initialize the tile grid string
      # and get the other values from the parameter file
      tile_grid_str = ' '
-     lon_subtr = p.opt["LON_SUBTR"]
+     lon_subtr = p.opt["LON_ADJ"]
      adj_lon = float(lon) - lon_subtr
-     lat_subtr = p.opt["LAT_SUBTR"]
+     lat_subtr = p.opt["LAT_ADJ"]
      adj_lat = float(lat) - lat_subtr
      nlat = str(p.opt["NLAT"])
      nlon = str(p.opt["NLON"])
@@ -352,11 +440,11 @@ def create_tile_grid_string(lat,lon,logger,p):
      lat0 = str(util.round_0p5(adj_lat))
 
      #Format is: latlon lon0:nlon:dlat lat0:nlat:dlat
-     grid_list = ['latlon ', lon0, ':', nlon, ':', dlon, ' ',  lat0, ':', nlat, ':', dlat]
+     grid_list = ['latlon ', lon0, ':', nlon, ':', dlon, ' ',  
+                  lat0, ':', nlat, ':', dlat]
      tile_grid_str = ''.join(grid_list)
     
      return tile_grid_str
-
 
 
 
