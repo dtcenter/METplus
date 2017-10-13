7a22b824be37b758abad3b7152715e27f00a5f85
Minna Win <minnawin@ucar.edu>
2017-10-13 23:57:17 +0000

    Now runnable via master_metplus.py

--- ush/usage_wrapper.py
+++ ush/usage_wrapper.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
+import os
+import sys
+import produtil.setup
+from command_builder import CommandBuilder
+import config_metplus
+
 
 ## @namespace UsageWrapper
 # @brief Provides a default process for master_metplus.py.  Indicates what
@@ -10,18 +16,20 @@ from __future__ import print_function
 # usage_wrapper.py [-c /path/to/user.template.conf]
 # @endcode#
 
-class UsageWrapper(object):
+class UsageWrapper(CommandBuilder):
     """! A default process, prints out usage when nothing is defined in
          the PROCESS_LIST of the parm/metplus_config/metplus_runtime.conf
          and no lower level config files are included.
     """
-    def __init__(self):
+    def __init__(self, p, logger):
+        super(UsageWrapper, self).__init__(p, logger)
+        self.logger = logger
         self.available_processes = ['TcPairs', 'ExtractTiles', 'SeriesByInit',
                                     'SeriesByLead', 'PcpCombine',
                                     'RegridDataPlane',
                                     'GridStat', 'Mode', 'RegridDataPlane']
 
-    def main(self):
+    def run_all_times(self):
         print("This is a default process, please indicate more specific " +
               "processes in the PROCESS_LIST variable in one of the " +
               "following configuration files:\n " +
@@ -35,5 +43,24 @@ class UsageWrapper(object):
 
 
 if __name__ == "__main__":
-    uw = UsageWrapper()
-    uw.main()
+    try:
+        if 'JLOGFILE' in os.environ:
+            produtil.setup.setup(send_dbn=False, jobname='SeriesByLeadWrapper',
+                                 jlogfile=os.environ['JLOGFILE'])
+        else:
+            produtil.setup.setup(send_dbn=False, jobname='SeriesByLeadWrapper')
+        produtil.log.postmsg('SeriesByLeadWrapper is starting')
+
+        # Read in the configuration object CONFIG
+        CONFIG = config_metplus.setup()
+        if 'MET_BASE' not in os.environ:
+            os.environ['MET_BASE'] = CONFIG.getdir('MET_BASE')
+
+        UW = UsageWrapper(CONFIG, logger=None)
+        UW.run_all_times()
+
+        produtil.log.postmsg('series analysis by lead time completed')
+    except Exception as exc:
+        produtil.log.jlogger.critical(
+            'SeriesByLeadWrapper failed: %s' % (str(exc),), exc_info=True)
+        sys.exit(2)
