df4193f696f3c8eb19433cfbde8c6e658d46c12e
Minna Win <minnawin@ucar.edu>
2017-10-16 20:06:19 +0000

    Include imports that support the usage wrapper.

--- ush/master_met_plus.py
+++ ush/master_met_plus.py
@@ -26,6 +26,8 @@ from extract_tiles_wrapper import ExtractTilesWrapper
 from series_by_lead_wrapper import SeriesByLeadWrapper
 from series_by_init_wrapper import SeriesByInitWrapper
 #from mode_wrapper import ModeWrapper
+from usage_wrapper import UsageWrapper
+from command_builder import CommandBuilder
 
 '''!@var logger
 The logging.Logger for log messages
