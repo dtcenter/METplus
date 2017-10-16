945be923581bda9f6dbb32fb2fbb244abe77c086
Minna Win <minnawin@ucar.edu>
2017-10-16 20:04:50 +0000

    Build the MET executable path instead of requiring the GRID_STAT value under the exe section in the metplus_system.conf file.

--- ush/grid_stat_wrapper.py
+++ ush/grid_stat_wrapper.py
@@ -32,7 +32,9 @@ class GridStatWrapper(CommandBuilder):
 
     def __init__(self, p, logger):
         super(GridStatWrapper, self).__init__(p, logger)
-        self.app_path = self.p.getstr('exe', 'GRID_STAT')
+        met_build_base = p.getdir('MET_BUILD_BASE')
+        # self.app_path = self.p.getstr('exe', 'GRID_STAT')
+        self.app_path = os.path.join(met_build_base, 'bin/grid_stat')
         self.app_name = os.path.basename(self.app_path)
 
     def set_output_dir(self, outdir):
