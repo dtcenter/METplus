6bb49d1434e74b5360d31cc986f4c7e7dd029f8f
Minna Win <minnawin@ucar.edu>
2017-10-16 20:19:36 +0000

    Build up MET regrid_data_plane executable rather than requiring REGRID_DATA_PLANE_EXE to be defined in the metplus_system.conf file. Also some reformatting to conform to pep8.

--- ush/regrid_data_plane_wrapper.py
+++ ush/regrid_data_plane_wrapper.py
@@ -27,46 +27,46 @@ from command_builder import CommandBuilder
 
 
 class RegridDataPlaneWrapper(CommandBuilder):
-
     def __init__(self, p, logger):
         super(RegridDataPlaneWrapper, self).__init__(p, logger)
-        self.app_path = self.p.getstr('exe', 'REGRID_DATA_PLANE_EXE')
+        self.app_path = os.path.join(self.p.getdir('MET_BUILD_BASE'),
+                                     'bin/regrid_data_plane')
+        # self.app_path = self.p.getstr('exe', 'REGRID_DATA_PLANE_EXE')
         self.app_name = os.path.basename(self.app_path)
-
 
     def run_at_time(self, init_time):
         task_info = TaskInfo()
         task_info.init_time = init_time
         fcst_vars = util.getlist(self.p.getstr('config', 'FCST_VARS'))
-        lead_seq = util.getlistint(self.p.getstr('config', 'LEAD_SEQ'))        
+        lead_seq = util.getlistint(self.p.getstr('config', 'LEAD_SEQ'))
         for lead in lead_seq:
             task_info.lead = lead
             for fcst_var in fcst_vars:
-                task_info.fcst_var = fcst_var            
+                task_info.fcst_var = fcst_var
                 # loop over models to compare
-                accums = util.getlist(self.p.getstr('config', fcst_var+"_ACCUM"))
-                ob_types = util.getlist(self.p.getstr('config', fcst_var+"_OBTYPE"))
+                accums = util.getlist(
+                    self.p.getstr('config', fcst_var + "_ACCUM"))
+                ob_types = util.getlist(
+                    self.p.getstr('config', fcst_var + "_OBTYPE"))
                 for accum in accums:
                     task_info.level = accum
                     for ob_type in ob_types:
                         task_info.ob_type = ob_type
                         if lead < int(accum):
                             continue
-#                        self.run_at_time_fcst(task_info)
+                        #                        self.run_at_time_fcst(task_info)
                         self.run_at_time_once(task_info.getValidTime(),
                                               task_info.level,
                                               task_info.ob_type)
 
-
-        
     def run_at_time_once(self, valid_time, accum, ob_type):
-        obs_var = self.p.getstr('config', ob_type+"_VAR")
-        bucket_dir = self.p.getstr('config', ob_type+'_BUCKET_DIR')
+        obs_var = self.p.getstr('config', ob_type + "_VAR")
+        bucket_dir = self.p.getstr('config', ob_type + '_BUCKET_DIR')
         bucket_template = self.p.getraw('filename_templates',
-                                        ob_type+'_BUCKET_TEMPLATE')
-        regrid_dir = self.p.getstr('config', ob_type+'_REGRID_DIR')
+                                        ob_type + '_BUCKET_TEMPLATE')
+        regrid_dir = self.p.getstr('config', ob_type + '_REGRID_DIR')
         regrid_template = self.p.getraw('filename_templates',
-                                        ob_type+'_REGRID_TEMPLATE')
+                                        ob_type + '_REGRID_TEMPLATE')
 
         ymd_v = valid_time[0:8]
         if not os.path.exists(os.path.join(regrid_dir, ymd_v)):
@@ -88,15 +88,15 @@ class RegridDataPlaneWrapper(CommandBuilder):
         self.set_output_path(os.path.join(regrid_dir, regrid_file))
         field_name = "{:s}_{:s}".format(obs_var, str(accum).zfill(2))
         self.add_arg("-field 'name=\"{:s}\"; level=\"(*,*)\";'".format(
-                                                                 field_name))
+            field_name))
         self.add_arg("-method BUDGET")
         self.add_arg("-width 2")
-        self.add_arg("-name "+field_name)
+        self.add_arg("-name " + field_name)
         cmd = self.get_command()
         if cmd is None:
             print("ERROR: regrid_data_plane could not generate command")
             return
-        print("RUNNING: "+str(cmd))
+        print("RUNNING: " + str(cmd))
         self.logger.info("")
         self.build()
         self.clear()
