18a07ad0df96642de1420a2aa0b7352baaf28096
Minna Win <minnawin@ucar.edu>
2017-10-19 19:00:01 +0000

    Remove code that retrieves MET executable from metplus_system.conf.  Builds executable path via MET_BUILD_BASE

--- ush/regrid_data_plane_wrapper.py
+++ ush/regrid_data_plane_wrapper.py
@@ -31,7 +31,6 @@ class RegridDataPlaneWrapper(CommandBuilder):
         super(RegridDataPlaneWrapper, self).__init__(p, logger)
         self.app_path = os.path.join(self.p.getdir('MET_BUILD_BASE'),
                                      'bin/regrid_data_plane')
-        # self.app_path = self.p.getstr('exe', 'REGRID_DATA_PLANE_EXE')
         self.app_name = os.path.basename(self.app_path)
 
     def run_at_time(self, init_time):
