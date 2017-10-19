9c1cd3a361a68a577892f7bedd75613a164917d9
Minna Win <minnawin@ucar.edu>
2017-10-19 16:02:16 +0000

    Build MET executable via MET_BUILD_BASE to remove unneccessary entry in metplus_system.conf

--- ush/met_util.py
+++ ush/met_util.py
@@ -487,7 +487,9 @@ def retrieve_and_regrid(tmp_filename, cur_init, cur_storm, out_dir, logger,
 
     # Get variables, etc. from param/config file.
     model_data_dir = config.getdir('MODEL_DATA_DIR')
-    regrid_data_plane_exe = config.getexe('REGRID_DATA_PLANE_EXE')
+    metplus_base = config.getdir('EXE', 'MET_BUILD_BASE')
+    regrid_data_plane_exe = os.path.join(metplus_base, 'bin/regrid_data_plane')
+    # regrid_data_plane_exe = config.getexe('REGRID_DATA_PLANE_EXE')
     wgrib2_exe = config.getexe('WGRIB2')
     egrep_exe = config.getexe('EGREP_EXE')
     regrid_with_met_tool = config.getbool('config', 'REGRID_USING_MET_TOOL')
