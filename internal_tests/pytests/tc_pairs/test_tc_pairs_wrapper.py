0b11bc6f9f55022a7677acec01413d85631881d2
Minna Win <minnawin@ucar.edu>
2018-04-20 18:31:22 +0000

    Working tests ported from the unittest version.

--- internal_tests/pytests/tc_pairs/test_tc_pairs_wrapper.py
+++ internal_tests/pytests/tc_pairs/test_tc_pairs_wrapper.py
@@ -63,136 +63,130 @@ def metplus_config():
         sys.exit(2)
 
 
-def test_no_empty_mod_dir():
-    """ Verify that we are creating the ATCF files. """
-    rtcp = tc_pairs_wrapper()
-    tc_pairs_dir = rtcp.config.getdir('TC_PAIRS_DIR')
-    dirs_list = os.listdir(tc_pairs_dir)
-    assert len(dirs_list) == 1
+# def test_no_empty_mod_dir():
+#     """ Verify that we are creating the ATCF files. """
+#     rtcp = tc_pairs_wrapper()
+#     tc_pairs_dir = rtcp.config.getdir('TC_PAIRS_DIR')
+#     dirs_list = os.listdir(tc_pairs_dir)
+#     assert len(dirs_list) == 1
+#
+#
+# def test_no_empty_tcp_dir():
+#     """ Verify that we are creating tc pair output"""
+#     rtcp = tc_pairs_wrapper()
+#     rtcp.run_all_times()
+#     tc_pairs_dir = rtcp.config.getdir('TC_PAIRS_DIR')
+#     assert os.listdir(tc_pairs_dir)
 
 
-def test_no_empty_tcp_dir():
-    """ Verify that we are creating tc pair output"""
-    rtcp = tc_pairs_wrapper()
-    rtcp.run_all_times()
-    tc_pairs_dir = rtcp.config.getdir('TC_PAIRS_DIR')
-    assert os.listdir(tc_pairs_dir)
-
-
-def test_one_less_column(self):
-    """ Tests the read_modify_write_file() method.
-        Verify that the output: in the track_data_atcf directory
-        has one fewer column than the input track data.
+# def test_one_less_column():
+#     """ Tests the read_modify_write_file() method.
+#         Verify that the output: in the track_data_atcf directory
+#         has one fewer column than the input track data.
+#     """
+#     rtcp = tc_pairs_wrapper()
+#     rtcp.run_all_times()
+#     track_data_dir = rtcp.config.getdir('TRACK_DATA_DIR')
+#     track_data_subdir_mod = rtcp.config.getdir('TRACK_DATA_SUBDIR_MOD')
+#
+#     # So that we have independence of the start and end times indicated
+#     # in the metplus.conf or any other conf file, perform the test as
+#     # follows:
+#     # 1) get a list of directories in the track_data_dir and
+#     #    track_data_subdir_mod
+#     # 2) use the first directory of each in #1
+#     # 3) then look at the first file in each of those directories in #2
+#     # We don't need to have matching dates and filename in the original and
+#     # modified files, since we are only interested in the *number* of
+#     # columns in the files.
+#     orig_dir_list = os.listdir(track_data_dir)
+#     mod_dir_list = os.listdir(track_data_subdir_mod)
+#     orig_first_file_list = os.listdir(
+#             os.path.join(track_data_dir, orig_dir_list[0]))
+#     mod_first_file_list = os.listdir(
+#             os.path.join(track_data_subdir_mod, mod_dir_list[0]))
+#     orig_first_file = orig_first_file_list[0]
+#     mod_first_file = mod_first_file_list[0]
+#     orig_tc_file = os.path.join(track_data_dir, orig_dir_list[0],
+#                                 orig_first_file)
+#     mod_tc_file = os.path.join(track_data_subdir_mod, mod_dir_list[0],
+#                                mod_first_file)
+#     # Get the number of columns from the first row of the original and
+#     # modified files, respectively.
+#     with open(orig_tc_file) as f:
+#         reader = csv.reader(f, delimiter=' ', skipinitialspace=True)
+#         first_row = next(reader)
+#         orig_num_cols = len(first_row)
+#     with open(mod_tc_file) as f:
+#         reader = csv.reader(f, delimiter=' ', skipinitialspace=True)
+#         first_row = next(reader)
+#         mod_num_cols = len(first_row)
+#
+#     assert mod_num_cols == (orig_num_cols - 1)
+#
+#
+def test_col2_format_ok():
+    """Tests for proper behavior in the read_modify_write_file() method
+       Verify that the second column of one of the modified tc track files
+       does indeed have the month followed by the storm id:
+       Get the first row of the first file in the same subdirectory of the
+       modified and input directory (i.e. same month)
     """
     rtcp = tc_pairs_wrapper()
-    rtcp.run_at_time(self.request_time)
+    rtcp.run_all_times()
     track_data_dir = rtcp.config.getdir('TRACK_DATA_DIR')
     track_data_subdir_mod = rtcp.config.getdir('TRACK_DATA_SUBDIR_MOD')
-
-    # So that we have independence of the start and end times indicated
-    # in the metplus.conf or any other conf file, perform the test as
-    # follows:
-    # 1) get a list of directories in the track_data_dir and
-    #    track_data_subdir_mod
-    # 2) use the first directory of each in #1
-    # 3) then look at the first file in each of those directories in #2
-    # We don't need to have matching dates and filename in the original and
-    # modified files, since we are only interested in the *number* of
-    # columns in the files.
-    orig_dir_list = os.listdir(track_data_dir)
     mod_dir_list = os.listdir(track_data_subdir_mod)
-    orig_first_file_list = os.listdir(
-            os.path.join(track_data_dir, orig_dir_list[0]))
+
+    # Get the first directory in the TRACK_DATA_SUBDIR_MOD directory and
+    # the matching year-month dir in the TRACK_DATA_DIR directory.
+    year_month_dir = mod_dir_list[0]
     mod_first_file_list = os.listdir(
-            os.path.join(track_data_subdir_mod, mod_dir_list[0]))
-    orig_first_file = orig_first_file_list[0]
-    mod_first_file = mod_first_file_list[0]
-    orig_tc_file = os.path.join(track_data_dir, orig_dir_list[0],
-                                orig_first_file)
-    mod_tc_file = os.path.join(track_data_subdir_mod, mod_dir_list[0],
-                               mod_first_file)
-    # Get the number of columns from the first row of the original and
-    # modified files, respectively.
-    with open(orig_tc_file) as f:
-        reader = csv.reader(f, delimiter=' ', skipinitialspace=True)
-        first_row = next(reader)
-        orig_num_cols = len(first_row)
-    with open(mod_tc_file) as f:
-        reader = csv.reader(f, delimiter=' ', skipinitialspace=True)
-        first_row = next(reader)
-        mod_num_cols = len(first_row)
+        os.path.join(track_data_subdir_mod, year_month_dir))
 
-    assert mod_num_cols == (orig_num_cols - 1)
+    # Get the track file that corresponds to the modified track file's
+    # first file.
+    first_track_file = mod_first_file_list[0]
+    mod_tc_file = os.path.join(track_data_subdir_mod, year_month_dir,
+                               first_track_file)
+    orig_tc_file = os.path.join(track_data_dir, year_month_dir,
+                                first_track_file)
 
+    # Get the month from the year_month_dir
+    month_match = re.match(r'[0-9]{4}([0-9]{2})', year_month_dir)
+    if month_match:
+        month = month_match.group(1)
 
-    def test_col2_format_ok():
-        """Tests for proper behavior in the read_modify_write_file() method
-           Verify that the second column of one of the modified tc track files
-           does indeed have the month followed by the storm id:
-           Get the first row of the first file in the same subdirectory of the
-           modified and input directory (i.e. same month)
-        """
-        rtcp = tc_pairs_wrapper()
-        rtcp.run_at_time(self.request_time)
-        track_data_dir = self.config.getdir('TRACK_DATA_DIR')
-        track_data_subdir_mod = self.config.getdir('TRACK_DATA_SUBDIR_MOD')
-        mod_dir_list = os.listdir(track_data_subdir_mod)
-
-        # Get the first directory in the TRACK_DATA_SUBDIR_MOD directory and
-        # the matching year-month dir in the TRACK_DATA_DIR directory.
-        year_month_dir = mod_dir_list[0]
-        mod_first_file_list = os.listdir(
-            os.path.join(track_data_subdir_mod, year_month_dir))
-
-        # Get the track file that corresponds to the modified track file's
-        # first file.
-        first_track_file = mod_first_file_list[0]
-        mod_tc_file = os.path.join(track_data_subdir_mod, year_month_dir,
-                                   first_track_file)
-        orig_tc_file = os.path.join(track_data_dir, year_month_dir,
-                                    first_track_file)
-
-        # Get the month from the year_month_dir
-        month_match = re.match(r'[0-9]{4}([0-9]{2})', year_month_dir)
-        if month_match:
-            month = month_match.group(1)
-
-            # Get the number of columns from the first row of the original and
-            # modified files, respectively.
-            with open(orig_tc_file) as f:
-                reader = csv.reader(f, delimiter=' ', skipinitialspace=True)
-                first_row = next(reader)
-                orig_storm_id = first_row[1]
-            with open(mod_tc_file) as f:
-                reader = csv.reader(f, delimiter=' ', skipinitialspace=True)
-                first_row = next(reader)
-                mod_storm_id = first_row[1]
-            # Create the expected storm id and compare it to what was produced
-            # in the modified file.
-            expected_storm_id = month + orig_storm_id
-            assert mod_storm_id == expected_storm_id
-        else:
-            # If no match, something is wrong, force test to fail.
-            assert 0
-
-        # Verify that the second column is the correct month and is the
-        # combination of month and storm id.  Get the storm id from the
-        # second column of the original (input file), create the expected
-        # string and compare that to what we observe for the second column
+        # Get the number of columns from the first row of the original and
+        # modified files, respectively.
+        with open(orig_tc_file) as f:
+            reader = csv.reader(f, delimiter=' ', skipinitialspace=True)
+            first_row = next(reader)
+            orig_storm_id = first_row[1]
+        with open(mod_tc_file) as f:
+            reader = csv.reader(f, delimiter=' ', skipinitialspace=True)
+            first_row = next(reader)
+            mod_storm_id = first_row[1]
+        # Create the expected storm id and compare it to what was produced
         # in the modified file.
+        expected_storm_id = month + orig_storm_id
+        assert mod_storm_id == expected_storm_id
+    else:
+        # If no match, something is wrong, force test to fail.
         assert 0
 
 
-    def test_same_dates_in_mod_and_final_dir():
+#
+def test_same_dates_in_mod_and_final_dir():
         """ Test that the dirs (i.e. dates) that were found in the modified dir
             are also found in the final track data output dir.  Verify that the
             list of dates in the TRACK_DATA_SUBDIR_MOD directory are equal to
             the list of dates in the TRACK_DATA_DIR directory.
         """
         rtcp = tc_pairs_wrapper()
-        tc_pairs_dir = self.config.getdir('TC_PAIRS_DIR')
-        track_data_subdir_mod = self.config.getdir('TRACK_DATA_SUBDIR_MOD')
-        rtcp.run_at_time(self.request_time)
+        tc_pairs_dir = rtcp.config.getdir('TC_PAIRS_DIR')
+        track_data_subdir_mod = rtcp.config.getdir('TRACK_DATA_SUBDIR_MOD')
+        rtcp.run_all_times()
 
         # First check that there are the same number of subdirectories in the
         # TC_PAIRS_DIR (the final output dir) and TRACK_DATA_SUBDIR_MOD
@@ -216,12 +210,11 @@ def test_one_less_column(self):
 
 
 def test_num_files_in_subdir_mod_for_201412():
-    """ Verify that for INIT_DATE_BEG=20141205 and
-        INIT_DATE_END=20141206 for GFS data in /d1/SBU/GFS,
-        the track_data_atcf directory contains 450 files"""
+    """ Verify that for 201412 GFS data in /d1/SBU/GFS,
+        the track_data_atcf directory (containing the reformatted extra-tropical cyclone
+        data now in ATCF format) contains 450 files"""
     rtcp = tc_pairs_wrapper()
-    request_time = "20141205_00"
-    rtcp.run_at_time(request_time)
+    rtcp.run_all_times()
     request_subdir = "201412"
     atcf_dir = os.path.join(
             rtcp.config.getdir('TRACK_DATA_SUBDIR_MOD'),
