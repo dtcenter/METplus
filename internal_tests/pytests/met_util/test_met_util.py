c8d5772c8c5a6ffade8e7ad8ed5b3b8fb29cb00c
Minna Win <minnawin@ucar.edu>
2018-08-24 17:06:02 +0000

    Fixed formatting, added comment to indicate location of data.

--- internal_tests/pytests/met_util/test_met_util.py
+++ internal_tests/pytests/met_util/test_met_util.py
@@ -5,6 +5,7 @@ import met_util as util
 
 def test_get_date_from_path_subdirs_found():
     date_regex = ".*nam.(2[0-9]{7})"
+    # This data is on the DTC NCAR host 'eyewall'
     dir_to_search = "/d1/METplus_Pointstat/conus_sfc"
     dates_from_path = util.get_date_from_path(dir_to_search, date_regex)
 
@@ -12,13 +13,15 @@ def test_get_date_from_path_subdirs_found():
     num_expected_dates = 7
     assert len(dates_from_path) == num_expected_dates
 
+
 def test_get_date_from_path_correct_dates():
     expected_dates = ['20170601', '20170602', '20170603', '20170604',
                       '20170605', '20170606', '20170607']
     date_regex = ".*nam.(2[0-9]{7})"
+    # This data is on the DTC NCAR host 'eyewall'
     dir_to_search = "/d1/METplus_Pointstat/conus_sfc"
     dates_from_path = util.get_date_from_path(dir_to_search, date_regex)
-    counter =0
+    counter = 0
     expected_count = 7
     for date_tuple in dates_from_path:
         if date_tuple.data_date in expected_dates:
