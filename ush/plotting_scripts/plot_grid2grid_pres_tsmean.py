0ae9740756470139a58d0a0568c180b9f974eb42
Mallory Row <mallory.row@noaa.gov>
2018-10-15 15:04:07 +0000

    Rework of plot_grid2grid_pres_tsmean.py

--- ush/plotting_scripts/plot_grid2grid_pres_tsmean.py
+++ ush/plotting_scripts/plot_grid2grid_pres_tsmean.py
@@ -3,7 +3,7 @@
 Program Name: plot_grid2grid_pres_tsmean.py
 Contact(s): Mallory Row
 Abstract: Reads mean forecast hour files from plot_grid2grid_pres_ts.py to make dieoff plots
-History Log:  Initial version
+History Log:  Second version
 Usage: 
 Parameters: None
 Input Files: ASCII files
@@ -20,6 +20,8 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import warnings
 import logging
+import plot_defs as pd
+import pandas as pandas
 #############################################################################
 ##### Settings
 np.set_printoptions(suppress=True)
@@ -42,15 +44,15 @@ syear = int(sdate[:4])
 smon = int(sdate[4:6])
 smonth = month_name[smon-1]
 sday = int(sdate[6:8])
-edate=os.environ['END_T']
+edate = os.environ['END_T']
 eyear = int(edate[:4])
 emon = int(edate[4:6])
 emonth = month_name[emon-1]
 eday = int(edate[6:8])
 date_filter_method = os.environ['DATE_FILTER_METHOD']
 #input info
-model_list = os.environ['MODEL_LIST'].replace(", ", ",").split(",")
-nmodels = len(model_list)
+model_names = os.environ['MODEL_NAMES'].replace(" ", ",").split(",")
+nmodels = len(model_names)
 cycle = os.environ['CYCLE']
 lead_list = os.environ['LEAD_LIST'].replace(", ", ",").split(",")
 leads = np.asarray(lead_list).astype(float)
@@ -73,7 +75,8 @@ file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 ch = logging.StreamHandler()
 logger.addHandler(ch)
-plotting_out_dir = os.environ['PLOTTING_OUT_DIR']
+plotting_out_dir_base = os.environ['PLOTTING_OUT_DIR']
+plotting_out_dir = os.path.join(plotting_out_dir_base, "pres")
 ####################################################################
 logger.info("------> Running "+os.path.realpath(__file__))
 logger.debug("----- for "+date_filter_method+" start date:"+sdate+" "+date_filter_method+" end date:"+edate+" cycle:"+cycle+"Z forecast hour means for region:"+region+" fcst var:"+fcst_var_name+" obs var:"+obs_var_name)
@@ -88,118 +91,120 @@ while vl <= nlevels:
     s=1
     while s <= nstats: #loop over statistics
         stat_now = plot_stats_list[s-1]
+        stat_formal_name_now = pd.get_stat_formal_name(stat_now)
         logger.debug("--- "+stat_now)
      	m=1
      	while m <= nmodels: #loop over models
-             model_now = model_list[m-1]
+             model_now = model_names[m-1]
              logger.debug(str(m)+" "+model_now)
+             #intialize data array
+             model_now_stat_now_means = np.ones_like(leads) * np.nan
              #get forecast hour mean file
              model_now_mean_file = plotting_out_dir+"/data/"+cycle+"Z/"+model_now+"/"+stat_now+"_mean_"+region+"_fcst"+fcst_var_name+fcst_var_level_now+"_obs"+obs_var_name+obs_var_level_now+".txt"
              if os.path.exists(model_now_mean_file):
                  nrow = sum(1 for line in open(model_now_mean_file))
                  if nrow == 0: #file blank
-                     logger.warning(model_now_mean_file+" was empty! Setting to NaN")
-                     model_now_stat_now_mean = np.ones_like(leads) * np.nan 
+                     logger.warning("Model "+str(m)+" "+model_now+": "+model_now_mean_file+" empty")
                  else:
-                     logger.debug("Found "+model_now_mean_file)
-                     #read data file and put in array
-                     data = list()
-                     with open(model_now_mean_file) as f:
-                         for line in f:
-                             line_split = line.split()
-                             data.append(line_split)
-                     data_array = np.asarray(data)
-                     #assign variables
-                     model_now_leads = data_array[:,0].astype(float)
-                     model_now_stat_now_mean = data_array[:,1]
-                     #check for missing data
+                     logger.debug("Model "+str(m)+" "+model_now+": found "+model_now_mean_file)
+                     mean_cols = [ "LEADS", "VALS" ]
+                     model_now_data = pandas.read_csv(model_now_mean_file, sep=" ", header=None, names=mean_cols)
+                     model_now_stat_now_leads = model_now_data.loc[:]['LEADS'] 
+                     model_now_stat_now_vals = model_now_data.loc[:]['VALS']
+                     #check for any missing data in current model for requested forecast leads
                      for l in range(len(leads)):
-                         if leads[l] == model_now_leads[l]:
-                              model_now_stat_now_mean[l] = model_now_stat_now_mean[l]
+                         if leads[l] == model_now_stat_now_leads[l]:
+                              if model_now_stat_now_vals[l] == '--':
+                                  model_now_stat_now_means[l] = np.nan
+                              else:
+                                  model_now_stat_now_means[l] = model_now_stat_now_vals[l]
                          else:
-                            ll = np.where(model_now_leads == leads[l])[0]
+                            ll = np.where(model_now_stat_now_leads == leads[l])[0]
                             if len(ll) != 0:
-                                 model_now_stat_now_mean[l] = model_now_stat_now_mean[ll[0]]
+                                 if model_now_stat_now_vals[ll[0]] == '--':
+                                     model_now_stat_now_means[l] = np.nan
+                                 else:
+                                     model_now_stat_now_means[l] = model_now_stat_now_vals[ll[0]]
                             else:
-                                 model_now_stat_now_mean[l] = np.nan
+                                 model_now_stat_now_means[l] = np.nan
              else:
-                 logger.error(model_now_mean_file+" NOT FOUND! Setting to NaN")
-                 model_now_stat_now_mean = np.ones_like(leads) * np.nan
-             model_now_stat_now_mean[model_now_stat_now_mean == '--'] = np.nan
-             model_now_stat_now_mean = model_now_stat_now_mean.astype(float)
-             model_now_stat_now_mean = np.ma.masked_array(model_now_stat_now_mean, mask=model_now_stat_now_mean == np.nan)
-             count_masked = np.ma.count_masked(model_now_stat_now_mean)
+                 logger.warning("Model "+str(m)+" "+model_now+": "+model_now_mean_file+" missing")                 
+             model_now_stat_now_means = np.ma.masked_invalid(model_now_stat_now_means)
+             count_masked = np.ma.count_masked(model_now_stat_now_means)
              #create image directory if does not exist
              if not os.path.exists(os.path.join(plotting_out_dir, "imgs", cycle+"Z")):
                 os.makedirs(os.path.join(plotting_out_dir, "imgs", cycle+"Z"))
              #plot individual statistic forecast hour mean with CI time seres
              if m == 1:
-                 fig, (ax1, ax2) = plt.subplots(2,1,figsize=(10,6), sharex=True)
-                 #forecast hour mean
-                 if count_masked != len(model_now_stat_now_mean): 
-                      logger.debug("Plotting "+stat_now+" lead forecast hour means for "+model_now)
-                      ax1.plot(leads, model_now_stat_now_mean, color=colors[m-1], ls='-', linewidth=2.0, marker='o', markersize=7, label=model_now)
-                 model_1_stat_now_mean = model_now_stat_now_mean
+                 #set up plot
+                 fig, (ax1, ax2) = plt.subplots(2,1,figsize=(10,12), sharex=True)
+                 #forecast hour means
                  ax1.grid(True)
+                 ax1.tick_params(axis='x', pad=10)
                  ax1.set_xticks(leads)
                  ax1.set_xlim([leads[0],leads[-1]])
-                 ax1.tick_params(axis='x', pad=10)
                  ax1.tick_params(axis='y', pad=15)
-                 #difference from model 1 set up
-                 ax2.plot(leads, np.zeros_like(leads), color='k')
-                 ax2.text(0.0, -0.35, "Differences outside the outline bars are significant at the 95% confidence interval", fontsize=10, bbox={'facecolor':'white', 'alpha':0, 'pad':5}, transform=ax2.transAxes)
-                 ax2.set_xlabel("Forecast Hour")
-                 ax2.tick_params(axis='x', pad=10)
-                 ax2.tick_params(axis='y', pad=15)
-                 ax2.set_title("Difference with Respect to "+model_list[0])
+                 ax1.set_ylabel("Mean")
+                 #ax1.set_ylim()
+                 #difference from model 1
                  ax2.grid(True)
+                 ax2.tick_params(axis='x', pad=10)
+                 ax2.set_xlabel("Forecast Hour")
+                 ax2.tick_params(axis='y', pad=15)
+                 ax2.set_ylabel("Difference")
+                 #ax2.set_ylim()
+                 ax2.set_title("Difference from "+model_names[0], loc="left")
+                 boxstyle = matplotlib.patches.BoxStyle("Square", pad=0.25)
+                 props = {'boxstyle': boxstyle,
+                 'facecolor': 'white',
+                 'linestyle': 'solid',
+                 'linewidth': 1,
+                 'edgecolor': 'black',}
+                 ax2.text(0.7055, 1.05, "Note: differences outside the outline bars are significant\n at the 95% confidence interval", ha="center", va="center", fontsize=10, bbox=props, transform=ax2.transAxes)
+                 logger.debug("Plotting "+stat_now+" lead forecst hour means for "+model_now)
+                 ax1.plot(leads, model_now_stat_now_means, color=colors[m-1], ls='-', linewidth=2.0, marker='o', markersize=7, label=model_now)
+                 ax2.plot(leads, np.zeros_like(leads), color=colors[m-1], ls='-', linewidth=2.0)
+                 model1_stat_now_means = model_now_stat_now_means
              else:
+                 #get CI data
+                 #intialize data array
+                 model_now_stat_now_CI = np.ones_like(leads) * np.nan
+                 #get forecast hour mean file
                  model_now_CI_file = plotting_out_dir+"/data/"+cycle+"Z/"+model_now+"/"+stat_now+"_CI_"+region+"_fcst"+fcst_var_name+fcst_var_level_now+"_obs"+obs_var_name+obs_var_level_now+".txt"
                  if os.path.exists(model_now_CI_file):
                      nrow = sum(1 for line in open(model_now_CI_file))
                      if nrow == 0: #file blank
-                         logger.warning(model_now_CI_file+" was empty! Setting to NaN")
-                         model_now_stat_now_CI = np.ones_like(leads) * np.nan
+                         logger.warning("Model "+str(m)+" "+model_now+": "+model_now_CI_file+" empty")
                      else:
-                         logger.debug("Found "+model_now_CI_file)
-                         #read data file and put in array
-                         CI_data = list()
-                         with open(model_now_CI_file) as f:
-                             for line in f:
-                                  line_split = line.split()
-                                  CI_data.append(line_split)
-                         CI_data_array = np.asarray(CI_data)
-                         #assign variables
-                         model_now_leads_CI = CI_data_array[:,0].astype(float)
-                         model_now_stat_now_CI = CI_data_array[:,1]
-                         #check for missing data
+                         logger.debug("Model "+str(m)+" "+model_now+": found "+model_now_CI_file)
+                         mean_cols = [ "LEADS", "VALS" ]
+                         model_now_data_CI = pandas.read_csv(model_now_CI_file, sep=" ", header=None, names=mean_cols)
+                         model_now_stat_now_leads_CI = model_now_data_CI.loc[:]['LEADS']
+                         model_now_stat_now_vals_CI = model_now_data_CI.loc[:]['VALS']
+                         #check for any missing data in current model for requested forecast leads
                          for l in range(len(leads)):
-                              if leads[l] == model_now_leads_CI[l]:
-                                   model_now_stat_now_CI[l] = model_now_stat_now_CI[l]
-                              else:
-                                   ll = np.where(model_now_leads_CI == leads[l])[0]
-                                   if len(ll) != 0:
-                                        model_now_stat_now_CI[l] = model_now_stat_now_CI[ll[0]]
-                                   else:
-                                        model_now_stat_now_CI[l] = np.nan
+                             if leads[l] == model_now_stat_now_leads_CI[l]:
+                                 if model_now_stat_now_vals_CI[l] == '--':
+                                     model_now_stat_now_CI[l] = np.nan
+                                 else:
+                                     model_now_stat_now_CI[l] = model_now_stat_now_vals_CI[l]
+                             else:
+                                 ll = np.where(model_now_stat_now_leads_CI == leads[l])[0]
+                                 if len(ll) != 0:
+                                     if model_now_stat_now_vals_CI[ll[0]] == '--':
+                                         model_now_stat_now_means_CI[l] = np.nan
+                                     else:
+                                         model_now_stat_now_means_CI[l] = model_now_stat_now_vals_CI[ll[0]]
+                                 else:
+                                     model_now_stat_now_means_CI[l] = np.nan
                  else:
-                     logger.error(model_now_CI_file+" NOT FOUND! Setting to NaN")
-                     model_now_stat_now_CI = np.ones_like(leads) * np.nan
-                 model_now_stat_now_CI[model_now_stat_now_CI == '--'] = np.nan
-                 model_now_stat_now_CI = model_now_stat_now_CI.astype(float)
-                 model_now_stat_now_CI = np.ma.masked_array(model_now_stat_now_CI, mask=model_now_stat_now_CI == np.nan)
-                 count_masked_CI = np.ma.count_masked(model_now_stat_now_CI)
-                 #forecast hour mean
-                 if count_masked != len(model_now_stat_now_mean):
-                     logger.debug("Plotting "+stat_now+" lead forecast hour means for "+model_now)
-                     ax1.plot(leads, model_now_stat_now_mean, color=colors[m-1], ls='-', marker='o', markersize=7, label=model_now)
-                     #differnce from model 1 with 95% CI
-                     logger.debug("Plotting "+stat_now+" lead forecast hour means for "+model_now+" - "+model_list[0])
-                     ax2.plot(leads, model_now_stat_now_mean - model_1_stat_now_mean, color=colors[m-1], ls='-',  marker='o', markersize=7)
-                 if count_masked_CI != len(model_now_stat_now_CI):
-                     logger.debug("Plotting "+stat_now+" CIs for "+model_now+" - "+model_list[0])
-                     #adjusted bar graph for CI to be centered on 0
-                     ax2.bar(leads, 2*model_now_stat_now_CI, bottom=-1*model_now_stat_now_CI, color='None', width=1.5+((m-2)*0.2), edgecolor=colors[m-1], linewidth='1')
+                     logger.warning("Model "+str(m)+" "+model_now+": "+model_now_CI_file+" missing")
+                 model_now_stat_now_CI = np.ma.masked_invalid(model_now_stat_now_CI)
+                 logger.debug("Plotting "+stat_now+" lead forecst hour means for "+model_now)
+                 ax1.plot(leads, model_now_stat_now_means, color=colors[m-1], ls='-', linewidth=2.0, marker='o', markersize=7, label=model_now)
+                 logger.debug("Plotting "+stat_now+" difference and CIs for "+model_now+" from "+model_names[0])
+                 ax2.plot(leads, model_now_stat_now_means - model1_stat_now_means, color=colors[m-1], ls='-', linewidth=2.0, marker='o', markersize=7)
+                 ax2.bar(leads, 2*model_now_stat_now_CI, bottom=-1*model_now_stat_now_CI, color='None', width=1.5+((m-2)*0.2), edgecolor=colors[m-1], linewidth='1')
              m+=1
         ax1.legend(bbox_to_anchor=(0.0, 1.02, 1.0, .102), loc=3, ncol=nmodels, fontsize='13', mode="expand", borderaxespad=0.)
         ax1.set_title("Fcst: "+fcst_var_name+"_"+fcst_var_level_now+" Obs: "+obs_var_name+"_"+obs_var_level_now+" "+str(stat_now)+'\n'+grid+"-"+region+" "+date_filter_method+" "+cycle+"Z "+str(sday)+smonth+str(syear)+"-"+str(eday)+emonth+str(eyear)+" Means\n\n", fontsize=14, fontweight='bold')
