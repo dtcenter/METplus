aad30fd3ad42c5eb94debcf9b962ca21fbb5a9ba
George McCabe <mccabe@ucar.edu>
2019-01-08 00:38:10 +0000

    Removed conf variables not used in this wrapper

--- ush/mode_wrapper.py
+++ ush/mode_wrapper.py
@@ -59,8 +59,6 @@ class ModeWrapper(CompareGriddedWrapper):
           self.p.getstr('config', 'FCST_MODE_INPUT_DATATYPE', '')
         self.cg_dict['OUTPUT_DIR'] = self.p.getdir('MODE_OUT_DIR')
         self.cg_dict['INPUT_BASE'] = self.p.getdir('INPUT_BASE')
-        self.cg_dict['FCST_MAX_FORECAST'] = self.p.getint('config', 'FCST_MAX_FORECAST')
-        self.cg_dict['FCST_INIT_INTERVAL']= self.p.getint('config', 'FCST_INIT_INTERVAL')
         self.cg_dict['WINDOW_RANGE_BEG'] = \
           self.p.getint('config', 'WINDOW_RANGE_BEG', -3600)
         self.cg_dict['WINDOW_RANGE_END'] = \
