63ac62799d359ed539f403cc45bce08dc3152733
James Frimel <jfrimel@eyewall.rap.ucar.edu>
2019-04-28 10:01:42 +0000

    Changed conf variable from TCMPR_DATA to TCMPR_DATA_DIR Added INIT_TIME_FMT = %Y%m%d to cyclone_plotter.conf

--- ush/tcmpr_plotter_wrapper.py
+++ ush/tcmpr_plotter_wrapper.py
@@ -61,7 +61,7 @@ class TCMPRPlotterWrapper(CommandBuilder):
 
         # The only required argument for plot_tcmpr.R, the name of
         # the tcst file to plot.
-        self.input_data = self.config.getdir('TCMPR_DATA')
+        self.input_data = self.config.getdir('TCMPR_DATA_DIR')
 
         # Optional arguments
         self.plot_config_file = self.config.getstr('config', 'CONFIG_FILE')
