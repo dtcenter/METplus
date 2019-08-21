cdac5821bfcfb6740ede8cc75a2334522ea01a3d
George McCabe <mccabe@ucar.edu>
2019-08-21 15:37:49 +0000

    Removed requirement to set EXAMPLE_INPUT_[DIR/TEMPLATE] in Example wrapper so it can be run to see time looping results by simply changing the PROCESS_LIST value to Example

--- ush/example_wrapper.py
+++ ush/example_wrapper.py
@@ -34,8 +34,16 @@ class ExampleWrapper(CommandBuilder):
         c_dict = super(ExampleWrapper, self).create_c_dict()
         # get values from config object and set them to be accessed by wrapper
         c_dict['INPUT_TEMPLATE'] = self.config.getraw('filename_templates',
-                                                      'EXAMPLE_INPUT_TEMPLATE')
-        c_dict['INPUT_DIR'] = self.config.getdir('EXAMPLE_INPUT_DIR')
+                                                      'EXAMPLE_INPUT_TEMPLATE', '')
+        c_dict['INPUT_DIR'] = self.config.getdir('EXAMPLE_INPUT_DIR', '')
+
+        if c_dict['INPUT_TEMPLATE'] == '':
+            self.logger.info('[filename_templates] EXAMPLE_INPUT_TEMPLATE was not set. '
+                             'You should set this variable to see how the runtime is '
+                             'substituted. For example: {valid?fmt=%Y%m%d%H}.ext')
+
+        if c_dict['INPUT_DIR'] == '':
+            self.logger.debug('EXAMPLE_INPUT_DIR was not set')
 
         return c_dict
 
