353a9950995c3dd9af588dc502e1bafc45c70280
Daniel Adriaansen <daniel.adriaansen@gmail.com>
2019-09-26 10:28:30 -0600

    Testing automodule extension and code documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -4,17 +4,16 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os
-
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../ush'))
+print(sys.path)
 
 
 # -- Project information -----------------------------------------------------
@@ -32,8 +31,7 @@ release = '3.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- docs/index.rst
+++ docs/index.rst
@@ -3,6 +3,9 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
+.. automodule:: time_util
+.. automodule:: command_builder
+
 .. toctree::
    :maxdepth: 2
    :hidden:
