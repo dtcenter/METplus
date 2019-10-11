39ee1df6a100c12a573354db014087871493fb50
georgemccabe <mccabe@corrinado.rap.ucar.edu>
2019-10-11 22:06:58 +0000

    Run pytests on any machine that has a minimum_pytest.corrinado.conf file

--- internal_tests/pytests/run_pytests.sh
+++ internal_tests/pytests/run_pytests.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
-host=$HOSTNAME
-
-if [ $host != "eyewall" ] && [ $host != "dakota" ]; then
-    echo Cannot only run run_pytests.sh on eyewall or dakota. Create minimum_pytest.$host.conf to run on this host
-    exit
-fi
-
-
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pytest_rel_path="internal_tests/pytests"
 ush_dir=${script_dir%"$pytest_rel_path"}ush
 export PYTHONPATH=$ush_dir:$PYTHONPATH
 
+host=$HOSTNAME
+if [ ! -e $script_dir"/minimum_pytest."$host".conf" ]; then
+    echo Cannot only run run_pytests.sh with minimum pytest config file. Create minimum_pytest.$host.conf to run on this host
+    exit
+fi
+
 cd $script_dir/config
-pytest -c ../minimum_pytest.eyewall.conf
+pytest -c ../minimum_pytest.$host.conf
 cd $script_dir/grid_stat
-pytest -c ../minimum_pytest.eyewall.conf
+pytest -c ../minimum_pytest.$host.conf
 cd $script_dir/logging
-pytest -c ../minimum_pytest.eyewall.conf
+pytest -c ../minimum_pytest.$host.conf
 cd $script_dir/met_util
-pytest -c ../minimum_pytest.eyewall.conf
+pytest -c ../minimum_pytest.$host.conf
 cd $script_dir/mtd
-pytest -c ../minimum_pytest.eyewall.conf
+pytest -c ../minimum_pytest.$host.conf
 cd $script_dir/pcp_combine
-pytest -c ../minimum_pytest.eyewall.conf -c ./test1.conf
+pytest -c ../minimum_pytest.$host.conf -c ./test1.conf
 cd $script_dir/StringTemplateSubstitution
-pytest -c ../minimum_pytest.eyewall.conf
+pytest -c ../minimum_pytest.$host.conf
 cd $script_dir/compare_gridded
-pytest -c ../minimum_pytest.eyewall.conf
+pytest -c ../minimum_pytest.$host.conf
 cd $script_dir/time_util
-pytest -c ../minimum_pytest.eyewall.conf
+pytest -c ../minimum_pytest.$host.conf
 cd $script_dir/pb2nc
-pytest -c ../minimum_pytest.eyewall.conf -c ./conf1
+pytest -c ../minimum_pytest.$host.conf -c ./conf1
