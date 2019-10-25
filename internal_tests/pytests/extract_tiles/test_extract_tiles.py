aa616c8e46f93002a8c3edc15ead68b8df2c957b
Minna Win <minnawin@ucar.edu>
2019-10-25 17:30:11 +0000

    Working version of tests written in pytest for the extract tiles wrapper.

--- internal_tests/pytests/extract_tiles/test_extract_tiles.py
+++ internal_tests/pytests/extract_tiles/test_extract_tiles.py
@@ -46,6 +46,19 @@ def pytest_addoption(parser):
 
 
 # -----------------FIXTURES THAT CAN BE USED BY ALL TESTS--------------
+def tc_pairs_wrapper():
+    """! Return a default TcPairsWrapper with /path/to entries
+         in the metplus_system.conf and metplus_runtime.conf
+         config files.  This is necessary for creating the
+         input to the extract tiles wrapper.
+
+    """
+    conf = metplus_config()
+    logger = logging.getLogger("dummy1")
+    conf.set('config', 'LOOP_ORDER', 'processes')
+    tcpw = TcPairsWrapper(conf, logger)
+    tcpw.run_all_times()
+
 def extract_tiles_wrapper():
     """! Returns a default ExtractTilesWrapper with /path/to entries in the
          metplus_system.conf and metplus_runtime.conf configuration
@@ -55,7 +68,7 @@ def extract_tiles_wrapper():
     # ExtractTilesWrapper with configuration values determined by what is set in
     # the extract_tiles_wrapper.conf file.
     conf = metplus_config()
-    logger = logging.getLogger("dummy")
+    logger = logging.getLogger("dummy2")
 
     conf.set('config', 'LOOP_ORDER', 'processes')
     etw =  ExtractTilesWrapper(conf, logger)
@@ -112,23 +125,17 @@ def create_input_dict(etw):
 
 
 # ------------------------ TESTS GO HERE --------------------------
-# def test_dummy():
-#     etw = extract_tiles_wrapper()
-#     etw.run_all_times()
-#
-#     assert True
 
 def test_output_exists():
     """
-    Expect 1 tcst file to be generated from running extract_tiles.
+    Expect 1 .tcst file to be generated from running extract_tiles.
     The input directory (input data for extract tiles) is set in
     the TC_PAIRS_OUTPUT_DIR value in the extract_tiles_test.conf
-    file.
+    file. Also expect 12 subdirectories that have .nc file
     """
 
     etw = extract_tiles_wrapper()
     input_dict = create_input_dict(etw)
-    etw = extract_tiles_wrapper()
     etw.run_at_time(input_dict)
     config = etw.config
     dir_section = config.items('dir')
@@ -157,63 +164,76 @@ def test_output_exists():
     # is amiss.
     assert(len(actual_num_tcst_files) == 1)
 
+def test_correct_basin():
+    """
+       Verify that only the ML basin
+       paired results were returned by the
+       ExtractTilesWrapper, since we requested
+       this in the extract_tiles_test.conf file:
+       EXTRACT_TILES_FILTER_OPTS = -basin ML
+
+       Under the OUTPUT_BASE directory, there
+       should be subdirectories that have
+       names beginning with 'ML", which
+       indicates that the output is
+       for the ML basin.
+    """
+    expected_ml_subdirs = ['ML1200942014', 'ML1200972014', 'ML1200992014',
+                           'ML1201002014', 'ML1201032014', 'ML1201042014',
+                           'ML1201052014', 'ML1201062014', 'ML1201072014',
+                           'ML1201082014', 'ML1201092014', 'ML1201102014']
+
+    expected_num_ml_subdirs = len(expected_ml_subdirs)
+    expected_num_nc_files = int(186)
+    etw = extract_tiles_wrapper()
+    input_dict = create_input_dict(etw)
+    etw.run_at_time(input_dict)
+    config = etw.config
+    dir_section = config.items('dir')
+
+    for section, value in dir_section:
+        match = re.match(r'OUTPUT_BASE', section)
+        if match:
+            output_dir = os.path.join(value, 'extract_tiles')
+            # break out as soon as we find a match to the OUTPUT_BASE in the dir section.
+            break
+
+    # Saving the ML subdirs that were created for this
+    # test run...
+    created_ml_subdirs = []
+    total_nc_files = []
+    if output_dir:
+        for root, dirs, files in os.walk(output_dir):
+            for cur_dir in dirs:
+                match = re.match(r'^ML.*', cur_dir)
+                if match:
+                    created_ml_subdirs.append(cur_dir)
+            for curfile in files:
+                filematch = re.match(r'.*.nc', curfile)
+                if filematch:
+                    total_nc_files.append(curfile)
+
+    # Verify that we have ML subdirs, indicating
+    # that ML basin results were found, if not,
+    # this is a Fail.
+    if len(created_ml_subdirs) != expected_num_ml_subdirs:
+        assert(False)
+
+    # Assert we have the correct total number of nc files created
+    if len(total_nc_files) != expected_num_nc_files:
+        assert False
+
+    ml_subdir_counter = 0
+    for ml_subdir in created_ml_subdirs:
+        if ml_subdir in expected_ml_subdirs:
+            ml_subdir_counter = ml_subdir_counter + 1
+
+    # We should have the same number of ML subdirs that
+    # were created for this test as the expected number.
+    # In addition, we should have the same number of nc files as expected.
+    # print( "Number ML subdirs: ", ml_subdir_counter, " number expected: ", len(expected_ml_subdirs))
+    # print("#########\n Total number of nc files: ", len(total_nc_files))
+    assert( ml_subdir_counter == len(expected_ml_subdirs))
+    assert (expected_num_nc_files == len(total_nc_files))
 
 
-
-# def test_correct_basin(metplus_config):
-#     """
-#        Verify that only the ML basin
-#        paired results were returned by the
-#        ExtractTilesWrapper, since we requested
-#        this in the extract_tiles_test.conf file:
-#        EXTRACT_TILES_FILTER_OPTS = -basin ML
-#
-#        Under the OUTPUT_BASE directory, there
-#        should be subdirectories that have
-#        names beginning with 'ML", which
-#        indicates that the output is
-#        for the ML basin.
-#     """
-#     expected_ml_subdirs = ['ML1200942014', 'ML1200972014', 'ML1200992014',
-#                            'ML1201002014', 'ML1201032014', 'ML1201042014',
-#                            'ML1201052014', 'ML1201062014', 'ML1201072014',
-#                            'ML1201082014', 'ML1201092014', 'ML1201102014']
-#
-#     input_dict = create_input_dict(metplus_config)
-#     etw = extract_tiles_wrapper()
-#     etw.run_at_time(input_dict)
-#     dir_section = metplus_config.items('dir')
-#     for section, value in dir_section:
-#         match = re.match(r'OUTPUT_BASE', section)
-#         if match:
-#             output_dir = os.path.join(value, 'extract_tiles')
-#             # break out as soon as we find a match to the OUTPUT_BASE in the dir section.
-#             break
-#
-#     # Saving the ML subdirs that were created for this
-#     # test run...
-#     created_ml_subdirs = []
-#     if output_dir:
-#         for root, dirs, files in os.walk(output_dir):
-#             for cur_dir in dirs:
-#                 match = re.match(r'^ML.*', cur_dir)
-#                 if match:
-#                     created_ml_subdirs.append(cur_dir)
-#
-#     # Verify that we have ML subdirs, indicating
-#     # that ML basin results were found, if not,
-#     # this is a Fail.
-#     if len(created_ml_subdirs) == 0:
-#         assert(False)
-#
-#     ml_subdir_counter = 0
-#     for ml_subdir in created_ml_subdirs:
-#         if ml_subdir in expected_ml_subdirs:
-#             ml_subdir_counter = ml_subdir_counter + 1
-#
-#     # We should have the same number of ML subdirs that
-#     # were created for this test as the expected number.
-#     # print( "Number ML subdirs: ", ml_subdir_counter, " number expected: ", len(expected_ml_subdirs))
-#     assert( ml_subdir_counter == len(expected_ml_subdirs))
-#
-#
