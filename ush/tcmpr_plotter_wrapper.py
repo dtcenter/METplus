81ebbd4d97667029acd2025fbc3d18d5239f7726
Minna Win <minnawin@ucar.edu>
2019-11-15 19:48:33 +0000

    Github issue #317 Updated logger info message to indicate that the MET_INSTALL_DIR is coming from the user's METplus config file.

--- ush/tcmpr_plotter_wrapper.py
+++ ush/tcmpr_plotter_wrapper.py
@@ -108,7 +108,7 @@ class TCMPRPlotterWrapper(CommandBuilder):
         # config files.
         if 'MET_INSTALL_DIR' in os.environ:
             self.logger.info('Using MET_INSTALL_DIR setting from user '
-                             'environment instead of metplus configuration '
+                             'metplus configuration '
                              'file. Using: %s' % os.environ['MET_INSTALL_DIR'])
         else:
             # MET_INSTALL_DIR is required, so we want to throw an error if it is
