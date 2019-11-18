e32b831d10d469df448c38456dcdac674605d499
George McCabe <mccabe@ucar.edu>
2019-11-18 19:02:45 +0000

    cleaned up formatting for documentation

--- parm/use_cases/met_tool_wrapper/GridStat.py
+++ parm/use_cases/met_tool_wrapper/GridStat.py
@@ -17,8 +17,11 @@ gridded observation data.
 # Datasets
 # --------
 #
-# | Forecast: WRF 3 hour precipitation accumulation
-# | Observation: MU 3 hour precipitation accumulation
+# | **Forecast:** WRF 3 hour precipitation accumulation
+# | **Observation:** MU 3 hour precipitation accumulation
+#
+# | **Location:** All of the input data required for this use case can be found in the sample data tarball. Click here to download: https://github.com/NCAR/METplus/releases/download/v2.2/sample_data-met_test-8.1.tgz
+# | This tarball should be unpacked into the directory that you will set the value of INPUT_BASE. See 'Running METplus' section for more information.
 
 ##############################################################################
 # METplus Components
@@ -35,8 +38,8 @@ gridded observation data.
 # GridStat is the only tool called in this example. It processes the following
 # run times:
 #
-# | Init: 2005-08-07_0Z 
-# | Forecast lead: 12
+# | **Init:** 2005-08-07_0Z
+# | **Forecast lead:** 12
 
 ##############################################################################
 # METplus Configuration
@@ -46,7 +49,7 @@ gridded observation data.
 # then it loads any configuration files passed to METplus via the command line
 # with the -c option, i.e. -c parm/use_cases/met_tool_wrapper/GridStat.conf
 #
-# .. highlight:: none
+# .. highlight:: bash
 # .. literalinclude:: ../../../parm/use_cases/met_tool_wrapper/GridStat.conf
 
 ##############################################################################
@@ -65,9 +68,9 @@ gridded observation data.
 #
 # The former method is recommended. Whether you add them to a user-specific configuration file or modify the metplus_config files, the following variables must be set correctly:
 #
-# * INPUT_BASE - Path to directory where sample data tarballs found on the METplus release page are unpacked. This is not required to run METplus, but it is required to run the examples in parm/use_cases
-# * OUTPUT_BASE - Path where METplus output will be written. This must be in a location where you have write permissions
-# * MET_INSTALL_DIR - Path to location where MET is installed locally
+# * **INPUT_BASE** - Path to directory where sample data tarballs are unpacked (See Datasets section to obtain tarballs). This is not required to run METplus, but it is required to run the examples in parm/use_cases
+# * **OUTPUT_BASE** - Path where METplus output will be written. This must be in a location where you have write permissions
+# * **MET_INSTALL_DIR** - Path to location where MET is installed locally
 #
 # Example User Configuration File::
 #
@@ -76,24 +79,24 @@ gridded observation data.
 #   OUTPUT_BASE = /path/to/output/dir
 #   MET_INSTALL_DIR = /path/to/met-X.Y 
 #
-# NOTE: All of these items must be found under the [dir] section.
+# **NOTE:** All of these items must be found under the [dir] section.
 #
 
 ##############################################################################
 # Expected Output
 # ---------------
 #
-# A successful run will output the following both to the screen and to the logfile:
+# A successful run will output the following both to the screen and to the logfile::
 #
-# INFO: METplus has successfully finished running.
+#   INFO: METplus has successfully finished running.
 #
-# Refer to the value set for OUTPUT_BASE to find where the output data was generated.
-# Output for this use case will be found in grid_stat/2005080700 (relative to OUTPUT_BASE)
+# Refer to the value set for **OUTPUT_BASE** to find where the output data was generated.
+# Output for this use case will be found in grid_stat/2005080700 (relative to **OUTPUT_BASE**)
 # and will contain the following files:
 #
-#   grid_stat_QPF_APCP_vs_QPE_APCP_03_120000L_20050807_120000V_eclv.txt
-#   grid_stat_QPF_APCP_vs_QPE_APCP_03_120000L_20050807_120000V_grad.txt
-#   grid_stat_QPF_APCP_vs_QPE_APCP_03_120000L_20050807_120000V.stat
+# * grid_stat_QPF_APCP_vs_QPE_APCP_03_120000L_20050807_120000V_eclv.txt
+# * grid_stat_QPF_APCP_vs_QPE_APCP_03_120000L_20050807_120000V_grad.txt
+# * grid_stat_QPF_APCP_vs_QPE_APCP_03_120000L_20050807_120000V.stat
 
 ##############################################################################
 # Keywords
