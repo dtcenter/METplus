54241e5ba4f2b2af47aac57315f4e68f883e63f1
bikegeek <minnawin@ucar.edu>
2019-12-14 11:21:08 -0700

    Over-complicated things by creating a list of dictionaries.  Saving this just in case we need to revisit any of this logic.

--- check_python.py
+++ check_python.py
@@ -3,33 +3,52 @@ import subprocess
 import sys
 import re
 
+# Global
+# SUPPORTED_VERSION = '3.6'
+SUPPORTED_VERSION = '3.7'
+
 def main():
     if is_python_version_compatible():
         check_packages()
     else:
         print('Exiting')
         sys.exit(1)
+
+def get_supported_major_minor():
+    '''Deconstruct the supported version string into
+       a major and minor number
+       Args:
+
+       Returns:
+       :return:  A tuple of major, minor values
+    '''
+
+    match = re.match(r'([0-9]{1,3}).([0-9]{1,3})' , SUPPORTED_VERSION)
+    if match:
+        supported_major = match.group(1)
+        supported_minor = match.group(2)
+    return supported_major, supported_minor
+
 def is_python_version_compatible():
     """Determine whether the user's Python version is what is
         supported.
-
         Args:
-
         Returns:
             True if user's python version is compatible with what is supported,
             False otherwise.
     """
-    major = sys.version_info.major
-    minor = sys.version_info.minor
-    your_version = (str)(major)+ '.' + (str)(minor)
-    if major != 3:
-        print('Only Python 3.6 is supported, you have version ', your_version)
+    major = str(sys.version_info.major)
+    minor = str(sys.version_info.minor)
+    your_version = major + '.' + minor
+    supported_major, supported_minor = get_supported_major_minor()
+    if major != supported_major:
+        print('Only Python {SUPPORTED_VERSION} is supported, you have version ', your_version)
         return False
     else:
-        if minor != 6:
+        if minor != supported_minor:
             # Print warning that minimum supported
             # version of Python is 3.6
-          print(f'Only Python 3.6 is supported, you have version {your_version}')
+          print(f'Only Python {SUPPORTED_VERSION} is supported, you have version {your_version}')
           return False
         else:
             # OK
@@ -40,37 +59,32 @@ def check_packages():
     """Check if the packages installed on the computer on which you
         are executing this script is what is needed.
     """
-    required_packages = get_required()
+    required_packages = get_required_packages()
     print(f'required: {required_packages}')
-    actual_packages = get_actual()
+    actual_packages = get_actual_packages()
     print(f'actual_packages: {actual_packages}')
+    missing_packages_list = find_missing_packages(required_packages, actual_packages)
 
-def get_required():
-    # Retrieve the requirements.txt file from the METplus top-level directory
-    # and store this information in a dictionary, where the key is the package/module
-    # and the value is the version.
+def get_required_packages():
+    ''''Retrieve the requirements.txt file from the METplus top-level directory
+       and store this information in a dictionary, where the key is the package/module
+       and the value is the version.
+   '''
     requirements_file = "./requirements.txt"
     required_packages = []
-    required_dict = {}
 
     # Get the packages that are required
     with open(requirements_file, 'r') as file:
         for line in file:
-            #print(line.strip())
             split_str = line.split('==')
-            # print(split_str)
+            required_dict = {}
             required_dict['package'] = split_str[0]
-            #print(required_dict)
             required_dict['version'] = split_str[1].rstrip()
-            # print(f'required dict {required_dict}')
             required_packages.append(required_dict)
-            required_dict={}
-
-    # print(required_packages)
     return(required_packages)
 
-def get_actual():
-    """""Now run 'pip freeze' on this computer to get the
+def get_actual_packages():
+    """Now run 'pip freeze' on this computer to get the
              actual, currently installed packages/modules.
     """
     with open("actual.txt", "w+") as output:
@@ -80,23 +94,49 @@ def get_actual():
     # of any missing packages/modules and also keep track of mismatched
     # versions.
     actual_packages = []
-    actual_dict = {}
     with open("actual.txt", "r") as file:
         for line in file:
-            # print(line.strip())
             split_str = line.split('==')
-            # print(split_str)
-            actual_dict['package'] = split_str[0]
-            # print(required_dict)
-            actual_dict['version'] = split_str[1].rstrip()
-            # print(f'required dict {required_dict}')
-            actual_packages.append(actual_dict)
             actual_dict = {}
-        # print(f'actual dict: {actual_packages}')
+            actual_dict['package'] = split_str[0]
+            actual_dict['version'] = split_str[1].rstrip()
+            actual_packages.append(actual_dict)
         return actual_packages
 
+def find_missing_packages(required_packages, actual_packages):
+    '''Find any packages that are missing from the user's host and print
+       output to stdout and create a text file indicating the missing package and its
+       version.
+
+       Args:
+           required_packages:  The list of required packages
+           actual_packages:  The list of packages on the user's host
+
+       Returns:
+            A list containing the missing packages (and versions)
+
+    '''
+
+    missing_packages_list = []
+    # Get a list of all the keys in the required_packages and for the
+    # actual_packages and check for the absence of packages based
+    # on the keys.
+    required_keys = []
+    for required in required_packages:
+        required_keys.append(required['package'])
+    actual_keys = []
+    for actual in actual_packages:
+        actual_keys.append(actual['package'])
+
+    # Now search for missing packages in the user's environment
+    for required in required_keys:
+        if required not in actual_keys:
+            missing_packages_list.append(required)
+    print(f'missing packages: {missing_packages_list}')
 
 if __name__ == "__main__":
-    v = '3.6'
-    print('Checking your python version is  {}...'.format(v))
+
+    print('Checking your python version is  {}...'.format(SUPPORTED_VERSION))
+    maj, min = get_supported_major_minor()
+    print(f'major version {maj}, minor version {min}')
     main()
