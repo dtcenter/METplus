ae73cd62d14468c3db16f6c42bbb2a7d968011e2
George McCabe <mccabe@ucar.edu>
2019-12-19 16:42:31 +0000

    updated use case tests to includ run script that automatically sets env var file. tests now output into a directory and output from a previous run will get put into the previous run output base to compare

--- internal_tests/use_cases/metplus_test_env.eyewall.sh
+++ internal_tests/use_cases/metplus_test_env.eyewall.sh
@@ -1,11 +1,21 @@
-export METPLUS_TEST_A_METPLUS_BASE=/d1/$USER/METplus.a
-export METPLUS_TEST_A_OUTPUT_BASE=/d1/$USER/test-a
-export METPLUS_TEST_A_MET_INSTALL_DIR=/usr/local/met-8.1.1
+# set this to an empty string to use the working METPLUS_BASE
+# set it to another METPLUS BASE location to test that version
+export METPLUS_TEST_METPLUS_BASE=
 
-export METPLUS_TEST_B_METPLUS_BASE=/d1/$USER/METplus.b
-export METPLUS_TEST_B_OUTPUT_BASE=/d1/$USER/test-b
-export METPLUS_TEST_B_MET_INSTALL_DIR=/usr/local/met-8.1.1
+# version of METplus to test
+export METPLUS_TEST_MET_INSTALL_DIR=/usr/local/met-8.1.1
 
+# location to write output from test run
+export METPLUS_TEST_OUTPUT_BASE=/d1/$USER/test-use-case-b
+
+# location of output from previous test run to compare to current run
+export METPLUS_TEST_PREV_OUTPUT_BASE=/d1/$USER/test-use-case-a
+
+# location of input data to use in tests
 export METPLUS_TEST_INPUT_BASE=/d1/METplus_Data
+
+# location of GempakToCF to run tests that use Gempak
 export METPLUS_TEST_GEMPAKTOCF_BIN=/d1/mccabe/bin/GempakToCF
+
+# Location of ncap2 exe
 export METPLUS_TEST_NCAP2=/usr/local/nco/bin/ncap2
--- internal_tests/use_cases/test_use_cases.py
+++ internal_tests/use_cases/test_use_cases.py
@@ -3,6 +3,7 @@
 import os
 from os.path import dirname, realpath
 import glob
+import shutil
 import subprocess
 import filecmp
 import logging
@@ -13,11 +14,6 @@ import met_util as util
 
 metplus_home = dirname(dirname(dirname(realpath(__file__))))
 use_case_dir = os.path.join(metplus_home,"parm/use_cases")
-
-
-# change these values to chose to run A or B
-run_a = False
-run_b = True
 
 # list of use cases to run
 param_files = [
@@ -63,83 +59,93 @@ param_files = [
                 use_case_dir+"/track_and_intensity/examples/track_and_intensity_ATCF.conf",
               ]
 
-def get_param_list(param_a, param_b):
-    metplus_home = dirname(dirname(dirname(realpath(__file__))))
-    a_conf = metplus_home+"/internal_tests/use_cases/system.a.conf"
-    b_conf = metplus_home+"/internal_tests/use_cases/system.b.conf"
-    params_a = param_a.split(",")
-    params_b = param_b.split(",")
-    params_a = params_a + [a_conf]
-    params_b = params_b + [b_conf]
-    return params_a, params_b
+def get_param_list(param):
+    conf = metplus_home+"/internal_tests/use_cases/system.conf"
+    params = param.split(",")
+    params = params + [conf]
+    return params
 
 
-def get_params(param_a, param_b):
-    params_a, params_b = get_param_list(param_a, param_b)
+def get_params(param):
+    params = get_param_list(param)
 
     logger = logging.getLogger('master_metplus')    
 
-    # read A confs
-    (parm, infiles, moreopt) = config_launcher.parse_launch_args(params_a,
+    # read confs
+    (parm, infiles, moreopt) = config_launcher.parse_launch_args(params,
                                                                  None, None,
                                                                  logger,
                                                                  util.baseinputconfs)
     p = config_launcher.launch(infiles, moreopt)
 
-    # read B confs     
-    (parm, infiles, moreopt) = config_launcher.parse_launch_args(params_b,
-                                                                 None, None,
-                                                                 logger,
-                                                                 util.baseinputconfs)
-    p_b = config_launcher.launch(infiles, moreopt)
-    return p, p_b
+    return params, p
 
-def run_test_use_case(param_a, param_b, run_a, run_b):
-    params_a, params_b = get_param_list(param_a, param_b)
-    p, p_b = get_params(param_a, param_b)
-    a_dir = os.path.join(p.getdir('OUTPUT_BASE'), os.path.basename(params_a[-2]))
-    b_dir = os.path.join(p_b.getdir('OUTPUT_BASE'), os.path.basename(params_b[-2]))
+def run_test_use_case(param, test_metplus_base):
 
-    # run A
-    if run_a:
-        metplus_base_a = os.environ['METPLUS_TEST_A_METPLUS_BASE']
-        cmd = os.path.join(metplus_base_a, "ush", "master_metplus.py")
-        for parm in params_a:
-            cmd += " -c "+parm
-        cmd += ' -c dir.OUTPUT_BASE='+a_dir
-        print("CMD A:"+cmd)
-        process = subprocess.Popen(cmd, shell=True)
-        process.wait()
+    params, p = get_params(param)
+    out_dir = os.path.join(p.getdir('OUTPUT_BASE'), os.path.basename(params[-2]))
 
-    # run B
-    if run_b:
-        metplus_base_b = os.environ['METPLUS_TEST_B_METPLUS_BASE']
-        cmd = os.path.join(metplus_base_b, "ush", "master_metplus.py")
-        for parm in params_b:
-            cmd += " -c "+parm
-        cmd += ' -c dir.OUTPUT_BASE='+b_dir
-        print("CMD B:"+cmd)
-        process = subprocess.Popen(cmd, shell=True)
-        process.wait()
+    cmd = os.path.join(test_metplus_base, "ush", "master_metplus.py")
+    for parm in params:
+        cmd += " -c "+parm
+    cmd += ' -c dir.OUTPUT_BASE='+out_dir
+    print("CMD:"+cmd)
+    process = subprocess.Popen(cmd, shell=True)
+    process.wait()
 
 def main():
-    metplus_base_a = os.environ['METPLUS_TEST_A_METPLUS_BASE']
-    metplus_base_b = os.environ['METPLUS_TEST_B_METPLUS_BASE']
+    if os.environ.get('METPLUS_TEST_METPLUS_BASE') is None:
+        test_metplus_base = metplus_home
+    else:
+        test_metplus_base = os.environ['METPLUS_TEST_METPLUS_BASE']
+
+    if not test_metplus_base:
+        test_metplus_base = metplus_home
+
     print("Starting test script")
+    print("Running " + test_metplus_base + " to test")
+
+    output_base_prev = os.environ['METPLUS_TEST_PREV_OUTPUT_BASE']
+    output_base = os.environ['METPLUS_TEST_OUTPUT_BASE']
+
+
+    # if there are files in output base, prompt user to copy them to prev output base
+    if os.path.exists(output_base) and os.listdir(output_base):
+
+        # if prev exists, ask user to wipe it out
+        if os.path.exists(output_base_prev):
+
+            print("OUTPUT_BASE for previous run exists:" + output_base_prev)
+            user_answer = input("Would you like to remove all files? (y/n)[n]")
+
+            if user_answer and user_answer[0] == 'y':
+                print("Removing " + output_base_prev + " and all files under")
+                shutil.rmtree(output_base_prev)
+            else:
+                print("Directory must be empty to proceed with tests")
+                exit(1)
+
+        print("Moving " + output_base + " to " + output_base_prev)
+        os.rename(output_base, output_base_prev)
 
     for param_file in param_files:
-        param_a = param_file.replace(metplus_home, metplus_base_a)
-        param_b = param_file.replace(metplus_home, metplus_base_b)
-        run_test_use_case(param_a, param_b, run_a, run_b)
+        param = param_file.replace(metplus_home, test_metplus_base)
+        run_test_use_case(param, test_metplus_base)
 
-    # compare results with commands
-    output_base_a = os.environ['METPLUS_TEST_A_OUTPUT_BASE']
-    output_base_b = os.environ['METPLUS_TEST_B_OUTPUT_BASE']
+    # compare results with commands if prev output base has files
+    if not os.path.exists(output_base_prev) or not os.listdir(output_base_prev):
+        print("No files were found in previous OUTPUT_BASE: " + output_base_prev +\
+              "\nRun this script again to compare results to previous run")
+        exit(0)
 
+    print("\nIf files or directories were only found in one run, they will appear here:\n")
+    diff_cmd = f"diff -r {output_base_prev} {output_base} | grep Only"
+    process = subprocess.Popen(diff_cmd, shell=True)
+    process.wait()
 
-    print("Run the following to compare results:")
-    print(f"diff -r {output_base_a} {output_base_b} | grep -v Binary | grep -v SSH | grep -v CONDA | grep -v OLDPWD | grep -v tmp | grep -v CLOCK_TIME | grep -v XDG | grep -v GSL | grep -v METPLUS | grep -v \"METplus took\" | grep -v \"Finished\" | grep -v \"\-\-\-\" | egrep -v \"^[[:digit:]]*c[[:digit:]]*$\" | grep -v diff")
-    print("NOTE: pipe results to 'grep Only' to see which files were only in one output directory")
+    print("\nCompare the output from previous run (" + output_base_prev + ") to this run"+\
+          " (" + output_base + ").\nRun the following to compare results:")
+    print(f"diff -r {output_base_prev} {output_base} | grep -v Binary | grep -v SSH | grep -v CONDA | grep -v OLDPWD | grep -v tmp | grep -v CLOCK_TIME | grep -v XDG | grep -v GSL | grep -v METPLUS | grep -v \"METplus took\" | grep -v \"Finished\" | grep -v \"\-\-\-\" | egrep -v \"^[[:digit:]]*c[[:digit:]]*$\" | less")
     
 if __name__ == "__main__":
     main()
