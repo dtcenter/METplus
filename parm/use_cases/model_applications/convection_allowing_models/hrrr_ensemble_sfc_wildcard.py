4557f316a7a39a65701605252438e1f9938b0c97
George McCabe <mccabe@ucar.edu>
2020-01-08 15:40:13 +0000

    added documentation for ensemble use cases

---
+++ parm/use_cases/model_applications/convection_allowing_models/hrrr_ensemble_sfc_wildcard.py
@@ -0,0 +1,9 @@
+"""
+HRRR Ensemble Wildcard Use Case
+========
+
+This use case performs the same actions as hrrr_ensemble_sfc.conf, but utilized wildcards
+to find ensemble members instead of listing each member individually. See that use case's
+documentation for more information on this use case. LINK?
+
+"""
