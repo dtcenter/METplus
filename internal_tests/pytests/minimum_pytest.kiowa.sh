944bfb627c7b66e5b8f7bfd1fc400a98aa63947d
George McCabe <mccabe@kiowa.rap.ucar.edu>
2020-02-03 16:03:30 -0700

    use kiowa /d1/personal dirs, couldn't change tmp dir from /tmp to relative to user output base because stat analysis fails if the tmp dir doesn't exist

--- internal_tests/pytests/minimum_pytest.kiowa.sh
+++ internal_tests/pytests/minimum_pytest.kiowa.sh
@@ -1,6 +1,7 @@
 export METPLUS_TEST_INPUT_BASE=/d1/METplus_Data
-export METPLUS_TEST_OUTPUT_BASE=/d1/${USER}/pytest
+export METPLUS_TEST_OUTPUT_BASE=/d1/personal/${USER}/pytest
 export METPLUS_TEST_MET_INSTALL_DIR=/d1/projects/MET/MET_releases/met-9.0_beta2
+#export METPLUS_TEST_TMP_DIR=${METPLUS_TEST_OUTPUT_BASE}/tmp
 export METPLUS_TEST_TMP_DIR=/tmp
 export METPLUS_TEST_EXE_WGRIB2=/usr/local/bin/wgrib2
 export METPLUS_TEST_EXE_CUT=/usr/bin/cut
