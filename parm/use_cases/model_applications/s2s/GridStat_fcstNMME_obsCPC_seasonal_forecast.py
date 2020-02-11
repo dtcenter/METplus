1546a835037044a6c6b02e417e9b0ddf3c4f9552
George McCabe <mccabe@ucar.edu>
2020-02-11 17:43:27 +0000

    cleaned up formatting

--- parm/use_cases/model_applications/s2s/GridStat_fcstNMME_obsCPC_seasonal_forecast.py
+++ parm/use_cases/model_applications/s2s/GridStat_fcstNMME_obsCPC_seasonal_forecast.py
@@ -20,86 +20,81 @@ Brief summary of this use case...
 # the expansion of options to describe time as well as the strategic selection
 # of hindcasts.
 #
-# Time variable
-# Commonly, MET and METplus use the time variable with units such as seconds, minutes
-# and possibly hours. With limited number of digits available to express the time,
-# durations of months is hitting limits of this format. It is therefore imperative that
-# MET and METplus expand their ability to offer time variable capabilities that deal
-# with days, months, and years. A change in year is common when a 6 months forecast is
-# initiated after July 1. The lead time of 6 months then invariably runs into the
-# following calendar year. The development requirements for MET and METplus thus include
-# the ability to provide time-stepping of days and months rather than seconds and minutes,
-# and the treatment of forecast leads needs to cover days, months and years.
+# Time:
+#  Commonly METplus expresses time intervals in the minutes, hours, and days. 
+#  Month and year intervals were not supported since there is not a constant length for these units.
+#  Therefore, modifications to METplus were made to support these intervals by determining the offset
+#  relative to a given time.
 #
-# Input data
-# The input data from seasonal forecasts is generally based on daily, weekly, decadal
-# (10-day), monthly or seasonal time integrated intervals. The time variable therefore is
-# often no longer a simple snapshot of the system but rather representing an average, a
-# sum (precipitation), or a particular statistic (maximum wind, minimum temperature, wind
-# variability) over the integration time period. This requires some adjustment from the
-# traditional approach in forecast verification where forecast time ("valid-time") is
-# simply a snapshot out of a continuous run.
+# Input data:
+#  The input data from seasonal forecasts is generally based on daily, weekly, decadal
+#  (10-day), monthly or seasonal time integrated intervals. The time variable therefore is
+#  often no longer a simple snapshot of the system but rather representing an average, a
+#  sum (precipitation), or a particular statistic (maximum wind, minimum temperature, wind
+#  variability) over the integration time period. This requires some adjustment from the
+#  traditional approach in forecast verification where forecast time ("valid-time") is
+#  simply a snapshot out of a continuous run.
 #
-# Hindcasts
-# The objective of seasonal forecasts is no longer the exact location and intensity
-# of one particular weather event, such as a storm, a frontal passage, or high wind
-# conditions. Rather, seasonal forecasting focuses more on the statistical properties
-# over a period of time, be it a 10-day interval, a month, or even a three month season.
-# The verification of a new, forward looking seasonal forecast requires assessments of
-# the forecast systems ability to appropriately forecast that longrange behavior of the
-# weather (here, only atmospheric verification is considered, but the same concept would
-# apply ocean or any other longrange forecast system). Because weather properties
-# commonly change significantly over the course of the season, samples to verify the
-# prognostic system can not be taken from the immediate days, weeks of months before the
-# forecast. Hindcasting in the seasonal context requires a complete set of forecasts
-# based on the same season but during past years. A current July-1 2019 forecast, therefore
-# requires many July-1 forecasts for as many years in the past as possible, given that
-# the forecast system is the same as the one used for the current forecast cycle into the
-# future. Operational centers offer hindcasts, also sometimes called "re-forecasts", with
-# the current, most up-to-date forecast system. MET and METplus therefore need to be able
-# to extract the appropriate collection of past forecasts. This includes the identification
-# of the same Julian-day-of-Year init-dates from forecasts cycles from past years, and then
-# identify the different lead-times of interest generally ranging from one to 6 or more
-# months.
+# Hindcasts:
+#  The objective of seasonal forecasts is no longer the exact location and intensity
+#  of one particular weather event, such as a storm, a frontal passage, or high wind
+#  conditions. Rather, seasonal forecasting focuses more on the statistical properties
+#  over a period of time, be it a 10-day interval, a month, or even a three month season.
+#  The verification of a new, forward looking seasonal forecast requires assessments of
+#  the forecast systems ability to appropriately forecast that longrange behavior of the
+#  weather (here, only atmospheric verification is considered, but the same concept would
+#  apply ocean or any other longrange forecast system). Because weather properties
+#  commonly change significantly over the course of the season, samples to verify the
+#  prognostic system can not be taken from the immediate days, weeks of months before the
+#  forecast. Hindcasting in the seasonal context requires a complete set of forecasts
+#  based on the same season but during past years. A current July-1 2019 forecast, therefore
+#  requires many July-1 forecasts for as many years in the past as possible, given that
+#  the forecast system is the same as the one used for the current forecast cycle into the
+#  future. Operational centers offer hindcasts, also sometimes called "re-forecasts", with
+#  the current, most up-to-date forecast system. MET and METplus therefore need to be able
+#  to extract the appropriate collection of past forecasts. This includes the identification
+#  of the same Julian-day-of-Year init-dates from forecasts cycles from past years, and then
+#  identify the different lead-times of interest generally ranging from one to 6 or more
+#  months.
 #
-# Verification
-# The verification steps can then utilize the existing collection of verification tools.
-# In comparison to weather forecasts, the only difference is that the data, as stated
-# above, are not snapshots but time-integrated values (averages, sums, statistics) that
-# are representing a whole period of time. The verification then focuses on comparisons
-# of these derivatives of the forecast simulations. In practice, a further step might be
-# added prior to, or as a key step during verification: the formation of anomalies of the
-# forecasts compared to long-term expected averages. A rainfall forecasts can therefore
-# be verified in both absolute as well as anomaly context where some analyses might focus
-# on extreme rainfall threshold exeedance of, for example, 500mm per month. At the same
-# time, the same forecast might be verified for the 3 months rainfall average in comparison
-# with the long-term expected mean. The verification might then assess how well the system
-# can foresee the occurrence of below average rainfall over the season, and possibly some
-# selected thresholds there (e.g., ability to forecast mean seasonal rainfall below
-# the 10-th percentile of seasonal rainfall). Finally, flexibility in formulating forecast
-# verification strategies is important as forecast skill might vary by location, the timing
-# within the seasonal cycle, or the state of the evolving coupled system (the rapid onset
-# of a strong El Nino will lead to significantly different forecast skill compared to a
-# neutral state in the Pacific). Memory from past months, for example when considering
-# accumulated soil moisture, might also influence the forecast skills. Seasonal forecast
-# verification therefore requires understanding of the climate system; MET and METplus
-# then need to offer the flexibility to tailor verification strategies and to potentially
-# craft conditional approaches.
+# Verification:
+#  The verification steps can then utilize the existing collection of verification tools.
+#  In comparison to weather forecasts, the only difference is that the data, as stated
+#  above, are not snapshots but time-integrated values (averages, sums, statistics) that
+#  are representing a whole period of time. The verification then focuses on comparisons
+#  of these derivatives of the forecast simulations. In practice, a further step might be
+#  added prior to, or as a key step during verification: the formation of anomalies of the
+#  forecasts compared to long-term expected averages. A rainfall forecasts can therefore
+#  be verified in both absolute as well as anomaly context where some analyses might focus
+#  on extreme rainfall threshold exeedance of, for example, 500mm per month. At the same
+#  time, the same forecast might be verified for the 3 months rainfall average in comparison
+#  with the long-term expected mean. The verification might then assess how well the system
+#  can foresee the occurrence of below average rainfall over the season, and possibly some
+#  selected thresholds there (e.g., ability to forecast mean seasonal rainfall below
+#  the 10-th percentile of seasonal rainfall). Finally, flexibility in formulating forecast
+#  verification strategies is important as forecast skill might vary by location, the timing
+#  within the seasonal cycle, or the state of the evolving coupled system (the rapid onset
+#  of a strong El Nino will lead to significantly different forecast skill compared to a
+#  neutral state in the Pacific). Memory from past months, for example when considering
+#  accumulated soil moisture, might also influence the forecast skills. Seasonal forecast
+#  verification therefore requires understanding of the climate system; MET and METplus
+#  then need to offer the flexibility to tailor verification strategies and to potentially
+#  craft conditional approaches.
 #
-# Overall, seasonal forecasts don't require a new verification approach. It does however
-# put demands on the flexibility of dealing with a significantly exapanded range of the
-# time variable as well as logistic infrastructure to select appropriate hindcast samples
-# from long hindcast or re-forecast archives. Scientifically, the challenges are mostly
-# restricted in the appropriate formulation of verificaation questions that address
-# specific forecast objectives. Compared to weather forecastsing, seasonal forecasts need
-# to draw their skill from slowly changing components in the coupled Earth system while
-# acknowledging the high-frequency noise of weather superposed on these 'climatologically'
-# evolving background conditions. In many regions of the world, the noise might dominate
-# that background climate and forecast skill is low. It is therefore the task of seasonal
-# forecast verification to identify where there is actually skill for particular properties
-# of the forecasts over a wide range of lead-times. The skill might be dependent on
-# location, on the timing within the seasonal cycle, or even on the evolving state of the
-# coupled system.
+#  Overall, seasonal forecasts don't require a new verification approach. It does however
+#  put demands on the flexibility of dealing with a significantly exapanded range of the
+#  time variable as well as logistic infrastructure to select appropriate hindcast samples
+#  from long hindcast or re-forecast archives. Scientifically, the challenges are mostly
+#  restricted in the appropriate formulation of verificaation questions that address
+#  specific forecast objectives. Compared to weather forecastsing, seasonal forecasts need
+#  to draw their skill from slowly changing components in the coupled Earth system while
+#  acknowledging the high-frequency noise of weather superposed on these 'climatologically'
+#  evolving background conditions. In many regions of the world, the noise might dominate
+#  that background climate and forecast skill is low. It is therefore the task of seasonal
+#  forecast verification to identify where there is actually skill for particular properties
+#  of the forecasts over a wide range of lead-times. The skill might be dependent on
+#  location, on the timing within the seasonal cycle, or even on the evolving state of the
+#  coupled system.
 
 ##############################################################################
 # Datasets
@@ -144,8 +139,7 @@ Brief summary of this use case...
 # METplus Workflow
 # ----------------
 #
-# The following tools are used for each run time:
-#  GridStat
+# The following tools are used for each run time: GridStat
 #
 # This example loops by initialization time. Each initialization time is July of each year from 1982 to 2010. For each init time it will run once, processing forecast leads 1 month through 5 months. The following times are processed:
 #
@@ -228,15 +222,15 @@ Brief summary of this use case...
 # 	contains all necessary configurations to compute the skill statistics. The options for
 # 	these statistics are listed in the MET-Users Guide. There are many options, and
 # 	turning them on is done in the context of the different groups or “packages”.
-
-
+#
 
 ##############################################################################
 # METplus Configuration
 # ---------------------
 #
-# .. highlight:: none
+# .. highlight:: bash
 # .. literalinclude:: ../../../../parm/use_cases/model_applications/s2s/GridStat_fcstNMME_obsCPC_seasonal_forecast.conf
+#
 
 ##############################################################################
 # Running METplus
@@ -267,35 +261,6 @@ Brief summary of this use case...
 # **NOTE:** All of these items must be found under the [dir] section.
 #
 #
-#  ------  METplus is run via a bash script -------
-#
-#
-
-#PROJECT="APIK"
-# Set the path to the version of METplus you will run
-#
-# METPLUS_BASE_DIR="/opt/utilities/METplus_feature_281_py_embed" 	# development version
-# The repository that contains the stand-alone use case
-#
-#PROJECT_CODE_DIR=${PROJECT_BASE_DIR}/${PROJECT}/"METplus-for-Indonesia-APIK"
-# inside the repository, here is the preliminary-release specific user configuration:
-#
-#USER_CONFIG_DIR=${PROJECT_BASE_DIR}/${PROJECT}/"METplus-for-Indonesia-APIK/METplus_feature_281_py_embed_parm/user_config"
-#  ... and the
-#USE_CASE_CONFIG_PATH=${PROJECT_CODE_DIR}/"METplus_feature_281_py_embed_parm/examples"
-# -------  METplus Execution --------- #
-#
-# Now the execution of METplus following a specified use case config file and user config files
-#
-# point to configuration file
-# USE_CASE_CONFIG_FILENAME="grid_to_grid_s2s_use_case_1_August-December.conf"
-# now launch the METplus python master script
-#
-# echo "${METPLUS_BASE_DIR}/ush/master_metplus.py -c ${USER_CONFIG_DIR}/apik.conf -c ${USE_CASE_CONFIG_PATH}/${USE_CASE_CONFIG_FILENAME}"
-#${METPLUS_BASE_DIR}/ush/master_metplus.py -c ${USER_CONFIG_DIR}/apik.conf -c ${USE_CASE_CONFIG_PATH}/${USE_CASE_CONFIG_FILENAME}
-#
-
-
 
 ##############################################################################
 # Expected Output
