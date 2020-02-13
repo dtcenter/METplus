2f3a440e6764fd1a9ae71daef618e18df99b5997
George McCabe <mccabe@dakota.rap.ucar.edu>
2020-02-12 17:28:26 -0700

    use MET 9.0 beta 3 on dakota

--- internal_tests/pytests/minimum_pytest.dakota.sh
+++ internal_tests/pytests/minimum_pytest.dakota.sh
@@ -1,7 +1,7 @@
 export METPLUS_TEST_INPUT_BASE=/d3/projects/MET/METplus_Data
 export METPLUS_TEST_OUTPUT_BASE=/d3/personal/${USER}/pytest
-export METPLUS_TEST_MET_INSTALL_DIR=/usr/local/met-8.1.1
-export METPLUS_TEST_TMP_DIR=/tmp
+export METPLUS_TEST_MET_INSTALL_DIR=/d3/projects/MET/MET_releases/met-9.0_beta3
+export METPLUS_TEST_TMP_DIR=${METPLUS_TEST_OUTPUT_BASE}/tmp
 
 export METPLUS_TEST_EXE_WGRIB2=/usr/local/bin/wgrib2
 export METPLUS_TEST_EXE_CUT=/usr/bin/cut
