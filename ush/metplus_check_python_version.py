26fcc6af0323f5ff4192ed8861a6c02ee409da8c
George McCabe <mccabe@ucar.edu>
2020-02-19 19:54:45 +0000

    fixed python version check bug

--- ush/metplus_check_python_version.py
+++ ush/metplus_check_python_version.py
@@ -2,11 +2,13 @@
 
 from sys import version, exit
 
-supported_py_version = '3.6.3'.split('.')
-py_version = version.split(' ')[0].split('.')
+supported_py_version = '3.6.3'
+supported_list = supported_py_version.split('.')
+py_version = version.split(' ')[0]
+user_list = py_version.split('.')
 
-for user, supported in zip(py_version, supported_py_version):
-    if user < supported:
+for user, supported in zip(user_list, supported_list):
+    if int(user) < int(supported):
         print("Must be using Python {} or higher.".format(supported_py_version))
         print("You are using {}.".format(py_version))
         exit(1)
