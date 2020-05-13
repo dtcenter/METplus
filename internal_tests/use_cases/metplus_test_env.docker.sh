15d807997d6e1e52a394e2db2889086e57b22700
George McCabe <mccabe@kiowa.rap.ucar.edu>
2020-05-13 10:26:16 -0600

    updated docker env file for use case tests to use env vars set by docker

--- internal_tests/use_cases/metplus_test_env.docker.sh
+++ internal_tests/use_cases/metplus_test_env.docker.sh
@@ -4,16 +4,15 @@ export METPLUS_TEST_METPLUS_BASE=
 
 # version of METplus to test
 export METPLUS_TEST_MET_INSTALL_DIR=/usr/local
-#export METPLUS_TEST_MET_INSTALL_DIR=/d1/projects/MET/MET_releases/met-9.0_beta4
 
 # location to write output from test run
-export METPLUS_TEST_OUTPUT_BASE=/metplus/test-use-case-output
+export METPLUS_TEST_OUTPUT_BASE=${OWNER_BUILD_DIR}/test-use-case-output
 
 # location of output from previous test run to compare to current run
-export METPLUS_TEST_PREV_OUTPUT_BASE=/metplus/test-use-case-output-old
+export METPLUS_TEST_PREV_OUTPUT_BASE=${OWNER_BUILD_DIR}/test-use-case-output-old
 
 # location of input data to use in tests
-export METPLUS_TEST_INPUT_BASE=/metplus/test.metplus.data
+export METPLUS_TEST_INPUT_BASE=${OWNER_BUILD_DIR}/test.metplus.data
 
 # location of GempakToCF to run tests that use Gempak
 export METPLUS_TEST_GEMPAKTOCF_JAR=${METPLUS_TEST_INPUT_BASE}/GempakToCF.jar
