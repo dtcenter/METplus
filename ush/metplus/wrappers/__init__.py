5c182372f08b1de08522f071da4cf783feb014e9
George McCabe <mccabe@kiowa.rap.ucar.edu>
2020-05-21 14:16:47 -0600

    added skip import for plotting wrappers if env var is set

--- ush/metplus/wrappers/__init__.py
+++ ush/metplus/wrappers/__init__.py
@@ -1,17 +1,23 @@
 from os.path import dirname, basename, isfile, join
+from os import environ
 import glob
 import sys
 import pkgutil
 import importlib
 
+# these wrappers should not be imported if plotting is disabled
+plotting_wrappers = [
+    'tcmpr_plotter_wrapper',
+    'cyclone_plotter_wrapper',
+    'make_plots_wrapper',
+]
+
+# loop through modules and skip plotting wrappers if they are disabled
 for (module_loader, name, ispkg) in pkgutil.iter_modules([dirname(__file__)]):
+    if environ.get('METPLUS_DISABLE_PLOT_WRAPPERS', False) and name in plotting_wrappers:
+        continue
+
     importlib.import_module('.'+name, __package__)
-#for module in modules:
-#    module = basename(module)
-#    if module == '__init__.py' or module[-3:] != '.py':
-#        continue
-#    __import__(module[:-3], locals(), globals())
-#del module
 
 modules = glob.glob(join(dirname(__file__), "*_wrapper.py"))
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
