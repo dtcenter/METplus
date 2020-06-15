4bc1dbe068d4fe4b56f795d65d50c490022b5385
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2020-06-15 15:24:07 -0600

    MTD can be run on just fcst or obs, so
    template is not required

--- metplus/wrappers/mtd_wrapper.py
+++ metplus/wrappers/mtd_wrapper.py
@@ -55,8 +55,6 @@ class MTDWrapper(MODEWrapper):
             c_dict['FCST_INPUT_TEMPLATE'] = \
               self.config.getraw('filename_templates',
                                  'FCST_MTD_INPUT_TEMPLATE')
-            if not c_dict['FCST_INPUT_TEMPLATE']:
-                self.log_error("FCST_MTD_INPUT_TEMPLATE required to run")
 
             c_dict['FCST_INPUT_DATATYPE'] = \
                 self.config.getstr('config', 'FCST_MTD_INPUT_DATATYPE', '')
@@ -90,8 +88,7 @@ class MTDWrapper(MODEWrapper):
             c_dict['OBS_INPUT_TEMPLATE'] = \
               self.config.getraw('filename_templates',
                                    'OBS_MTD_INPUT_TEMPLATE')
-            if not c_dict['OBS_INPUT_TEMPLATE']:
-                self.log_error("OBS_MTD_INPUT_TEMPLATE required to run")
+
             c_dict['OBS_INPUT_DATATYPE'] = \
                 self.config.getstr('config', 'OBS_MTD_INPUT_DATATYPE', '')
 
