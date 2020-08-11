8b240c153921ace55ef723fb56f6c15fbeaae11e
George McCabe <mccabe@ucar.edu>
2020-08-11 13:23:45 -0600

    list full path to docker directory so docker build succeeds - previously resorted to docker pull

--- ci/travis_jobs/docker_setup.sh
+++ ci/travis_jobs/docker_setup.sh
@@ -6,7 +6,7 @@ echo Get Docker image: ${DOCKERHUB_TAG}
 
 # Note: adding --build-arg <arg-name> without any value tells docker to
 #  use value from local environment (export DO_GIT_CLONE)
-docker build -t ${DOCKERHUB_TAG} --build-arg SOURCE_BRANCH=${DOCKERHUB_DEFAULT_TAGNAME} --build-arg MET_BRANCH=${DOCKERHUB_MET_TAGNAME} --build-arg DO_GIT_CLONE internal_tests/docker
+docker build -t ${DOCKERHUB_TAG} --build-arg SOURCE_BRANCH=${DOCKERHUB_DEFAULT_TAGNAME} --build-arg MET_BRANCH=${DOCKERHUB_MET_TAGNAME} --build-arg DO_GIT_CLONE ${TRAVIS_BUILD_DIR}/internal_tests/docker
 docker images
 docker run --rm -e "PATH=/metplus/METplus/ush:$PATH" -v ${OWNER_BUILD_DIR}:/metplus ${DOCKERHUB_TAG} /bin/bash -c 'which master_metplus.py;ls -al /metplus;python3 -V'
 
