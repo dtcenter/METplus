0e59fc6ee6b530b2ef66038afd72d95f58a87390
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2020-08-19 12:58:23 -0600

    More verbose docker_run prints

--- ci/travis_jobs/docker_run_metplus.sh
+++ ci/travis_jobs/docker_run_metplus.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo "in docker_run_metplus.sh"
-echo DOCKERHUB_TAG = ${DOCKERHUB_TAG}
+echo 'In docker_run_metplus.sh...'
+echo 'In docker_run_metplus.sh, DOCKERHUB_TAG =' ${DOCKERHUB_TAG}
 
 ret=`docker run --rm -v ${OWNER_BUILD_DIR}:/metplus ${DOCKERHUB_TAG} /bin/bash -c "$1"`
 
-echo "docker run $1" $1
-echo "docker run $ret" $ret
+echo 'In docker_run_metplus.sh $1:' $1
+echo 'In docker_run_metplus.sh $2:' $2
+echo 'In docker_run_metplus.sh $ret:' $ret
 
 #if [ $ret != 0 ]; then
 #  exit $ret
