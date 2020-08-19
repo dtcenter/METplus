c90989df9c3ea0b56091c1bde602896089484353
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2020-08-19 09:53:08 -0600

    added echo statements to docker_run_metplus.sh

--- ci/travis_jobs/docker_run_metplus.sh
+++ ci/travis_jobs/docker_run_metplus.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 ret=`docker run --rm -v ${OWNER_BUILD_DIR}:/metplus ${DOCKERHUB_TAG} /bin/bash -c "$1"`
+
+echo "docker run $1" $1
+echo "docker run $ret" $ret
+
 if [ $ret != 0 ]; then
   exit $ret
 else
