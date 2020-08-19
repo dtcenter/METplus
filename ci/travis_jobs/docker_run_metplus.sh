b86f047dc93a1fc7684e35dae9e8e5b560d9c678
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2020-08-19 14:12:17 -0600

    fixed catching return code

--- ci/travis_jobs/docker_run_metplus.sh
+++ ci/travis_jobs/docker_run_metplus.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#print 'In docker_run_metplus.sh, DOCKERHUB_TAG =', ${DOCKERHUB_TAG}
+echo 'In docker_run_metplus.sh, DOCKERHUB_TAG =', ${DOCKERHUB_TAG}
 
-ret=`docker run --rm -v ${OWNER_BUILD_DIR}:/metplus ${DOCKERHUB_TAG} /bin/bash -c "$1"`
+#ret=`docker run --rm -v ${OWNER_BUILD_DIR}:/metplus ${DOCKERHUB_TAG} /bin/bash -c "$1"`
+docker run --rm -v ${OWNER_BUILD_DIR}:/metplus ${DOCKERHUB_TAG} /bin/bash -c "$1"
+ret=$?
 
-#print "In docker_run_metplus.sh $1:", $1
-#print "In docker_run_metplus.sh $2:", $2
-#print "In docker_run_metplus.sh $ret:", $ret
+echo "In docker_run_metplus.sh $1:", $1
+echo "In docker_run_metplus.sh $2:", $2
+echo "In docker_run_metplus.sh $ret:", $ret
 
 if [ $ret != 0 ]; then
   exit $ret
