0abd4583c1917e3bce1bb65b79fb848f0bc9a34d
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2020-08-19 14:06:18 -0600

    commented out all print statements

--- ci/travis_jobs/docker_run_metplus.sh
+++ ci/travis_jobs/docker_run_metplus.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-print 'In docker_run_metplus.sh, DOCKERHUB_TAG =', ${DOCKERHUB_TAG}
+#print 'In docker_run_metplus.sh, DOCKERHUB_TAG =', ${DOCKERHUB_TAG}
 
 ret=`docker run --rm -v ${OWNER_BUILD_DIR}:/metplus ${DOCKERHUB_TAG} /bin/bash -c "$1"`
 
-print "In docker_run_metplus.sh $1:", $1
-print "In docker_run_metplus.sh $2:", $2
-print "In docker_run_metplus.sh $ret:", $ret
+#print "In docker_run_metplus.sh $1:", $1
+#print "In docker_run_metplus.sh $2:", $2
+#print "In docker_run_metplus.sh $ret:", $ret
 
-#if [ $ret != 0 ]; then
-#  exit $ret
-#else
-#  exit $2
-#fi
+if [ $ret != 0 ]; then
+  exit $ret
+else
+  exit $2
+fi
 
 exit 999
