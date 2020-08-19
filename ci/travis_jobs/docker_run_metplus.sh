33be7ca07bfd9d5084fec43dcbcf3a0cc2685355
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2020-08-19 13:12:27 -0600

    added another echo

--- ci/travis_jobs/docker_run_metplus.sh
+++ ci/travis_jobs/docker_run_metplus.sh
@@ -5,6 +5,7 @@ echo 'In docker_run_metplus.sh, DOCKERHUB_TAG =' ${DOCKERHUB_TAG}
 
 ret=`docker run --rm -v ${OWNER_BUILD_DIR}:/metplus ${DOCKERHUB_TAG} /bin/bash -c "$1"`
 
+echo 'In docker_run_metplus.sh $ret:' $ret
 echo 'In docker_run_metplus.sh $1:' $1
 echo 'In docker_run_metplus.sh $2:' $2
 echo 'In docker_run_metplus.sh $ret:' $ret
