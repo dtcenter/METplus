8ad23f809194dd1e8b6a5000fa2e28bcc10f566b
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2020-08-19 13:56:46 -0600

    changed single quotes to double quotes in print statements

--- ci/travis_jobs/docker_run_metplus.sh
+++ ci/travis_jobs/docker_run_metplus.sh
@@ -4,10 +4,9 @@ print 'In docker_run_metplus.sh, DOCKERHUB_TAG =', ${DOCKERHUB_TAG}
 
 ret=`docker run --rm -v ${OWNER_BUILD_DIR}:/metplus ${DOCKERHUB_TAG} /bin/bash -c "$1"`
 
-print 'In docker_run_metplus.sh $ret:', $ret
-print 'In docker_run_metplus.sh $1:', $1
-print 'In docker_run_metplus.sh $2:', $2
-print 'In docker_run_metplus.sh $ret:', $ret
+print "In docker_run_metplus.sh $1:", $1
+print "In docker_run_metplus.sh $2:", $2
+print "In docker_run_metplus.sh $ret:", $ret
 
 #if [ $ret != 0 ]; then
 #  exit $ret
