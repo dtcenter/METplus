8b4820f6f359a22f98e6b67d6f947de717f6e94e
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2020-08-24 14:09:11 -0600

    changed from v3.0 to v3.1 in spaceweather tarball

--- ci/travis_jobs/test_use_cases_model_applications.sh
+++ ci/travis_jobs/test_use_cases_model_applications.sh
@@ -15,7 +15,7 @@ cryosphere_tar=v3.0/sample_data-cryosphere-3.0.tgz
 medium_range_tar=v3.1/sample_data-medium_range-3.1.tgz
 precipitation_tar=v3.0/sample_data-precipitation-3.0.tgz
 s2s_tar=v3.0/sample_data-s2s-3.0.tgz
-space_weather_tar=v3.0/sample_data-space_weather-3.0.tgz
+space_weather_tar=v3.1/sample_data-space_weather-3.1.tgz
 tc_and_extra_tc_tar=v3.1/sample_data-tc_and_extra_tc-3.1.tgz
 
 met_tool_wrapper_tarball=https://github.com/dtcenter/METplus/releases/download/v3.1/sample_data-met_tool_wrapper-3.1.tgz
