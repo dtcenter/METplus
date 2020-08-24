c8e2f6efd2280c6767fbfee023e2e35c5fa03ac2
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2020-08-24 09:59:19 -0600

    changed trailing  back to

--- ci/travis_jobs/docker_run_metplus.sh
+++ ci/travis_jobs/docker_run_metplus.sh
@@ -3,7 +3,7 @@
 echo 'In docker_run_metplus.sh, DOCKERHUB_TAG =', ${DOCKERHUB_TAG}
 
 #docker run --rm -v ${OWNER_BUILD_DIR}:/metplus ${DOCKERHUB_TAG} /bin/bash -c "$1"
-docker run --rm -v ${OWNER_BUILD_DIR}:/metplus -v ${OWNER_BUILD_DIR}/test.metplus.data:/input -v ${OWNER_BUILD_DIR}/test-use-case-output:/output ${DOCKERHUB_TAG} /bin/bash -c "$2"
+docker run --rm -v ${OWNER_BUILD_DIR}:/metplus -v ${OWNER_BUILD_DIR}/test.metplus.data:/input -v ${OWNER_BUILD_DIR}/test-use-case-output:/output ${DOCKERHUB_TAG} /bin/bash -c "$1"
 ret=$?
 
 echo "In docker_run_metplus.sh 1:", $1
