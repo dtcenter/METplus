a732d09c5e0f8181b21196c71e73c971eb6d38f8
George McCabe <mccabe@ucar.edu>
2020-08-26 12:38:09 -0600

    added sudo to mkdir call

--- ci/travis_jobs/get_cartopy.sh
+++ ci/travis_jobs/get_cartopy.sh
@@ -3,7 +3,7 @@
 #Shell script for installing cartopy
 #Called from a docker run command inside "test_use_cases_met_tool_wrappers.sh"
 
-mkdir /cartopy
+sudo mkdir /cartopy
 cd /cartopy
 wget https://download.osgeo.org/proj/proj-4.9.1.tar.gz
 tar zxf proj-4.9.1.tar.gz
