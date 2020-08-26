85a6c1934553a6e0cae9e2ed85794072329a53dd
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2020-08-25 18:13:00 -0600

    Feature 581 mtd py embed error (#582)

--- metplus/util/met_util.py
+++ metplus/util/met_util.py
@@ -2539,7 +2539,10 @@ def preprocess_file(filename, data_type, config, allow_dir=False):
 
     # if using python embedding for input, return the keyword
     if os.path.basename(filename) in PYTHON_EMBEDDING_TYPES:
-            return os.path.basename(filename)
+        return os.path.basename(filename)
+
+    if data_type is not None and 'PYTHON' in data_type:
+        return filename
 
     stage_dir = config.getdir('STAGING_DIR')
 
