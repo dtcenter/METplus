1bdf11662cfd961a74f6cc2c314278f70759f3b0
John Halley Gotway <johnhg@ucar.edu>
2020-09-01 13:41:06 -0600

    Per #589 and as discussed via Slack, consolidate the / down into just  that includes the source METplus version to avoid confusion with the current METplus version which is being used to create the Docker tag.

--- internal_tests/docker_data/build_docker_images.sh
+++ internal_tests/docker_data/build_docker_images.sh
@@ -4,16 +4,14 @@ METPLUS_VERSION=3.1
 
 for ASSET in $(cat metplus_sample_data); do
   IMGNAME="dtcenter/metplus-data:${METPLUS_VERSION}-`echo ${ASSET} | cut -d':' -f1`"
-  VERSION=`echo ${ASSET} | cut -d':' -f2`
-  TARFILE=`echo ${ASSET} | cut -d':' -f3`
-  MOUNTPT=`echo ${ASSET} | cut -d':' -f4`
+  TARFILE=`echo ${ASSET} | cut -d':' -f2`
+  MOUNTPT=`echo ${ASSET} | cut -d':' -f3`
 
   echo
   echo "Building image ... ${IMGNAME}" 
   echo
 
   docker build -t ${IMGNAME} . \
-    --build-arg VERSION=${VERSION} \
     --build-arg TARFILE=${TARFILE} \
     --build-arg MOUNTPT=${MOUNTPT}
 
