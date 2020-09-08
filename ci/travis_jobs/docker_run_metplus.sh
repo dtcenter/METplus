63615eb314bd2e5fe5ed1a21ecacd03bf2700fcc
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2020-09-08 15:59:19 -0600

    does  propagate into docker_run_metplus.sh

--- ci/travis_jobs/docker_run_metplus.sh
+++ ci/travis_jobs/docker_run_metplus.sh
@@ -3,6 +3,8 @@
 # set umask to 002 so that the travis user has (group) permission
 # to move files that are created by docker
 
+echo 'In docker_run_metplus, $VOLUMES= ',$VOLUMES
+
 echo  In docker_run_metplus.sh, RUNNING: $1
 docker run --rm --user root:$UID -v ${OWNER_BUILD_DIR}:${DOCKER_WORK_DIR} -v ${OWNER_BUILD_DIR}/input:${DOCKER_DATA_INPUT} -v ${OWNER_BUILD_DIR}/output:/output ${DOCKERHUB_TAG} /bin/bash -c "umask 002; $1"
 ret=$?
