8ecf65a0b18994d11901cb7e6b43ded06d1394d3
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2020-09-18 15:05:39 -0600

    replaced 'internal_tests' with ci/docker

--- ci/docker/docker_setup.sh
+++ ci/docker/docker_setup.sh
@@ -5,7 +5,7 @@ echo 'doing docker build'
 
 #${TRAVIS_BUILD_DIR}/ci/docker/get_data_volumes.py
 
-docker build -t ${DOCKERHUB_TAG} --build-arg SOURCE_BRANCH=${DOCKERHUB_DEFAULT_TAGNAME} --build-arg MET_BRANCH=${DOCKERHUB_MET_TAGNAME} --build-arg DO_GIT_CLONE ${TRAVIS_BUILD_DIR}/internal_tests/docker
+docker build -t ${DOCKERHUB_TAG} --build-arg SOURCE_BRANCH=${DOCKERHUB_DEFAULT_TAGNAME} --build-arg MET_BRANCH=${DOCKERHUB_MET_TAGNAME} --build-arg DO_GIT_CLONE ${TRAVIS_BUILD_DIR}/ci/docker
 
 ### GitHub 607
 ### eg change ${TRAVIS_BUILD_DIR}/internal_tests/docker to ${TRAVIS_BUILD_DIR}/ci/docker
