49bf76b8c242c38e748bf5ed3118c7cc24faf1ad
johnhg <johnhg@ucar.edu>
2020-09-18 10:36:46 -0600

    Update docker_setup.sh

--- ci/travis_jobs/docker_setup.sh
+++ ci/travis_jobs/docker_setup.sh
@@ -5,7 +5,7 @@ echo 'doing docker build'
 
 docker build -t ${DOCKERHUB_TAG} --build-arg SOURCE_BRANCH=${DOCKERHUB_DEFAULT_TAGNAME} --build-arg MET_BRANCH=${DOCKERHUB_MET_TAGNAME} --build-arg DO_GIT_CLONE ${TRAVIS_BUILD_DIR}/internal_tests/docker
 
-### GitHUb 607
+### GitHub 607
 ### eg change ${TRAVIS_BUILD_DIR}/internal_tests/docker to ${TRAVIS_BUILD_DIR}/ci/docker
 ### use "git mv" to move files to different directories
 ### use future_branch (METplus 4)
