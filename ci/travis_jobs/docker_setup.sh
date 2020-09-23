77f589fd7b6b2daf527043867d2547e0f518be46
George McCabe <mccabe@ucar.edu>
2020-09-23 17:57:04 -0600

    time docker build command

--- ci/travis_jobs/docker_setup.sh
+++ ci/travis_jobs/docker_setup.sh
@@ -5,6 +5,11 @@ echo 'doing docker build'
 
 #${TRAVIS_BUILD_DIR}/ci/travis_jobs/get_data_volumes.py
 
+echo Timing docker build...
+SECONDS=0
+
 docker build -t ${DOCKERHUB_TAG} --build-arg SOURCE_BRANCH=${DOCKERHUB_DEFAULT_TAGNAME} --build-arg MET_BRANCH=${DOCKERHUB_MET_TAGNAME} --build-arg DO_GIT_CLONE ${TRAVIS_BUILD_DIR}/ci/docker
 
+duration=$SECONDS
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 echo 'done'
