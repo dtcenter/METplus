5530e479ac812e80e5df6b8b59cb7b31faf95cbb
George McCabe <mccabe@ucar.edu>
2020-09-25 11:12:51 -0600

    fixed typo in argument

--- ci/travis_jobs/docker_setup.sh
+++ ci/travis_jobs/docker_setup.sh
@@ -18,7 +18,7 @@ echo "Docker pull took $(($duration / 60)) minutes and $(($duration % 60)) secon
 echo Timing docker build with --cache-from...
 start_seconds=$SECONDS
 
-docker build --cached-from ${DOCKERHUB_TAG} -t ${DOCKERHUB_TAG} --build-arg SOURCE_BRANCH=${DOCKERHUB_DEFAULT_TAGNAME} --build-arg MET_BRANCH=${DOCKERHUB_MET_TAGNAME} --build-arg DO_GIT_CLONE ${TRAVIS_BUILD_DIR}/ci/docker
+docker build --cache-from ${DOCKERHUB_TAG} -t ${DOCKERHUB_TAG} --build-arg SOURCE_BRANCH=${DOCKERHUB_DEFAULT_TAGNAME} --build-arg MET_BRANCH=${DOCKERHUB_MET_TAGNAME} --build-arg DO_GIT_CLONE ${TRAVIS_BUILD_DIR}/ci/docker
 
 duration=$(( SECONDS - start_seconds ))
 echo "Docker build took $(($duration / 60)) minutes and $(($duration % 60)) seconds."
