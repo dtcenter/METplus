1d2e160e3cf5ab55f22274b6dc7008b5350c7a3a
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2020-10-01 19:09:55 -0600

    moved travis login to travis.yml

--- ci/travis_jobs/docker_setup.sh
+++ ci/travis_jobs/docker_setup.sh
@@ -9,7 +9,7 @@ ${TRAVIS_BUILD_DIR}/ci/travis_jobs/get_data_volumes.py
 echo Timing docker pull...
 start_seconds=$SECONDS
 
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+#echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker pull ${DOCKERHUB_TAG} || true
 
 duration=$(( SECONDS - start_seconds ))
@@ -27,7 +27,7 @@ echo
 
 #${TRAVIS_BUILD_DIR}/ci/docker/docker_data/${TRAVIS_BUILD_DIR}/ci/docker/docker_data/build_docker_images.sh -pull ${DOCKERHUB_TAG} -push ${DOCKERHUB_TAB}
 
-echo "$DOCKER_PASSWORD" | docker login -username "$DOCKER_USERNAME" --password-stdin
+#echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker push ${DOCKERHUB_TAG}
 
 echo DOCKER IMAGES after DOCKER_SETUP
