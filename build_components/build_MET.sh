2ede5ec9889296d36e2bbb0cededb40d525522c0
Hank Fisher <fisherh@ucar.edu>
2020-10-06 09:16:02 -0600

    Added METplotpy, METcalcpy and GFDL to component list

--- build_components/build_MET.sh
+++ build_components/build_MET.sh
@@ -11,26 +11,34 @@ if hash wget 2>/dev/null; then
         curl https://dtcenter.org/sites/default/files/community-code/met/compile_scripts/tar_files.tgz -o tar_files.tgz
     fi
 
+## Grab GFDL tracker tar file and start building it
+if hash wget 2>/dev/null; then
+        wget http://dtcenter.org/sites/default/files/community-code/gfdl/standalone_gfdl-vortextracker_v3.9a.tar.gz
+    else
+        curl http://dtcenter.org/sites/default/files/community-code/gfdl/standalone_gfdl-vortextracker_v3.9a.tar.gz -o gfdl_vortextracker_v3.9a.tar.gz 
+    fi
+
 ## Extract the build script
-tar -xzvf compile_MET_all.sh.tgz
+#tar -xzvf compile_MET_all.sh.tgz
 ## Copy the current MET build all script and sample configurations from the cloned git repo
-cp ../MET/scripts/installation/compile_MET_all.sh . 
-cp -r ../MET/scripts/installation/config .
+#cp ../MET/scripts/installation/compile_MET_all.sh . 
+#cp -r ../MET/scripts/installation/config .
 
 ## Extract the supporting library contents into tar_files directory
-tar -xzvf tar_files.tgz
+#tar -xzvf tar_files.tgz
 
 ## link the git hub source code directory to the current directory
-ln -s ../MET/met met
+#ln -s ../MET/met met
 
 ## Create a tarball which is what the compile script wants right now - hopefully change this later
-tar -cvzhf tar_files/met.tar.gz met
+#tar -cvzhf tar_files/met.tar.gz met
 
 ### Source environment variables and run the compile_all script
-source env_vars.bash
-cd met
-./bootstrap
-cd ../
+#source env_vars.bash
+#cd met
+#./bootstrap
+#cd ../
 
-bash compile_MET_all.sh config/install_met_env.generic
+#bash compile_MET_all.sh config/install_met_env.generic
+
 
