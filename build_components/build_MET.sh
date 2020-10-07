16c4b2491f6d31c3d86b20d8b512a83e37bcbf3a
Hank Fisher <fisherh@kiowa.rap.ucar.edu>
2020-10-07 10:12:54 -0600

    Added a README about building GFDL tracker and instructions in build_MET.sh

--- build_components/build_MET.sh
+++ build_components/build_MET.sh
@@ -11,7 +11,7 @@ if hash wget 2>/dev/null; then
         curl https://dtcenter.org/sites/default/files/community-code/met/compile_scripts/tar_files.tgz -o tar_files.tgz
     fi
 
-## Grab GFDL tracker tar file and start building it
+## Grab GFDL tracker tar file
 if hash wget 2>/dev/null; then
         wget http://dtcenter.org/sites/default/files/community-code/gfdl/standalone_gfdl-vortextracker_v3.9a.tar.gz
     else
@@ -19,6 +19,7 @@ if hash wget 2>/dev/null; then
     fi
 
 ## Extract the build script
+echo "Extracting File"
 tar -xzvf compile_MET_all.sh.tgz
 ## Copy the current MET build all script and sample configurations from the cloned git repo
 cp ../MET/scripts/installation/compile_MET_all.sh . 
@@ -30,7 +31,7 @@ tar -xzvf tar_files.tgz
 ## link the git hub source code directory to the current directory
 ln -s ../MET/met met
 
-## Create a tarball which is what the compile script wants right now - hopefully change this later
+## Create a tarball which is what the compile script wants right now 
 tar -cvzhf tar_files/met.tar.gz met
 
 ### Source environment variables and run the compile_all script
