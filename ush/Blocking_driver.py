b9c1a0a6d77ed80f1a2cf874fe407f926c09d0eb
Hank Fisher <fisherh@ucar.edu>
2020-10-19 17:04:03 -0600

    Added a command line parameter to grab a list of steps if desired

--- ush/Blocking_driver.py
+++ ush/Blocking_driver.py
@@ -15,10 +15,27 @@ def main():
     from ush.master_metplus import get_config_inputs_from_command_line
     from metplus.wrappers import PCPCombineWrapper
     from metplus.wrappers import RegridDataPlaneWrapper
-    import plot_blocking as pb
-    from CBL_plot import create_cbl_plot
+    #import plot_blocking as pb
+    #from CBL_plot import create_cbl_plot
+    all_steps = ["CBL","PLOTCBL","IBL","IBLS","GIBL","CALCBLOCKS","PLOTFREQ"]
 
     config_list = get_config_inputs_from_command_line()
+
+    # If the user has defined the steps they want to run
+    # grab the command line parameter 
+    steps_config_part = [s for s in config_list if "STEPS" in s]
+    steps_list = []
+
+    # If no steps have been listed on the command line run them all
+    if not steps_config_part:
+        steps_list = all_steps 
+    else:
+        # If a list of steps has been added to the command line
+        # parse them, put them in a python list and remove that
+        # parameter from the original config list
+        steps_param = steps_config_part[0].split("=")[1]
+        steps_list = steps_param.split("+")
+        config_list.remove(steps_config_part[0])
 
     ######################################################################
     # Pre-Process Data:
@@ -51,39 +68,46 @@ def main():
     steps = BlockingCalculation(config)
 
     # Calculate Central Blocking Latitude
-    print('Computing CBLs')
-    cbls,lats,lons,yr,mhweight = steps.run_CBL()
+    if ("CBL" in steps_list):
+        print('Computing CBLs')
+        cbls,lats,lons,yr,mhweight = steps.run_CBL()
 
     #Plot Central Blocking Latitude
-    cbl_plot_mthstr = config.getstr('Blocking','CBL_PLOT_MTHSTR')
-    cbl_plot_outname = config.getstr('Blocking','CBL_PLOT_OUTPUT_NAME')
-    create_cbl_plot(lons, lats, cbls, mhweight, cbl_plot_mthstr, cbl_plot_outname, do_averaging=True)
+    if ("PLOTCBL" in steps_list ):
+        cbl_plot_mthstr = config.getstr('Blocking','CBL_PLOT_MTHSTR')
+        cbl_plot_outname = config.getstr('Blocking','CBL_PLOT_OUTPUT_NAME')
+        create_cbl_plot(lons, lats, cbls, mhweight, cbl_plot_mthstr, cbl_plot_outname, do_averaging=True)
 
 
     # Run IBL
-    print('Computing IBLs')
-    ibls = steps.run_Calc_IBL(cbls)
-    daynum = np.arange(0,len(ibls[0,:,0]),1)
+    if( "IBL" in steps_list):
+        print('Computing IBLs')
+        ibls = steps.run_Calc_IBL(cbls)
+        daynum = np.arange(0,len(ibls[0,:,0]),1)
 
     # Plot IBLS
-    ibl_plot_title = config.getstr('Blocking','IBL_PLOT_TITLE')
-    ibl_plot_outname = config.getstr('Blocking','IBL_PLOT_OUTPUT_NAME')
-    pb.plot_ibls(ibls,lons,ibl_plot_title,ibl_plot_outname)
+    if( "IBLS" in steps_list):
+        ibl_plot_title = config.getstr('Blocking','IBL_PLOT_TITLE')
+        ibl_plot_outname = config.getstr('Blocking','IBL_PLOT_OUTPUT_NAME')
+        pb.plot_ibls(ibls,lons,ibl_plot_title,ibl_plot_outname)
 
 
     # Run GIBL
-    print('Computing GIBLs')
-    gibls = steps.run_Calc_GIBL(ibls,lons)
+    if( "GIBL" in steps_list):
+        print('Computing GIBLs')
+        gibls = steps.run_Calc_GIBL(ibls,lons)
 
 
     # Calc Blocks
-    print('Computing Blocks')
-    block_freq = steps.run_Calc_Blocks(ibls,gibls,lons,daynum,yr)
+    if( "CALCBLOCKS" in steps_list):
+        print('Computing Blocks')
+        block_freq = steps.run_Calc_Blocks(ibls,gibls,lons,daynum,yr)
 
     # Plot Blocking Frequency
-    blocking_plot_title = config.getstr('Blocking','BLOCKING_PLOT_TITLE')
-    blocking_plot_outname = config.getstr('Blocking','BLOCKING_PLOT_OUTPUT_NAME')
-    pb.plot_blocks(block_freq,gibls,ibls,lons,blocking_plot_title,blocking_plot_outname)
+    if( "PLOTFREQ" in steps_list):
+        blocking_plot_title = config.getstr('Blocking','BLOCKING_PLOT_TITLE')
+        blocking_plot_outname = config.getstr('Blocking','BLOCKING_PLOT_OUTPUT_NAME')
+        pb.plot_blocks(block_freq,gibls,ibls,lons,blocking_plot_title,blocking_plot_outname)
 
 
 if __name__ == "__main__":
