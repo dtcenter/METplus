eb479652eed1638cb0fa3e822a08e84df7e10610
George McCabe <mccabe@ucar.edu>
2020-10-29 09:58:19 -0600

    updated test to look for master_metplus log file because the conf used for the pytests now writes a single log file with no timestamp in the name

--- internal_tests/pytests/plotting/make_plots/test_make_plots_wrapper.py
+++ internal_tests/pytests/plotting/make_plots/test_make_plots_wrapper.py
@@ -155,6 +155,4 @@ def test_create_c_dict(metplus_config):
     assert(c_dict['VERIF_GRID'] == 'G002')
     assert(c_dict['EVENT_EQUALIZATION'] == 'False')
     assert(c_dict['LOG_METPLUS'] == mp.config.getdir('OUTPUT_BASE')
-                                    +'/logs/master_metplus.log.'
-                                    +mp.config.getstr('config',
-                                                      'LOG_TIMESTAMP'))
+                                    +'/logs/master_metplus.log')
