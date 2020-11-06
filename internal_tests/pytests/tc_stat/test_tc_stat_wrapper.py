89987fc6c9aced37bd0346b46de0647b37de91ce
George McCabe <mccabe@ucar.edu>
2020-11-05 18:24:37 -0700

    added new tests

--- internal_tests/pytests/tc_stat/test_tc_stat_wrapper.py
+++ internal_tests/pytests/tc_stat/test_tc_stat_wrapper.py
@@ -3,9 +3,12 @@
 import os
 import sys
 import pytest
+import datetime
+
 import produtil
 
 from metplus.wrappers.tc_stat_wrapper import TCStatWrapper
+from metplus.util import ti_calculate
 
 
 #
@@ -112,13 +115,18 @@ def test_validate_config_values(metplus_config):
           'STORM_NAME': None, }),
 
         ({'TC_STAT_JOB_ARGS': '-job filter -dump_row filter_201401214_00.tcst',},
-         {'JOBS': '-job filter -dump_row filter_201401214_00.tcst'}),
+         {'JOBS': ['-job filter -dump_row filter_201401214_00.tcst']}),
+
+        ({'TC_STAT_JOB_ARGS': ('-job filter -dump_row filter_201401214_00.tcst,'
+                              '-job summary -dump_row summary.tcst  '), },
+         {'JOBS': ['-job filter -dump_row filter_201401214_00.tcst',
+                   '-job summary -dump_row summary.tcst']}),
 
         ({'TC_STAT_MATCH_POINTS': 'yes', },
-         {'MATCH_POINTS': True}),
+         {'MATCH_POINTS': 'match_points = TRUE;'}),
 
-        ({'TC_STAT_INPUT_DIR': '/my/new/input/dir', },
-         {'INPUT_DIR': '/my/new/input/dir'}),
+        ({'TC_STAT_LOOKIN_DIR': '/my/new/input/dir', },
+         {'LOOKIN_DIR': '/my/new/input/dir'}),
 
     ]
     )
@@ -126,3 +134,137 @@ def test_override_config_in_c_dict(metplus_config, overrides, c_dict):
     wrapper = TCStatWrapper(get_config(metplus_config), overrides)
     for key, expected_value in c_dict.items():
         assert (wrapper.c_dict.get(key) == expected_value)
+
+@pytest.mark.parametrize(
+    'jobs, init_dt, expected_output', [
+        # single fake job
+            (['job1'],
+             None,
+             'jobs = ["job1"];'
+            ),
+        # 2 jobs, no time info
+            (['-job filter -dump_row <output_dir>/filt.tcst',
+              '-job rirw -line_type TCMPR '],
+             None,
+             'jobs = ["-job filter -dump_row <output_dir>/filt.tcst",'
+             '"-job rirw -line_type TCMPR"];'
+            ),
+
+        # 2 jobs, time info sub
+        (['-job filter -dump_row <output_dir>/{init?fmt=%Y%m%d%H}.tcst',
+          '-job rirw -line_type TCMPR '],
+         datetime.datetime(2019, 10, 31, 12),
+         'jobs = ["-job filter -dump_row <output_dir>/2019103112.tcst",'
+         '"-job rirw -line_type TCMPR"];'
+         ),
+    ]
+)
+def test_handle_jobs(metplus_config, jobs, init_dt, expected_output):
+    if init_dt:
+        time_info = ti_calculate({'init': init_dt})
+    else:
+        time_info = None
+
+    wrapper = tc_stat_wrapper(metplus_config)
+    output_base = wrapper.config.getdir('OUTPUT_BASE')
+    output_dir = os.path.join(output_base, 'test_handle_jobs')
+
+    wrapper.c_dict['JOBS'] = []
+    for job in jobs:
+        wrapper.c_dict['JOBS'].append(job.replace('<output_dir>', output_dir))
+
+    output = wrapper.handle_jobs(time_info, create_parent_dir=False)
+    assert(output == expected_output.replace('<output_dir>', output_dir))
+
+
+def cleanup_test_dirs(parent_dirs, output_dir):
+    if parent_dirs:
+        for parent_dir in parent_dirs:
+            parent_dir_sub = parent_dir.replace('<output_dir>', output_dir)
+            if os.path.exists(parent_dir_sub):
+                os.removedirs(parent_dir_sub)
+
+@pytest.mark.parametrize(
+    'jobs, init_dt, expected_output, parent_dirs', [
+        # single fake job, no parent dir
+            (['job1'],
+             None,
+             'jobs = ["job1"];',
+             None
+            ),
+        # 2 jobs, no time info, 1 parent dir
+            (['-job filter -dump_row <output_dir>/filt.tcst',
+              '-job rirw -line_type TCMPR '],
+             None,
+             'jobs = ["-job filter -dump_row <output_dir>/filt.tcst",'
+             '"-job rirw -line_type TCMPR"];',
+             ['<output_dir>'],
+            ),
+
+        # 2 jobs, time info sub, 1 parent dir
+        (['-job filter -dump_row <output_dir>/{init?fmt=%Y%m%d%H}.tcst',
+          '-job rirw -line_type TCMPR '],
+         datetime.datetime(2019, 10, 31, 12),
+         'jobs = ["-job filter -dump_row <output_dir>/2019103112.tcst",'
+         '"-job rirw -line_type TCMPR"];',
+         ['<output_dir>'],
+         ),
+
+        # 2 jobs, no time info, 2 parent dirs
+        (['-job filter -dump_row <output_dir>/subdir1/filt.tcst',
+          '-job filter -dump_row <output_dir>/subdir2/filt2.tcst '],
+         None,
+         'jobs = ["-job filter -dump_row <output_dir>/subdir1/filt.tcst",'
+         '"-job filter -dump_row <output_dir>/subdir2/filt2.tcst"];',
+         ['<output_dir>/subdir1',
+          '<output_dir>/subdir2',
+          ],
+         ),
+
+        # 2 jobs, time info sub, 2 parent dirs
+        (['-job filter -dump_row <output_dir>/sub1/{init?fmt=%Y%m%d%H}.tcst',
+          '-job filter -dump_row <output_dir>/sub2/{init?fmt=%Y%m%d}.tcst '],
+         datetime.datetime(2019, 10, 31, 12),
+         'jobs = ["-job filter -dump_row <output_dir>/sub1/2019103112.tcst",'
+         '"-job filter -dump_row <output_dir>/sub2/20191031.tcst"];',
+         ['<output_dir>/sub1',
+          '<output_dir>/sub2',],
+         ),
+    ]
+)
+def test_handle_jobs_create_parent_dir(metplus_config, jobs, init_dt,
+                                       expected_output, parent_dirs):
+    # if init time is provided, calculate other time dict items
+    if init_dt:
+        time_info = ti_calculate({'init': init_dt})
+    else:
+        time_info = None
+
+    wrapper = tc_stat_wrapper(metplus_config)
+
+    # create directory path relative to OUTPUT_BASE to test that function
+    # creates parent directories properly
+    # Used to replace <output_dir> string found in test arguments
+    output_base = wrapper.config.getdir('OUTPUT_BASE')
+    output_dir = os.path.join(output_base, 'test_handle_jobs')
+
+    # remove parent dirs if they exist
+    cleanup_test_dirs(parent_dirs, output_dir)
+
+    wrapper.c_dict['JOBS'] = []
+    for job in jobs:
+        wrapper.c_dict['JOBS'].append(job.replace('<output_dir>', output_dir))
+
+    output = wrapper.handle_jobs(time_info, create_parent_dir=True)
+    if output != expected_output.replace('<output_dir>', output_dir):
+        assert False
+
+    # check if parent dir was created
+    if parent_dirs:
+        for parent_dir in parent_dirs:
+            parent_dir_sub = parent_dir.replace('<output_dir>', output_dir)
+            if not os.path.exists(parent_dir_sub):
+                assert False
+
+    # remove parent dirs to clean up for next run
+    cleanup_test_dirs(parent_dirs, output_dir)
