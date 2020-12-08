8a3a0b7a9af8b373bfffbaf2b35c21b4ef34b825
George McCabe <mccabe@ucar.edu>
2020-12-08 10:24:26 -0700

    fixed call to dockerhub tag script

--- ci/travis_jobs/gha_get_dockerhub_tag.sh
+++ ci/travis_jobs/gha_get_dockerhub_tag.sh
@@ -1,1 +1,2 @@
-echo dtcenter/metplus-dev:`./gha_get_current_branch.sh`
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo dtcenter/metplus-dev:`$current_dir/gha_get_current_branch.sh`
