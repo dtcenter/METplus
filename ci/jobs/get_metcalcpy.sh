773bdbb2d55e899ffed9fc2826d199afeca41a3b
George McCabe <mccabe@ucar.edu>
2021-01-07 13:43:00 -0700

    moved metcalcpy dependencies into script to get it

--- ci/jobs/get_metcalcpy.sh
+++ ci/jobs/get_metcalcpy.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+pip3 install matplotlib
+pip3 install scipy
+
 # run manage externals to obtain METcalcpy
 ${DOCKER_WORK_DIR}/METplus/manage_externals/checkout_externals -e ${DOCKER_WORK_DIR}/METplus/ci/parm/Externals_for_tests.cfg
 
