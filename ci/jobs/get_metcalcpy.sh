e1632cd6641ba304a07fccfe1ce209aab68e3c19
George McCabe <mccabe@ucar.edu>
2021-01-07 12:26:18 -0700

    updated externals filename

--- ci/jobs/get_metcalcpy.sh
+++ ci/jobs/get_metcalcpy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # run manage externals to obtain METcalcpy
-${DOCKER_WORK_DIR}/METplus/manage_externals/checkout_externals -e ${DOCKER_WORK_DIR}/METplus/ci/parm/Externals.cfg
+${DOCKER_WORK_DIR}/METplus/manage_externals/checkout_externals -e ${DOCKER_WORK_DIR}/METplus/ci/parm/Externals_for_tests.cfg
