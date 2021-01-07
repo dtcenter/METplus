2d3ae829aa7ca5cbf13b7baf3ce8f1e585e4426c
George McCabe <mccabe@ucar.edu>
2021-01-07 12:58:08 -0700

    pip3 install METcalcpy and METplotpy from directory

--- ci/jobs/get_metcalcpy.sh
+++ ci/jobs/get_metcalcpy.sh
@@ -2,3 +2,6 @@
 
 # run manage externals to obtain METcalcpy
 ${DOCKER_WORK_DIR}/METplus/manage_externals/checkout_externals -e ${DOCKER_WORK_DIR}/METplus/ci/parm/Externals_for_tests.cfg
+
+pip3 install ${DOCKER_WORK_DIR}/METcalcpy
+pip3 install ${DOCKER_WORK_DIR}/METplotpy
