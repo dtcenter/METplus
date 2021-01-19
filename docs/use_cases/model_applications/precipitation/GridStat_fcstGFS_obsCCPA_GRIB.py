29f62ddaab937fd55e05359ff4202641f69ac329
George McCabe <mccabe@ucar.edu>
2021-01-19 14:09:24 -0700

    moved moved py embed scripts into subdir and updated conf file to reflect new location for precipitation

--- docs/use_cases/model_applications/precipitation/GridStat_fcstGFS_obsCCPA_GRIB.py
+++ docs/use_cases/model_applications/precipitation/GridStat_fcstGFS_obsCCPA_GRIB.py
@@ -70,7 +70,7 @@ _obsCCPA_Grib.conf
 # These variables are referenced in the MET configuration file. **YOU SHOULD NOT SET ANY OF THESE ENVIRONMENT VARIABLES YOURSELF! THEY WILL BE OVERWRITTEN BY METPLUS WHEN IT CALLS THE MET TOOLS!** If there is a setting in the MET configuration file that is not controlled by an environment variable, you can add additional environment variables to be set only within the METplus environment using the [user_env_vars] section of the METplus configuration files. See the 'User Defined Config' section on the 'System Configuration' page of the METplus User's Guide for more information.
 #
 # .. highlight:: bash
-# .. literalinclude:: ../../../../parm/use_cases/model_applications/precipitation/GridStatConfig_precip
+# .. literalinclude:: ../../../../parm/use_cases/model_applications/precipitation/GridStat_fcstGFS_obsCCPA_GRIB/GridStatConfig_precip
 #
 # See the following files for more information about the environment variables set in this configuration file.
 #
