466b789698c7d113c674d81dd224785603515ec0
George McCabe <mccabe@ucar.edu>
2021-01-19 13:32:17 -0700

    moved moved py embed scripts into subdir and updated conf file to reflect new location for convection allowing models

--- docs/use_cases/model_applications/convection_allowing_models/GridStat_fcstHRRR_obsPracPerfect_SurrogateSevereProb.py
+++ docs/use_cases/model_applications/convection_allowing_models/GridStat_fcstHRRR_obsPracPerfect_SurrogateSevereProb.py
@@ -62,7 +62,7 @@ _SurrogateSevereProb.conf
 # These variables are referenced in the MET configuration file. **YOU SHOULD NOT SET ANY OF THESE ENVIRONMENT VARIABLES YOURSELF! THEY WILL BE OVERWRITTEN BY METPLUS WHEN IT CALLS THE MET TOOLS!** If there is a setting in the MET configuration file that is not controlled by an environment variable, you can add additional environment variables to be set only within the METplus environment using the [user_env_vars] section of the METplus configuration files. See the 'User Defined Config' section on the 'System Configuration' page of the METplus User's Guide for more information.
 #
 # .. highlight:: bash
-# .. literalinclude:: ../../../../parm/use_cases/model_applications/convection_allowing_models/GridStatConfig_ss_prob
+# .. literalinclude:: ../../../../parm/use_cases/model_applications/convection_allowing_models/GridStat_fcstHRRR_obsPracPerfect_SurrogateSevereProb/GridStatConfig_ss_prob
 #
 # See the following files for more information about the environment variables set in this configuration file.
 #
