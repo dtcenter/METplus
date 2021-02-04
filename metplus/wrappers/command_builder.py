8ebef1102c45831786380c07863ef00d3db34e94
George McCabe <mccabe@ucar.edu>
2021-02-04 13:20:31 -0700

    finally fixed broken logic from failed cherry-pick attempt

--- metplus/wrappers/command_builder.py
+++ metplus/wrappers/command_builder.py
@@ -78,7 +78,8 @@ class CommandBuilder:
 
         # if wrapper has a config file, read unsupported MET config variable
         if 'CONFIG_FILE' in self.c_dict:
-            config_name = f"{self.app_name.upper()}_{self.MET_OVERRIDES_KEY}"
+            config_name = self.MET_OVERRIDES_KEY.replace('METPLUS',
+                                                         self.app_name.upper())
             self.c_dict[self.MET_OVERRIDES_KEY] = (
                 self.config.getraw('config', config_name)
             )
@@ -169,8 +170,8 @@ class CommandBuilder:
         self.set_user_environment(time_info)
 
         # set MET config overrides that are not set by other variables
-        self.add_env_var(f'METPLUS_{self.MET_OVERRIDES}',
-                         self.c_dict.get(self.MET_OVERRIDES, ''))
+        self.add_env_var(self.MET_OVERRIDES_KEY,
+                         self.c_dict.get(self.MET_OVERRIDES_KEY, ''))
 
         # send environment variables to logger
         for msg in self.print_all_envs():
