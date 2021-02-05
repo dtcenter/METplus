60dbe7ea8b06aaa6dc9f98303f0ee13f5bf87a75
George McCabe <mccabe@ucar.edu>
2021-02-05 10:05:02 -0700

    adding echo statements to show what is being installed

--- ci/jobs/get_miniconda.sh
+++ ci/jobs/get_miniconda.sh
@@ -3,6 +3,7 @@
 PYTHON_VERS=38
 MINIC_VERS=4.8.3
 
+echo Checking if Miniconda is installed
 # check if conda is already available and exit if it is
 conda --version
 if [ $? == 0 ]; then
@@ -10,6 +11,7 @@ if [ $? == 0 ]; then
     exit 0
 fi
 
+echo Installing Miniconda
 curl -sSL https://repo.continuum.io/miniconda/Miniconda3-py${PYTHON_VERS}_${MINIC_VERS}-Linux-x86_64.sh -o /tmp/miniconda.sh
 
 bash /tmp/miniconda.sh -bfp /usr/local/
--- ci/jobs/get_xesmf.sh
+++ ci/jobs/get_xesmf.sh
@@ -2,4 +2,5 @@
 
 ./get_miniconda.sh
 
+echo Installing xesmf with conda
 conda install -c conda-forge xesmf
