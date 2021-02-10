c90d916f9e6c28c3ab5f5422e9fc196cd0006f58
George McCabe <mccabe@ucar.edu>
2021-02-09 18:29:16 -0700

    rearrange files to call docker run/build inside another entrypoint script

---
+++ ci/actions/run_use_cases/docker-action/entrypoint.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+echo In Docker: ${DOCKERHUB_TAG}
+echo DOCKERHUB_TAG2: ${DOCKERHUB_TAG2}
+ls /metplus/METplus
+echo end
--- ci/actions/run_use_cases/entrypoint.sh
+++ ci/actions/run_use_cases/entrypoint.sh
@@ -1,6 +1,16 @@
 #! /bin/sh
 
-echo In Docker: ${DOCKERHUB_TAG}
-echo DOCKERHUB_TAG2: ${DOCKERHUB_TAG2}
-ls /metplus/METplus
-echo end
+DOCKERUB_TAG=$1
+
+# The repo source code is cloned to $RUNNER_WORKSPACE/$REPO_NAME
+# Setup the workspace path to that for easier access later
+REPO_NAME=$(basename $RUNNER_WORKSPACE)
+WS_PATH=$RUNNER_WORKSPACE/$REPO_NAME
+
+cd /docker-action
+
+echo "Creating a docker image with Dockerhub Tag: $DOCKERHUB_TAG"
+docker build -t docker-action --build-arg dockerhub_tag="$DOCKERHUB_TAG" .
+
+echo "Run Docker Action container"
+docker run -e DOCKERHUB_TAG2 -v $WS_PATH:$GITHUB_WORKSPACE  --workdir $GITHUB_WORKSPACE docker-action
