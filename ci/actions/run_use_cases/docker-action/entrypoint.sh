f7928372c0a16223b1ad8007421520945683910c
George McCabe <mccabe@ucar.edu>
2021-02-10 09:23:45 -0700

    file was uncommitted

--- ci/actions/run_use_cases/docker-action/entrypoint.sh
+++ ci/actions/run_use_cases/docker-action/entrypoint.sh
@@ -3,3 +3,9 @@
 echo In Docker: ${INPUT_DOCKERHUB_TAG}
 echo BRANCH_NAME: ${BRANCH_NAME}
 echo CATEGORIES: ${INPUT_CATEGORIES}
+
+echo Installing Python dependencies for all use cases
+python -m pip install --upgrade pip python-dateutil requests
+
+echo Running use cases
+${GITHUB_WORKSPACE}/ci/jobs/run_use_cases.py ${CATEGORIES}
