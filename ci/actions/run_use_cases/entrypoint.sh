d1b78bbb895fbc19fe20b2e610105d2d01a8893d
George McCabe <mccabe@ucar.edu>
2021-02-10 13:02:17 -0700

    call get_data_volumes.py and then set --volumes-from arguments to docker run

--- ci/actions/run_use_cases/entrypoint.sh
+++ ci/actions/run_use_cases/entrypoint.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 # The repo source code is cloned to $RUNNER_WORKSPACE/$REPO_NAME
 # Setup the workspace path to that for easier access later
@@ -10,5 +10,13 @@ cd /docker-action
 echo "Creating a docker image with Dockerhub Tag: $INPUT_DOCKERHUBTAG"
 docker build -t docker-action --build-arg dockerhub_tag="$INPUT_DOCKERHUBTAG" .
 
+echo "Get Docker data volumes for input data"
+${GITHUB_WORKSPACE}/ci/jobs/get_data_volumes.py $INPUT_CATEGORIES
+
+VOLUMES_FROM=""
+for (( n=0; n < ${#category_list[*]}; n++)); do
+  VOLUMES_FROM+=`echo --volumes-from "${category_list[n]} "`
+done
+
 echo "Run Docker Action container"
-docker run -e GITHUB_WORKSPACE -e BRANCH_NAME -e INPUT_DOCKERHUBTAG -e INPUT_CATEGORIES -e DOCKER_WORK_DIR -e DOCKER_DATA_DIR -e DOCKER_USERNAME -e DOCKER_PASSWORD -v $WS_PATH:$GITHUB_WORKSPACE  --workdir $GITHUB_WORKSPACE docker-action
+docker run ${VOLUMES_FROM} -e GITHUB_WORKSPACE -e BRANCH_NAME -e INPUT_DOCKERHUBTAG -e INPUT_CATEGORIES -e DOCKER_WORK_DIR -e DOCKER_DATA_DIR -e DOCKER_USERNAME -e DOCKER_PASSWORD -v $WS_PATH:$GITHUB_WORKSPACE  --workdir $GITHUB_WORKSPACE docker-action
