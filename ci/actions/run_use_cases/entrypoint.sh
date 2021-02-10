3298647fe3678d0273589e8c2620598c8995f113
George McCabe <mccabe@ucar.edu>
2021-02-09 18:54:42 -0700

    change var name

--- ci/actions/run_use_cases/entrypoint.sh
+++ ci/actions/run_use_cases/entrypoint.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-DOCKERUB_TAG=$1
-
 # The repo source code is cloned to $RUNNER_WORKSPACE/$REPO_NAME
 # Setup the workspace path to that for easier access later
 REPO_NAME=$(basename $RUNNER_WORKSPACE)
@@ -9,8 +7,8 @@ WS_PATH=$RUNNER_WORKSPACE/$REPO_NAME
 
 cd /docker-action
 
-echo "Creating a docker image with Dockerhub Tag: $DOCKERHUB_TAG"
-docker build -t docker-action --build-arg dockerhub_tag="$DOCKERHUB_TAG" .
+echo "Creating a docker image with Dockerhub Tag: $INPUT_DOCKERHUBTAG"
+docker build -t docker-action --build-arg dockerhub_tag="$INPUT_DOCKERHUBTAG" .
 
 echo "Run Docker Action container"
-docker run -e DOCKERHUB_TAG2 -e DOCKERHUB_TAG -v $WS_PATH:$GITHUB_WORKSPACE  --workdir $GITHUB_WORKSPACE docker-action
+docker run -e DOCKERHUB_TAG2 -e DOCKERHUB_TAG -e INPUT_DOCKERHUBTAG -v $WS_PATH:$GITHUB_WORKSPACE  --workdir $GITHUB_WORKSPACE docker-action
