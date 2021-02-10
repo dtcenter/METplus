f2ea5f56adba96cbe74bef6ae2e23ed6ef76029d
George McCabe <mccabe@ucar.edu>
2021-02-10 15:22:43 -0700

    added new script to call use cases from inside Docker

--- ci/actions/run_use_cases/docker-action/entrypoint.sh
+++ ci/actions/run_use_cases/docker-action/entrypoint.sh
@@ -4,7 +4,7 @@ echo In Docker: ${INPUT_DOCKERHUB_TAG}
 echo BRANCH_NAME: ${BRANCH_NAME}
 echo CATEGORIES: ${INPUT_CATEGORIES}
 
-python3 -m pip install requests
+#python3 -m pip install requests
 
 echo Running use cases
-${GITHUB_WORKSPACE}/ci/jobs/run_use_cases.py ${INPUT_CATEGORIES}
+${GITHUB_WORKSPACE}/ci/jobs/run_use_cases_docker.py ${INPUT_CATEGORIES}
---
+++ ci/jobs/run_use_cases_docker.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+import sys
+import subprocess
+import shlex
+
+import get_use_case_commands
+
+def main():
+    categories, subset_list = get_use_case_commands.handle_command_line_args()
+    all_commands = get_use_case_commands.main(categories, subset_list)
+
+    isOK = True
+    for cmd in all_commands:
+        print(cmd)
+        try:
+            subprocess.run(shlex.split(cmd), check=True)
+        except subprocess.CalledProcessError as err:
+            print(f"ERROR: Command failed: {cmd} -- {err}")
+            isOK = False
+
+    is not isOK:
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
