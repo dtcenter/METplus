a5983ce02fb772a6662a30d029b2aca525bd3db7
George McCabe <mccabe@ucar.edu>
2021-02-10 13:09:09 -0700

    print volumes from args

--- ci/actions/run_use_cases/entrypoint.sh
+++ ci/actions/run_use_cases/entrypoint.sh
@@ -18,5 +18,7 @@ for (( n=0; n < ${#category_list[*]}; n++)); do
   VOLUMES_FROM+=`echo --volumes-from "${category_list[n]} "`
 done
 
+echo VOLUMES_FROM: $VOLUMES_FROM
+
 echo "Run Docker Action container"
 docker run ${VOLUMES_FROM} -e GITHUB_WORKSPACE -e BRANCH_NAME -e INPUT_DOCKERHUBTAG -e INPUT_CATEGORIES -e DOCKER_WORK_DIR -e DOCKER_DATA_DIR -e DOCKER_USERNAME -e DOCKER_PASSWORD -v $WS_PATH:$GITHUB_WORKSPACE  --workdir $GITHUB_WORKSPACE docker-action
