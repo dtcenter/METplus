05ddc6b8e7bca9a79f8899d6b596c307515c7570
George McCabe <mccabe@ucar.edu>
2021-02-11 08:20:02 -0700

    add category subset list, add mount for output data to obtain output as artifact

--- ci/actions/run_use_cases/docker-action/entrypoint.sh
+++ ci/actions/run_use_cases/docker-action/entrypoint.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 
-echo CATEGORIES: ${INPUT_CATEGORIES}
+echo INPUT_CATEGORIES: ${INPUT_CATEGORIES}
+echo INPUT_SUBSETLIST: ${INPUT_SUBSETLIST}
 
 #python3 -m pip install requests
 
 echo Running use cases
-${GITHUB_WORKSPACE}/ci/jobs/run_use_cases_docker.py ${INPUT_CATEGORIES}
+${GITHUB_WORKSPACE}/ci/jobs/run_use_cases_docker.py ${INPUT_CATEGORIES} ${INPUT_SUBSETLIST}
--- ci/actions/run_use_cases/entrypoint.sh
+++ ci/actions/run_use_cases/entrypoint.sh
@@ -5,6 +5,10 @@
 REPO_NAME=$(basename $RUNNER_WORKSPACE)
 WS_PATH=$RUNNER_WORKSPACE/$REPO_NAME
 
+DOCKER_DATA_DIR=/data
+DOCKER_OUTPUT_DIR=${DOCKER_DATA_DIR}/output
+GHA_OUTPUT_DIR=$RUNNER_WORKSPACE/output
+
 cd /docker-action
 
 echo "Creating a docker image with Dockerhub Tag: $INPUT_DOCKERHUBTAG"
@@ -13,14 +17,13 @@ docker build -t docker-action --build-arg dockerhub_tag="$INPUT_DOCKERHUBTAG" .
 echo "Get Docker data volumes for input data"
 ${GITHUB_WORKSPACE}/ci/jobs/get_data_volumes.py $INPUT_CATEGORIES
 
+# build --volumes-from arguments for docker run command
 category_list=`echo $INPUT_CATEGORIES | tr "," "\n"`
-
 VOLUMES_FROM=""
 for n in ${category_list}; do
   VOLUMES_FROM=${VOLUMES_FROM}`echo --volumes-from $n" "`
 done
-
 echo VOLUMES_FROM: $VOLUMES_FROM
 
 echo "Run Docker Action container: $INPUT_DOCKERHUBTAG"
-docker run -e GITHUB_WORKSPACE -e INPUT_CATEGORIES -v $WS_PATH:$GITHUB_WORKSPACE ${VOLUMES_FROM} --workdir $GITHUB_WORKSPACE docker-action
+docker run -e GITHUB_WORKSPACE -e INPUT_CATEGORIES -e INPUT_SUBSETLIST -v $GHA_OUTPUT_DIR:$DOCKER_OUTPUT_DIR -v $WS_PATH:$GITHUB_WORKSPACE ${VOLUMES_FROM} --workdir $GITHUB_WORKSPACE docker-action
