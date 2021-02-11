77f6072a74811eb6193088f47cc96b84ab21b4cd
George McCabe <mccabe@ucar.edu>
2021-02-11 09:50:04 -0700

    test check for ref branch and pull request, added stubs for obtaining output ref data, comment docker cmds for testing

--- ci/actions/run_use_cases/entrypoint.sh
+++ ci/actions/run_use_cases/entrypoint.sh
@@ -12,26 +12,45 @@ GHA_OUTPUT_DIR=$RUNNER_WORKSPACE/output
 cd /docker-action
 
 echo "Creating a docker image with Dockerhub Tag: $INPUT_DOCKERHUBTAG"
-docker build -t docker-action --build-arg dockerhub_tag="$INPUT_DOCKERHUBTAG" .
+#docker build -t docker-action --build-arg dockerhub_tag="$INPUT_DOCKERHUBTAG" .
 
+# add input volumes to run command
 echo "Get Docker data volumes for input data"
-${GITHUB_WORKSPACE}/ci/jobs/get_data_volumes.py $INPUT_CATEGORIES
+#${GITHUB_WORKSPACE}/ci/jobs/get_data_volumes.py $INPUT_CATEGORIES
 
-# build --volumes-from arguments for docker run command
-category_list=`echo $INPUT_CATEGORIES | tr "," "\n"`
+# keep track of --volumes-from arguments to docker run command
 VOLUMES_FROM=""
-for n in ${category_list}; do
-  VOLUMES_FROM=${VOLUMES_FROM}`echo --volumes-from $n" "`
+
+# split list of categories by comma
+category_list=`echo $INPUT_CATEGORIES | tr "," "\n"`
+
+# add input category --volumes-from arguments for docker run command
+for category in ${category_list}; do
+  VOLUMES_FROM=${VOLUMES_FROM}`echo --volumes-from $category" "`
 done
+
+# get Docker data volumes for output data if running a pull request
+#if [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
+#  echo "Get Docker data volumes for output data"
+#  output_categories=""
+#  for category in ${category_list}; do
+#    output_category=output-${category}
+#    output_categories=${output_categories} ${output_category}
+#    VOLUMES_FROM=${VOLUMES_FROM}`echo --volumes-from $output_category" "`
+#  done
+#
+#  ${GITHUB_WORKSPACE}/ci/jobs/get_data_volumes.py $output_categories
+#fi
+
 echo VOLUMES_FROM: $VOLUMES_FROM
 
 echo "Run Docker Action container: $INPUT_DOCKERHUBTAG"
-docker run -e GITHUB_WORKSPACE -e INPUT_CATEGORIES -e INPUT_SUBSETLIST -v $GHA_OUTPUT_DIR:$DOCKER_OUTPUT_DIR -v $WS_PATH:$GITHUB_WORKSPACE ${VOLUMES_FROM} --workdir $GITHUB_WORKSPACE docker-action
+#docker run -e GITHUB_WORKSPACE -e INPUT_CATEGORIES -e INPUT_SUBSETLIST -v $GHA_OUTPUT_DIR:$DOCKER_OUTPUT_DIR -v $WS_PATH:$GITHUB_WORKSPACE ${VOLUMES_FROM} --workdir $GITHUB_WORKSPACE docker-action
 ret=$?
 
 # if branch ends with -ref and not a pull request, create/update Docker
 # data volume for output data
-if [ "$GITHUB_EVENT_NAME" == "pull_request" || "${BRANCH_NAME: -4}" != "-ref"]; then
+if [ "$GITHUB_EVENT_NAME" == "pull_request" || "${BRANCH_NAME: -4}" != "-ref" ]; then
   echo $BRANCH_NAME is not a reference branch.
   exit $ret
 fi
