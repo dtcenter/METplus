564fab869a118508d122f04fb0c274098c30ac1a
George McCabe <mccabe@ucar.edu>
2021-02-12 12:39:51 -0700

    fixed for loop

--- ci/jobs/create_output_data_volumes.sh
+++ ci/jobs/create_output_data_volumes.sh
@@ -2,8 +2,11 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-for vol_name in artifacts/*; do
-    echo $vol_name
+ls artifact
+ls artifact/use_cases*
+
+for vol_name in artifact/*; do
+    echo vol name is $vol_name
     image_name=dtcenter/metplus-data-dev:output-${vol_name}
     echo docker build -t ${image_name} --build-arg vol_name=${vol_name} ci/actions/run_use_cases/docker-action/output_data_volumes
     docker build -t ${image_name} --build-arg vol_name=${vol_name} ci/actions/run_use_cases/docker-action/output_data_volumes
