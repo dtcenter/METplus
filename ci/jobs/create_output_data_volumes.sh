ed86d8bfa162061a2a9369eb6228f68d61438623
George McCabe <mccabe@ucar.edu>
2021-02-16 09:07:11 -0700

    copy data in script, add branch name to output data volume name

--- ci/jobs/create_output_data_volumes.sh
+++ ci/jobs/create_output_data_volumes.sh
@@ -9,9 +9,13 @@ if [ $? != 0 ]; then
   exit 0
 fi
 
+branch_name=$((cat artifact/branch_name.txt))
+
 for vol_name in use_cases*; do
     echo vol name is $vol_name
-    image_name=dtcenter/metplus-data-dev:output-${vol_name}
+    cp -r $vol_name ci/actions/run_use_cases/docker-action/output_data_volumes/
+
+    image_name=dtcenter/metplus-data-dev:output-${branch_name}-${vol_name}
     echo docker build -t ${image_name} --build-arg vol_name=${vol_name} ci/actions/run_use_cases/docker-action/output_data_volumes
     docker build -t ${image_name} --build-arg vol_name=${vol_name} ci/actions/run_use_cases/docker-action/output_data_volumes
     echo docker push ${image_name}
