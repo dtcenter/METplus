9a9bc8a8f7ba52eef0cd7abd4a2386d721f6627d
George McCabe <mccabe@ucar.edu>
2021-02-17 09:30:43 -0700

    change + to p for artifact name

--- ci/jobs/get_artifact_name.sh
+++ ci/jobs/get_artifact_name.sh
@@ -3,5 +3,6 @@
 artifact_name=$1
 artifact_name=`echo $artifact_name | tr , _`
 artifact_name=`echo $artifact_name | tr : _`
+artifact_name=`echo $artifact_name | tr + p`
 artifact_name=use_cases_${artifact_name}
 echo $artifact_name
