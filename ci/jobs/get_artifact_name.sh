c8c43d6d3dffaba47535929ec849439489b24d74
George McCabe <mccabe@ucar.edu>
2021-02-16 17:37:02 -0700

    replace commas with underscores for output artifact name

--- ci/jobs/get_artifact_name.sh
+++ ci/jobs/get_artifact_name.sh
@@ -3,4 +3,5 @@
 artifact_name=$1
 artifact_name=`echo $artifact_name | awk -F: '{print $1}'`
 artifact_name=use_cases_${artifact_name}
+artifact_name=`echo $artifact_name | tr , _`
 echo $artifact_name
