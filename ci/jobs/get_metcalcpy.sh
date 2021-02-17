1e6e93f68e21584beea26ac16562033e246e838d
George McCabe <mccabe@ucar.edu>
2021-02-17 14:01:59 -0700

    fixed relative path to script

--- ci/jobs/get_metcalcpy.sh
+++ ci/jobs/get_metcalcpy.sh
@@ -4,7 +4,7 @@ pip3 install matplotlib
 pip3 install scipy
 
 basedir=$(dirname "$0")
-work_dir=$basedir/../../..
+work_dir=$basedir/../..
 
 # run manage externals to obtain METcalcpy
 ${work_dir}/METplus/manage_externals/checkout_externals -e ${work_dir}/METplus/ci/parm/Externals_metcalcpy.cfg
