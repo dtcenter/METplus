47d90ca6e8f885a2d06c49b0ad13b66d6fee358d
George McCabe <mccabe@ucar.edu>
2021-02-17 14:38:51 -0700

    change path to scripts

--- ci/jobs/get_metcalcpy.sh
+++ ci/jobs/get_metcalcpy.sh
@@ -7,6 +7,6 @@ basedir=$(dirname "$0")
 work_dir=$basedir/../..
 
 # run manage externals to obtain METcalcpy
-${work_dir}/METplus/manage_externals/checkout_externals -e ${work_dir}/METplus/ci/parm/Externals_metcalcpy.cfg
+${work_dir}/manage_externals/checkout_externals -e ${work_dir}/ci/parm/Externals_metcalcpy.cfg
 
 pip3 install ${work_dir}/METcalcpy
