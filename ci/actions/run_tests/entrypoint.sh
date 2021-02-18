ca60c798eee75f37a599dd6881ed132bc23c8344
George McCabe <mccabe@ucar.edu>
2021-02-18 16:02:14 -0700

    test env vars to try to handle obtaining docker image if credentials aren't set

--- ci/actions/run_tests/entrypoint.sh
+++ ci/actions/run_tests/entrypoint.sh
@@ -15,6 +15,10 @@ if [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
 fi
 DOCKERHUBTAG=dtcenter/metplus-dev:${branch_name}
 
+echo full name: $GITHUB_EVENT_PULL_REQUEST_HEAD_REPO_FULL_NAME
+echo repo: $GITHUB_REPOSITORY
+exit 0
+
 echo "Pulling docker image: $DOCKERHUBTAG"
 docker pull $DOCKERHUBTAG
 if [ $? != 0 ]; then
