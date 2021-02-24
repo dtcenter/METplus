111968a0e3b6976931896637e84d7a46048b23a2
Christina Kalb <kalb@kiowa.rap.ucar.edu>
2021-02-24 15:08:56 -0700

    Added grid_stat case on raw brightness temperature

--- docs/use_cases/model_applications/convection_allowing_models/MODE_fcstFV3_obsGOES_BrightnessTempObjs.py
+++ docs/use_cases/model_applications/convection_allowing_models/MODE_fcstFV3_obsGOES_BrightnessTempObjs.py
@@ -1,10 +1,11 @@
 """
-MODE: Brightness Temperature Verification  
+MODE: Brightness Temperature Verification
+Grid-Stat: Brightness Temperature Distance Maps 
 =========================================================================
 
 model_applications/
 convection_allowing_model/
-MODE_fcstFV3_obsGOES_BrightnessTemp.conf
+MODE_fcstFV3_obsGOES_BrightnessTempObjs.conf
 
 """
 ##############################################################################
@@ -30,13 +31,13 @@ MODE_fcstFV3_obsGOES_BrightnessTemp.conf
 #
 # This use case runs MODE to create object statistics on brightness temperatures 
 # below 235 K. Then it runs grid_stat to compute neighborhood contingency table
-# statistics and distance maps for the forecast and observations. 
+# counts and distance maps for the forecast and observations. 
 
 ##############################################################################
 # METplus Workflow
 # ----------------
 #
-# The MODE and grid_stat tools are run for each of the 4 ensemble members and for each
+# The MODE and grid_stat tools are run for each of 2 ensemble members and for each
 # time. This example loops by initialization time.  It processes 2 lead times, listed 
 # below.
 #
@@ -52,10 +53,10 @@ MODE_fcstFV3_obsGOES_BrightnessTemp.conf
 #
 # METplus first loads all of the configuration files found in parm/metplus_config,
 # then it loads any configuration files passed to METplus via the command line
-# with the -c option, i.e. -c parm/use_cases/model_applications/convection_allowing_models/MODE_GridStat_fcstFV3_obsGOES_BrightnessTemp.conf
+# with the -c option, i.e. -c parm/use_cases/model_applications/convection_allowing_models/MODE_fcstFV3_obsGOES_BrightnessTempObjs.conf
 #
 # .. highlight:: bash
-# .. literalinclude:: ../../../../parm/use_cases/model_applications/convection_allowing_models/MODE_GridStat_fcstFV3_obsGOES_BrightnessTemp.conf
+# .. literalinclude:: ../../../../parm/use_cases/model_applications/convection_allowing_models/MODE_fcstFV3_obsGOES_BrightnessTempObjs.conf
 
 ##############################################################################
 # MET Configuration
@@ -65,11 +66,13 @@ MODE_fcstFV3_obsGOES_BrightnessTemp.conf
 # These variables are referenced in the MET configuration file. **YOU SHOULD NOT SET ANY OF THESE ENVIRONMENT VARIABLES YOURSELF! THEY WILL BE OVERWRITTEN BY METPLUS WHEN IT CALLS THE MET TOOLS!** If there is a setting in the MET configuration file that is not controlled by an environment variable, you can add additional environment variables to be set only within the METplus environment using the [user_env_vars] section of the METplus configuration files. See the 'User Defined Config' section on the 'System Configuration' page of the METplus User's Guide for more information.
 #
 # .. highlight:: bash
-# .. literalinclude:: ../../../../parm/use_cases/model_applications/convection_allowing_models/MODE_fcstFV3_obsGOES_BrightnessTemp/MODEConfig_BrightnessTemp
+# .. literalinclude:: ../../../../parm/use_cases/model_applications/convection_allowing_models/MODE_fcstFV3_obsGOES_BrightnessTempObjs/MODEConfig_BrightnessTemp
+# .. literalinclude:: ../../../../parm/use_cases/model_applications/convection_allowing_models/MODE_fcstFV3_obsGOES_BrightnessTempObjs/GridStatConfig_BrightnessTempObj
 #
 # See the following files for more information about the environment variables set in this configuration file.
 #
 # parm/use_cases/met_tool_wrapper/MODE/MODE.py
+# parm/use_cases/met_tool_wrapper/GridStat/GridStat.py
 
 ##############################################################################
 # Running METplus
@@ -77,13 +80,13 @@ MODE_fcstFV3_obsGOES_BrightnessTemp.conf
 #
 # This use case can be run two ways:
 #
-# 1) Passing in MODE_GridStat_fcstFV3_obsGOES_BrightnessTemp.conf then a user-specific system configuration file::
+# 1) Passing in MODE_fcstFV3_obsGOES_BrightnessTempObjs.conf then a user-specific system configuration file::
 #
-#        run_metplus.py -c /path/to/METplus/parm/use_cases/model_applications/convection_allowing_models/MODE_GridStat_fcstFV3_obsGOES_BrightnessTemp.conf -c /path/to/user_system.conf
+#        run_metplus.py -c /path/to/METplus/parm/use_cases/model_applications/convection_allowing_models/MODE_fcstFV3_obsGOES_BrightnessTempObjs.conf -c /path/to/user_system.conf
 #
-# 2) Modifying the configurations in parm/metplus_config, then passing in MODE_GridStat_fcstFV3_obsGOES_BrightnessTemp.conf::
+# 2) Modifying the configurations in parm/metplus_config, then passing in MODE_fcstFV3_obsGOES_BrightnessTempObjs.conf::
 #
-#        run_metplus.py -c /path/to/METplus/parm/use_cases/model_applications/convection_allowing_models/MODE_gridStat_fcstFV3_obsGOES_BrightnessTemp.conf
+#        run_metplus.py -c /path/to/METplus/parm/use_cases/model_applications/convection_allowing_models/MODE_fcstFV3_obsGOES_BrightnessTempObjs.conf
 #
 # The former method is recommended. Whether you add them to a user-specific configuration file or modify the metplus_config files, the following variables must be set correctly:
 #
@@ -129,14 +132,29 @@ MODE_fcstFV3_obsGOES_BrightnessTemp.conf
 # mode/mode_FV3_core_mp1_010000L_20190521_020000V_NAA_obj.nc
 # mode/mode_FV3_core_mp1_010000L_20190521_020000V_NAA_obj.txt
 # mode/mode_FV3_core_mp1_010000L_20190521_020000V_NAA.ps
-# grid_stat_obj/
+# grid_stat_obj/grid_stat_FV3_core_lsm1_000000L_20190521_010000V_dmap.txt
+# grid_stat_obj/grid_stat_FV3_core_lsm1_000000L_20190521_010000V_nbrctc.txt
+# grid_stat_obj/grid_stat_FV3_core_lsm1_000000L_20190521_010000V_pairs.nc
+# grid_stat_obj/grid_stat_FV3_core_lsm1_000000L_20190521_010000V.stat
+# grid_stat_obj/grid_stat_FV3_core_lsm1_000000L_20190521_020000V_dmap.txt
+# grid_stat_obj/grid_stat_FV3_core_lsm1_000000L_20190521_020000V_nbrctc.txt
+# grid_stat_obj/grid_stat_FV3_core_lsm1_000000L_20190521_020000V_pairs.nc
+# grid_stat_obj/grid_stat_FV3_core_lsm1_000000L_20190521_020000V.stat
+# grid_stat_obj/grid_stat_FV3_core_mp1_000000L_20190521_010000V_dmap.txt
+# grid_stat_obj/grid_stat_FV3_core_mp1_000000L_20190521_010000V_nbrctc.txt
+# grid_stat_obj/grid_stat_FV3_core_mp1_000000L_20190521_010000V_pairs.nc
+# grid_stat_obj/grid_stat_FV3_core_mp1_000000L_20190521_010000V.stat
+# grid_stat_obj/grid_stat_FV3_core_mp1_000000L_20190521_020000V_dmap.txt
+# grid_stat_obj/grid_stat_FV3_core_mp1_000000L_20190521_020000V_nbrctc.txt
+# grid_stat_obj/grid_stat_FV3_core_mp1_000000L_20190521_020000V_pairs.nc
+# grid_stat_obj/grid_stat_FV3_core_mp1_000000L_20190521_020000V.stat
 
 
 ##############################################################################
 # Keywords
 # --------
 #
-# sphinx_gallery_thumbnail_path = '_static/convection_allowing_models-MODE_GridStat_fcstFV3_obsGOES_BrightnessTemp.png'
+# sphinx_gallery_thumbnail_path = '_static/convection_allowing_models-MODE_fcstFV3_obsGOES_BrightnessTempObjs.png'
 #
 # .. note:: `MODEToolUseCase <https://dtcenter.github.io/METplus/search.html?q=MODEToolUseCase&check_keywords=yes&area=default>`_,
 #  `GridStatToolUseCase <https://dtcenter.github.io/METplus/search.html?q=GridStatToolUseCase&check_keywords=yes&area=default>`_,
