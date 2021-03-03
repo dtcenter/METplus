cc6e442376d6e2cd79dfb6078c54652dfdb5051d
George McCabe <mccabe@ucar.edu>
2021-03-03 07:09:17 -0700

    missing import

--- ci/jobs/print_python_version.py
+++ ci/jobs/print_python_version.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 import sys
+import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
                                                 os.pardir,
