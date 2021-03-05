e3100b32db94d1b2aa950a16968f3c1409fa8ff3
George McCabe <mccabe@ucar.edu>
2021-03-04 17:17:32 -0700

    handle if either text file is empty (no lines), changed log output and variable names to be consistent to call one file A and the other B

--- ci/util/diff_util.py
+++ ci/util/diff_util.py
@@ -49,60 +49,60 @@ def compare_dir(dir_a, dir_b, debug=False):
             continue
 
         for filename in files:
-            filepath = os.path.join(root, filename)
+            filepath_a = os.path.join(root, filename)
 
             # skip directories
-            if not os.path.isfile(filepath):
+            if not os.path.isfile(filepath_a):
                 continue
 
             # skip metplus_final.conf
-            if filepath.endswith('metplus_final.conf'):
+            if filepath_a.endswith('metplus_final.conf'):
                 continue
 
-            filepath2 = filepath.replace(dir_a, dir_b)
+            filepath_b = filepath_a.replace(dir_a, dir_b)
             if debug:
                 print("\n# # # # # # # # # # # # # # # # # # # # # # # # # # "
                       "# # # #\n")
-                rel_path = filepath.replace(f'{dir_a}/', '')
+                rel_path = filepath_a.replace(f'{dir_a}/', '')
                 print(f"COMPARING {rel_path}")
-                print(f"file1: {filepath}")
-                print(f"file2: {filepath2}\n")
+                print(f"file_A: {filepath_a}")
+                print(f"file_B: {filepath_b}\n")
 
             # if file does not exist in dir_b, report difference
-            if not os.path.exists(filepath2):
+            if not os.path.exists(filepath_b):
                 if debug:
-                    print(f"ERROR: File does not exist: {filepath2}")
-                diff_files.append((filepath, '', 'file not found'))
+                    print(f"ERROR: File does not exist: {filepath_b}")
+                diff_files.append((filepath_a, '', 'file not found'))
                 continue
 
-            file_type = get_file_type(filepath)
+            file_type = get_file_type(filepath_a)
             if file_type == 'skip':
                 print(f'Skipping')
                 continue
 
             if file_type == 'netcdf':
                 print("Comparing NetCDF")
-                if not nc_is_equal(filepath, filepath2):
-                    diff_files.append((filepath, filepath2, 'NetCDF diff'))
+                if not nc_is_equal(filepath_a, filepath_b):
+                    diff_files.append((filepath_a, filepath_b, 'NetCDF diff'))
                 else:
                     print("No differences in NetCDF files")
                 continue
 
             if file_type == 'image':
                 print("Comparing images")
-                if not compare_image_files(filepath, filepath2):
-                    diff_files.append((filepath, filepath2, 'Image diff'))
+                if not compare_image_files(filepath_a, filepath_b):
+                    diff_files.append((filepath_a, filepath_b, 'Image diff'))
                 else:
                     print("No differences in image files")
                 continue
 
             # if not any of the above types, use diff to compare
             print("Comparing text files")
-            if not filecmp.cmp(filepath, filepath2):
+            if not filecmp.cmp(filepath_a, filepath_b):
                 # if files differ, open files and handle expected diffs
-                if not compare_txt_files(filepath, filepath2, dir_a, dir_b):
-                    print(f"ERROR: File differs: {filepath2}")
-                    diff_files.append((filepath, filepath2, 'Text diff'))
+                if not compare_txt_files(filepath_a, filepath_b, dir_a, dir_b):
+                    print(f"ERROR: File differs: {filepath_b}")
+                    diff_files.append((filepath_a, filepath_b, 'Text diff'))
                 else:
                     print("No differences in text files")
             else:
@@ -117,11 +117,11 @@ def compare_dir(dir_a, dir_b, debug=False):
 
     return diff_files
 
-def compare_image_files(filepath, filepath2):
+def compare_image_files(filepath_a, filepath_b):
     diff_count = 0
 
-    image_a = Image.open(filepath)
-    image_b = Image.open(filepath2)
+    image_a = Image.open(filepath_a)
+    image_b = Image.open(filepath_b)
     image_diff = ImageChops.difference(image_a, image_b)
     nx, ny = image_diff.size
     for x in range(0, int(nx)):
@@ -134,12 +134,29 @@ def compare_image_files(filepath, filepath2):
         return False
     return True
 
-def compare_txt_files(filepath, filepath2, dir_a, dir_b):
-    with open(filepath, 'r') as file_handle:
+def compare_txt_files(filepath_a, filepath_b, dir_a, dir_b):
+    with open(filepath_a, 'r') as file_handle:
         lines_a = file_handle.read().splitlines()
 
-    with open(filepath2, 'r') as file_handle:
+    with open(filepath_b, 'r') as file_handle:
         lines_b = file_handle.read().splitlines()
+
+    # handle if either file (or both) is empty
+    # filepath_b is empty
+    if not len(lines_b):
+        # filepath_a is also empty
+        if not len(lines_a):
+            print("Both text files are empty, so they are equal")
+            return True
+        else:
+            print(f"Empty file: {filepath_b}\n"
+                  f"Not empty: {filepath_a}")
+            return False
+    # filepath_b is not empty but filepath_a is empty
+    elif not len(lines_a):
+        print(f"Empty file: {filepath_a}\n"
+              f"Not empty: {filepath_b}")
+        return False
 
     # check if the files are "file list" files
     # remove file_list first line for comparison
@@ -164,15 +181,15 @@ def compare_txt_files(filepath, filepath2, dir_a, dir_b):
         header_b = lines_b.pop(0).split()[1:]
 
     if len(lines_a) != len(lines_b):
-        print(f"ERROR: Different number of lines in {filepath2}")
-        print(f"A: {len(lines_a)}, B: {len(lines_b)}")
+        print(f"ERROR: Different number of lines in {filepath_b}")
+        print(f" File_A: {len(lines_a)}\n File_B: {len(lines_b)}")
         return False
 
     for line_a, line_b in zip(lines_a, lines_b):
         compare_a = line_a
         compare_b = line_b
         # if files are file list files, compare each line after replacing
-        # dir_b with dir_a in filepath2
+        # dir_b with dir_a in filepath_b
         if is_file_list:
             compare_b = compare_b.replace(dir_b, dir_a)
 
@@ -188,7 +205,7 @@ def compare_txt_files(filepath, filepath2, dir_a, dir_b):
                         print(f"ERROR: {label} differs: {col_a} vs {col_b}")
                         all_good = False
             else:
-                print(f"ERROR: Line in {filepath2} differs")
+                print(f"ERROR: Line in {filepath_b} differs")
                 print(f"  A: {compare_a}")
                 print(f"  B: {compare_b}")
                 all_good = False
@@ -217,8 +234,8 @@ def nc_is_equal(file_a, file_b, fields=None, debug=False):
         b_fields = sorted(nc_b.variables.keys())
         if a_fields != b_fields:
             print("ERROR: Field list differs between files\n"
-                  f"A: {a_fields}\nB:{b_fields}\n"
-                  f"Using A fields.")
+                  f" File_A: {a_fields}\n File_B:{b_fields}\n"
+                  f"Using File_A fields.")
 
         field_list = a_fields
 
@@ -231,7 +248,7 @@ def nc_is_equal(file_a, file_b, fields=None, debug=False):
 
             if debug:
                 print(f"Field: {field}")
-                print(f"Var_a:{var_a}\nVar_b:{var_b}")
+                print(f"Var_A:{var_a}\nVar_B:{var_b}")
                 print(f"Instance type: {type(var_a[0])}")
             try:
                 values_a = var_a[:]
@@ -263,7 +280,7 @@ def nc_is_equal(file_a, file_b, fields=None, debug=False):
                     if any(var_a[:].flatten() != var_b[:].flatten()):
                         print(f"ERROR: Field ({field}) values (non-numeric) "
                               "differ\n"
-                              f"A: {var_a}, B: {var_b}")
+                              f" File_A: {var_a}\n File_B: {var_b}")
                         is_equal = False
                 except:
                     print("ERROR: Couldn't diff NetCDF files, need to update diff method")
