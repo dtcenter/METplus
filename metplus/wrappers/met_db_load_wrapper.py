1fd537526df3f275e33f8841649317505d7da537
George McCabe <mccabe@ucar.edu>
2021-03-16 12:01:40 -0600

    implemented xml file string substitution

--- metplus/wrappers/met_db_load_wrapper.py
+++ metplus/wrappers/met_db_load_wrapper.py
@@ -18,18 +18,18 @@ from ..util import time_util
 from . import RuntimeFreqWrapper
 from ..util import do_string_sub
 
-'''!@namespace METdataDBWrapper
+'''!@namespace METDbLoadWrapper
 @brief Parent class for wrappers that run over a grouping of times
 @endcode
 '''
 
-class METdataDBWrapper(RuntimeFreqWrapper):
+class METDbLoadWrapper(RuntimeFreqWrapper):
     def __init__(self, config, instance=None, config_overrides={}):
         met_data_db_dir = config.getdir('MET_DATA_DB_DIR')
         self.app_path = os.path.join(met_data_db_dir,
                                      'METdbLoad',
                                      'ush',
-                                     'met_db_load.py')
+                                     'met_db_load')
         self.app_name = os.path.basename(self.app_path)
         super().__init__(config,
                          instance=instance,
@@ -38,11 +38,11 @@ class METdataDBWrapper(RuntimeFreqWrapper):
     def create_c_dict(self):
         c_dict = super().create_c_dict()
 
-        c_dict['XML_FILE'] = (
+        c_dict['XML_TEMPLATE'] = (
             self.config.getraw('config',
                                'MET_DB_LOAD_XML_FILE')
         )
-        if not c_dict['XML_FILE']:
+        if not c_dict['XML_TEMPLATE']:
             self.log_error("Must supply an XML file with "
                            "MET_DB_LOAD_XML_FILE")
 
@@ -77,7 +77,7 @@ class METdataDBWrapper(RuntimeFreqWrapper):
            @rtype string
            @return Returns a MET command with arguments that you can run
         """
-        return f"python3 {self.app_path} {self.c_dict['XML_FILE']}"
+        return f"python3 {self.app_path}.py {self.c_dict.get('XML_TMP_FILE')}"
 
     def run_at_time_once(self, time_info):
         """! Process runtime and build command to run
@@ -107,21 +107,17 @@ class METdataDBWrapper(RuntimeFreqWrapper):
 
             self.set_environment_variables(time_info)
 
-            # substitute values from time dictionary
-            input_path = (
-                do_string_sub(self.c_dict['INPUT_TEMPLATE'],
-                              **time_info)
-            )
-            # set up dictionary of text to substitute in XML file
-            substitution_dict = {}
-            substitution_dict['METPLUS_INPUT_PATH'] = input_path
-            substitution_dict['METPLUS_USER'] = self.c_dict['MV_USER']
-            substitution_dict['METPLUS_HOST'] = self.c_dict['MV_HOST']
-            substitution_dict['METPLUS_DATABASE'] = self.c_dict['MV_DATABASE']
+            if not self.replace_values_in_xml(time_info):
+                return
 
             # run command
             if not self.build():
                 success = False
+
+            # remove tmp file
+#            xml_file = self.c_dict.get('XML_TMP_FILE')
+#            if xml_file and os.path.exists(xml_file):
+#                os.remove(xml_file)
 
         return success
 
@@ -130,4 +126,47 @@ class METdataDBWrapper(RuntimeFreqWrapper):
 
             @returns True to report that no failures occurred
         """
+        return True
+
+    def replace_values_in_xml(self, time_info):
+        self.c_dict['XML_TMP_FILE'] = None
+
+        xml_template = self.c_dict.get('XML_TEMPLATE')
+        if not xml_template:
+            return False
+
+        # set up dictionary of text to substitute in XML file
+        substitution_dict = {}
+
+        # substitute values from time dictionary
+        input_path = (
+            do_string_sub(self.c_dict['INPUT_TEMPLATE'],
+                          **time_info)
+        )
+        substitution_dict['METPLUS_INPUT_PATH'] = input_path
+        substitution_dict['METPLUS_USER'] = self.c_dict['MV_USER']
+        substitution_dict['METPLUS_HOST'] = self.c_dict['MV_HOST']
+        substitution_dict['METPLUS_DATABASE'] = self.c_dict['MV_DATABASE']
+
+        # open XML template file and replace any values encountered
+        with open(xml_template, 'r') as file_handle:
+            input_lines = file_handle.read().splitlines()
+
+        output_lines = []
+        for input_line in input_lines:
+            output_line = input_line
+            for replace_string, value in substitution_dict.items():
+                output_line = output_line.replace(f"${{{replace_string}}}",
+                                                  value)
+            output_lines.append(output_line)
+
+        # write tmp file with XML content with substituted values
+        out_filename = util.generate_tmp_filename()
+        out_path = os.path.join(self.config.getdir('TMP_DIR'),
+                                out_filename)
+        with open(out_path, 'w') as file_handle:
+            for line in output_lines:
+                file_handle.write(f'{line}\n')
+
+        self.c_dict['XML_TMP_FILE'] = out_path
         return True
