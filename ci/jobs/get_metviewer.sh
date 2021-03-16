ab5a9061c9e30e5da5a79a2a9e8e2a63aae1c490
George McCabe <mccabe@ucar.edu>
2021-03-16 17:57:26 -0600

    added scripts to obtain METviewer

---
+++ ci/jobs/get_metviewer.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+basedir=$(dirname "$0")
+work_dir=$basedir/../..
+
+export METVIEWER_DATA=$RUNNER_WORKSPACE
+export MYSQL_DIR=$RUNNER_WORKSPACE/mysql
+#export METVIEWER_DIR=/metplus/METplus/METviewer
+export METVIEWER_DIR=$RUNNER_WORKSPACE/metviewer
+export METVIEWER_DOCKER_IMAGE=dtcenter/metviewer
+
+# run manage externals to obtain METcalcpy
+#${work_dir}/manage_externals/checkout_externals -e ${work_dir}/ci/parm/Externals_metviewer.cfg
+#cd ${work_dir}/METviewer
+#git checkout develop
+#ls docker/docker-compose.yml
+
+cd ${work_dir}
+curl -SL https://raw.githubusercontent.com/dtcenter/METviewer/develop/docker/docker-compose.yml > docker-compose.yml
+
+docker-compose up -d
+
+docker images
+docker ps -a
