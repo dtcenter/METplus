15afd5d958a0edb4fd33e92ba4fc1a2630399abe
Christina Kalb <kalb@cheyenne6.cheyenne.ucar.edu>
2021-03-17 11:22:51 -0600

    Fixed typo in blocking driver

--- parm/use_cases/model_applications/s2s/UserScript_obsERA_obsOnly_Blocking/Blocking_driver.py
+++ parm/use_cases/model_applications/s2s/UserScript_obsERA_obsOnly_Blocking/Blocking_driver.py
@@ -5,13 +5,6 @@ import numpy as np
 import netCDF4
 import re
 
-mp_fpath = os.path.abspath(__file__)
-mp_fpath_split = str.split(mp_fpath,os.path.sep)
-mp_loc_ind = mp_fpath_split.index('METplus')
-
-sys.path.insert(0,os.path.sep.join(mp_fpath_split[0:mp_loc_ind+1]))
-sys.path.insert(0, os.path.sep.join(mp_fpath_split[0:mp_loc_ind])+"/METplotpy")
-
 from Blocking import BlockingCalculation
 from metplus.util import pre_run_setup, config_metplus, get_start_end_interval_times, get_lead_sequence
 from metplus.util import get_skip_times, skip_time, is_loop_by_init, ti_calculate, do_string_sub
