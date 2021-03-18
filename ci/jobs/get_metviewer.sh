d80f1454c174d1878ac0ff8df630a608fcfc1873
George McCabe <mccabe@ucar.edu>
2021-03-18 13:41:32 -0600

    added steps to create METviewer database in CI

--- ci/jobs/get_metviewer.sh
+++ ci/jobs/get_metviewer.sh
@@ -3,28 +3,34 @@
 basedir=$(dirname "$0")
 work_dir=$basedir/../..
 
+# set environment variables needed by METviewer docker-compose.yml
 export METVIEWER_DATA=$RUNNER_WORKSPACE
 export MYSQL_DIR=$RUNNER_WORKSPACE/mysql
-#export METVIEWER_DIR=/metplus/METplus/METviewer
 export METVIEWER_DIR=$RUNNER_WORKSPACE/metviewer
 export METVIEWER_DOCKER_IMAGE=dtcenter/metviewer
 
-# run manage externals to obtain METcalcpy
-#${work_dir}/manage_externals/checkout_externals -e ${work_dir}/ci/parm/Externals_metviewer.cfg
-#cd ${work_dir}/METviewer
-#git checkout develop
-#ls docker/docker-compose.yml
+# create directories for METviewer and database output
+mkdir -p $METVIEWER_DIR
+mkdir -p $MYSQL_DIR
 
-cd ${work_dir}
-curl -SL https://raw.githubusercontent.com/dtcenter/METviewer/develop/docker/docker-compose.yml > docker-compose.yml
+# install docker-compose
+apk add docker-compose
 
+# download docker-compose.yml file from METviewer develop branch
+wget https://raw.githubusercontent.com/dtcenter/METviewer/develop/docker/docker-compose.yml
+
+# Run docker-compose to create the containers
 docker-compose up -d
 
-docker images
+# print list of currently running containers to
+# verify mysql and metviewer are running
 docker ps -a
 
 # commands to run inside METviewer container
-# mysql -hmysql_mv -uroot -pmvuser -e"create database mv_met_out;"
-# mysql -hmysql_mv -uroot -pmvuser mv_met_out < /METviewer/sql/mv_mysql.sql
-# chmod +x /METviewer/bin/mv_load.sh
-# /METviewer/bin/mv_load.sh /data/path/to/xml/file.xml
+cmd="mysql -hmysql_mv -uroot -pmvuser -e\"create database mv_metplus_test;\";"
+cmd+=" mysql -hmysql_mv -uroot -pmvuser mv_metplus_test < /METviewer/sql/mv_mysql.sql"
+
+# execute commands inside metviewer container to create database
+echo Executing commands inside metviewer_1 container to create database
+echo docker exec metviewer_1 /bin/bash -c "$cmd"
+docker exec metviewer_1 /bin/bash -c "$cmd"
