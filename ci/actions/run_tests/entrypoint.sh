d0ee05dd3200ba4edac12d27940817b06a82be58
George McCabe <mccabe@ucar.edu>
2021-03-22 12:13:33 -0600

    print docker info

--- ci/actions/run_tests/entrypoint.sh
+++ ci/actions/run_tests/entrypoint.sh
@@ -89,6 +89,12 @@ fi
 
 echo VOLUMES_FROM: $VOLUMES_FROM
 
+echo docker ps:
+docker ps -a
+
+echo docker port mysql_mv
+docker port mysql_mv
+
 echo "Run Docker container: $DOCKERHUBTAG"
 echo docker run -e GITHUB_WORKSPACE -v $GHA_OUTPUT_DIR:$DOCKER_OUTPUT_DIR -v $GHA_DIFF_DIR:$DOCKER_DIFF_DIR -v $GHA_ERROR_LOG_DIR:$DOCKER_ERROR_LOG_DIR -v $WS_PATH:$GITHUB_WORKSPACE ${VOLUMES_FROM} --workdir $GITHUB_WORKSPACE $DOCKERHUBTAG bash -c "${pip_command};${command}"
 docker run -e GITHUB_WORKSPACE -v $GHA_OUTPUT_DIR:$DOCKER_OUTPUT_DIR -v $GHA_DIFF_DIR:$DOCKER_DIFF_DIR -v $GHA_ERROR_LOG_DIR:$DOCKER_ERROR_LOG_DIR -v $WS_PATH:$GITHUB_WORKSPACE ${VOLUMES_FROM} --workdir $GITHUB_WORKSPACE $DOCKERHUBTAG bash -c "${pip_command};${command}"
