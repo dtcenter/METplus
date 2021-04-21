020262a552165cb12c6ecf46b8e951175cc8ea25
Hank Fisher <fisherh@kiowa.rap.ucar.edu>
2021-04-21 08:32:49 -0600

    Added comments and reading in log level, cleaned up conf file

--- parm/use_cases/model_applications/s2s/SpaceTimeCoherenceDiagram/hovmoeller_diagram.py
+++ parm/use_cases/model_applications/s2s/SpaceTimeCoherenceDiagram/hovmoeller_diagram.py
@@ -9,7 +9,8 @@ import sys
 import logging
 import yaml
 import xarray as xr  # http://xarray.pydata.org/
-import METplotpy.metplotpy.plots.hovmoeller.hovmoeller as Hovmoeller
+#import METplotpy.metplotpy.plots.hovmoeller.hovmoeller as Hovmoeller
+import metplotpy.plots.hovmoeller.hovmoeller as Hovmoeller
 
 
 def main():
@@ -18,10 +19,13 @@ def main():
     create a default plot, using data in the METplus data store
     """
 
-    data_dir = os.environ.get("METplus_DATA_DIR",'/d2/METplus_Data')
-    input_file_name = os.environ.get("INPUT_FILE_NAME",'precip.erai.sfc.1p0.2x.2014-2016.nc')
-    plot_config_file = os.path.join(os.getenv('METPLOTPY_BASE'), 'plots', 'config', 'hovmoeller_defaults.yaml')
-    input_file = os.path.join(data_dir,'model_applications','s2s',input_file_name)
+    """
+    Read METplus config file paramaters
+    """
+    data_dir = os.environ.get("METplus_DATA_DIR","/d2/METplus_Data")
+    input_file_name = os.environ.get("INPUT_FILE_NAME","precip.erai.sfc.1p0.2x.2014-2016.nc")
+    plot_config_file = os.path.join(os.getenv("METPLOTPY_BASE","/d2/METplotpy/metplotpy"), "plots", "config", "hovmoeller_defaults.yaml")
+    input_file = os.path.join(data_dir,"model_applications","s2s","SpaceTimeCoherenceDiagram",input_file_name)
 
     """
     Read Hovmoeller YAML configuration file
@@ -38,7 +42,7 @@ def main():
     Setup logging
     """
     logfile = "Hovmoeller_diagram.log"
-    logging_level = logging.INFO
+    logging_level = os.environ.get("LOG_LEVEL","logging.INFO")
     logging.basicConfig(stream=logfile, level=logging_level)
 
     if os.path.isdir(data_dir):
