7d9423998d9d63b4ef8e7059525d405711b849d9
George McCabe <mccabe@ucar.edu>
2021-04-26 19:54:01 -0600

    obtain METcalcpy and METplotpy code one level above METplus repo in test environment

--- ci/jobs/python_requirements/get_metcalcpy.sh
+++ ci/jobs/python_requirements/get_metcalcpy.sh
@@ -10,4 +10,4 @@ work_dir=$basedir/../../..
 # run manage externals to obtain METcalcpy
 ${work_dir}/manage_externals/checkout_externals -e ${work_dir}/ci/parm/Externals_metcalcpy.cfg
 
-pip3 install ${work_dir}/METcalcpy
+pip3 install ${work_dir}/../METcalcpy
--- ci/jobs/python_requirements/get_metplotpy.sh
+++ ci/jobs/python_requirements/get_metplotpy.sh
@@ -10,4 +10,4 @@ work_dir=$basedir/../../..
 # run manage externals to obtain METcalcpy
 ${work_dir}/manage_externals/checkout_externals -e ${work_dir}/ci/parm/Externals_metplotpy.cfg
 
-pip3 install ${work_dir}/METplotpy
+pip3 install ${work_dir}/../METplotpy
