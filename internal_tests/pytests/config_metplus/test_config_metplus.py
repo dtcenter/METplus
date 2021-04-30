2330e17ad2540d4f9dd8a7218f7e248a91a05cb9
George McCabe <mccabe@ucar.edu>
2021-04-30 13:55:35 -0600

    added support to use old metplus default confs files if they are found to enable users to set PARM_BASE to an old location and still use the new source code

---
+++ internal_tests/pytests/config_metplus/test_config_metplus.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import pytest
+import os
+
+from metplus.util import config_metplus
+
+def test_get_default_config_list():
+    test_data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                 os.pardir,
+                                                 os.pardir,
+                                                 'data',
+                                                 'config_metplus'))
+    old_parm_base = os.path.join(test_data_dir, 'old')
+    new_parm_base = os.path.join(test_data_dir, 'new')
+
+    old_list = ['metplus_config/metplus_system.conf',
+                'metplus_config/metplus_data.conf',
+                'metplus_config/metplus_runtime.conf',
+                'metplus_config/metplus_logging.conf']
+    new_list = ['metplus_config.conf']
+    old_config = [os.path.join(old_parm_base, item) for item in old_list]
+    new_config = [os.path.join(new_parm_base, item) for item in new_list]
+
+    assert config_metplus.get_default_config_list(old_parm_base) == old_config
+    assert config_metplus.get_default_config_list(new_parm_base) == new_config
