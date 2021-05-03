f87760319f717ec0c270ca8a810b5a5e0cf59a74
George McCabe <mccabe@ucar.edu>
2021-05-03 14:49:01 -0600

    updated tests to ensure defaults.conf gets priority if old confs are somehow found in parm/metplus_config

--- internal_tests/pytests/config_metplus/test_config_metplus.py
+++ internal_tests/pytests/config_metplus/test_config_metplus.py
@@ -13,14 +13,24 @@ def test_get_default_config_list():
                                                  'config_metplus'))
     old_parm_base = os.path.join(test_data_dir, 'old')
     new_parm_base = os.path.join(test_data_dir, 'new')
+    both_parm_base = os.path.join(test_data_dir, 'both')
 
     old_list = ['metplus_config/metplus_system.conf',
                 'metplus_config/metplus_data.conf',
                 'metplus_config/metplus_runtime.conf',
                 'metplus_config/metplus_logging.conf']
     new_list = ['metplus_config/defaults.conf']
-    old_config = [os.path.join(old_parm_base, item) for item in old_list]
-    new_config = [os.path.join(new_parm_base, item) for item in new_list]
 
-    assert config_metplus.get_default_config_list(old_parm_base) == old_config
-    assert config_metplus.get_default_config_list(new_parm_base) == new_config
+    # old conf files should be parsed before new if both are found
+    both_list = old_list + new_list
+
+    expected_old = [os.path.join(old_parm_base, item) for item in old_list]
+    expected_new = [os.path.join(new_parm_base, item) for item in new_list]
+    expected_both = [os.path.join(both_parm_base, item) for item in both_list]
+
+    actual_old = config_metplus.get_default_config_list(old_parm_base)
+    actual_new = config_metplus.get_default_config_list(new_parm_base)
+    actual_both = config_metplus.get_default_config_list(both_parm_base)
+    assert actual_old == expected_old
+    assert actual_new == expected_new
+    assert actual_both == expected_both
