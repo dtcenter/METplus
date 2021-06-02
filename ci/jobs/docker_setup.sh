ba8247502e5d3be86cf54297e7a666218102dbc0
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2021-06-02 15:06:29 -0600

    add docker pull back, removed --pull from docker build command

--- ci/jobs/docker_setup.sh
+++ ci/jobs/docker_setup.sh
@@ -21,15 +21,15 @@ echo 'doing docker build'
 # Note: adding --build-arg <arg-name> without any value tells docker to
 #  use value from local environment (export DO_GIT_CLONE)
 
-#echo Timing docker pull...
-#start_seconds=$SECONDS
+echo Timing docker pull...
+start_seconds=$SECONDS
 
 # pipe result to true because it will fail if image has not yet been built
-#docker pull ${DOCKERHUB_TAG} &> /dev/null || true
+docker pull ${DOCKERHUB_TAG} &> /dev/null || true
 
-#duration=$(( SECONDS - start_seconds ))
-#echo TIMING docker_setup
-#echo "Docker pull took $(($duration / 60)) minutes and $(($duration % 60)) seconds."
+duration=$(( SECONDS - start_seconds ))
+echo TIMING docker_setup
+echo "Docker pull took $(($duration / 60)) minutes and $(($duration % 60)) seconds."
 
 echo Timing docker build with --cache-from...
 start_seconds=$SECONDS
@@ -40,7 +40,8 @@ export DOCKERFILE_PATH=${GITHUB_WORKSPACE}/ci/docker/Dockerfile.copy
 MET_TAG=`${GITHUB_WORKSPACE}/ci/docker/hooks/get_met_version`
 echo Running docker build with MET_TAG=$MET_TAG
 
-docker build --pull --cache-from ${DOCKERHUB_TAG} \
+#docker build --pull --cache-from ${DOCKERHUB_TAG} \
+docker build --cache-from ${DOCKERHUB_TAG} \
 -t ${DOCKERHUB_TAG} \
 --build-arg MET_TAG=$MET_TAG \
 -f ${DOCKERFILE_PATH} ${GITHUB_WORKSPACE}
