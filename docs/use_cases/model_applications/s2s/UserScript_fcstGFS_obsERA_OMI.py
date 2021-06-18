d0a309923dd6ca1e6b0b3a992e473e8f7f481c68
Minna Win <minnawin@kiowa.rap.ucar.edu>
2021-06-18 16:23:39 -0600

    Github Issue #892 initial OMI documentation (incomplete)

---
+++ docs/use_cases/model_applications/s2s/UserScript_fcstGFS_obsERA_OMI.py
@@ -0,0 +1,137 @@
+"""
+UserScript: Make OMI plot from calculated MJO indices
+===========================================================================
+
+model_applications/
+s2s/
+UserScript_fcstGFS_obsERA_OMI.py
+
+"""
+
+##############################################################################
+# Scientific Objective
+# --------------------
+#
+# To generate ... XXXX
+# 
+# 
+
+##############################################################################
+# Datasets
+# --------
+#
+#  * Forecast dataset:  XXXX
+#  * Observation dataset: XXXX
+
+##############################################################################
+# External Dependencies
+# ---------------------
+#
+# You will need to use a version of Python 3.6+ that has the following packages installed::
+#
+# * numpy
+# * netCDF4
+# * datetime
+# * xarray
+# * matplotlib
+# * scipy
+# * pandas 
+#
+# If the version of Python used to compile MET did not have these libraries at the time of compilation, you will need to add these packages or create a new Python environment with these packages.
+#
+# If this is the case, you will need to set the MET_PYTHON_EXE environment variable to the path of the version of Python you want to use. If you want this version of Python to only apply to this use case, set it in the [user_env_vars] section of a METplus configuration file.:
+#
+#    [user_env_vars]
+#    MET_PYTHON_EXE = /path/to/python/with/required/packages/bin/python
+#
+
+##############################################################################
+# METplus Components
+# ------------------
+#
+# This use case runs the OMI driver... XXXX
+#
+#
+
+##############################################################################
+# METplus Workflow
+# ----------------
+# XXXX
+
+##############################################################################
+# METplus Configuration
+# ---------------------
+#
+# METplus first loads all of the configuration files found in parm/metplus_config,
+# then it loads any configuration files passed to METplus via the command line
+# i.e. parm/use_cases/model_applications/s2s/UserScript_fcstGFS_obsERA_OMI.conf.  
+# The file OMI_driver.py runs the python program and  
+# UserScript_fcstGFS_obsERA_OMI/UserScript_fcstGFS_obsERA_OMI.conf sets the 
+# variables for all steps of the OMI use case.
+#
+# .. highlight:: bash
+# .. literalinclude:: ../../../../parm/use_cases/model_applications/s2s/UserScript_fcstGFS_obsERA_OMI.conf
+
+##############################################################################
+# MET Configuration
+# ---------------------
+#
+# METplus sets environment variables based on the values in the METplus configuration file.
+# These variables are referenced in the MET configuration file. **YOU SHOULD NOT SET ANY OF THESE ENVIRONMENT VARIABLES YOURSELF! THEY WILL BE OVERWRITTEN BY METPLUS WHEN IT CALLS THE MET TOOLS!** If there is a setting in the MET configuration file that is not controlled by an environment variable, you can add additional environment variables to be set only within the METplus environment using the [user_env_vars] section of the METplus configuration files. See the 'User Defined Config' section on the 'System Configuration' page of the METplus User's Guide for more information.
+#
+#
+
+##############################################################################
+# Python Scripts
+# ----------------
+#
+# The OMI driver script orchestrates the calculation of the MJO indices and 
+# the generation of three OMI plots:
+# parm/use_cases/model_applications/s2s/UserScript_fcstGFS_obsERA_OMI/OMI_driver.py:
+#
+# .. highlight:: python
+# .. literalinclude:: ../../../../parm/use_cases/model_applications/s2s/UserScript_fcstGFS_obsERA_OMI/OMI_driver.py
+#
+
+##############################################################################
+# Running METplus
+# ---------------
+#
+# This use case is run in the following ways:
+#
+# 1) Passing in UserScript_fcstGFS_obsERA_OMI.conf then a user-specific system configuration file::
+#
+#        run_metplus.py -c /path/to/METplus/parm/use_cases/model_applications/s2s/UserScript_fcstGFS_obsERA_OMI.conf -c /path/to/user_system.conf
+#
+# 2) Modifying the configurations in parm/metplus_config, then passing in UserScript_fcstGFS_obsERA_OMI.py::
+#
+#        run_metplus.py -c /path/to/METplus/parm/use_cases/model_applications/s2s/UserScript_fcstGFS_obsERA_OMI.conf
+#
+# The following variables must be set correctly:
+#
+# * **INPUT_BASE** - Path to directory where sample data tarballs are unpacked (See Datasets section to obtain tarballs). This is not required to run METplus, but it is required to run the examples in parm/use_cases
+# * **OUTPUT_BASE** - Path where METplus output will be written. This must be in a location where you have write permissions
+# * **MET_INSTALL_DIR** - Path to location where MET is installed locally
+#
+# Example User Configuration File::
+#
+#   [dir]
+#   INPUT_BASE = /path/to/sample/input/data
+#   OUTPUT_BASE = /path/to/output/dir
+#   MET_INSTALL_DIR = /path/to/met-X.Y 
+#
+
+##############################################################################
+# Expected Output
+# ---------------
+#
+# Refer to the value set for **OUTPUT_BASE** to find where the output data was generated. Output for this use 
+# case will be found in XXXX 
+
+##############################################################################
+# Keywords
+# --------
+#
+# sphinx_gallery_thumbnail_path = '_static/XXXX'
+#
+# .. note:: `XXXX`, `S2SAppUseCase <https://dtcenter.github.io/METplus/search.html?q=S2SAppUseCase&check_keywords=yes&area=default>`_ 
