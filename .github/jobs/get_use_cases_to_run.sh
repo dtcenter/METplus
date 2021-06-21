e36b0d32aa0a5f476a5b0a59c356c9e367d8c960
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2021-06-21 14:03:50 -0600

    moved quotes

--- .github/jobs/get_use_cases_to_run.sh
+++ .github/jobs/get_use_cases_to_run.sh
@@ -29,12 +29,12 @@ if [ "$run_use_cases" == "true" ]; then
   jq_filter=${jq_filter}" | .category"
 
   # add square brackets around filter to create array
-  jq_filter="["${jq_filter}"]"
+  jq_filter="'["${jq_filter}"]'"
 
   echo Filter is $jq_filter
 
   # perform JQ query to get list of use case groups to run
-  matrix=$(jq '${jq_filter}' $use_case_groups_filepath)
+  matrix=$(jq ${jq_filter} $use_case_groups_filepath)
 fi
 
 # if unit tests will be run, add "pytests" to beginning of matrix list
