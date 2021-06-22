d577845008ce5dce18c090664e09eaef1748a962
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2021-06-22 08:56:21 -0600

    run conda list instead of printing contents of conda-meta/history

--- ci/jobs/get_use_case_commands.py
+++ ci/jobs/get_use_case_commands.py
@@ -95,8 +95,10 @@ def main(categories, subset_list, work_dir=None, host_name='docker'):
             if host_name == 'docker':
                 setup_env += (
                     f'echo Using environment: {conda_env};'
-                    'echo -------------------------; echo Conda history:;'
-                    f'cat /usr/local/envs/{conda_env}/conda-meta/history;'
+                    f'echo -------------------------;'
+                    f' echo conda list --name {conda_env}:;'
+                    f'conda list --name {conda_env}'
+#                    f'cat /usr/local/envs/{conda_env}/conda-meta/history;'
                     'echo End of history.; echo -------------------------;'
                 )
 
