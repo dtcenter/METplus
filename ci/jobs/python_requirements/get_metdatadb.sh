fb30a97577e3e1546bc31f72c7d611f3791ed6ce
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2021-06-22 14:29:32 -0600

    cleaned up unused

--- ci/jobs/python_requirements/get_metdatadb.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-basedir=$(dirname "$0")
-work_dir=$basedir/../../..
-
-# run manage externals to obtain METdatadb
-${work_dir}/manage_externals/checkout_externals -e ${work_dir}/ci/parm/Externals_metdatadb.cfg
