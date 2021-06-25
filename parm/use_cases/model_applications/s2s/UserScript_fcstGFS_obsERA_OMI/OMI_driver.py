94169329f6dc5b03d82911d53c9349d112b1687c
Christina Kalb <kalb@kiowa.rap.ucar.edu>
2021-06-25 00:19:27 -0600

    Cleanup to OMI_driver

--- parm/use_cases/model_applications/s2s/UserScript_fcstGFS_obsERA_OMI/OMI_driver.py
+++ parm/use_cases/model_applications/s2s/UserScript_fcstGFS_obsERA_OMI/OMI_driver.py
@@ -9,6 +9,7 @@ import datetime
 import glob
 import os
 import sys
+import warnings
 import pandas as pd
 
 from metplus.util import pre_run_setup, config_metplus, get_start_end_interval_times, get_lead_sequence
@@ -16,30 +17,53 @@ from metplus.util import get_skip_times, skip_time, is_loop_by_init, ti_calculat
 #from metcalcpy.util import read_file
 import metcalcpy.contributed.rmm_omi.compute_mjo_indices as cmi
 import metplotpy.contributed.mjo_rmm_omi.plot_mjo_indices as pmi
-from RMM_OMI_util import find_input_files, find_times, compute_plot_times, read_omi_eofs
+#from RMM_OMI_util import find_input_files, find_times, compute_plot_times, read_omi_eofs
 
 
-def run_omi_steps(inlabel, inconfig, spd, EOF1, EOF2, oplot_dir):
+def read_omi_eofs(eof1_files, eof2_files):
+    """
+    Read the OMI EOFs from file and into a xarray DataArray.
+    :param eofpath: filepath to the location of the eof files
+    :return: EOF1 and EOF2 3D DataArrays
+    """
 
-    # Get dates to read using METplus time variables
-    fileconfig = config_metplus.replace_config_from_section(inconfig,'compute_omi')
-    use_init =  is_loop_by_init(inconfig)
-    alldata_time = find_times(fileconfig, use_init)
+    # observed EOFs from NOAA PSL are saved in individual text files for each doy
+    # horizontal resolution of EOFs is 2.5 degree
+    EOF1 = xr.DataArray(np.empty([366,17,144]),dims=['doy','lat','lon'],
+    coords={'doy':np.arange(1,367,1), 'lat':np.arange(-20,22.5,2.5), 'lon':np.arange(0,360,2.5)})
+    EOF2 = xr.DataArray(np.empty([366,17,144]),dims=['doy','lat','lon'],
+    coords={'doy':np.arange(1,367,1), 'lat':np.arange(-20,22.5,2.5), 'lon':np.arange(0,360,2.5)})
+    nlat = len(EOF1['lat'])
+    nlon = len(EOF1['lon'])
 
-     # Get input File names and Directories
-    olr_template = os.path.join(fileconfig.getraw('config',inlabel+'_OLR_INPUT_DIR'),
-        fileconfig.getraw('config',inlabel+'_OLR_INPUT_TEMPLATE'))
+    for doy in range(len(eof1_files)):
+        doystr = str(doy).zfill(3)
+        tmp1 = pd.read_csv(eof1_files[doy], header=None, delim_whitespace=True, names=['eof1'])
+        tmp2 = pd.read_csv(eof2_files[doy], header=None, delim_whitespace=True, names=['eof2'])
+        eof1 = xr.DataArray(np.reshape(tmp1.eof1.values,(nlat, nlon)),dims=['lat','lon'])
+        eof2 = xr.DataArray(np.reshape(tmp2.eof2.values,(nlat, nlon)),dims=['lat','lon'])
+        EOF1[doy,:,:] = eof1.values
+        EOF2[doy,:,:] = eof2.values
 
-    # Find Files
-    olr_input_files = find_input_files(alldata_time, olr_template)
+    return EOF1, EOF2
 
-     # Create a time variable
-    time = np.array([t_obj['valid'] for t_obj in alldata_time],dtype='datetime64')
-    ntim = len(time)
+
+def run_omi_steps(inlabel, spd, EOF1, EOF2, oplot_dir):
+
+    # Get OLR file listing
+    olr_filetxt = os.environ[inlabel+'_OLR_OMI_INPUT_TEXTFILE']
+
+    # Read the listing of EOF files
+    with open(olr_filetxt) as ol:
+        olr_input_files = ol.read().splitlines()
 
     # read OLR data from file
     #### This will need to be converted to the dbload versions
     #### I've cut the domain using regrid_data_plane, so that can be omitted once switched to dbload
+    datestrt = '1979-01-01'
+    datelast = '2012-12-31'
+    time = np.arange(datestrt,datelast, dtype='datetime64[D]')
+    ntim = len(time)
     ds = xr.open_dataset('UserScript_fcstGFS_obsERA_OMI/olr.1x.7920.anom7901.nc')
     olr = ds['olr'].sel(lat=slice(-20,20),time=slice(time[0],time[-1]))
     lat = ds['lat'].sel(lat=slice(-20,20))
@@ -47,54 +71,48 @@ def run_omi_steps(inlabel, inconfig, spd, EOF1, EOF2, oplot_dir):
     print(olr.min(), olr.max())
 
     # project OLR onto EOFs
-    #### The code called here goes into METcalcpy
-    #### It is currently from the compute_mjo_indices.py
     PC1, PC2 = cmi.omi(olr[0:ntim,:,:], time, spd, EOF1, EOF2)
 
-    print(PC1.min(), PC1.max())
+    # Get times for the PC phase diagram
+    plase_plot_time_format = os.environ['PHASE_PLOT_TIME_FMT']
+    phase_plot_start_time = datetime.datetime.strptime(os.environ['PHASE_PLOT_TIME_BEG'],plase_plot_time_format)
+    phase_plot_end_time = datetime.datetime.strptime(os.environ['PHASE_PLOT_TIME_END'],plase_plot_time_format)
+    PC1_plot = PC1.sel(time=slice(phase_plot_start_time,phase_plot_end_time))
+    PC2_plot = PC2.sel(time=slice(phase_plot_start_time,phase_plot_end_time))
 
-    # Setup the PC phase diagram
-    ####  This grabs the time window for the plot from the configuration file
-    plot_time, months, days, ntim_plot = compute_plot_times(inconfig,use_init,'compute_omi','PHASE_PLOT')
-    PC1_plot = PC1.sel(time=slice(plot_time[0],plot_time[-1]))
-    PC2_plot = PC2.sel(time=slice(plot_time[0],plot_time[-1]))
-    PC1_plot = PC1_plot[0:ntim_plot]
-    PC2_plot = PC2_plot[0:ntim_plot]
-    phase_plot_name = os.path.join(oplot_dir,inconfig.getstr('compute_omi',
-        inlabel+'_PHASE_PLOT_OUTPUT_NAME','obs_OMI_comp_phase'))
-    phase_plot_format = inconfig.getstr('compute_omi',inlabel+'_PHASE_PLOT_OUTPUT_FORMAT','png')
+    # Get the output name and format for the PC plase diagram
+    phase_plot_name = os.path.join(oplot_dir,os.environ.get(inlabel+'_PHASE_PLOT_OUTPUT_NAME','obs_OMI_comp_phase'))
+    phase_plot_format = os.environ.get(inlabel+'_PHASE_PLOT_OUTPUT_FORMAT','png')
+
+    # Get times for plotting
+    plot_time = PC1_plot['time'].dt.strftime("%Y-%m-%d").values
+    months = PC1_plot['time.month'].values
+    days = PC1_plot['time.day'].values
+
     # plot the PC phase diagram
-    ####  This will need to go into METplotpy
-    ####  It is currently in plot_mjo_indices.py
     pmi.phase_diagram('OMI',PC1,PC2,plot_time,months,days,phase_plot_name,phase_plot_format)
 
 
 def main():
 
-    # Start configs
-    config_list = sys.argv[1:]
-    config = pre_run_setup(config_list)
+    # Read in EOF filenames
+    eof1_filetxt = os.environ['EOF1_INPUT_TEXTFILE']
+    eof2_filetxt = os.environ['EOF2_INPUT_TEXTFILE']
 
-    # Read in EOF filenames and number of obs per day
-    eofdir = config.getraw('compute_omi','EOF_INPUT_DIR','')
-    olr_eoffile1 = os.path.join(eofdir,config.getraw('compute_omi','EOF1_FILENAME_TEMPLATE','eof{valid?fmt=%j}.txt'))
-    olr_eoffile2 = os.path.join(eofdir,config.getraw('compute_omi','EOF2_FILENAME_TEMPLATE','eof{valid?fmt=%j}.txt'))
-    eofconfig = config_metplus.replace_config_from_section(config,'compute_omi')
-    use_init_eof =  is_loop_by_init(eofconfig)
-    if use_init_eof:
-        eofconfig.set('config','INIT_BEG',eofconfig.getstr('config','EOF_INIT_BEG'))
-        eofconfig.set('config','INIT_END',eofconfig.getstr('config','EOF_INIT_END'))
-    else:
-        eofconfig.set('config','VALID_BEG',eofconfig.getstr('config','EOF_VALID_BEG'))
-        eofconfig.set('config','VALID_END',eofconfig.getstr('config','EOF_VALID_END'))
-    eof_time = find_times(eofconfig, use_init_eof)
-    eof1_input_files = find_input_files(eof_time, olr_eoffile1)
-    eof2_input_files = find_input_files(eof_time, olr_eoffile2)
+    # Read the listing of EOF files
+    with open(eof1_filetxt) as ef1:
+        eof1_input_files = ef1.read().splitlines()
+    with open(eof2_filetxt) as ef2:
+        eof2_input_files = ef2.read().splitlines()
+
+    # Read in the EOFs
     EOF1, EOF2 = read_omi_eofs(eof1_input_files, eof2_input_files)
-    spd = config.getint('compute_omi','OBS_PER_DAY',1)
+
+    # Get Number of Obs per day
+    spd = os.environ.get('OBS_PER_DAY',1)
 
     # Check for an output plot directory in the configs.  Create one if it does not exist
-    oplot_dir = config.getstr('compute_omi','OMI_PLOT_OUTPUT_DIR','')
+    oplot_dir = os.environ.get('OMI_PLOT_OUTPUT_DIR','')
     if not oplot_dir:
         obase = config.getstr('config','OUTPUT_BASE')
         oplot_dir = os.path.join(obase,'plots')
@@ -102,17 +120,22 @@ def main():
         os.makedirs(oplot_dir)
 
     #  Determine if doing forecast or obs
-    run_obs_omi = config.getbool('compute_omi','OBS_RUN', False)
-    run_fcst_omi = config.getbool('compute_omi','FCST_RUN', False)
+    run_obs_omi = os.environ.get('RUN_OBS',False)
+    run_fcst_omi = os.environ.get('FCST_RUN_FCST', False)
 
     # Run the steps to compute OMM
     # Observations
     if run_obs_omi:
-        run_omi_steps('OBS', config, spd, EOF1, EOF2, oplot_dir)
+        run_omi_steps('OBS', spd, EOF1, EOF2, oplot_dir)
 
     # Forecast
     if run_fcst_omi:
-        run_omi_steps('FCST', config, spd, EOF1, EOF2, oplot_dir)
+        run_omi_steps('FCST', spd, EOF1, EOF2, oplot_dir)
+
+    # nothing selected
+    if not run_obs_omi and not run_fcst_omi:
+        warnings.warn('Forecast and Obs runs not selected, nothing will be calculated')
+        warnings.warn('Set RUN_FCST or RUN_OBS in the [user_en_vars] section to generate output')
 
 
 if __name__ == "__main__":
