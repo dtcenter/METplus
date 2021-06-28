babdaad36fb4f58edfcf7d9d797dbdb354f0b4a6
Julie.Prestopnik <jpresto@ucar.edu>
2021-06-28 16:08:56 -0600

    Per #963, removed note section under keywords and moved down the png name.

--- docs/use_cases/model_applications/climate/MODE_fcstCESM_obsGPCP_AsianMonsoonPrecip.py
+++ docs/use_cases/model_applications/climate/MODE_fcstCESM_obsGPCP_AsianMonsoonPrecip.py
@@ -159,10 +159,8 @@ AsianMonsoonPrecip.conf
 # Keywords
 # --------
 #
-# sphinx_gallery_thumbnail_path = '_static/climate-MODE_fcstCESM_obsGPCP_AsianMonsoonPrecip.png'
-#
-# .. note:: MODEToolUseCase, ClimateAppUseCase, NetCDFFileUseCase, NCAROrgUseCase
+#   MODEToolUseCase, ClimateAppUseCase, NetCDFFileUseCase, NCAROrgUseCase
 #
 #   Navigate to the :ref:`quick-search` page to discover other similar use cases.
 #  
-#  
+#   sphinx_gallery_thumbnail_path = '_static/climate-MODE_fcstCESM_obsGPCP_AsianMonsoonPrecip.png' 
