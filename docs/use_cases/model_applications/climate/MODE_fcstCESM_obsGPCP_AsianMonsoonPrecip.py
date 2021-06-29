ca0fd61d42ab8eb46ead8624404db8dd6d8d735d
Julie.Prestopnik <jpresto@ucar.edu>
2021-06-29 15:50:53 -0600

    Per #963, removing additonal comment to eliminate sphinx_gallery_thumbnail_path because it got rid of the thumbnail image

--- docs/use_cases/model_applications/climate/MODE_fcstCESM_obsGPCP_AsianMonsoonPrecip.py
+++ docs/use_cases/model_applications/climate/MODE_fcstCESM_obsGPCP_AsianMonsoonPrecip.py
@@ -168,6 +168,7 @@ AsianMonsoonPrecip.conf
 #
 #   Navigate to the :ref:`quick-search` page to discover other similar use cases.
 #
-# 
-# # sphinx_gallery_thumbnail_path = '_static/climate-MODE_fcstCESM_obsGPCP_AsianMonsoonPrecip.png'
+# .. code-block:: ini
+#
+#   sphinx_gallery_thumbnail_path = '_static/climate-MODE_fcstCESM_obsGPCP_AsianMonsoonPrecip.png'
 #
