2986e66c275c4078d0f2a608177b530e3633ee96
George McCabe <mccabe@seneca.rap.ucar.edu>
2021-07-06 13:11:47 -0600

    moved logic for creating conda environments for metplus base and py_embed base to a script so it can be called on a local machine to create these environments

---
+++ ci/docker/docker_env/scripts/metplus_base_env.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+################################################################################
+# Environment: metplus_base
+# Last Updated: 2021-07-06 (mccabe@ucar.edu)
+# Notes: Move logic to create METplus base env to script so it can be called
+#   on a local machine to create the environment
+# Python Packages:
+#   python-dateutil==2.8.1
+#
+# Other Content: None
+################################################################################
+
+# Conda environment to create
+ENV_NAME=metplus_base
+
+conda update -y -n base -c defaults conda
+conda create -y --name ${ENV_NAME} python=3.6.8
+conda install -y --name ${ENV_NAME} -c conda-forge python-dateutil==2.8.1
