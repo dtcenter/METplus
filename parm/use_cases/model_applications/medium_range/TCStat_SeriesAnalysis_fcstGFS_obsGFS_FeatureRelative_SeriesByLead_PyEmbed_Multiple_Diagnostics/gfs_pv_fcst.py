Corrects pressure units.

parm/use_cases/model_applications/medium_range/TCStat_SeriesAnalysis_fcstGFS_obsGFS_FeatureRelative_SeriesByLead_PyEmbed_Multiple_Diagnostics/gfs_pv_fcst.py
@@ -66,13 +66,13 @@ def pv(input_file):
     ds = ds.sel(isobaricInhPa=ds.isobaricInhPa[ds.isobaricInhPa>=100.0].values)
 
     # Add pressure
-    ds['p'] = xr.DataArray(ds.isobaricInhPa.values*100.0,dims=['isobaricInhPa'],coords={'isobaricInhPa':ds.isobaricInhPa.values},attrs={'units':'hectopascals'}).broadcast_like(ds['t']) 
+    ds['p'] = xr.DataArray(ds.isobaricInhPa.values,dims=['isobaricInhPa'],coords={'isobaricInhPa':ds.isobaricInhPa.values},attrs={'units':'hPa'}).broadcast_like(ds['t']) 
 
     # Calculate potential temperature
     ds['theta'] = mpcalc.potential_temperature(ds['p']*units('Pa'),ds['t'])
   
     # Compute baroclinic PV
-    ds['pv'] = mpcalc.potential_vorticity_baroclinic(ds['theta'],ds['p']*units('Pa'),ds['u'],ds['v'],latitude=ds.latitude)
+    ds['pv'] = mpcalc.potential_vorticity_baroclinic(ds['theta'],ds['p']*units('Pa'),ds['u'],ds['v'],latitude=ds.latitude)*(1.0e-6)
 
     met_data = ds['pv'].mean(axis=0).values
 

parm/use_cases/model_applications/medium_range/TCStat_SeriesAnalysis_fcstGFS_obsGFS_FeatureRelative_SeriesByLead_PyEmbed_Multiple_Diagnostics/gfs_sept_analysis.py
@@ -10,6 +10,7 @@ import os
 import re
 import datetime as dt
 from metpy import calc as mpcalc
+from metpy.units import units
 import xarray as xr
 import cfgrib
 
@@ -65,10 +66,10 @@ def pv(input_file):
     ds = ds.sel(isobaricInhPa=ds.isobaricInhPa[ds.isobaricInhPa>=100.0].values)
 
     # Add pressure
-    ds['p'] = xr.DataArray(ds.isobaricInhPa.values*100.0,dims=['isobaricInhPa'],coords={'isobaricInhPa':ds.isobaricInhPa.values},attrs={'units':'hectopascals'}).broadcast_like(ds['t'])
+    ds['p'] = xr.DataArray(ds.isobaricInhPa.values,dims=['isobaricInhPa'],coords={'isobaricInhPa':ds.isobaricInhPa.values},attrs={'units':'hPa'}).broadcast_like(ds['t'])
 
     # Calculate saturation equivalent potential temperature
-    ds['sept'] = mpcalc.saturation_equivalent_potential_temperature(ds['p'],ds['t'])
+    ds['sept'] = mpcalc.saturation_equivalent_potential_temperature(ds['p']*units('Pa'),ds['t'])
 
     met_data = ds['sept'].mean(axis=0).values
 