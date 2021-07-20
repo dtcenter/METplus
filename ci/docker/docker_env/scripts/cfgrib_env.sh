f946a0a8c6642a04bcd671b3847732d5938b72de
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2021-07-20 11:57:53 -0600

    updated cfgrib environment to prevent package conflicts

--- ci/docker/docker_env/scripts/cfgrib_env.sh
+++ ci/docker/docker_env/scripts/cfgrib_env.sh
@@ -3,10 +3,12 @@
 ################################################################################
 # Environment: cfgrib
 # Last Updated: 2021-07-20 (mccabe@ucar.edu)
-# Notes: Adds Python packages needed to read GRIB data
+# Notes: Adds Python packages needed to read GRIB data into Xarray and
+#   so it can easily be processed with MetPy
 # Python Packages:
-#   cfgrib==0.9.9.0
 #   metpy==1.0.1
+#   netcdf4==1.5.6
+#   cfgrib==0.9.9.0
 #
 # Other Content: None
 ################################################################################
@@ -14,11 +16,7 @@
 # Conda environment to create
 ENV_NAME=cfgrib
 
-# Conda environment to use as base for new environment
-BASE_ENV=$1
-
-
-conda create -y --clone ${BASE_ENV} --name ${ENV_NAME}
-
+conda create -y --name ${ENV_NAME} python=3.6.8
 conda install -y --name ${ENV_NAME} -c conda-forge metpy==1.0.1
+conda install -y --name ${ENV_NAME} -c conda-forge netcdf4=1.5.6
 conda install -y --name ${ENV_NAME} -c conda-forge cfgrib==0.9.9.0
