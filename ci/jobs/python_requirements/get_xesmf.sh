de8a2f7620daba7fc067ad515c4dfca30a586bac
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2021-07-27 09:11:31 -0600

    bugfix 1021 main_v4.0 climo mean/stdev field (#1040)
    * per #1021, fixed logic for setting field for climo_mean and climo_stdev
    
    * install xarray 0.18.2 for xesmf environment because conda update from 4.10.1 to 4.10.3 installs 0.19.0 which appears to break import in use case
    
    * use python 3.8 to prevent xarray import from failing from "SyntaxError: future feature annotations is not defined"

--- ci/jobs/python_requirements/get_xesmf.sh
+++ ci/jobs/python_requirements/get_xesmf.sh
@@ -2,7 +2,7 @@
 
 script_dir=$(dirname "$0")
 
-${script_dir}/get_miniconda.sh
+${script_dir}/get_miniconda.sh 3.8
 
 echo Installing xesmf with conda
 conda install -c conda-forge dask netCDF4 xesmf
