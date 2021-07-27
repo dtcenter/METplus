105dda8da200241713f966fad5407d5ad1ba3f9a
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2021-07-27 07:34:48 -0600

    install xarray 0.18.2 for xesmf environment because conda update from 4.10.1 to 4.10.3 installs 0.19.0 which appears to break import in use case

--- ci/jobs/python_requirements/get_xesmf.sh
+++ ci/jobs/python_requirements/get_xesmf.sh
@@ -5,4 +5,5 @@ script_dir=$(dirname "$0")
 ${script_dir}/get_miniconda.sh
 
 echo Installing xesmf with conda
+conda install -c conda-forge xarray==0.18.2
 conda install -c conda-forge dask netCDF4 xesmf
