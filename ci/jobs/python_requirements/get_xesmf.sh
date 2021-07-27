685dd819f0a21f05f8bebfa297d2efb6d36c665a
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2021-07-27 08:26:40 -0600

    use python 3.8 to prevent xarray import from failing from "SyntaxError: future feature annotations is not defined"

--- ci/jobs/python_requirements/get_xesmf.sh
+++ ci/jobs/python_requirements/get_xesmf.sh
@@ -2,8 +2,7 @@
 
 script_dir=$(dirname "$0")
 
-${script_dir}/get_miniconda.sh
+${script_dir}/get_miniconda.sh 3.8
 
 echo Installing xesmf with conda
-conda install -c conda-forge xarray==0.18.2
 conda install -c conda-forge dask netCDF4 xesmf
