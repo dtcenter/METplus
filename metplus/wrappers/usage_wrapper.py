bf8228527c9d930709dc2982d6e5222d8de9dc8a
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2021-08-10 08:36:57 -0600

    fixed import error in usage wrapper

--- metplus/wrappers/usage_wrapper.py
+++ metplus/wrappers/usage_wrapper.py
@@ -4,7 +4,7 @@
 # @endcode#
 
 from . import CommandBuilder
-from ..util import met_util
+from ..util.doc_util import LOWER_TO_WRAPPER_NAME
 
 class UsageWrapper(CommandBuilder):
     """! A default process, prints out usage when nothing is defined in
