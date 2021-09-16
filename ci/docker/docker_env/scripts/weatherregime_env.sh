6f453dec188f9bbd80abfe9218b80045fb39d16e
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2021-09-16 15:34:41 -0600

    download cartopy shape files for weatherregime python environment because files were not found from base env (metplotpy)

--- ci/docker/docker_env/scripts/weatherregime_env.sh
+++ ci/docker/docker_env/scripts/weatherregime_env.sh
@@ -24,3 +24,6 @@ conda create -y --clone ${BASE_ENV} --name ${ENV_NAME}
 conda install -y --name ${ENV_NAME} -c conda-forge scikit-learn==0.24.2
 conda install -y --name ${ENV_NAME} -c conda-forge eofs==1.4.0
 
+yum -y install wget
+wget https://raw.githubusercontent.com/SciTools/cartopy/master/tools/cartopy_feature_download.py
+/usr/local/envs/${ENV_NAME}/bin/python3 cartopy_feature_download.py cultural physical
