2d88833c88af292de0c0d881d2161d91e9d25bbc
Mrinal Biswas <biswas@ucar.edu>
2021-10-21 17:16:49 +0000

    Removing temp file

--- parm/use_cases/model_applications/marine_and_coastal/GridStat_fcstRTOFS_obsSMAP_climWOA_sss/run_metplus.sh
+++
@@ -1,5 +0,0 @@
-/d1/biswas/METplus_feature_satellite_smap/ush/run_metplus.py \
-        /d1/biswas/METplus_feature_satellite_smap/parm/use_cases/model_applications/marine_and_coastal/GridStat_fcstRTOFS_obsSMAP_climWOA_sss.conf \
-        config.INPUT_BASE=/d1/biswas/feature_satellite_smap \
-        config.MET_INSTALL_DIR=/usr/local/met \
-        config.OUTPUT_BASE=/d1/biswas/sat_output/outdir/smap
