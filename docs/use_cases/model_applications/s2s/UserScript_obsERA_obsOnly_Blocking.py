35615d5e0e7b8e5ddeed78f93ba50240ae00418c
Christina Kalb <kalb@ucar.edu>
2022-02-09 14:23:22 -0700

    Update UserScript_obsERA_obsOnly_Blocking.py

--- docs/use_cases/model_applications/s2s/UserScript_obsERA_obsOnly_Blocking.py
+++ docs/use_cases/model_applications/s2s/UserScript_obsERA_obsOnly_Blocking.py
@@ -114,19 +114,19 @@ UserScript_obsERA_obsOnly_Blocking.py
 # parm/use_cases/model_applications/s2s/UserScript_obsERA_obsOnly_Blocking/Blocking_driver.py:
 # This script calls the requested steps in the blocking analysis for a forecast, observation, or both.
 #
-# parm/use_cases/model_applications/s2s/UserScript_obsERA_obsOnly_Blocking/Blocking.py:
+# metcalcpy/contributed/blocking_weather_regime/Blocking.py:
 # This script runs the requested steps, containing the code for computing CBLs, computing IBLs, computing GIBLs,
 # and computing blocks.
 #
-# parm/use_cases/model_applications/s2s/UserScript_obsERA_obsOnly_Blocking/Blocking_WeatherRegime_util.py:
+# metcalcpy/contributed/blocking_weather_regime/Blocking_WeatherRegime_util.py:
 # This script contains functions used by both the blocking anwd weather regime analysis, including the code for 
 # determining which steps the user wants to run, and finding and reading the input files in the format from the output
 # pre-processing steps
 #
 # .. highlight:: python
 # .. literalinclude:: ../../../../parm/use_cases/model_applications/s2s/UserScript_obsERA_obsOnly_Blocking/Blocking_driver.py
-# .. literalinclude:: ../../../../parm/use_cases/model_applications/s2s/UserScript_obsERA_obsOnly_Blocking/Blocking.py
-# .. literalinclude:: ../../../../parm/use_cases/model_applications/s2s/UserScript_obsERA_obsOnly_Blocking/Blocking_WeatherRegime_util.py
+# `Blocking Calculation <https://github.com/dtcenter/METcalcpy/blob/develop/metcalcpy/contributed/blocking_weather_regime/Blocking.py>`_
+# `Utility script <https://github.com/dtcenter/METcalcpy/blob/develop/metcalcpy/contributed/blocking_weather_regime/Blocking_WeatherRegime_util.py>`_
 #
 
 ##############################################################################
