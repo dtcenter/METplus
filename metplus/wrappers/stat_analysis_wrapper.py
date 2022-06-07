30e36e17fb704b34fe0e61dd5361a59b0e682264
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-06-07 13:59:38 -0600

    bugfix #1641 main_v4.1 StatAnalysis list format (#1647)

--- metplus/wrappers/stat_analysis_wrapper.py
+++ metplus/wrappers/stat_analysis_wrapper.py
@@ -1774,7 +1774,7 @@ class StatAnalysisWrapper(CommandBuilder):
                         'INTERP_PNTS',
                         'FCST_THRESH',
                         'OBS_THRESH',
-                        'CONV_THRESH',
+                        'COV_THRESH',
                         'ALPHA',
                         'LINE_TYPE'
                         ]
@@ -1793,9 +1793,6 @@ class StatAnalysisWrapper(CommandBuilder):
                         'FCST_INIT_END',
                         'OBS_INIT_BEG',
                         'OBS_INIT_END',
-                        'DESC',
-                        'OBTYPE',
-                        'FCST_LEAD'
                         ]
             for mp_item in mp_items:
                 if not runtime_settings_dict.get(mp_item, ''):
