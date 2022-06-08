bce462d7965d84d9899b9cceb2f74330cf001e1d
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-06-08 13:52:38 -0600

    added another unit test to check if generic is set to true but wrapper-specific is set to false

--- internal_tests/pytests/grid_stat/test_grid_stat_wrapper.py
+++ internal_tests/pytests/grid_stat/test_grid_stat_wrapper.py
@@ -77,6 +77,9 @@ def set_minimum_config_settings(config):
         # 7 generic FCST false, wrapper FCST true
         ({'FCST_IS_PROB': False, 'FCST_GRID_STAT_IS_PROB': True},
          {'FCST_IS_PROB': True, 'OBS_IS_PROB': False}),
+        # 8 generic FCST true, wrapper FCST false
+        ({'FCST_IS_PROB': True, 'FCST_GRID_STAT_IS_PROB': False},
+         {'FCST_IS_PROB': False, 'OBS_IS_PROB': False}),
     ]
 )
 def test_grid_stat_is_prob(metplus_config, config_overrides, expected_values):
