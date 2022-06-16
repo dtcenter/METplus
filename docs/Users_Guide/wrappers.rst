3ea66687ad02ca54b9691bb7f699ed295a88bc6a
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-06-16 14:08:57 -0600

    feature 1585 multi-variate MODE support (#1665)

--- docs/Users_Guide/wrappers.rst
+++ docs/Users_Guide/wrappers.rst
@@ -4110,6 +4110,7 @@ METplus Configuration
 | :term:`MODE_NC_PAIRS_FLAG_OBJECT_ID`
 | :term:`MODE_NC_PAIRS_FLAG_CLUSTER_ID`
 | :term:`MODE_NC_PAIRS_FLAG_POLYLINES`
+| :term:`MODE_MASK_MISSING_FLAG`
 | :term:`MODE_MATCH_FLAG`
 | :term:`MODE_MAX_CENTROID_DIST`
 | :term:`MODE_TOTAL_INTEREST_THRESH`
@@ -4120,6 +4121,7 @@ METplus Configuration
 | :term:`MODE_CT_STATS_FLAG`
 | :term:`FCST_MODE_IS_PROB`
 | :term:`FCST_MODE_PROB_IN_GRIB_PDS`
+| :term:`MODE_MULTIVAR_LOGIC`
 | :term:`FCST_MODE_VAR<n>_NAME` (optional)
 | :term:`FCST_MODE_VAR<n>_LEVELS` (optional)
 | :term:`FCST_MODE_VAR<n>_THRESH` (optional)
@@ -4231,6 +4233,17 @@ see :ref:`How METplus controls MET config file settings<metplus-control-met>`.
      - MET Config File
    * - :term:`MODE_QUILT`
      - quilt
+
+**${METPLUS_MULTIVAR_LOGIC}**
+
+.. list-table::
+   :widths: 5 5
+   :header-rows: 0
+
+   * - METplus Config(s)
+     - MET Config File
+   * - :term:`MODE_MULTIVAR_LOGIC`
+     - multivar_logic
 
 **${METPLUS_FCST_FIELD}**
 
@@ -4540,6 +4553,17 @@ see :ref:`How METplus controls MET config file settings<metplus-control-met>`.
      - mask.poly
    * - :term:`MODE_MASK_POLY_FLAG`
      - mask.poly_flag
+
+**${METPLUS_MASK_MISSING_FLAG}**
+
+.. list-table::
+   :widths: 5 5
+   :header-rows: 0
+
+   * - METplus Config(s)
+     - MET Config File
+   * - :term:`MODE_MASK_MISSING_FLAG`
+     - mask_missing_flag
 
 **${METPLUS_MATCH_FLAG}**
 
--- metplus/wrappers/compare_gridded_wrapper.py
+++ metplus/wrappers/compare_gridded_wrapper.py
@@ -237,7 +237,17 @@ that reformat gridded data
         if not model_path:
             return
 
-        self.infiles.extend(model_path)
+        # if there is more than 1 file, create file list file
+        if len(model_path) > 1:
+            list_filename = (f"{time_info['init_fmt']}_"
+                             f"{time_info['lead_hours']}_"
+                             f"{self.app_name}_fcst.txt")
+            model_path = self.write_list_file(list_filename, model_path)
+        else:
+            model_path = model_path[0]
+
+        self.infiles.append(model_path)
+
         # get observation to from first var compare
         obs_path, time_info = self.find_obs_offset(time_info,
                                                    var_list[0],
@@ -246,7 +256,16 @@ that reformat gridded data
         if obs_path is None:
             return
 
-        self.infiles.extend(obs_path)
+        # if there is more than 1 file, create file list file
+        if len(obs_path) > 1:
+            list_filename = (f"{time_info['init_fmt']}_"
+                             f"{time_info['lead_hours']}_"
+                             f"{self.app_name}_obs.txt")
+            obs_path = self.write_list_file(list_filename, obs_path)
+        else:
+            obs_path = obs_path[0]
+
+        self.infiles.append(obs_path)
 
         fcst_field_list = []
         obs_field_list = []
@@ -295,12 +314,6 @@ that reformat gridded data
 
         # set environment variables needed by MET config file
         self.set_environment_variables(time_info)
-
-        # check if METplus can generate the command successfully
-        cmd = self.get_command()
-        if cmd is None:
-            self.log_error("Could not generate command")
-            return
 
         # run the MET command
         self.build()
