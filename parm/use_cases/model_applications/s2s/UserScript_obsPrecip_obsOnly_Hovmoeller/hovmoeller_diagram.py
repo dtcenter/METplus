6d1efac4c62658a50eb7b14eae526a89368ee8a1
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-06-21 16:44:27 -0600

    removed commented code

--- parm/use_cases/model_applications/s2s/UserScript_obsPrecip_obsOnly_Hovmoeller/hovmoeller_diagram.py
+++ parm/use_cases/model_applications/s2s/UserScript_obsPrecip_obsOnly_Hovmoeller/hovmoeller_diagram.py
@@ -9,5 +9,4 @@ import metplotpy.plots.hovmoeller.hovmoeller as Hovmoeller
 
 if __name__ == '__main__':
     config_file = os.environ.get('YAML_CONFIG_NAME')
-#    config_file = os.path.join(os.path.dirname(__file__), "hovmoeller.yaml")
     Hovmoeller.main(config_file)
