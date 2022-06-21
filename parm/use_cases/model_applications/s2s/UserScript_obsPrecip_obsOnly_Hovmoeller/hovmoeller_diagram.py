463c1580f814ecfbf4c70e2a1086cf365cc49107
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-06-21 15:56:03 -0600

    use env var that contains path to yaml config file instead of hard-coded yaml filename relative to user script, ci-skip-unit-tests

--- parm/use_cases/model_applications/s2s/UserScript_obsPrecip_obsOnly_Hovmoeller/hovmoeller_diagram.py
+++ parm/use_cases/model_applications/s2s/UserScript_obsPrecip_obsOnly_Hovmoeller/hovmoeller_diagram.py
@@ -8,5 +8,6 @@ import os
 import metplotpy.plots.hovmoeller.hovmoeller as Hovmoeller
 
 if __name__ == '__main__':
-    config_file = os.path.join(os.path.dirname(__file__), "hovmoeller.yaml")
+    config_file = os.environ.get('YAML_CONFIG_NAME')
+#    config_file = os.path.join(os.path.dirname(__file__), "hovmoeller.yaml")
     Hovmoeller.main(config_file)
