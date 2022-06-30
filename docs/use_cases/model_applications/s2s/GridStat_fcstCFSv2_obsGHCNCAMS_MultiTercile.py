2b9de263fa3870b850a6a7bb397a1fe7412b547e
j-opatz <jopatz@ucar.edu>
2022-06-30 15:31:56 -0600

    first pass at documentation, config, and python scripts

---
+++ docs/use_cases/model_applications/s2s/GridStat_fcstCFSv2_obsGHCNCAMS_MultiTercile.py
@@ -0,0 +1,144 @@
+"""
+GridStat: Determine dominant ensemble members terciles and calculate categorical outputs
+========================================================================================
+
+model_applications/s2s/GridStat_fcstCFSv2_obsGHCNCAMS_MultiTercile.conf
+
+"""
+##############################################################################
+# Scientific Objective
+# --------------------
+# This use case ingests a CFSv2 Ensemble forecast, with all ensemble members in a single file for a given year. 
+# 29 years of forecast ensembles are used to create probabilities for each tercile, which is accomplished by a Python script. 
+# Of the terciles, each gridpoint is assigned a value corresponding to the tercile that is most likely to occur. This is compared to an observation set
+# that contains the tercile data and MCTS line type is requested.
+# This use case highlights the inclusion of tercile data for calculating HSS; in particular, how to utilize the hss_ec_value option to 
+# preset the expected values rather than relying on categorical values.
+
+##############################################################################
+# Datasets
+# ---------------------
+#
+# | **Forecast:** 29 CFSv2 Ensemble files, 2m temperature fields
+#
+# | **Observations:** GHCNCAMS, 2m temperature field
+#
+#
+# | **Location:** All of the input data required for this use case can be found in the met_test sample data tarball. Click here to the METplus releases page and download sample data for the appropriate release: https://github.com/dtcenter/METplus/releases
+# | This tarball should be unpacked into the directory that you will set the value of INPUT_BASE. See `Running METplus`_ section for more information.
+#
+# | **Data Source:** CPC
+
+##############################################################################
+# METplus Components
+# ------------------
+#
+# This use case calls a Python script 29 times, once for each year of data of the CFSv2 ensemble.
+# Each time a successful call to the script is made, a grid of 1s, 2s, and 3s is returned, representing which tercile was dominant for the gridpoint.
+# GridStat processes the forecast and observation fields, and outputs the requested line types.
+
+##############################################################################
+# METplus Workflow
+# ----------------
+#
+# This use case utilizes 29 years of forecast data, with 24 members in each ensemble forecast.
+# The following boundary times are used for the entire script:
+#
+# | **Init Beg:** 1982-01-01
+# | **Init End:** 2010-01-02
+# 
+# Because the increment is 1 year, all January 1st from 1982 to 2010 are processed for a total of 29 years.
+# 
+
+##############################################################################
+# METplus Configuration
+# ---------------------
+#
+# METplus first loads all of the configuration files found in parm/metplus_config,
+# then it loads any configuration files passed to METplus via the command line
+# i.e. -c parm/use_cases/model_applications/s2s/GridStat_fcstCFSv2_obsGHCNCAMS_MultiTercile.conf
+#
+# .. highlight:: bash
+# .. literalinclude:: ../../../../parm/use_cases/model_applications/s2s/GridStat_fcstCFSv2_obsGHCNCAMS_MultiTercile.conf
+#
+
+##############################################################################
+# MET Configuration
+# ---------------------
+#
+# METplus sets environment variables based on the values in the METplus configuration file. These variables are referenced in the MET configuration file. **YOU SHOULD NOT SET ANY OF THESE ENVIRONMENT VARIABLES YOURSELF! THEY WILL BE OVERWRITTEN BY METPLUS WHEN IT CALLS THE MET TOOLS!** If there is a setting in the MET configuration file that is not controlled by an environment variable, you can add additional environment variables to be set only within the METplus environment using the [user_env_vars] section of the METplus configuration files. See the ‘User Defined Config’ section on the ‘System Configuration’ page of the METplus User’s Guide for more information.
+#
+# .. highlight:: bash
+# .. literalinclude:: ../../../../parm/met_config/GridStatConfig_wrapped
+#
+
+##############################################################################
+# Running METplus
+# ---------------
+#
+# This use case can be run two ways:
+#
+# 1) Passing in GridStat_fcstCFSv2_obsGHCNCAMS_MultiTercile.conf then a user-specific system configuration file::
+#
+#        run_metplus.py /path/to/METplus/parm/use_cases/model_applications/s2s/GridStat_fcstCFSv2_obsGHCNCAMS_MultiTercile /path/to/user_system.conf
+#
+# 2) Modifying the configurations in parm/metplus_config, then passing in GridStat_fcstCFSv2_obsGHCNCAMS_MultiTercile::
+#
+#        run_metplus.py /path/to/METplus/parm/use_cases/model_applications/marine_and_cryosphere/GridStat_fcstCFSv2_obsGHCNCAMS_MultiTercile.conf
+#
+# The former method is recommended. Whether you add them to a user-specific configuration file or modify the metplus_config files, the following variables must be set correctly:
+#
+# * **INPUT_BASE** - Path to directory where sample data tarballs are unpacked (See Datasets section to obtain tarballs). This is not required to run METplus, but it is required to run the examples in parm/use_cases
+# * **OUTPUT_BASE** - Path where METplus output will be written. This must be in a location where you have write permissions
+# * **MET_INSTALL_DIR** - Path to location where MET is installed locally
+#
+# Example User Configuration File::
+#
+#   [config]
+#   INPUT_BASE = /path/to/sample/input/data
+#   OUTPUT_BASE = /path/to/output/dir
+#   MET_INSTALL_DIR = /path/to/met-X.Y 
+#
+#
+
+##############################################################################
+# Expected Output
+# ---------------
+#
+# A successful run will output the following both to the screen and to the logfile::
+#
+#   INFO: METplus has successfully finished running.
+#
+# Refer to the value set for **OUTPUT_BASE** to find where the output data was generated.
+# Output for the use case will be found in 29 folders(relative to **OUTPUT_BASE**).
+# The output will follow the time information of the run. Specifically:
+#
+# * YYYY01
+#
+# where YYYY will be replaced by values corresponding to each of the years (1982 through 2010).
+# Each of those folders will have the following files:
+#
+# * grid_stat_000000L_19820101_000000V_pairs.nc
+# * grid_stat_000000L_19820101_000000V_mctc.txt
+# * grid_stat_000000L_19820101_000000V_mcts.txt
+# * grid_stat_000000L_19820101_000000V.stat
+#
+
+##############################################################################
+# Keywords
+# --------
+#
+# .. note::
+#
+#   * GridStatUseCase
+#   * ProbabilityVerificationUseCase
+#   * PythonEmbeddingFileUseCase
+#   * S2SAppUseCase
+#   * NETCDFFileUseCase
+#
+#   Navigate to the :ref:`quick-search` page to discover other similar use cases.
+#
+#
+#
+# sphinx_gallery_thumbnail_path = '_static/s2s-GridStat_fcstCFSv2_obsGHCNCAMS_MultiTercile.png'
+
---
+++ parm/use_cases/model_applications/s2s/GridStat_fcstCFSv2_obsGHCNCAMS_MultiTercile/forecast_read-in_CFSv2_categoricalthresholds_obs.py
@@ -0,0 +1,186 @@
+
+import sys
+import re
+import numpy as np
+import datetime as dt
+from dateutil.relativedelta import *    # New import
+from netCDF4 import Dataset, chartostring
+from preprocessFun_Modified import preprocess, dominant_tercile_fcst, dominant_tercile_obs, get_init_year # New import
+
+#grab input from user
+#should be (1)input file using full path (2) variable name (3) valid time for the forecast in %Y%m%d%H%M format and (4) ensemble member number, all separated by ':' characters
+#program can only accept that 1 input, while still maintaining user flexability to change multiple
+#variables, including valid time, ens member, etc.
+# Addition by Johnna - model, lead, init taken from file name
+# Addition by Johnna - climatology and std dev caclulated for given lead, model, init
+# Addition by Johnna - added command line pull for lead
+
+print('Type in input_file using full path:variable name:valid time in %Y%m%d%H%M:ensemble member number:lead')
+# Example
+# /cpc/nmme/CFSv2/hcst_new/010100/CFSv2.tmp2m.198201.fcst.nc:tmp2m:198201010101:0:0
+
+input_file, var_name, init_time, ens_mem, lead = sys.argv[1].split(':')
+ens_mem = int(ens_mem)
+lead    = int(lead)    # Added by Johnna
+init_time = dt.datetime.strptime(init_time,"%Y%m%d%H%M")   
+
+# ---
+# Added by Johnna
+input_file_split = input_file.split('/')
+#EDIT BY JOHN O
+init_temp = "010100"
+#init_temp = input_file_split[5]
+
+#fil_name = input_file_split[6]
+fil_name = input_file
+year_temp = fil_name.split('.')
+#year = year_temp[2]
+year = year_temp[-3]
+print('YYYYMM: ' + str(year))
+
+# Setup Climatology
+#EDIT BY JOHN O
+#model = input_file_split[3]
+model = "CFSv2"
+init  = init_temp.replace('0100','')
+lead  = lead
+clim_per = '1982_2010'
+member = ens_mem
+variable = var_name
+
+# Get path based on model (only works for this file name)
+input_file_split2 = input_file.split(model + '.')
+path = input_file_split2[0]
+
+# Calculate climatologies and standard deviations/etc.  This calculates every time the loop runs (i.e. for each file read into MET)
+# There might be a better positioning for this function such that it doesn't recalc each loop, but I pulled from the command line input 
+# above to create the function, so right now it has dependance on the file names/user input/etc.
+# Fine for now since data are smallish, but if theres something higher res might slow things down.
+print('Model: ' + model + ' Init: ' + str(init) + ' lead: ' + str(lead) + ' Member: ' + str(ens_mem) + ' Variable: ' + variable)
+
+# We only need the fcst_cat_thresh
+# Commenting out the original preprocess function
+#clim, stddev, anom, std_anom = preprocess(path, model, init, variable, lead, clim_per, member)
+
+# New preprocessing function to get the cat thresholds
+# In order 0, 1, 2 where 0 is LT, 1 is MT, 2 is UT
+# fcst_cat_thresh has ALL times (28), and is calculated for ALL 24 members
+# So the array fcst_cat_thresh is time | 29, lat | 181, lon | 360)
+# I wrote a clumsy function to split this into years based on the filename read in (get_init_year)
+# But it works!  Basically just a bunch of if statements like if the filename is 198201 then its index 0 of the array and so on to the last index
+# I also swap the fcst lats to be -90 to 90 instead of 90 to -90 and match up the longitudes (theres a cyclic point in the fcst so its actually 361 pts)
+fcst_cat_thresh  = dominant_tercile_fcst(path, model, init, variable, clim_per, lead)
+idx = get_init_year(year)
+fcst_cat_thresh_1year = fcst_cat_thresh[idx,::-1,0:360]
+
+# Going to do the obs in the same wrapper. I realized this would be easier so I hope this is okay...  I think its just a flag...?
+# Using same clunky function to get the right year out of the big array
+#EDIT BY JOHN O
+obs_cat_thresh  = dominant_tercile_obs(path)
+obs_cat_thresh_1year = obs_cat_thresh[idx,:,0:360]
+
+# Redefine var_name to fcst (necessary for below)
+var_name = 'fcst'
+# ---
+
+try:
+    print('The file you are working on is: ' + input_file)
+    # all of this is actually pointless eexcept to get the dimensions and times, all of the calculations are done in the functions
+    #set pointers to file and group name in file
+
+    f = Dataset(input_file)
+    v = f[var_name][member,lead,:,:]
+
+    #grab data from file
+    lat = np.float64(f.variables['lat'][::-1])
+    lon = np.float64(f.variables['lon'][:])
+    
+    # Grab and format time, this is taken from the file name, which might not be the best way of doing it
+    # Can also potentially pull from the netCDF; but need an extra package in netCDF4 to do that, and its a little weird
+    # given units of months since.  This was a bit easier.
+    # Do need to add relativedelta package but thats fairly common (its from dateutil)
+    val_time = init_time + relativedelta(months=lead)
+    print('Valid Time: ' + str(val_time))
+
+
+    # Coming from the function
+    # uncomment out the obs one if you want to use the obs?    
+    #v = fcst_cat_thresh_1year
+    v = obs_cat_thresh_1year
+    print('Shape of variable to read into MET: ' + str(v.shape)) 
+
+    # --------------------------
+    # Commented out by Johnna, defined above in user input
+    # Print statement erroring so commented out
+    #grab intialization time from file name and hold
+    #also compute the lead time
+    #i_time_ind = input_file.split("_").index("aod.nc")-1
+    #i_time = input_file.split("_")[i_time_ind]
+    #i_time_obj = dt.datetime.strptime(i_time,"%Y%m%d%H")
+    #lead, rem = divmod((val_time - i_time_obj).total_seconds(), 3600) 
+
+    #print("Ensemble Member evaluation for: "+f.members.split(',')[ens_mem])
+
+    #checks if the the valid time for the forecast from user is present in file.
+    #Exits if the time is not present with a message
+    #if not val_time.timestamp() in f['time'][:]:
+    #        print("valid time of "+str(val_time)+" is not present. Check file initialization time, passed valid time.")
+    #        f.close()
+    #        sys.exit(1)
+
+    #grab index in the time array for the valid time provided by user (val_time)
+    #val_time_ind = np.where(f['time'][:] == val_time.timestamp())[0][0]
+    #var = np.float64(v[val_time_ind:val_time_ind+1,ens_mem:ens_mem+1,::-1,:])
+    # --------------------------
+    
+    #squeeze out all 1d arrays, add fill value, convert to float64
+    var = np.float64(v)  
+    var[var < -800] = -9999
+
+    met_data = np.squeeze(var).copy()
+    #JOHN ADDED TO REMOVE EXTRA LON
+    #met_data = met_data[:,:-1]
+    #JOHN O ADDED TO FLIP OBS GRID
+    met_data = met_data[::-1,:]
+    met_data = np.nan_to_num(met_data, nan=-1)
+    print('Done, no exceptions')
+    print('New shape of variable to read into MET: ' + str(met_data.shape)) 
+
+except NameError:
+    print("Can't find input file")
+    sys.exit(1)
+
+##########
+#create a metadata dictionary
+
+attrs = {
+
+        'valid': str(val_time.strftime("%Y%m%d"))+'_'+str(val_time.strftime("%H%M%S")),
+        'init': str(init_time.strftime("%Y%m%d"))+'_'+str(init_time.strftime("%H%M%S")),
+        'name': var_name,
+        'long_name': input_file,
+        'lead': str(int(lead)),
+        'accum': '00',
+        'level': 'sfc',
+        'units': 'Degrees K',
+
+        'grid': {
+            'name': 'Global 1 degree',
+            'type': 'LatLon',
+            'lat_ll': -90.0,
+            'lon_ll': 0.0,
+            'delta_lat': 1.0,
+            'delta_lon': 1.0,
+
+            'Nlon': 360,
+            'Nlat': f.dimensions['lat'].size,
+            }
+        }
+
+#print some output to show script ran successfully
+print("Input file: " + repr(input_file))
+print("Variable name: " + repr(var_name))
+print("valid time: " + repr(val_time.strftime("%Y%m%d%H%M")))
+print("Attributes:\t" + repr(attrs))
+f.close()
+
