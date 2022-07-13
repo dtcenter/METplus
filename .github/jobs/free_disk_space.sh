8ebcf4dbbbc5afe1a8fc973707dec374e2e14962
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-07-13 10:23:10 -0600

    attempt to free up unused disk space in GHA runner environment

---
+++ .github/jobs/free_disk_space.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo Checking disk usage before cleanup
+df -h
+
+echo Removing files as suggested by https://github.com/actions/virtual-environments/issues/2840
+
+sudo rm -rf /usr/share/dotnet
+sudo rm -rf /opt/ghc
+sudo rm -rf "/usr/local/share/boost"
+sudo rm -rf "$AGENT_TOOLSDIRECTORY"
+
+echo Checking disk usage after cleanup
+df -h
