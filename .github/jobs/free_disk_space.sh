e669be2f09f0975a439f927b235411509bea652f
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-07-13 18:18:26 -0600

    add back commands to free up disk space because issue with METdbLoad use case was likely not related, ci-skip-unit-tests

--- .github/jobs/free_disk_space.sh
+++ .github/jobs/free_disk_space.sh
@@ -5,10 +5,10 @@ df -h
 
 echo Removing files as suggested by https://github.com/actions/virtual-environments/issues/2840
 
-#sudo rm -rf /usr/share/dotnet
-#sudo rm -rf /opt/ghc
-#sudo rm -rf "/usr/local/share/boost"
-#sudo rm -rf "$AGENT_TOOLSDIRECTORY"
+sudo rm -rf /usr/share/dotnet
+sudo rm -rf /opt/ghc
+sudo rm -rf "/usr/local/share/boost"
+sudo rm -rf "$AGENT_TOOLSDIRECTORY"
 
 echo Checking disk usage after cleanup
 df -h
