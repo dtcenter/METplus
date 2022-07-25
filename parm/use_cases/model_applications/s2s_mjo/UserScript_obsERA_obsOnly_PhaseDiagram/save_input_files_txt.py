b3a1fd7667a082b79c67f5b2da0ea617a032582a
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-07-25 14:40:17 -0600

    per #1706, create output directory if it does not already exist because the file i/o logic will fail if the directory does not exist

--- parm/use_cases/model_applications/s2s_mjo/UserScript_obsERA_obsOnly_PhaseDiagram/save_input_files_txt.py
+++ parm/use_cases/model_applications/s2s_mjo/UserScript_obsERA_obsOnly_PhaseDiagram/save_input_files_txt.py
@@ -6,6 +6,11 @@ import sys
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
+output_dir = os.path.dirname(output_file)
+if not os.path.exists(output_dir):
+    print(f'Creating output dir: {output_dir}')
+    os.makedirs(output_dir)
+
 filelist = open(output_file,'a+')
 filelist.write(input_file + '\n')
 filelist.close()
