2f234daf1959e0f9370c04b8d4aa689e0380a493
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-07-26 15:00:20 -0600

    Update Develop-ref after #1710 (#1714)
    Co-authored-by: George McCabe <23407799+georgemccabe@users.noreply.github.com>
    Co-authored-by: Mrinal Biswas <biswas@ucar.edu>
    Co-authored-by: Hank Fisher <fisherh@kiowa.rap.ucar.edu>
    Co-authored-by: Minna Win <minnawin@kiowa.rap.ucar.edu>
    Co-authored-by: bikegeek <3753118+bikegeek@users.noreply.github.com>
    Co-authored-by: Christina Kalb <kalb@kiowa.rap.ucar.edu>
    Co-authored-by: Julie Prestopnik <jpresto@seneca.rap.ucar.edu>
    Co-authored-by: johnhg <johnhg@ucar.edu>
    Co-authored-by: Hank Fisher <fisherh@seneca.rap.ucar.edu>
    Co-authored-by: Mrinal Biswas <biswas@seneca.rap.ucar.edu>
    Co-authored-by: jprestop <jpresto@ucar.edu>
    Co-authored-by: j-opatz <jopatz@ucar.edu>
    Co-authored-by: Kathryn Newman <knewman@seneca.rap.ucar.edu>
    Co-authored-by: j-opatz <59586397+j-opatz@users.noreply.github.com>
    Co-authored-by: Lisa Goodrich <lisag@ucar.edu>
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>
    Co-authored-by: Christina Kalb <kalb@mohawk.rap.ucar.edu>
    Co-authored-by: bikegeek <minnawin@ucar.edu>
    Co-authored-by: Hank Fisher <fisherh@ucar.edu>
    Co-authored-by: Christina Kalb <kalb@ucar.edu>
    Co-authored-by: lisagoodrich <33230218+lisagoodrich@users.noreply.github.com>
    Co-authored-by: Howard Soh <hsoh@seneca.rap.ucar.edu>
    Co-authored-by: Molly Smith <molly.b.smith@noaa.gov>
    Co-authored-by: hsoh-u <hsoh@ucar.edu>

--- scripts/docker/docker_env/scripts/cycloneplotter_env.sh
+++ scripts/docker/docker_env/scripts/cycloneplotter_env.sh
@@ -2,27 +2,27 @@
 
 ################################################################################
 # Environment: cycloneplotter
-# Last Updated: 2021-06-09 (mccabe@ucar.edu)
+# Last Updated: 2022-07-08 (mccabe@ucar.edu)
 # Notes: Adds packages needed to run CyclonePlotter wrapper
 #   Added pandas because it is used by tc_and_extra_tc use case
 # Python Packages:
-#   cartopy==0.17.0
-#   matplotlib==3.3.0
-#   pandas==?
+#   cartopy==0.20.3
+#   matplotlib==3.5.2
+#   pandas==1.4.3
 #
 # Other Content: None
 ################################################################################
 
 # Conda environment to create
-ENV_NAME=cycloneplotter
+ENV_NAME=cycloneplotter.v5
 
 # Conda environment to use as base for new environment
-BASE_ENV=$1
+BASE_ENV=metplus_base.v5
 
 conda create -y --clone ${BASE_ENV} --name ${ENV_NAME}
-conda install -y --name ${ENV_NAME} -c conda-forge cartopy==0.17.0
-conda install -y --name ${ENV_NAME} -c conda-forge matplotlib==3.3.0
-conda install -y --name ${ENV_NAME} -c conda-forge pandas
+conda install -y --name ${ENV_NAME} -c conda-forge cartopy==0.20.3
+conda install -y --name ${ENV_NAME} -c conda-forge matplotlib==3.5.2
+conda install -y --name ${ENV_NAME} -c conda-forge pandas==1.4.3
 
 yum -y install wget
 wget https://raw.githubusercontent.com/SciTools/cartopy/master/tools/cartopy_feature_download.py
--- scripts/docker/docker_env/scripts/diff_env.sh
+++ scripts/docker/docker_env/scripts/diff_env.sh
@@ -1,27 +1,27 @@
 #! /bin/sh
 
 ################################################################################
-# Environment: diff
-# Last Updated: 2021-06-08 (mccabe@ucar.edu)
+# Environment: diff.v5
+# Last Updated: 2022-07-08 (mccabe@ucar.edu)
 # Notes: Adds packages needed to run differences tests to compare output to
 #   truth data.
 # Python Packages:
-#   pillow==?
-#   pdf2image==?
+#   pillow==9.2.0
+#   pdf2image==1.16.0
 #
 # Other Content:
 #   poppler-utils
 ################################################################################
 
 # Conda environment to create
-ENV_NAME=diff
+ENV_NAME=diff.v5
 
 # Conda environment to use as base for new environment
-BASE_ENV=$1
+BASE_ENV=netcdf4.v5
 
 conda create -y --clone ${BASE_ENV} --name ${ENV_NAME}
-conda install -y --name ${ENV_NAME} -c conda-forge pillow
+conda install -y --name ${ENV_NAME} -c conda-forge pillow==9.2.0
 
 yum -y install poppler-utils
 
-conda install -y --name ${ENV_NAME} -c conda-forge pdf2image
+conda install -y --name ${ENV_NAME} -c conda-forge pdf2image==1.16.0
