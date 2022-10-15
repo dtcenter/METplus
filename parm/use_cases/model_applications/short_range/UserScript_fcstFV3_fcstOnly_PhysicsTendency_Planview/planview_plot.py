f2775ec5d8c41c991cac9505c171fb6bb86692a2
bikegeek <minnawin@ucar.edu>
2022-10-15 17:58:01 -0600

    issue #1852 cleared away unused/unneccessary code

--- parm/use_cases/model_applications/short_range/UserScript_fcstFV3_fcstOnly_PhysicsTendency_Planview/planview_plot.py
+++ parm/use_cases/model_applications/short_range/UserScript_fcstFV3_fcstOnly_PhysicsTendency_Planview/planview_plot.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 import os
 import sys
 import yaml
+from metplotpy.contributed.fv3_physics_tend import planview_fv3
 
 '''
 Generate the plan view plot.  
@@ -10,27 +13,6 @@ the planview_fv3.py file
 '''
 
 
-
-def run_example(config_file):
-    '''Run the example in the user's guide'''
-    config = open_config(config_file)
-    command_str = "python " + config['source_dir'] + "/planview_fv3.py " + " ./fv3_physics_tend_defaults.yaml " + \
-                  config['history_file'] + " " + config[
-                      'grid_file'] + " tmp pbl -p 500 -t 1 -v 20190504T14 --nofineprint "
-
-    print("command string: ", command_str)
-    os.system(command_str)
-
-
-def open_config(config_file):
-    with open(config_file, 'r') as stream:
-        try:
-            config_obj = yaml.load(stream, Loader=yaml.FullLoader)
-        except yaml.YAMLError as exc:
-            print(exc)
-    return config_obj
-
-
 if __name__ == "__main__":
-    config_file = sys.argv[1]
-    run_example(config_file)
+    planview_fv3.main()
+
