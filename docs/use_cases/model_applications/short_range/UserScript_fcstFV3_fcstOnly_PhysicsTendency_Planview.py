f9a5da8976b5a5a93a3411ecc9e4394c9e3f8a79
bikegeek <minnawin@ucar.edu>
2022-10-15 18:45:36 -0600

    Issue #1852 renamed to include fcstOnly label

---
+++ docs/use_cases/model_applications/short_range/UserScript_fcstFV3_fcstOnly_PhysicsTendency_Planview.py
@@ -0,0 +1,137 @@
+"""
+UserScript: Physics Tendency Planview plot
+=========================================================================
+
+model_applications/
+short_range/
+UserScript_fcstFV3_PhysicsTendency_Planview.conf
+
+"""
+##############################################################################
+# Scientific Objective
+# --------------------
+#
+# To plot tendencies of temperature, moisture, and wind components averaged over 
+# a time window and spatial domain.  Tendencies are partitioned into physics 
+# parameterizations and dynamics.  Physics parameterizations include schemes like 
+# deep convection, convective gravity wave drag, short wave radiatioin, plantetary
+# boundary layer, microphysics, and others.  Non-physics tendencies (or dynamics)
+# are due to horizontal and vertical motion.  The residual (which should be zero)
+# is the difference between the actual change in the state variable over the requested
+# time window and the expected change due to physics parameterizations and dynamics
+# tendencies.  One can plpt a single tendency component at multiple pressure levels or
+# plot all tendency components at a single pressure level.  This use case illustrates 
+# how to generate plan views (horizontal cross sections).
+
+##############################################################################
+# Datasets
+# --------
+#
+#  * Forecast dataset: FV3 Model member data
+#  * Grid specification: Grid specification data 
+#
+
+##############################################################################
+# METplus Components
+# ------------------
+#
+# This use case runs the METplotpy planview_fv3.py script to generate the plan views. 
+
+##############################################################################
+# METplus Workflow
+# ----------------
+#
+#
+# | **Valid:** 2019-05-04
+# |
+# |
+#
+# |
+
+##############################################################################
+# METplus Configuration
+# ---------------------
+#
+# METplus first loads all of the configuration files found in parm/metplus_config,
+# then it loads any configuration files passed to METplus via the command line
+# with the -c option, i.e. -c parm/use_cases/model_applications/short_range/UserScript_fcstFV3_PhysicsTendency_Planview.conf
+#
+# .. highlight:: bash
+# .. literalinclude:: ../../../../parm/use_cases/model_applications/short_range/UserScript_fcstFV3__fcstOnly_PhysicsTendency_Planview.conf
+
+##############################################################################
+# MET Configuration
+# ---------------------
+#
+# No MET tools are used in this use case.
+#
+# METplus sets environment variables based on user settings in the METplus configuration file. 
+# See :ref:`How METplus controls MET config file settings<metplus-control-met>` for more details. 
+#
+# .. note:: See the :ref:`MODE MET Configuration<mode-met-conf>` section of the User's Guide for more information on the environment variables used in the file below:
+#
+
+##############################################################################
+# Running METplus
+# ---------------
+#
+# This use case can be run in the following way: 
+#
+# 1) Passing in UserScript_fcstFV3_fcstOnly_PhysicsTendency_Planview.conf then a user-specific system configuration file::
+#
+#        run_metplus.py -c /path/to/METplus/parm/use_cases/model_applications/short_range/UserScript_fcstFV3_fcstOnly_PhysicsTendency_Planview.conf  /path/to/user_system.conf
+#
+# 2) Modifying the configurations in parm/metplus_config, then passing in UserScript_fcstFV3_fcstOnly_PhysicsTendency_Planview.conf::
+#
+#        run_metplus.py  /path/to/METplus/parm/use_cases/model_applications/short_range/UserScript_fcstFV3_fcstOnly_PhysicsTendency_Planview.conf
+#
+# The former method is recommended. Whether you add them to a user-specific configuration file or modify the metplus_config files, the following variables must be set correctly:
+#
+# * **INPUT_BASE** - Path to directory where sample data tarballs are unpacked (See Datasets section to obtain tarballs). This is not required to run METplus, but it is required to run the examples in parm/use_cases
+# * **OUTPUT_BASE** - Path where METplus output will be written. This must be in a location where you have write permissions
+# * **MET_INSTALL_DIR** - Path to location where MET is installed locally
+#
+# Example User Configuration File::
+#
+#   [dir]
+#   INPUT_BASE = /path/to/sample/input/data
+#   OUTPUT_BASE = /path/to/output/dir
+#   MET_INSTALL_DIR = /path/to/met-X.Y 
+#
+# **NOTE:** All of these items must be found under the [dir] section.
+#
+
+##############################################################################
+# Expected Output
+# ---------------
+#
+# A successful run will output the following both to the screen and to the logfile::
+#
+#   INFO: METplus has successfully finished running.
+#
+# Refer to the value set for **OUTPUT_BASE** to find where the output data was generated.
+# Output for this use case will be found in short_range/brightness_temperature
+# (relative to **OUTPUT_BASE**) and will contain the following files:
+#
+# fvs_history.nc
+# grid_spec.nc
+
+
+##############################################################################
+# Keywords
+# --------
+#
+#
+# .. note::
+#
+#   * MediumRangeAppUseCase
+#   * PhysicsTendency
+#   * ValidationUseCase
+#   * ShortRangeAppUseCase
+#
+#   Navigate to the :ref:`quick-search` page to discover other similar use cases.
+#
+#
+#
+# sphinx_gallery_thumbnail_path = '_static/short_range-MODE_fcstFV3_obsGOES_BrightnessTemp.png'
+#
