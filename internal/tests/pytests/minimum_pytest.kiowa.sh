beedf87aafd0eb1af160e0db3fa59d465abdd2cb
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-10-21 09:43:54 -0600

    use latest release of MET for tests

--- internal/tests/pytests/minimum_pytest.kiowa.sh
+++ internal/tests/pytests/minimum_pytest.kiowa.sh
@@ -1,5 +1,5 @@
 export METPLUS_TEST_INPUT_BASE=/d1/projects/METplus/METplus_Data
 export METPLUS_TEST_OUTPUT_BASE=/d1/personal/${USER}/pytest
-export METPLUS_TEST_MET_INSTALL_DIR=/usr/local/met-9.0
+export METPLUS_TEST_MET_INSTALL_DIR=/usr/local/met
 #export METPLUS_TEST_MET_INSTALL_DIR=/d1/projects/MET/MET_releases/met-9.0_beta4
 export METPLUS_TEST_TMP_DIR=${METPLUS_TEST_OUTPUT_BASE}/tmp
