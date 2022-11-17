9811d5adf5fec36b9f22f439fd0ca6d26f051f02
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-11-17 15:47:38 -0700

    Feature #1566 update Python version to 3.8.6 (#1958)

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ with open("README.md", "r") as fh:
 with open("metplus/VERSION", "r") as fh:
     version = fh.read().strip()
 
-with open("metplus/PYTHON_VERSION", "r") as fh:
-    python_version = fh.read().strip()
+with open("metplus/PYTHON_VERSION_MIN", "r") as fh:
+    python_version_req = fh.read().strip()
 
 # get list of additional files needed to add to package
 data_files = []
@@ -18,6 +18,7 @@ data_files.append(('metplus',
                   ['metplus/VERSION',
                    'metplus/RELEASE_DATE',
                    'metplus/PYTHON_VERSION',
+                   'metplus/PYTHON_VERSION_MIN',
                   ]))
 
 for root, _, files in os.walk('parm'):
@@ -47,7 +48,7 @@ setup(
          "License :: OSI Approved :: MIT License", 
          "Operating System :: OS Independent",
     ],
-    python_requires=f'>={python_version}',
+    python_requires=f'>={python_version_req}',
     data_files=data_files,
     zip_safe=False,
 )
