eb065aa7e35bb0764e43af6bc18da97ea993c87c
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-12-09 15:47:19 -0700

    rotate author list

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,6 @@ author = 'UCAR/NCAR, NOAA, CSU/CIRA, and CU/CIRES'
 # To rotate this list of authors for each official release,
 # move the first author to the end of the list
 CURRENT_AUTHORS = [
-    'George McCabe',
     'Julie Prestopnik',
     'John Opatz',
     'John Halley Gotway',
@@ -42,6 +41,7 @@ CURRENT_AUTHORS = [
     'Lisa Goodrich',
     'Dan Adriaansen',
     'Minna Win-Gildenmeister',
+    'George McCabe',
 ]
 
 # list of former contributors who are no longer involved with the project
