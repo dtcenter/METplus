806f7689422bf4c487d2f074c57c20be95ca2f79
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-12-15 17:10:44 -0700

    added GHA log groups for all commands run through time_command, moved TIMING info after ERROR info to match other command order

--- .github/jobs/bash_functions.sh
+++ .github/jobs/bash_functions.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
 
 # utility function to run command get log the time it took to run
+# ::group:: and ::endgroup:: create collapsible log groups in GitHub Actions
 function time_command {
   local start_seconds=$SECONDS
-  echo "RUNNING: $*"
+  echo "::group::RUNNING: $*"
   "$@"
   local error=$?
+  echo "::endgroup::"
 
-  local duration=$(( SECONDS - start_seconds ))
-  echo "TIMING: Command took `printf '%02d' $(($duration / 60))`:`printf '%02d' $(($duration % 60))` (MM:SS): '$*'"
   if [ ${error} -ne 0 ]; then
     echo "ERROR: '$*' exited with status = ${error}"
   fi
+
+  local duration=$(( SECONDS - start_seconds ))
+  echo "TIMING: Command took `printf '%02d' $(($duration / 60))`:`printf '%02d' $(($duration % 60))` (MM:SS): '$*'"
+
   return $error
 }
