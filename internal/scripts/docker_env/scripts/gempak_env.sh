07824ce68a9ff466d9f22bfb38fc55715ccd93af
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2023-02-09 12:46:29 -0700

    Feature #2005 updates to create v5.1 conda environments that use debian10 and Python 3.10.4 (#2043)

--- internal/scripts/docker_env/scripts/gempak_env.sh
+++ internal/scripts/docker_env/scripts/gempak_env.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 ################################################################################
-# Environment: gempak.v5
-# Last Updated: 2021-07-08 (mccabe@ucar.edu)
+# Environment: gempak.v5.1
+# Last Updated: 2023-02-03 (mccabe@ucar.edu)
 # Notes: Installs Java and obtains GempakToCF.jar to convert GEMPAK
 #   files to NetCDF format.
 # Python Packages: None
@@ -12,7 +12,13 @@
 #   - GempakToCF.jar (downloaded from DTCenter web server
 ################################################################################
 
-yum -y update
-yum -y install java-1.8.0-openjdk
+apt update
+apt -y upgrade
+apt install -y apt-transport-https ca-certificates wget dirmngr gnupg software-properties-common
+wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | apt-key add -
+add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
+apt update
+apt install -y adoptopenjdk-8-hotspot
+apt install -y curl
 mkdir -p /data/input
 curl -L -o /data/input/GempakToCF.jar -O https://dtcenter.org/sites/default/files/community-code/metplus/utilities/GempakToCF.jar
--- internal/scripts/docker_env/scripts/spacetime_env.sh
+++ internal/scripts/docker_env/scripts/spacetime_env.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 ################################################################################
-# Environment: spacetime
-# Last Updated: 2022-06-16 (mccabe@ucar.edu)
+# Environment: spacetime.v5.1
+# Last Updated: 2023-01-27 (mccabe@ucar.edu)
 # Notes: Adds Python packages needed to generate coherence spectra (METplotpy)
 # Python Packages:
 #   netCDF4==1.5.8
@@ -15,10 +15,13 @@
 # Other Content: None
 ################################################################################
 
-# Conda environment to create
-ENV_NAME=spacetime.v5
+# version of METplus when the environment was updated, e.g. v5.1
+METPLUS_VERSION=$1
 
-conda create -y --name ${ENV_NAME} -c conda-forge python=3.8.6
+# Conda environment to create
+ENV_NAME=spacetime.${METPLUS_VERSION}
+
+conda create -y --name ${ENV_NAME} -c conda-forge python=3.10.4
 
 conda install -y --name ${ENV_NAME} -c conda-forge netCDF4==1.5.8
 conda install -y --name ${ENV_NAME} -c conda-forge xarray==2022.3.0
