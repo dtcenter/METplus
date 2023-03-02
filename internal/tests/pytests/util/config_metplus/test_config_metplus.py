e9658facd043617e837fd9467a5954018a1ec690
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2023-03-02 13:23:17 -0700

    fix comment

--- internal/tests/pytests/util/config_metplus/test_config_metplus.py
+++ internal/tests/pytests/util/config_metplus/test_config_metplus.py
@@ -918,7 +918,7 @@ def test_format_var_items_options_semicolon(config_value,
 @pytest.mark.util
 def test_parse_var_list_double_digit(metplus_config):
     """!This test ensures that parse_var_list returns field info in
-    numeric order (1,2,...9,10,11) instead of alphabetical (1,10,11,2,3,etc)
+    numeric order (1,2,...,9,10,11) instead of alphabetical (1,10,11,2,3,etc)
     """
     config = metplus_config
     for n in range(1, 12, 1):
