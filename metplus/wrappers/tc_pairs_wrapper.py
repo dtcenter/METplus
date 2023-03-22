a8e8062668b912696081d6d11410ac4fb524f66c
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2023-03-22 14:52:48 -0600

    update TCPairs wrapper to use new add_x functionality to set diag_convert_map.convert

--- metplus/wrappers/tc_pairs_wrapper.py
+++ metplus/wrappers/tc_pairs_wrapper.py
@@ -439,7 +439,7 @@ class TCPairsWrapper(CommandBuilder):
         dict_items = {
             'diag_source': 'string',
             'key': 'list',
-            'convert': ('string', 'remove_quotes'),
+            'convert': ('string', 'remove_quotes,add_x'),
         }
         return_code = add_met_config_dict_list(config=self.config,
                                                app_name=self.app_name,
@@ -449,11 +449,6 @@ class TCPairsWrapper(CommandBuilder):
         if not return_code:
             self.isOK = False
             return
-
-        # change "convert =" to "convert(x) ="
-        value = self.env_var_dict.get('METPLUS_DIAG_CONVERT_MAP_LIST', '')
-        value = value.replace('convert =', 'convert(x) =')
-        self.env_var_dict['METPLUS_DIAG_CONVERT_MAP_LIST'] = value
 
     def _handle_diag(self, c_dict):
         diag_indices = list(
