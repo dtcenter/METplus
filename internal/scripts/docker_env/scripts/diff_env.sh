52f5cebef3b734986fd9a08195e740308d99f9f9
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2023-06-15 10:55:24 -0600

    update apt-get to fix poppler-utils install error, add specific version for pandas

--- internal/scripts/docker_env/scripts/diff_env.sh
+++ internal/scripts/docker_env/scripts/diff_env.sh
@@ -6,7 +6,7 @@
 # Notes: Adds packages needed to run differences tests to compare output to
 #   truth data.
 # Python Packages:
-#   pandas
+#   pandas==2.0.2
 #   pillow==9.2.0
 #   pdf2image==1.16.0
 #
@@ -24,9 +24,10 @@ ENV_NAME=diff.${METPLUS_VERSION}
 BASE_ENV=netcdf4.${METPLUS_VERSION}
 
 conda create -y --clone ${BASE_ENV} --name ${ENV_NAME}
-conda install -y --name ${ENV_NAME} -c conda-forge pandas
+conda install -y --name ${ENV_NAME} -c conda-forge pandas==2.0.2
 conda install -y --name ${ENV_NAME} -c conda-forge pillow==9.2.0
 
-apt install -y poppler-utils
+apt-get update
+apt-get install -y poppler-utils
 
 conda install -y --name ${ENV_NAME} -c conda-forge pdf2image==1.16.0
