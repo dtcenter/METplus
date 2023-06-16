7f8d3dbcebe08deb67d46c032215fba95bb14564
John Halley Gotway <johnhg@ucar.edu>
2023-06-16 15:05:39 -0600

    ci-run-all-diff, test how calling filecmp first impacts the time it takes to compute the file differences.

--- metplus/util/diff_util.py
+++ metplus/util/diff_util.py
@@ -192,7 +192,7 @@ def compare_files(filepath_a, filepath_b, debug=False, dir_a=None, dir_b=None,
         print(f"Unsupported file type encountered: {file_type.split('.')[1]}")
         return filepath_a, filepath_b, file_type, ''
 
-    # try file diff first
+    # try file diff first as a quick check
     if filecmp.cmp(filepath_a, filepath_b):
         print("No differences from filecmp")
         return True
