3b906a3d264c404af9fca66baa614a04e7531016
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2023-07-18 09:40:35 -0600

    add more tests for csv and stat files. move multiple tests into a single parameterized test

--- internal/tests/pytests/util/diff_util/test_diff_util.py
+++ internal/tests/pytests/util/diff_util/test_diff_util.py
@@ -13,7 +13,12 @@ test_output_dir = os.path.join(os.environ['METPLUS_TEST_OUTPUT_BASE'],
 stat_header = 'VERSION MODEL DESC     FCST_LEAD FCST_VALID_BEG  FCST_VALID_END  OBS_LEAD OBS_VALID_BEG   OBS_VALID_END   FCST_VAR FCST_UNITS FCST_LEV OBS_VAR OBS_UNITS OBS_LEV OBTYPE VX_MASK INTERP_MTHD INTERP_PNTS FCST_THRESH OBS_THRESH COV_THRESH ALPHA LINE_TYPE'
 mpr_line_1 = 'V11.1.0 HRRR  ALL_1.25 120000    20220701_200000 20220701_200000 000000   20220701_200000 20220701_200000 HPBL     m          L0       HPBL    m         L0      ADPSFC DENVER  BILIN       4           NA          NA         NA         NA    MPR       5    4       DENVER            39.78616    -104.41425       0         0       2160.80324 1498.06763 AMDAR NA NA NA'
 mpr_line_2 = 'V11.1.0 HRRR  ALL_1.25 120000    20220701_200000 20220701_200000 000000   20220701_200000 20220701_200000 HPBL     m          L0       HPBL    m         L0      ADPSFC DENVER  BILIN       4           NA          NA         NA         NA    MPR       5    4       DENVER            39.78616    -104.41425       0         0       2160.80324 1498.05994 AMDAR NA NA NA'
-
+file_path_1 = '/some/path/of/fake/file/one'
+file_path_2 = '/some/path/of/fake/file/two'
+file_path_3 = '/some/path/of/fake/file/three'
+csv_header = 'Last Name, First Name, Progress'
+csv_val_1 = 'Mackenzie, Stu, 0.9999'
+csv_val_2 = 'Kenny-Smith, Ambrose, 0.8977'
 
 def create_diff_files(files_a, files_b):
     unique_id = str(uuid.uuid4())[0:8]
@@ -21,12 +26,12 @@ def create_diff_files(files_a, files_b):
     dir_b = os.path.join(test_output_dir, f'diff_{unique_id}', 'b')
     mkdir_p(dir_a)
     mkdir_p(dir_b)
-    write_files(dir_a, files_a)
-    write_files(dir_b, files_b)
+    write_test_files(dir_a, files_a)
+    write_test_files(dir_b, files_b)
     return dir_a, dir_b
 
 
-def write_files(dirname, files):
+def write_test_files(dirname, files):
     for filename, lines in files.items():
         filepath = os.path.join(dirname, filename)
         if os.path.sep in filename:
@@ -38,132 +43,112 @@ def write_files(dirname, files):
                 file_handle.write(f'{line}\n')
 
 
+@pytest.mark.parametrize(
+    'a_files, b_files, rounding_override, expected_is_equal', [
+        # txt both empty dir
+        ({}, {}, None, True),
+        # txt A empty dir
+        ({}, {'filename.txt': ['some', 'text']}, None, False),
+        # txt B empty dir
+        ({'filename.txt': ['some', 'text']}, {}, None, False),
+        # txt both empty file
+        ({'filename.txt': []}, {'filename.txt': []}, None, True),
+        # txt A empty file
+        ({'filename.txt': []}, {'filename.txt': ['some', 'text']}, None, False),
+        # txt B empty file
+        ({'filename.txt': ['some', 'text']}, {'filename.txt': []}, None, False),
+        # stat header columns
+        ({'filename.stat': [stat_header, mpr_line_1]},
+         {'filename.stat': [f'{stat_header} NEW_COLUMN', mpr_line_1]},
+         None, False),
+        # stat number of lines
+        ({'filename.stat': [stat_header, mpr_line_1]},
+         {'filename.stat': [stat_header, mpr_line_1, mpr_line_2]},
+         None, False),
+        # stat number of columns
+        ({'filename.stat': [stat_header, mpr_line_1]},
+         {'filename.stat': [stat_header, f'{mpr_line_1} extra_value']},
+         None, False),
+        # stat string
+        ({'filename.stat': [stat_header, mpr_line_1]},
+         {'filename.stat': [stat_header, mpr_line_1.replace('L0', 'Z0')]},
+         None, False),
+        # stat default precision
+        ({'filename.stat': [stat_header, mpr_line_1]},
+         {'filename.stat': [stat_header, mpr_line_1.replace('39.78616', '39.78615')]},
+         None, False),
+        # stat float override precision
+        ({'filename.stat': [stat_header, mpr_line_1]},
+         {'filename.stat': [stat_header, mpr_line_1.replace('39.78616', '39.78615')]},
+         4, True),
+        # stat out of order
+        ({'filename.stat': [stat_header, mpr_line_1, mpr_line_2]},
+         {'filename.stat': [stat_header, mpr_line_2, mpr_line_1]},
+         4, True),
+        # stat version differs
+        ({'filename.stat': [stat_header, mpr_line_1]},
+         {'filename.stat': [stat_header, mpr_line_1.replace('V11.1.0', 'V12.0.0')]},
+         None, True),
+        # file_list A without file_list line
+        ({'file_list.txt': [file_path_1, file_path_2, file_path_3]},
+         {'file_list.txt': ['file_list', file_path_1, file_path_2, file_path_3]},
+         None, True),
+        # file_list B without file_list line
+        ({'file_list.txt': ['file_list', file_path_1, file_path_2, file_path_3]},
+         {'file_list.txt': [file_path_1, file_path_2, file_path_3]},
+         None, True),
+        # file_list out of order
+        ({'file_list.txt': ['file_list', file_path_1, file_path_2, file_path_3]},
+         {'file_list.txt': ['file_list', file_path_2, file_path_3, file_path_1]},
+         None, True),
+        # csv equal
+        ({'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
+         {'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
+         None, True),
+        # csv number of columns A
+        ({'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
+         {'file_list.csv': [f'{csv_header}, Position', f'{csv_val_1}, flute', f'{csv_val_2}, harmonica']},
+         None, False),
+        # csv number of columns B
+        ({'file_list.csv': [f'{csv_header}, Position', f'{csv_val_1}, flute', f'{csv_val_2}, harmonica']},
+         {'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
+         None, False),
+        # csv number of lines A
+        ({'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
+         {'file_list.csv': [csv_header, csv_val_1]},
+         None, False),
+        # csv number of lines B
+        ({'file_list.csv': [csv_header, csv_val_1]},
+         {'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
+         None, False),
+        # csv diff default precision
+        ({'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
+         {'file_list.csv': [csv_header, csv_val_1.replace('0.9999', '0.9998'), csv_val_2]},
+         None, False),
+        # csv diff default precision
+        ({'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
+         {'file_list.csv': [csv_header, csv_val_1.replace('0.9999', '0.9998'), csv_val_2]},
+         3, True),
+        # csv diff first item
+        ({'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
+         {'file_list.csv': [csv_header, csv_val_1.replace('Mackenzie', 'Art'), csv_val_2]},
+         None, False),
+    ]
+)
 @pytest.mark.util
-def test_diff_dirs_both_empty():
-    a_dir, b_dir = create_diff_files({}, {})
-    assert dirs_are_equal(a_dir, b_dir)
-    shutil.rmtree(os.path.dirname(a_dir))
+def test_diff_dir_text_files(a_files, b_files, rounding_override, expected_is_equal):
+    if rounding_override:
+        for filename in a_files:
+            ROUNDING_OVERRIDES[filename] = rounding_override
 
-
-@pytest.mark.util
-def test_diff_dirs_one_empty():
-    test_files = {'filename.txt': ['some', 'text']}
-    for empty_one in ('a', 'b'):
-        a_files = b_files = {}
-        if empty_one == 'a':
-            b_files = test_files
-        else:
-            a_files = test_files
-        a_dir, b_dir = create_diff_files(a_files, b_files)
-        assert not dirs_are_equal(a_dir, b_dir)
-        shutil.rmtree(os.path.dirname(a_dir))
-
-
-@pytest.mark.util
-def test_diff_files_both_empty():
-    test_files = {'filename.txt': []}
-    a_dir, b_dir = create_diff_files(test_files, test_files)
-    assert dirs_are_equal(a_dir, b_dir)
-    shutil.rmtree(os.path.dirname(a_dir))
-
-
-@pytest.mark.util
-def test_diff_files_one_empty():
-    filename = 'filename.txt'
-    test_content = ['some', 'text']
-    for empty_one in ('a', 'b'):
-        a_files = {filename: []}
-        b_files = {filename: []}
-        if empty_one == 'a':
-            b_files[filename].extend(test_content)
-        else:
-            a_files[filename].extend(test_content)
-        a_dir, b_dir = create_diff_files(a_files, b_files)
-        assert not dirs_are_equal(a_dir, b_dir)
-        shutil.rmtree(os.path.dirname(a_dir))
-
-
-@pytest.mark.util
-def test_diff_stat_header_columns():
-    filename = 'filename.stat'
-    a_content = [stat_header, mpr_line_1]
-    b_content = [f'{stat_header} NEW_COLUMN', mpr_line_1]
-    a_files = {filename: a_content}
-    b_files = {filename: b_content}
     a_dir, b_dir = create_diff_files(a_files, b_files)
-    assert not dirs_are_equal(a_dir, b_dir)
-    shutil.rmtree(os.path.dirname(a_dir))
+    assert dirs_are_equal(a_dir, b_dir) == expected_is_equal
 
+    # pass individual files instead of entire directory
+    for filename in a_files:
+        if filename in b_files:
+            a_path = os.path.join(a_dir, filename)
+            b_path = os.path.join(b_dir, filename)
+            assert dirs_are_equal(a_path, b_path) == expected_is_equal
 
-@pytest.mark.util
-def test_diff_stat_number_of_lines():
-    filename = 'filename.stat'
-    a_content = [stat_header, mpr_line_1]
-    b_content = [stat_header, mpr_line_1, mpr_line_2]
-    a_files = {filename: a_content}
-    b_files = {filename: b_content}
-    a_dir, b_dir = create_diff_files(a_files, b_files)
-    assert not dirs_are_equal(a_dir, b_dir)
-    shutil.rmtree(os.path.dirname(a_dir))
-
-
-@pytest.mark.util
-def test_diff_stat_number_of_columns():
-    filename = 'filename.stat'
-    a_content = [stat_header, mpr_line_1]
-    b_content = [stat_header, f'{mpr_line_1} extra_value']
-    a_files = {filename: a_content}
-    b_files = {filename: b_content}
-    a_dir, b_dir = create_diff_files(a_files, b_files)
-    assert not dirs_are_equal(a_dir, b_dir)
-    shutil.rmtree(os.path.dirname(a_dir))
-
-
-@pytest.mark.util
-def test_diff_stat_string():
-    filename = 'filename.stat'
-    a_content = [stat_header, mpr_line_1]
-    b_content = [stat_header, mpr_line_1.replace('L0', 'Z0')]
-    a_files = {filename: a_content}
-    b_files = {filename: b_content}
-    a_dir, b_dir = create_diff_files(a_files, b_files)
-    assert not dirs_are_equal(a_dir, b_dir)
-    shutil.rmtree(os.path.dirname(a_dir))
-
-
-@pytest.mark.util
-def test_diff_stat_float_default_precision():
-    filename = 'filename.stat'
-    a_content = [stat_header, mpr_line_1]
-    b_content = [stat_header, mpr_line_1.replace('39.78616', '39.78615')]
-    a_files = {filename: a_content}
-    b_files = {filename: b_content}
-    a_dir, b_dir = create_diff_files(a_files, b_files)
-    assert not dirs_are_equal(a_dir, b_dir)
-    shutil.rmtree(os.path.dirname(a_dir))
-
-
-@pytest.mark.util
-def test_diff_stat_float_override_precision():
-    filename = 'filename.stat'
-    ROUNDING_OVERRIDES[filename] = 4
-    a_content = [stat_header, mpr_line_1]
-    b_content = [stat_header, mpr_line_1.replace('39.78616', '39.78615')]
-    a_files = {filename: a_content}
-    b_files = {filename: b_content}
-    a_dir, b_dir = create_diff_files(a_files, b_files)
-    assert dirs_are_equal(a_dir, b_dir)
-    shutil.rmtree(os.path.dirname(a_dir))
-
-
-@pytest.mark.util
-def test_diff_stat_out_of_order():
-    filename = 'filename.stat'
-    ROUNDING_OVERRIDES[filename] = 4
-    a_content = [stat_header, mpr_line_1, mpr_line_2]
-    b_content = [stat_header, mpr_line_2, mpr_line_1]
-    a_files = {filename: a_content}
-    b_files = {filename: b_content}
-    a_dir, b_dir = create_diff_files(a_files, b_files)
-    assert dirs_are_equal(a_dir, b_dir)
     shutil.rmtree(os.path.dirname(a_dir))
