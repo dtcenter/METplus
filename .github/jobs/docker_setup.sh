51c6c53e4c9fe25eda779831042be257f0dc70b1
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2023-07-20 13:50:36 -0600

    per #2138, modify MET_FORCE_TAG value to include DockerHub repo name (excluding dtcenter/ prefix), e.g. met:11.1.0

--- .github/jobs/docker_setup.sh
+++ .github/jobs/docker_setup.sh
@@ -44,9 +44,12 @@ elif [ "${EXTERNAL_TRIGGER}" == "true" ]; then
 fi
 
 # if MET_FORCE_TAG variable is set and not empty, use that version instead
+# format is <REPO>:<TAG> where the repo used is dtcenter/<REPO> and
+# the tag used is <TAG>, e.g. met:11.1.0 uses dtcenter/met:11.1.0
 if [ ! -z "$MET_FORCE_TAG" ]; then
-    MET_TAG=$MET_FORCE_TAG
-    MET_DOCKER_REPO=met
+    force_info=(${MET_FORCE_TAG//:/ })
+    MET_DOCKER_REPO=${force_info[0]}
+    MET_TAG=${force_info[1]}
 fi
 
 echo Using MET_DOCKER_REPO=$MET_DOCKER_REPO
