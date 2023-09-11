95489f87814ed91da1f6185b4f2d024ae530db51
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2023-09-11 16:10:50 -0600

    remove versions from packages

--- internal/scripts/docker_env/scripts/spacetime_env.sh
+++ internal/scripts/docker_env/scripts/spacetime_env.sh
@@ -5,12 +5,12 @@
 # Last Updated: 2023-01-27 (mccabe@ucar.edu)
 # Notes: Adds Python packages needed to generate coherence spectra (METplotpy)
 # Python Packages:
-#   netCDF4==1.5.8
-#   xarray==2022.3.0
-#   scipy==1.8.1
-#   matplotlib==3.5.2
-#   pyngl==1.6.1
-#   pyyaml==6.0
+#   netCDF4==
+#   xarray==
+#   scipy==
+#   matplotlib==
+#   pyngl==
+#   pyyaml==
 #
 # Other Content: None
 ################################################################################
@@ -23,9 +23,9 @@ ENV_NAME=spacetime.${METPLUS_VERSION}
 
 conda create -y --name ${ENV_NAME} -c conda-forge python=3.10.4
 
-conda install -y --name ${ENV_NAME} -c conda-forge netCDF4==1.5.8
-conda install -y --name ${ENV_NAME} -c conda-forge xarray==2022.3.0
-conda install -y --name ${ENV_NAME} -c conda-forge scipy==1.8.1
-conda install -y --name ${ENV_NAME} -c conda-forge matplotlib==3.5.2
-conda install -y --name ${ENV_NAME} -c conda-forge pyngl==1.6.1
-conda install -y --name ${ENV_NAME} -c conda-forge pyyaml==6.0
+conda install -y --name ${ENV_NAME} -c conda-forge netCDF4
+conda install -y --name ${ENV_NAME} -c conda-forge xarray
+conda install -y --name ${ENV_NAME} -c conda-forge scipy
+conda install -y --name ${ENV_NAME} -c conda-forge matplotlib
+conda install -y --name ${ENV_NAME} -c conda-forge pyngl
+conda install -y --name ${ENV_NAME} -c conda-forge pyyaml
