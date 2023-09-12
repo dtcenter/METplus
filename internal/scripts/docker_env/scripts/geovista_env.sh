273287f33716c3ff83eb9a3a3779042fb2e2683d
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2023-09-12 09:42:46 -0600

    update geovista env to install libGL and libEGL to prevent dynamic library errors

--- internal/scripts/docker_env/scripts/geovista_env.sh
+++ internal/scripts/docker_env/scripts/geovista_env.sh
@@ -20,27 +20,11 @@ METPLUS_VERSION=$1
 # Conda environment to create
 ENV_NAME=geovista.${METPLUS_VERSION}
 
+# install libGL and libEGL to prevent ImportError of libGL and libEGL dynamic library in geovista
+apt install -y libgl1-mesa-glx
+apt install -y libegl1
+
 conda create -y --name ${ENV_NAME} -c conda-forge python=3.10.4
 conda install -y --name ${ENV_NAME} -c conda-forge geovista
 conda install -y --name ${ENV_NAME} -c conda-forge xarray
 conda install -y --name ${ENV_NAME} -c conda-forge iris
-
-exit 0
-
-# install libGL to prevent ImportError of libGL dynamic library in geovista
-apt install -y libgl1-mesa-glx
-
-# install git to clone geovista repo to get dev version of package
-apt install -y git
-git clone https://github.com/bjlittle/geovista.git
-cd geovista
-conda create -y -n ${ENV_NAME} --file requirements/locks/py310-lock-linux-64.txt
-
-# note: this command will not work on a local machine
-# it is specific to docker
-/usr/local/conda/envs/${ENV_NAME}/bin/pip3 install --no-deps .
-cd -
-
-conda install -y --name ${ENV_NAME} -c conda-forge xarray==2022.11.0
-conda install -y --name ${ENV_NAME} -c conda-forge iris==3.3.1
-
