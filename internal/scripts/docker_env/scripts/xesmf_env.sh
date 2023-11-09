7382be73f23342aed0a315e32ae8e6a857d9d905
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2023-11-09 10:47:41 -0700

    downgrade version of esmf package to fix bug described in https://github.com/pangeo-data/xESMF/issues/246

--- internal/scripts/docker_env/scripts/xesmf_env.sh
+++ internal/scripts/docker_env/scripts/xesmf_env.sh
@@ -5,10 +5,13 @@
 # Last Updated: 2023-11-08 (mccabe@ucar.edu)
 # Notes: Adds Python package to read Tripolar grids
 #  Updated xesmf version from 0.3.0 because <0.7.1 does not work with mamba
+#  Downgrade esmf to 8.3.1 due to know bug described in this issue:
+#  https://github.com/pangeo-data/xESMF/issues/246
 # Python Packages:
 #   netcdf4==1.6.2
 #   xarray==2022.3.0
 #   xesmf==0.8.2
+#   esmf==8.3.1
 #
 # Other Content: None
 ################################################################################
@@ -26,3 +29,5 @@ mamba create -y --clone ${BASE_ENV} --name ${ENV_NAME}
 mamba install -y --name ${ENV_NAME} -c conda-forge netcdf4==1.6.2
 mamba install -y --name ${ENV_NAME} -c conda-forge xarray==2022.3.0
 mamba install -y --name ${ENV_NAME} -c conda-forge xesmf==0.8.2
+mamba install -y --name ${ENV_NAME} -c conda-forge esmf==8.3.1
+
