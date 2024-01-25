bdc08774026e9626b0d765a16fcd8695556ef58a
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2024-01-25 16:31:41 -0700

    fix script, ci-run-diff

--- .github/jobs/check_for_error_logs.sh
+++ .github/jobs/check_for_error_logs.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
-json_data=$(curl -s -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/jobs")
-error_log_jobs=($(echo "$json_data" | jq -r '.jobs[] | select(.name | startswith("Use Case Tests")) | .steps[] | select(.name | startswith("Save error logs")) | select(.conclusion | startswith("success"))'))
+json_data=$(curl -s -H "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/jobs")
+error_log_jobs=$(echo "$json_data" | jq -r '.jobs[] | select(.name | startswith("Use Case Tests")) | .steps[] | select(.name | startswith("Save error logs")) | select(.conclusion | startswith("success"))')
 # save output variable to merge error logs if any error logs were created
 if [ ! -z "${error_log_jobs}" ]; then
   echo "has_error_logs=true" >> $GITHUB_OUTPUT
