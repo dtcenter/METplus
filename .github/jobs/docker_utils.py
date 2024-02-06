0b337076796b14ab3b605eab1b4e9e1cb2447090
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2024-02-06 12:05:50 -0700

    support commands that must run in the shell to see if || will work in docker image pruning step

--- .github/jobs/docker_utils.py
+++ .github/jobs/docker_utils.py
@@ -111,8 +111,9 @@ def run_commands(commands):
         print(f"::group::RUNNING {command}")
         start_time = time.time()
         try:
+            shell = any([x in command for x in ('|', '&', '<', '>')])
             process = subprocess.Popen(shlex.split(command),
-                                       shell=False,
+                                       shell=shell,
                                        encoding='utf-8',
                                        stdout=subprocess.PIPE,
                                        stderr=subprocess.STDOUT)
