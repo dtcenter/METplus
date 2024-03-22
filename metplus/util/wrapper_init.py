ff972e5e5baf316a640d8ba3c510708ed9d698d8
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2024-03-22 10:29:33 -0600

    per #2520, create function to get METplus wrapper class without initializing it and use that function to read list of deprecated env vars to prevent redundant initialization of wrappers that can cause unintended side effects

--- metplus/util/wrapper_init.py
+++ metplus/util/wrapper_init.py
@@ -3,21 +3,19 @@ from importlib import import_module
 from . import camel_to_underscore
 
 
-def get_wrapper_instance(config, process, instance=None):
-    """!Initialize METplus wrapper instance.
+def get_wrapper_class(config, process):
+    """!Get the METplus wrapper class object that is not initialized.
+    This can be used to read class variables from a wrapper without creating
+    an instance of the wrapper class.
 
     @param config METplusConfig object to pass to wrapper constructor
     @param process name of wrapper in camel case, e.g. GridStat
-    @param instance (optional) instance identifier for creating multiple
-    instances of a wrapper. Set to None (default) if no instance is specified
-    @returns CommandBuilder sub-class object or None if something went wrong
+    @returns CommandBuilder subclass or None if something went wrong
     """
-    package_name = (f'metplus.wrappers.{camel_to_underscore(process)}_wrapper')
+    package_name = f'metplus.wrappers.{camel_to_underscore(process)}_wrapper'
     try:
         module = import_module(package_name)
-        metplus_wrapper = (
-            getattr(module, f"{process}Wrapper")(config, instance=instance)
-        )
+        wrapper_class = getattr(module, f"{process}Wrapper")
     except AttributeError as err:
         config.logger.error(f"There was a problem loading {process} wrapper: {err}")
         return None
@@ -26,4 +24,20 @@ def get_wrapper_instance(config, process, instance=None):
                             "Wrapper may have been disabled.")
         return None
 
-    return metplus_wrapper
+    return wrapper_class
+
+
+def get_wrapper_instance(config, process, instance=None):
+    """!Initialize METplus wrapper instance.
+
+    @param config METplusConfig object to pass to wrapper constructor
+    @param process name of wrapper in camel case, e.g. GridStat
+    @param instance (optional) instance identifier for creating multiple
+    instances of a wrapper. Set to None (default) if no instance is specified
+    @returns initialized CommandBuilder subclass object or
+     None if something went wrong
+    """
+    wrapper_class = get_wrapper_class(config, process)
+    if wrapper_class is None:
+        return None
+    return wrapper_class(config, instance=instance)
