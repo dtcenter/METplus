f3def37e077fe8666d4ff65e659b5dcf19347c36
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2024-04-23 16:32:16 -0600

    Fix main_v5.1 GHA - disk space and METviewer version (#2561)
    * added more commands to free up disk space as suggested in https://github.com/apache/flink/blob/master/tools/azure-pipelines/free_disk_space.sh, ci-run-all-cases
    
    * update location of METviewer docker-compose.yml file that moved from PR dtcenter/METviewer#525
    
    * turn on use cases to test
    
    * use main_v5.1 tag of METviewer on DockerHub
    
    * turn off use case groups after confirming that they now run successfully

--- .github/jobs/get_metviewer.sh
+++ .github/jobs/get_metviewer.sh
@@ -4,13 +4,13 @@
 export METVIEWER_DATA=$RUNNER_WORKSPACE
 export MYSQL_DIR=$RUNNER_WORKSPACE/mysql
 export METVIEWER_DIR=$RUNNER_WORKSPACE/output/metviewer
-export METVIEWER_DOCKER_IMAGE=dtcenter/metviewer:develop
+export METVIEWER_DOCKER_IMAGE=dtcenter/metviewer:main_v5.1
 
 # install docker-compose
 apk add docker-cli-compose
 
 # download docker-compose.yml file from METviewer develop branch
-wget https://raw.githubusercontent.com/dtcenter/METviewer/develop/docker/docker-compose.yml
+wget https://raw.githubusercontent.com/dtcenter/METviewer/develop/internal/scripts/docker/docker-compose.yml
 
 # Run docker-compose to create the containers
 docker compose up -d
