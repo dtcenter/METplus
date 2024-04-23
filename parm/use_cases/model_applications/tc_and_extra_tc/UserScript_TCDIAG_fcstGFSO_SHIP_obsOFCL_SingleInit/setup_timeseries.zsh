b53ea465b8885f5761ddb3e76b17cf015050d077
bikegeek <3753118+bikegeek@users.noreply.github.com>
2024-04-23 13:06:41 -0600

    Delete parm/use_cases/model_applications/tc_and_extra_tc/UserScript_TCDIAG_fcstGFSO_SHIP_obsOFCL_SingleInit/setup_timeseries.zsh
    not needed

--- parm/use_cases/model_applications/tc_and_extra_tc/UserScript_TCDIAG_fcstGFSO_SHIP_obsOFCL_SingleInit/setup_timeseries.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/zsh
-
-export INPUT_BASE=$METPLUS_BASE/parm/use_case
-export PLOT_TIME_SERIES_INPUT_BASE=$METPLUS_BASE/output/reformatted
-export PLOT_TIME_SERIES_OUTPUT_BASE=$METPLUS_BASE/
-
-
