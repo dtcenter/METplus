062d771c960ff9b2d10293280009002f2de38f16
Christina Kalb <kalb@ucar.edu>
2024-06-28 12:07:26 -0600

    Trying to fix documentation error

--- docs/use_cases/model_applications/s2s_stratosphere/UserScript_fcstGFS_obsERA_StratosphereQBO.py
+++ docs/use_cases/model_applications/s2s_stratosphere/UserScript_fcstGFS_obsERA_StratosphereQBO.py
@@ -99,7 +99,7 @@ UserScript_fcstGFS_obsERA_StratosphereQBO.py
 # ----------------
 #
 # This use case runs the stratospher_qbo_driver.py python script.  The processing
-# performed by the script are detailed in the :ref:`metplus-workflow` section.
+# performed by the script are detailed in the METplus Workflow section.
 #
 
 ##############################################################################
