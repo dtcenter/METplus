09422fab2805186ea07eab4c144224befecac5b8
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2024-10-17 12:59:34 -0600

    add descriptions for tests

--- internal/tests/pytests/component_versions/test_component_versions.py
+++ internal/tests/pytests/component_versions/test_component_versions.py
@@ -26,18 +26,22 @@ def test_get_coordinated_version(component, version, expected_result):
 
 @pytest.mark.parametrize(
     'input_component, input_version, output_component, output_format, expected_result', [
+        # get MET version for Docker dtcenter/metplus
         ('metplus', '5.1.0', 'met', '{X}.{Y}.{Z}{N}', '11.1.1'),
         ('metplus', '5.1.0-beta3', 'met', '{X}.{Y}.{Z}{N}', '11.1.1-beta3'),
         ('metplus', '5.1.0-rc1', 'met', '{X}.{Y}.{Z}{N}', '11.1.1-rc1'),
         ('metplus', '5.1-latest', 'met', '{X}.{Y}{N}', '11.1-latest'),
         ('metplus', '5.1.0-beta3-dev', 'met', '{X}.{Y}.{Z}{N}', 'develop'),
+        # get METplus Analysis versions for Docker dtcenter/metplus-analysis
         ('METplus', '5.1.0', 'metplotpy', 'v{X}.{Y}.{Z}{N}', 'v2.1.0'),
         ('metplus', '5.1.0-beta3', 'METplotpy', 'v{X}.{Y}.{Z}{N}', 'v2.1.0-beta3'),
         ('metplus', '5.1.0-dev', 'METplotpy', 'v{X}.{Y}.{Z}{N}', 'develop'),
         ('metplus', '5.1.0-rc1', 'metplotpy', 'v{X}.{Y}.{Z}{N}', 'v2.1.0-rc1'),
         ('metplus', '5.1.0-beta3-dev', 'metplotpy', 'v{X}.{Y}.{Z}{N}', 'develop'),
+        # get METplus main branch to trigger workflow from other repos, e.g. MET
         ('MET', 'main_v11.1', 'METplus', 'main_v{X}.{Y}', 'main_v5.1'),
         ('MET', 'main_v11.1-ref', 'METplus', 'main_v{X}.{Y}', 'main_v5.1'),
+        # get latest bugfix version from main branch or X.Y version
         ('MET', 'main_v11.1', 'MET', '{X}.{Y}.{Z}{N}', '11.1.1'),
         ('MET', '11.1.Z', 'MET', '{X}.{Y}.{Z}{N}', '11.1.1'),
     ]
