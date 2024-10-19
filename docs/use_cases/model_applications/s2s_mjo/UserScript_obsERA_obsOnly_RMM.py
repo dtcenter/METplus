74e2c0e706371543dedc64036a74f12af95bcb01
Christina Kalb <kalb@ucar.edu>
2024-10-18 19:55:11 -0600

    Fixing typo

--- docs/use_cases/model_applications/s2s_mjo/UserScript_obsERA_obsOnly_RMM.py
+++ docs/use_cases/model_applications/s2s_mjo/UserScript_obsERA_obsOnly_RMM.py
@@ -1,6 +1,6 @@
 """
-UserScript: Make ERA RMM plot from calculated MJO indices
-=========================================================
+UserScript: Make ERA RMM plots from calculated MJO indices
+==========================================================
 
 model_applications/
 s2s_mjo/
@@ -62,9 +62,11 @@ UserScript_obsERA_obsOnly_RMM.py
 # METplus Workflow
 # ----------------
 #
-# This use case does not loop, but the UserScript to create and EOF filelist is run once and the RMM driver script is 
-# run once.  The RMM script has the ability to loop over lead time, although only one lead time is provided here.  The 
-# optional pre-processing step loops by valid time.  
+# The creation of anomalies using harmonic analysis and the calculation of RMM do not loop.  Rather, the
+# UserScripts are run once.  These scripts do have the ability to loop over lead time, although only one
+# lead time is provided here.  The optional pre-processing steps to create the mean daily annual cycle
+# and daily mean data loop by valid time with different timing settings needed used for the different 
+# steps.
 #
 
 ##############################################################################
@@ -74,12 +76,12 @@ UserScript_obsERA_obsOnly_RMM.py
 # METplus first loads all of the configuration files found in parm/metplus_config,
 # then it loads any configuration files passed to METplus via the command line
 # i.e. parm/use_cases/model_applications/s2s_mjo/UserScript_obsERA_obsOnly_RMM.conf.
-# The file UserScript_obsERA_obsOnly_RMM/RMM_driver.py runs the python program and the
-# variables for the RMM calculation are set in the [user_env_vars] section of the .conf 
-# file. 
+# The file UserScript_obsERA_obsOnly_RMM/RMM_driver.py runs the python program and  
+# UserScript_obsERA_obsOnly_RMM.conf sets the variables for all steps of the RMM use case.
 #
 # .. highlight:: bash
 # .. literalinclude:: ../../../../parm/use_cases/model_applications/s2s_mjo/UserScript_obsERA_obsOnly_RMM.conf
+#
 
 ##############################################################################
 # MET Configuration
@@ -99,21 +101,21 @@ UserScript_obsERA_obsOnly_RMM.py
 # Python Scripting
 # ----------------
 #
-# This use case runs the RMM driver which computes RMM and creates a phase diagram. Inputs to the 
-# RMM driver include netCDF files formatted in MET's netCDF version.  In addition, a txt file containing 
-# the listing of these input netCDF files is required, as well as text file listings of the EOF1 and 
-# EOF2 files.  These text files can be generated using the USER_SCRIPT_INPUT_TEMPLATES in the 
-# [create_eof_filelist] and [script_omi] sections.  Some optional pre-processing steps include using 
-# regrid_data_plane to either regrid your data or cut the domain to 20N - 20S.
+# RMM is computed using OLR, U850, and U200 data between 15N and 15S.  Anomalies of OLR, U850, and 
+# U200 are created using a harmonic analysis, 120 day day mean removed, and the data are normalized by 
+# normalization factors (generally the square root of the average variance)  The anomalies are projected 
+# onto Empirical Orthogonal Function (EOF) data.  The OLR is then filtered for 20 - 96 days, and regressed 
+# onto the daily EOFs.  Finally, it's normalized and these normalized components are plotted on a phase diagram 
+# and timeseries plot.  The RMM driver script orchestrates the calculation of the MJO indices and 
+# the generation of three RMM plots:
+# parm/use_cases/model_applications/s2s_mjo/UserScript_obsERA_obsOnly_RMM/RMM_driver.py:
 #
-# For the RMM calculation, the OLR data are then projected onto Empirical Orthogonal Function (EOF) 
-# data that is computed for each day of the year, latitude, and longitude.  The OLR is then filtered 
-# for 20 - 96 days, and regressed onto the daily EOFs.  Finally, it's normalized and these normalized 
-# components are plotted on a phase diagram.  The RMM driver script orchestrates the calculation of the 
-# MJO indices and the generation of a phase diagram RMM plot.
+# The harmonic anomalies script creates anomalies of input data using a harmonic analysis:
+# parm/use_cases/model_applications/s2s_mjo/UserScript_obsERA_obsOnly_RMM/compute_harmonic_anomalies.py
 #
 # .. highlight:: python
 # .. literalinclude:: ../../../../parm/use_cases/model_applications/s2s_mjo/UserScript_obsERA_obsOnly_RMM/RMM_driver.py
+# .. literalinclude:: ../../../../parm/use_cases/model_applications/s2s_mjo/UserScript_obsERA_obsOnly_RMM/compute_harmonic_anomalies.py
 #
 
 ##############################################################################
@@ -137,11 +139,13 @@ UserScript_obsERA_obsOnly_RMM.py
 #   INFO: METplus has successfully finished running.
 #
 # Refer to the value set for **OUTPUT_BASE** to find where the output data was generated. Output for this use 
-# case will be found in model_applications/s2s_mjo/UserScript_fcstGFS_obsERA_RMM/plots (relative to **OUTPUT_BASE**).  
-# The output may include the regridded data and daily averaged files if those steps are turned on.  A Phase diagram 
-# plots will be generated:
+# case will be found in model_applications/s2s_mjo/UserScript_obsERA_obsOnly_RMM/plots (relative to **OUTPUT_BASE**).
+# The output may include the regridded data and daily averaged files if those steps are turned on.  Three output
+# plots will be generated, a phase diagram, time series, and EOF plot:
 #
-#  * obs_RMM_comp_phase.png
+#  * plot1.png
+#  * plot2.png
+#  * plot3.png
 #
 
 ##############################################################################
@@ -152,8 +156,11 @@ UserScript_obsERA_obsOnly_RMM.py
 #
 #   * S2SAppUseCase
 #   * S2SMJOAppUseCase
+#   * NetCDFFileUseCase
 #   * RegridDataPlaneUseCase
 #   * PCPCombineUseCase
+#   * METcalcpyUseCase
+#   * METplotpyUseCase
 #
 #   Navigate to :ref:`quick-search` to discover other similar use cases.
 #
