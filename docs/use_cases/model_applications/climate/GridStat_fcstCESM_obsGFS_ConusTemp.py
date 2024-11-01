fec59c27acd1d5a82bc58ab2c3af9ef9ea0fbf1f
Julie Prestopnik <jpresto@ucar.edu>
2024-11-01 12:18:52 -0600

    Per #2741, updating sections based on template and template description

--- docs/use_cases/model_applications/climate/GridStat_fcstCESM_obsGFS_ConusTemp.py
+++ docs/use_cases/model_applications/climate/GridStat_fcstCESM_obsGFS_ConusTemp.py
@@ -1,9 +1,9 @@
 """
 Grid-Stat: CESM and GFS Analysis CONUS Temp 
-============================================================================
-model_applications/climate/
-GridStat_fcstCESM_obsGFS
-_ConusTemp.conf
+===========================================
+
+model_applications/climate/GridStat_fcstCESM_obsGFS_ConusTemp.conf
+
 """
 
 ##############################################################################
@@ -24,7 +24,6 @@ _ConusTemp.conf
 ##############################################################################
 # Version Added
 # -------------
-# [UPDATE_SECTION_CONTENT]
 #
 # METplus version 3.1
 
@@ -32,9 +31,9 @@ _ConusTemp.conf
 # Datasets
 # --------
 #
-#  * Forecast dataset: CESM Surface Temperature Data
-
-#  * Observation dataset: GFS Analysis 2m Temperature
+# **Forecast:** CESM Surface Temperature Data
+#
+# **Observation:** GFS Analysis 2m Temperature
 #
 # **Climatology:** None
 #
@@ -76,7 +75,7 @@ _ConusTemp.conf
 # ---------------------
 #
 # METplus first loads all of the configuration files found in parm/metplus_config,
-# then it loads any configuration files passed to METplus via the command line
+# then it loads any configuration files passed to METplus via the command line,
 # i.e. parm/use_cases/model_applications/climate/GridStat_fcstCESM_obsGFS_ConusTemp.conf
 #
 # .. highlight:: bash
@@ -102,47 +101,14 @@ _ConusTemp.conf
 ##############################################################################
 # Python Embedding
 # ----------------
-# [UPDATE_SECTION_CONTENT]
 #
-# This use case calls the read_ASCAT_data.py script to read and pass to PointStat 
-# the user-requested variable. The script needs 5 inputs in the following order: 
-# a path to a directory that contains only ASCAT data of the “ascat_YYYYMMDDHHMMSS_*” 
-# string, a start time in YYYYMMDDHHMMSS, an end time in the same format, 
-# a message type to code the variables as, and a variable name to read in. 
-# Currently the script puts the same station ID to each observation, but there is 
-# space in the code describing an alternate method that may be improved upon to 
-# allow different satellites to have their own station IDs. 
-# This code currently ingests all files it finds in the directory, pulls out the 
-# requested variable, and arranges the data in a list of lists following the 
-# 11-column format for point data. This list of lists is passed back 
-# to PointStat for evaluation and the requested statistical output. The location 
-# of the code is 
-# parm/use_cases/model_applications/marine_and_cryosphere/PointStat_fcstGFS_obsASCAT_satelliteWinds/read_ASCAT_data.py
-#
-# For more information on the basic requirements to utilize Python Embedding in METplus, 
-# please refer to the MET User’s Guide section on `Python embedding <https://met.readthedocs.io/en/latest/Users_Guide/appendixF.html#appendix-f-python-embedding>`_ 
-#
-# .. highlight:: python
-# .. literalinclude:: ../../../../parm/use_cases/model_applications/marine_and_cryosphere/PointStat_fcstGFS_obsASCAT_satelliteWinds/read_ASCAT_data.py
+# This use case does not use Python embedding.
 
 ##############################################################################
 # User Scripting
 # --------------
 # [UPDATE_SECTION_CONTENT]
-#
-# This use case uses a Python script to perform plotting, which at the time of 
-# this use case creation was not an ability METplus had. Additionally some of 
-# the plotting features used in this script are not currently slated for METplus 
-# analysis suite development.
-# In order to create the plots, the script reads in a yaml file and sets up 
-# the correct environment. Plot parameters (which are hard coded in the script) are set, 
-# and the datasets are read in from the input file. The desired variable fields 
-# are placed into arrays, which are then treated for bad data and squeezed to the 
-# appropriate dimensions. Additional basic math is completed on the resulting arrays 
-# to create the cross spectra values with the results being graphed.
-#
-# .. highlight:: python
-# .. literalinclude:: ../../../../parm/use_cases/model_applications/s2s/UserScript_fcstS2S_obsERAI_CrossSpectra/cross_spectra_plot.py
+
 
 ##############################################################################
 # Running METplus
@@ -151,7 +117,7 @@ _ConusTemp.conf
 # Pass the use case configuration file to the run_metplus.py script along 
 # with any user-specific system configuration files if desired:
 #
-#run_metplus.py /path/to/METplus/parm/use_cases/model_applications/climate/GridStat_fcstCESM_obsGFS_ConusTemp.conf /path/to/user_system.conf
+# run_metplus.py /path/to/METplus/parm/use_cases/model_applications/climate/GridStat_fcstCESM_obsGFS_ConusTemp.conf /path/to/user_system.conf
 #
 # See :ref:`running-metplus` for more information.
 
@@ -164,18 +130,13 @@ _ConusTemp.conf
 #   INFO: METplus has successfully finished running.
 #
 # Refer to the value set for **OUTPUT_BASE** to find where the output data was generated.
-# Output for this use case will be found in {OUPUT_BASE}/model_applications/climate/CESM_GridStat/grid_stat
-# and will contain the following files:
+# Output for this use case will be found in {OUTPUT_BASE}/model_applications/climate/CESM_GridStat/grid_stat
+# and will contain the following files::
 #
-# grid_stat_CESM_TMP_vs_GFS_ANALYS_060000L_20140801_060000V.stat
-# grid_stat_CESM_TMP_vs_GFS_ANALYS_120000L_20140801_120000V.stat
-# grid_stat_CESM_TMP_vs_GFS_ANALYS_060000L_20140802_060000V.stat
-# grid_stat_CESM_TMP_vs_GFS_ANALYS_120000L_20140802_120000V.stat
-#
-# Each file should contain corresponding statistics for the line type(s) requested.
-# Those variables are::
-#
-#  * UPDATE_FILES
+# * grid_stat_CESM_TMP_vs_GFS_ANALYS_060000L_20140801_060000V.stat
+# * grid_stat_CESM_TMP_vs_GFS_ANALYS_120000L_20140801_120000V.stat
+# * grid_stat_CESM_TMP_vs_GFS_ANALYS_060000L_20140802_060000V.stat
+# * grid_stat_CESM_TMP_vs_GFS_ANALYS_120000L_20140802_120000V.stat
 
 ##############################################################################
 # Keywords
