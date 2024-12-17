f67f2206fc084dc41a78423237c0af9066ca8c74
Christina Kalb <kalb@ucar.edu>
2024-12-17 13:40:55 -0700

    Update docs/use_cases/model_applications/s2s_mjo/UserScript_obsERA_obsOnly_OMI.py
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>

--- docs/use_cases/model_applications/s2s_mjo/UserScript_obsERA_obsOnly_OMI.py
+++ docs/use_cases/model_applications/s2s_mjo/UserScript_obsERA_obsOnly_OMI.py
@@ -60,7 +60,7 @@ model_applications/s2s_mjo/UserScript_obsERA_obsOnly_OMI.py
 # the OMI calculation.  In addition, there is one optional pre-processing steps, using
 # Regrid-Data-Plane. 
 #
-# This use case requires METcalcpy, METplotpy, and METdataio to run. The metcalcpy scripts accessed include the following:
+# This use case requires METcalcpy, METplotpy, and METdataio to run. The METcalcpy scripts accessed include the following:
 # 
 # * metcalcpy/contributed/rmm_omi/compute_mjo_indices.py
 #
