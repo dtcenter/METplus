8ef515c24ef630425ccc4d0bb9f4602ae702497e
Christina Kalb <kalb@ucar.edu>
2024-12-17 13:52:07 -0700

    Update docs/use_cases/model_applications/s2s_mid_lat/UserScript_fcstGFS_obsERA_WeatherRegime.py
    Co-authored-by: Julie Prestopnik <jpresto@ucar.edu>

--- docs/use_cases/model_applications/s2s_mid_lat/UserScript_fcstGFS_obsERA_WeatherRegime.py
+++ docs/use_cases/model_applications/s2s_mid_lat/UserScript_fcstGFS_obsERA_WeatherRegime.py
@@ -3,7 +3,6 @@ UserScript and StatAnalysis: Calculate and Evaluate Weather Regimes for GFS and 
 ===================================================================================
 
 model_applications/s2s_mid_lat/UserScript_fcstGFS_obsERA_WeatherRegime.py
-s2s_mid_lat/
 
 """
 
