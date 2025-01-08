6bc5edb36aa8440aef76a58e12ad75e0a37998cf
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2025-01-08 11:27:53 -0700

    test that this file is not needed anymore because RTD controls the version selector

--- docs/conf.py
+++ docs/conf.py
@@ -198,7 +198,7 @@ suppress_warnings = ['ref.citation']
 #
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = ["_themes", ]
-html_js_files = ['pop_ver.js']
+html_js_files = []
 html_css_files = ['theme_override.css']
 
 # Add any paths that contain custom static files (such as style sheets) here,
