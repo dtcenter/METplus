1f880853f6a5e7839770d8ca6633aa04d0a786d9
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2025-02-12 11:58:29 -0700

    remove fortran flags that are not needed

--- internal/scripts/conda/recipe/build.sh
+++ internal/scripts/conda/recipe/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -ex
 
-# turn off fortran optimization to prevent issues with external code
-export FFLAGS="-O0 ${FFLAGS}"
-
 export CFLAGS="-I${PREFIX}/include $CFLAGS"
 export CPPFLAGS="-I${PREFIX}/include $CPPFLAGS"
 export LIBRARY_PATH="${PREFIX}/lib:$LIBRARY_PATH"
