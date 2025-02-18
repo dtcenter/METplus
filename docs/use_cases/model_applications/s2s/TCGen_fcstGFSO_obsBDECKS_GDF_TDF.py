ea687e349dda73a6865f05528696ae3e88702e31
Daniel Adriaansen <dadriaan@ucar.edu>
2025-02-18 23:26:54 +0000

    Adds INIT_END and INIT_INCREMENT to workflow.

--- docs/use_cases/model_applications/s2s/TCGen_fcstGFSO_obsBDECKS_GDF_TDF.py
+++ docs/use_cases/model_applications/s2s/TCGen_fcstGFSO_obsBDECKS_GDF_TDF.py
@@ -120,6 +120,10 @@ model_applications/s2s/TCGen_fcstGFSO_obsBDECKS_GDF_TDF.conf
 #
 # **Beginning time (INIT_BEG):** 2016
 #
+# **End time (INIT_END):** 2016
+#
+# **Increment between beginning and end times (INIT_INCREMENT):** None
+#
 # **Sequence of forecast leads to process (LEAD_SEQ):** None
 #
 # The following tools are used for each run time: TCGen, Python
