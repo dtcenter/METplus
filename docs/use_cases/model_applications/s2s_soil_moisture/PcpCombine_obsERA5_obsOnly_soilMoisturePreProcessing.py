80979203fe7c08346c6f77684c0c2877de4889d9
Christina Kalb <kalb@ucar.edu>
2025-03-05 16:29:05 -0700

    Updating documentation location

--- docs/use_cases/model_applications/s2s_soil_moisture/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.py
+++ docs/use_cases/model_applications/s2s_soil_moisture/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.py
@@ -2,7 +2,7 @@
 PCP-Combine: Compute 1m Soil Moisture and 30 year Climatology
 =============================================================
 
-model_applications/s2s/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf
+model_applications/s2s_soil_moisture/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf
 
 """
 
@@ -42,18 +42,12 @@ model_applications/s2s/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf
 # for the appropriate release: https://github.com/dtcenter/METplus/releases
 # This tarball should be unpacked into the directory that you will
 # set the value of INPUT_BASE. See :ref:`running-metplus` section for more information.
-#
-# Data for the Regrid-Data-Plane runsis not contained in the sample data tar files due
-# to its size. Rather, it is stored as additional data in a separate tar file, named
-# additional_data_PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.tar.gz and can be
-# downloaded at https://dtcenter.ucar.edu/dfiles/code/METplus/METplus_Data/v6.1/.
 
 ##############################################################################
 # METplus Components
 # ------------------
 #
-# This use case calls PCP-Combine twice.  There is an additional call to
-# Regrid-Data-Plane that is commented out but may be turned back on by the user.
+# This use case calls Regrid-Data-Plane once and PCP-Combine twice.
 
 ##############################################################################
 # METplus Workflow
@@ -67,22 +61,16 @@ model_applications/s2s/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf
 #
 # **Sequence of forecast leads to process (LEAD_SEQ):** 0
 #
+# The call to Regrid-Data-Plane regrids the data to a 1 degree latitude/longitude grid.
+# It loops over valid time, running once for each month between 1991 and 2020, for a 
+# total of 360 runs.
+
 # The first PCP-Combine run computes the sum of soil moisture of the top three layers
-# multiplied by the thickness of each layer.  It loops over valid time, running once for
-# each valid time which is monthly data for 1991 through 2020.  This is a total of 360
-# PCP-Combine runs.  The second call to PCP-Combine computes a 30 year mean and standard
-# deviation.  It runs once for each month, for a total of 12 runs
-#
-# When turned on, Regrid-Data-Plane regrids the data to a 1 degree latitude/longitude grid.
-# Like the first PCP-Combine run, it loops over valid time, running once for each month
-# between 1991 and 2020, for a total of 360 runs. Regrid-Data-Plane can be turned back on
-# by using the PROCESS_LIST that is commented out:
-#
-# PROCESS_LIST = RegridDataPlane, PcpCombine(create_1m), PcpCombine(obs_mean_stdev)
-#
-# Settings for the Regrid-Data-Plane run are provided in the configuration file.  Data is
-# not included in the tarball, but can be downloaded from the link provided in the Datasets
-# section above.
+# multiplied by the thickness of each layer.  Like the call to Regrid-Data-Plane, it 
+# loops over valid time, running once for each valid time which is monthly data for 1991 
+# through 2020.  This is a total of 360 PCP-Combine runs.  The second call to PCP-Combine 
+# computes a 30 year mean and standard deviation.  It runs once for each month, for a total 
+# of 12 runs.
 
 ##############################################################################
 # METplus Configuration
@@ -90,10 +78,10 @@ model_applications/s2s/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf
 #
 # METplus first loads all of the configuration files found in parm/metplus_config,
 # then it loads any configuration files passed to METplus via the command line,
-# i.e. parm/use_cases/model_applications/s2s/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf
+# i.e. parm/use_cases/model_applications/s2s_soil_moisture/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf
 #
 # .. highlight:: bash
-# .. literalinclude:: ../../../../parm/use_cases/model_applications/s2s/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf
+# .. literalinclude:: ../../../../parm/use_cases/model_applications/s2s_soil_moisture/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf
 
 #############################################################################
 # MET Configuration
@@ -120,7 +108,7 @@ model_applications/s2s/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf
 # Pass the use case configuration file to the run_metplus.py script along
 # with any user-specific system configuration files if desired::
 #
-#   run_metplus.py /path/to/METplus/parm/use_cases/model_applications/s2s/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf /path/to/user_system.conf
+#   run_metplus.py /path/to/METplus/parm/use_cases/model_applications/s2s_soil_moisture/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf /path/to/user_system.conf
 #
 # See :ref:`running-metplus` for more information.
 
@@ -134,9 +122,16 @@ model_applications/s2s/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf
 #
 # Refer to the value set for **OUTPUT_BASE** to find where the output data was generated.
 # Output for this use case will be found in
-# {OUTPUT_BASE}/model_applications/s2s/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.
-# Output from the first netCDF run will be in the ERA5_land_soilm1m subdirectory and will
-# contain 360 files, one for each month, with the format shown below (for January and
+# {OUTPUT_BASE}/model_applications/s2s_soil_moisture/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.
+# Output for the Regrid-Data-Plane run will be in the ERA5_land_regrid subdirectory and will
+# contain 360 files, one for each month, with the format shown below (for January and 
+# February 1991)::
+#
+#  * ERA5_soilm_1x1_layers_mon_199101.nc
+#  * ERA5_soilm_1x1_layers_mon_199102.nc
+#
+# Output from the first PCP-Combine call will be in the ERA5_land_soilm1m subdirectory and 
+# will contain 360 files, one for each month, with the format shown below (for January and
 # February 1991)::
 #
 #  * ERA5_soilm1m_1x1_mon_199101.nc
@@ -158,6 +153,13 @@ model_applications/s2s/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf
 #  * ERA5_soilm1m_1x1_30year_1991_2020_11_mean_stdev.nc
 #  * ERA5_soilm1m_1x1_30year_1991_2020_12_mean_stdev.nc
 #
+# The netCDF output files for the Regrid-Data-Plane run contain three variables (not
+# including the lat/lon fields)::
+#
+#  * swvl1(lat, lon)
+#  * swvl2(lat, lon)
+#  * swvl3(lat, lon)
+#
 # The netCDF output files for the first PCP-Combine run contains one variable (not
 # including the lat/lon fields)::
 #
@@ -168,9 +170,6 @@ model_applications/s2s/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf
 #
 #  * soilm1m_mean(lat, lon)
 #  * soilm1m_stdev(lat, lon)
-#
-# If the Regrid-Data-Plane step is turned on 360 regridded files will be output to
-# the ERA5_land_regrid subdirectory.
 
 ##############################################################################
 # Keywords
@@ -179,6 +178,7 @@ model_applications/s2s/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf
 # .. note::
 #
 #   * S2SAppUseCase
+#   * S2SSoilMoistureAppUseCase
 #   * PCPCombineToolUseCase
 #   * RegridDataPlaneToolUseCase
 #   * NetCDFFileUseCase
@@ -186,4 +186,4 @@ model_applications/s2s/PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.conf
 #   Navigate to the :ref:`quick-search` page to discover other similar use cases.
 #
 #
-# sphinx_gallery_thumbnail_path = '_static/s2s-PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.png'
+# sphinx_gallery_thumbnail_path = '_static/s2s_soil_moisture-PcpCombine_obsERA5_obsOnly_soilMoisturePreProcessing.png'
