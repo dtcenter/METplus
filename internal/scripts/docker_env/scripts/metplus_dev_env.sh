0c1da1bcb9dfc6fd6fcc00c8853180c32c815b78
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2025-03-14 09:03:37 -0600

    add creation of conda env used for development for METplus wrappers (includes METplus wrappers requirements, unit test reqs, and documentation reqs)

---
+++ internal/scripts/docker_env/scripts/metplus_dev_env.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+################################################################################
+# Environment: metplus_dev.v6.1
+# Last Updated: 2025-03-14 (mccabe@ucar.edu)
+# Notes: Adds Python packages used to build documentation and run unit tests
+# Python Packages:
+#
+# Other Content: None
+################################################################################
+
+# version of METplus when the environment was updated, e.g. v5.1
+METPLUS_VERSION=$1
+
+# Conda environment to create
+ENV_NAME=metplus_dev.${METPLUS_VERSION}
+
+# Conda environment to use as base for new environment
+BASE_ENV=diff.${METPLUS_VERSION}
+
+mamba create -y --clone ${BASE_ENV} --name ${ENV_NAME}
+
+mamba install -y --name ${ENV_NAME} -c conda-forge pytest-cov
