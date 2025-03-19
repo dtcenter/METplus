634f2097e27807da8703a4d4b8186770e3e30621
Daniel Adriaansen <dadriaan@ucar.edu>
2025-03-19 22:53:44 +0000

    Adds restriction to only isobaric levels below 100 hPa, and adds some comments.

--- parm/use_cases/model_applications/clouds/GridStat_fcstGFS_obsGOES3D_cloudFractionLWC/read_interp_gfs_025.py
+++ parm/use_cases/model_applications/clouds/GridStat_fcstGFS_obsGOES3D_cloudFractionLWC/read_interp_gfs_025.py
@@ -5,6 +5,7 @@ import xarray as xr
 from metpy.interpolate import log_interpolate_1d
 from metpy.units import units
 
+# Command line input arguments
 model_file = sys.argv[1]
 level = sys.argv[2]
 valid = sys.argv[3]
@@ -19,6 +20,7 @@ if level!='L0':
 valid = datetime.datetime.strptime(valid,'%Y%m%d_%H%M%S')
 init = valid-datetime.timedelta(hours=int(leadhours))
 
+# For total cloud coverage
 if varname=='TCC':
   tcc_var = xr.open_dataset(model_file,engine='cfgrib',filter_by_keys={'typeOfLevel':'isobaricInhPa','shortName':'tcc'},indexpath='')
   gph_var = xr.open_dataset(model_file,engine='cfgrib',filter_by_keys={'typeOfLevel':'isobaricInhPa','shortName':'gh'},indexpath='')
@@ -32,17 +34,26 @@ if varname=='TCC':
   units = "%"
   level = 'Z%d' % (int(level.m))
 
+# For cloud water mixing ratio (content)
 elif varname=='CLWC':
   gph_var = xr.open_dataset(model_file,engine='cfgrib',filter_by_keys={'typeOfLevel':'isobaricInhPa','shortName':'gh'},indexpath='')
   tmp_var = xr.open_dataset(model_file,engine='cfgrib',filter_by_keys={'typeOfLevel':'isobaricInhPa','shortName':'t'},indexpath='')
   clw_var = xr.open_dataset(model_file,engine='cfgrib',filter_by_keys={'typeOfLevel':'isobaricInhPa','shortName':'clwmr'},indexpath='')
-  prs_var = clw_var.isobaricInhPa.broadcast_like(gph_var)
+  
+  # CLWMR is only available at isobaric levels >= 100 hPa
+  prs_lev = clw_var.isobaricInhPa[clw_var.isobaricInhPa>=100.0]
+  prs_var = prs_lev.broadcast_like(gph_var)
   prs_var.attrs['units'] = 'hPa'
   prs_var = prs_var*100.0
   prs_var.attrs['units'] = 'Pa'
   tmp_var['t'].attrs['units'] = 'degK'
   clw_var['clwmr'].attrs['units'] = 'kg/kg'
   gph_var['gh'].attrs['units'] = 'm'
+
+  # Subset the three fields to available pressure levels
+  tmp_var = tmp_var.sel(isobaricInhPa=prs_lev)
+  clw_var = clw_var.sel(isobaricInhPa=prs_lev)
+  gph_var = gph_var.sel(isobaricInhPa=prs_lev)
 
   # Convert units from kg/kg to g/m3
   clw_var = ((clw_var['clwmr']*prs_var)*1000.0/(287.05*tmp_var['t']))
@@ -54,18 +65,27 @@ elif varname=='CLWC':
   units = 'g/m3'
   level = 'Z%d' % (int(level.m))
 
+# For the total atmospheric cloud water content
 elif varname=='CLWP':
 
   gph_var = xr.open_dataset(model_file,engine='cfgrib',filter_by_keys={'typeOfLevel':'isobaricInhPa','shortName':'gh'},indexpath='')
   tmp_var = xr.open_dataset(model_file,engine='cfgrib',filter_by_keys={'typeOfLevel':'isobaricInhPa','shortName':'t'},indexpath='')
   clw_var = xr.open_dataset(model_file,filter_by_keys={'typeOfLevel':'isobaricInhPa','shortName':'clwmr'},indexpath='')
-  prs_var = clw_var.isobaricInhPa.broadcast_like(gph_var)
+
+  # CLWMR is only available at isobaric levels >= 100 hPa
+  prs_lev = clw_var.isobaricInhPa[clw_var.isobaricInhPa>=100.0]
+  prs_var = prs_lev.broadcast_like(gph_var)
   prs_var.attrs['units'] = 'hPa'
   prs_var = prs_var*100.0
   prs_var.attrs['units'] = 'Pa'
   tmp_var['t'].attrs['units'] = 'degK'
   clw_var['clwmr'].attrs['units'] = 'dimensionless'
   gph_var['gh'].attrs['units'] = 'm'
+
+  # Subset the three fields to available pressure levels
+  tmp_var = tmp_var.sel(isobaricInhPa=prs_lev)
+  clw_var = clw_var.sel(isobaricInhPa=prs_lev)
+  gph_var = gph_var.sel(isobaricInhPa=prs_lev)
 
   # Convert units from kg/kg to g/m3 to compare with obs
   clw_var = ((clw_var['clwmr']*prs_var)*1000.0/(287.05*tmp_var['t']))
@@ -79,6 +99,7 @@ else:
   print("UNSUPPORTED varname IN SCRIPT")
   exit(1)
 
+# Set the grid and data attributes as MET expects
 attrs = {}
 attrs['valid'] = valid.strftime('%Y%m%d_%H%M%S')
 attrs['init'] = init.strftime('%Y%m%d_%H%M%S')
