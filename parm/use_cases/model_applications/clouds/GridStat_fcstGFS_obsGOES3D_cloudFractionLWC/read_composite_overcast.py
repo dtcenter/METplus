503bb46d82221ca39b0619ec3e5185c9d73fcd5f
Daniel Adriaansen <dadriaan@ucar.edu>
2025-03-19 22:53:21 +0000

    Adds comments to the Python embedding script for OVERCAST data.

--- parm/use_cases/model_applications/clouds/GridStat_fcstGFS_obsGOES3D_cloudFractionLWC/read_composite_overcast.py
+++ parm/use_cases/model_applications/clouds/GridStat_fcstGFS_obsGOES3D_cloudFractionLWC/read_composite_overcast.py
@@ -3,16 +3,25 @@ import os
 import sys
 import xarray as xr
 
+# Split the input filename and extract the time from the filename.
+# The expected fielname format is:
+# GEO-stitch-cloud3d_L3_2.3.1_YYYYMMDDTHHMMZ_CONUS.nc
 satfile = sys.argv[1]
 elem = os.path.basename(satfile).split('_')
 tstring = elem[3]
 valid = datetime.datetime.strptime(tstring,"%Y%m%dT%H%MZ").strftime('%Y%M%d_%H%M%S')
 
+# Open the file with Xarray
 ds = xr.open_dataset(satfile)
 
+# Use Xarray to sum the cloud_water_content variable across the vertical dimension named altitude
+# to create a total atmospheric cloud water content variable
 met_data = ds['cloud_water_content'].sum(dim='altitude').values
+
+# Flip the data along the horizontal axis due to the way MET reads the data
 met_data = met_data[::-1].copy()
 
+# Dictionaries for data and grid attributes
 attrs = {}
 grid = {}
 
