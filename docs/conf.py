6cc8f6fae537f23185fb7b73ccb1552e88e5f239
jprestop <jpresto@ucar.edu>
2025-03-31 14:24:03 -0600

    Per #2771, adding ToC subprojects

--- docs/conf.py
+++ docs/conf.py
@@ -194,18 +194,18 @@ suppress_warnings = ['ref.citation']
 
 # -- Options for HTML output -------------------------------------------------
 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = ["_themes", ]
 html_js_files = ['pop_ver.js']
-html_css_files = ['theme_override.css']
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_css_files = ['theme_override.css','custom.css']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -225,6 +225,10 @@ sphinx_gallery_conf = {
 
 # -- Intersphinx control ---------------------------------------------------------------
 intersphinx_mapping = {'numpy':("https://docs.scipy.org/doc/numpy/", None)}
+
+# -- Include the CSS -------------------------------------------------------------------
+def setup(app):
+    app.add_css_file("custom.css")
 
 # -- Replace values in docs ------------------------------------------------------------
 rst_epilog = f"""
