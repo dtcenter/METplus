32921674176cdba6c98b43d29ebfc344016e6ca3
Daniel Adriaansen <dadriaan@ucar.edu>
2025-07-08 17:32:55 +0000

    Minor change to use case documentation.

--- docs/use_cases/model_applications/land_surface/PointStat_fcstUFS_obsISMN_SoilMoistureTemp.py
+++ docs/use_cases/model_applications/land_surface/PointStat_fcstUFS_obsISMN_SoilMoistureTemp.py
@@ -193,5 +193,5 @@ model_applications/land_surface/PointStat_fcstUFS_obsISMN_SoilMoistureTemp.conf
 #   * LandSurfaceAppUseCase
 #
 #   Navigate to the :ref:`quick-search` page to discover other similar use cases.
-#
+
 # sphinx_gallery_thumbnail_path = '_static/land_surface-PointStat_fcstUFS_obsISMN_SoilMoistureTemp.png'
