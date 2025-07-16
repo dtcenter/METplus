dbda76faa24f5291973803818f102efd73bfdaea
John Halley Gotway <johnhg@ucar.edu>
2025-07-16 11:21:05 -0600

    Per #3054, port over CVE enhancements for the main_v6.0 branch.

--- .github/jobs/docker_build_metplus_images.sh
+++ .github/jobs/docker_build_metplus_images.sh
@@ -7,12 +7,6 @@ source "${GITHUB_WORKSPACE}"/.github/jobs/bash_functions.sh
 dockerhub_repo=dtcenter/metplus
 dockerhub_repo_analysis=dtcenter/metplus-analysis
 
-# check if tag is official or bugfix release -- no -betaN or -rcN suffix
-is_official=1
-if [[ "${SOURCE_BRANCH}" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-  is_official=0
-fi
-
 # remove v prefix
 metplus_version=${SOURCE_BRANCH:1}
 
@@ -29,7 +23,7 @@ echo "$met_tag"
 
 MET_DOCKER_REPO=met
 if [ "$met_tag" == "develop" ] || [[ "${met_tag}" =~ ^main_v[0-9]+\.[0-9]+ ]]; then
-    MET_DOCKER_REPO=met-dev
+  MET_DOCKER_REPO=met-dev
 fi
 
 # get METplus Analysis tool versions
@@ -58,12 +52,4 @@ if ! time_command docker build -t "$METPLUS_A_IMAGE_NAME" \
        -f "${GITHUB_WORKSPACE}"/internal/scripts/docker/Dockerfile.metplus-analysis \
        "${GITHUB_WORKSPACE}"; then
     exit 1
-fi
-
-# if official release, create X.Y-latest tag as well
-if [ "${is_official}" == 0 ]; then
-    LATEST_TAG=$(echo "$metplus_version" | cut -f1,2 -d'.')-latest
-    docker tag "${METPLUS_IMAGE_NAME}" "${dockerhub_repo}:${LATEST_TAG}"
-    docker tag "${METPLUS_A_IMAGE_NAME}" "${dockerhub_repo_analysis}:${LATEST_TAG}"
-    echo LATEST_TAG="${LATEST_TAG}" >> "$GITHUB_OUTPUT"
 fi
