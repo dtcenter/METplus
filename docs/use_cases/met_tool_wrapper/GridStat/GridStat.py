e151b71047abfb163efaf470debd7f512d047791
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2025-08-27 12:39:15 -0600

    update use case docs to use latest template content

--- docs/use_cases/met_tool_wrapper/GridStat/GridStat.py
+++ docs/use_cases/met_tool_wrapper/GridStat/GridStat.py
@@ -46,26 +46,21 @@ met_tool_wrapper/GridStat/GridStat.conf
 # METplus Configuration
 # ---------------------
 #
-# METplus first loads all of the configuration files found in parm/metplus_config,
-# then it loads any configuration files passed to METplus via the command line
-# with the -c option, i.e. -c parm/use_cases/met_tool_wrapper/GridStat/GridStat.conf
+# parm/use_cases/met_tool_wrapper/GridStat/GridStat.conf
 #
 # .. highlight:: bash
 # .. literalinclude:: ../../../../parm/use_cases/met_tool_wrapper/GridStat/GridStat.conf
 
 ##############################################################################
 # MET Configuration
-# -----------------
+# ---------------------
 #
-# METplus sets environment variables based on user settings in the METplus configuration file. 
-# See :ref:`How METplus controls MET config file settings<metplus-control-met>` for more details. 
+# .. note::
+#     See the :ref:`GridStat MET Configuration<grid-stat-met-conf>`
+#     section of the User's Guide for more information on the environment
+#     variables used in the file below.
 #
-# **YOU SHOULD NOT SET ANY OF THESE ENVIRONMENT VARIABLES YOURSELF! THEY WILL BE OVERWRITTEN BY METPLUS WHEN IT CALLS THE MET TOOLS!**
-#
-# If there is a setting in the MET configuration file that is currently not supported by METplus you'd like to control, please refer to:
-# :ref:`Overriding Unsupported MET config file settings<met-config-overrides>`
-#
-# .. note:: See the :ref:`GridStat MET Configuration<grid-stat-met-conf>` section of the User's Guide for more information on the environment variables used in the file below:
+# parm/met_config/GridStatConfig_wrapped
 #
 # .. highlight:: bash
 # .. literalinclude:: ../../../../parm/met_config/GridStatConfig_wrapped
@@ -74,30 +69,12 @@ met_tool_wrapper/GridStat/GridStat.conf
 # Running METplus
 # ---------------
 #
-# This use case can be run two ways:
+# Provide the use case .conf configuration file to the run_metplus.py script.
 #
-# 1) Passing in GridStat.conf then a user-specific system configuration file::
+# /path/to/METplus/parm/use_cases/met_tool_wrapper/GridStat/GridStat.conf
 #
-#        run_metplus.py -c /path/to/METplus/parm/use_cases/met_tool_wrapper/GridStat/GridStat.conf -c /path/to/user_system.conf
-#
-# 2) Modifying the configurations in parm/metplus_config, then passing in GridStat.conf::
-#
-#        run_metplus.py -c /path/to/METplus/parm/use_cases/met_tool_wrapper/GridStat/GridStat.conf
-#
-# The former method is recommended. Whether you add them to a user-specific configuration file or modify the metplus_config files, the following variables must be set correctly:
-#
-# * **INPUT_BASE** - Path to directory where sample data tarballs are unpacked (See Datasets section to obtain tarballs). This is not required to run METplus, but it is required to run the examples in parm/use_cases
-# * **OUTPUT_BASE** - Path where METplus output will be written. This must be in a location where you have write permissions
-# * **MET_INSTALL_DIR** - Path to location where MET is installed locally
-#
-# Example User Configuration File::
-#
-#   [dir]
-#   INPUT_BASE = /path/to/sample/input/data
-#   OUTPUT_BASE = /path/to/output/dir
-#   MET_INSTALL_DIR = /path/to/met-X.Y 
-#
-# **NOTE:** All of these items must be found under the [dir] section.
+# See the :ref:`running-metplus` section of the System Configuration chapter
+# for more details.
 #
 
 ##############################################################################
