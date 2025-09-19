966d501a9738acef7418ca8c84b3110e01ff0c03
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2025-09-19 11:03:08 -0600

    update test to reflect change in log message

--- internal/tests/pytests/wrappers/command_builder/test_command_builder.py
+++ internal/tests/pytests/wrappers/command_builder/test_command_builder.py
@@ -1226,7 +1226,7 @@ def test_errors_and_defaults(metplus_config):
     cb.isOK = True
     cb.check_gempaktocf(False)
     assert cb.isOK == False
-    assert _in_last_err('[exe] GEMPAKTOCF_JAR was not set in configuration file.', cb.logger)
+    assert _in_last_err('[config] GEMPAKTOCF_JAR was not set in configuration file.', cb.logger)
 
     # test expected ensemble mismatch
     cb.c_dict['N_MEMBERS'] = 1
