e8a10ba1b37e5be1e9ada8627f884834b1143e8b
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2025-09-30 07:46:10 -0600

    Feature dtcenter/METplus-Internal#63 CVE scan workflows (#3119)
    * Per dtcenter/METplus-Internal#63, update workflows to use metplus-action-free-disk-space and metplus-action-scan-docker-images
    
    * Add info about custom METplus actions
    
    * do not cancel other scans if one fails
    
    * remove handling of logs directory because build logs are not saved and scan logs are handled by custom action
    
    * create unique names for artifacts for each version scanned
    
    * remove explicit fail-on-high in case we change the default to turn it on, change scan log artifact to use underscore instead of . to be consistent with other artifacts
    
    * change headers for custom METplus actions to be more descriptive
    
    * Apply suggestions from code review
    
    Co-authored-by: John Halley Gotway <johnhg@ucar.edu>
    
    ---------
    
    Co-authored-by: John Halley Gotway <johnhg@ucar.edu>

--- .github/jobs/docker_build_metplus_images.sh
+++ .github/jobs/docker_build_metplus_images.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# assumes SOURCE_BRANCH is set before calling script
+# assumes SOURCE_BRANCH and METPLUS_VERSION are set before calling script
+# SOURCE_BRANCH is METplus branch or tag used to clone METplus repo
+# METPLUS_VERSION is branch or tag with v prefix removed used to get
+#   component versions and set Docker tag names
 
 source "${GITHUB_WORKSPACE}"/.github/jobs/bash_functions.sh
 
-dockerhub_repo=dtcenter/metplus
-dockerhub_repo_analysis=dtcenter/metplus-analysis
-
-# remove v prefix
-metplus_version=${SOURCE_BRANCH:1}
+dockerhub_repo=${DOCKERHUB_BASE_REPO}
+dockerhub_repo_analysis=${DOCKERHUB_ANALYSIS_REPO}
 
 # if rc is in version number, get main_vX.Y, otherwise get X.Y-latest or develop
-if [[ "${metplus_version}" =~ rc ]]; then
+if [[ "${METPLUS_VERSION}" =~ rc ]]; then
   tag_format="main_v{X}.{Y}"
 else
   tag_format="{X}.{Y}-latest"
 fi
 
 # Get MET tag and adjust MET Docker repo if develop
-met_tag=$("${GITHUB_WORKSPACE}"/develop/metplus/component_versions.py -v "${metplus_version}" -o MET -f ${tag_format} --no-get_dev_version)
+met_tag=$("${GITHUB_WORKSPACE}"/develop/metplus/component_versions.py -v "${METPLUS_VERSION}" -o MET -f ${tag_format} --no-get_dev_version)
 echo "$met_tag"
 
 MET_DOCKER_REPO=met
@@ -27,12 +27,12 @@ if [ "$met_tag" == "develop" ] || [[ "${met_tag}" =~ ^main_v[0-9]+\.[0-9]+ ]]; t
 fi
 
 # get METplus Analysis tool versions
-METDATAIO_VERSION=$("${GITHUB_WORKSPACE}"/develop/metplus/component_versions.py -v "${metplus_version}" -o METdataio)
-METCALCPY_VERSION=$("${GITHUB_WORKSPACE}"/develop/metplus/component_versions.py -v "${metplus_version}" -o METcalcpy)
-METPLOTPY_VERSION=$("${GITHUB_WORKSPACE}"/develop/metplus/component_versions.py -v "${metplus_version}" -o METplotpy)
+METDATAIO_VERSION=$("${GITHUB_WORKSPACE}"/develop/metplus/component_versions.py -v "${METPLUS_VERSION}" -o METdataio)
+METCALCPY_VERSION=$("${GITHUB_WORKSPACE}"/develop/metplus/component_versions.py -v "${METPLUS_VERSION}" -o METcalcpy)
+METPLOTPY_VERSION=$("${GITHUB_WORKSPACE}"/develop/metplus/component_versions.py -v "${METPLUS_VERSION}" -o METplotpy)
 
 # Build metplus image
-METPLUS_IMAGE_NAME=${dockerhub_repo}:${metplus_version}
+METPLUS_IMAGE_NAME=${dockerhub_repo}:${METPLUS_VERSION}
 if ! time_command docker build -t "$METPLUS_IMAGE_NAME" \
        --build-arg SOURCE_VERSION="$SOURCE_BRANCH" \
        --build-arg MET_TAG="$met_tag" \
@@ -43,9 +43,9 @@ if ! time_command docker build -t "$METPLUS_IMAGE_NAME" \
 fi
 
 # Build metplus-analysis image
-METPLUS_A_IMAGE_NAME=${dockerhub_repo_analysis}:${metplus_version}
+METPLUS_A_IMAGE_NAME=${dockerhub_repo_analysis}:${METPLUS_VERSION}
 if ! time_command docker build -t "$METPLUS_A_IMAGE_NAME" \
-       --build-arg METPLUS_BASE_TAG="${metplus_version}" \
+       --build-arg METPLUS_BASE_TAG="${METPLUS_VERSION}" \
        --build-arg METDATAIO_VERSION="${METDATAIO_VERSION}" \
        --build-arg METCALCPY_VERSION="${METCALCPY_VERSION}" \
        --build-arg METPLOTPY_VERSION="${METPLOTPY_VERSION}" \
