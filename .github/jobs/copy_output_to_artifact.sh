61a2f611d97dba1eaa2aabe2080a1199b1f45e96
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2025-09-30 11:55:01 -0600

    update workflows to create output data artifacts from the output directory instead of copying the files to the artifact directory -- this prevents disk space issues when copying a large amount of output files which duplicates each file

--- .github/jobs/copy_output_to_artifact.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Called from .github/workflows/testing.yml
-# Creates directory for output data artifact and
-# copies output data into directory
-
-artifact_name=$1
-mkdir -p artifact/${artifact_name}
-mv ${RUNNER_WORKSPACE}/output/* artifact/${artifact_name}/
