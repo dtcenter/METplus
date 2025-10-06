1e4eb6470ee123e18a2033cc29947a99adeacf7f
John Halley Gotway <johnhg@ucar.edu>
2025-10-06 16:35:56 -0600

    Feature #3098 main_v6.2 diff_logic (#3128)
    * Per #3098, modify the diff logic slightly to handle headerless .tcst files and METdataio data files with the version in the second column.
    
    * Fix bug introduced with changes in this PR that cause an error if the content of the first line does not contain more than 1 space-separated items, e.g. file list files
    
    * Clean up regex: Use \d instead of [0-9] as recommended by SonarQube. Use \. for period character instead of . which is a wildcard character in regex. Support capital and lowercase V at start of version number in case version is formatted with a lower case in the future. Add documentation for method.
    
    * Use math.isclose instead of comparing floats. Initialize idx for print statement in case values_list is empty
    
    * Remove header argument because it isn't actually used. Add helper function to check and remove 'file_list' first line for comparison of equivalent file list files that do and do not contain the first line that has the string 'file_list'
    
    * fix bug where 4D list does not flatten to 2D properly with existing logic
    
    * Per #3098, added unit tests for diffing METdataio reformatter output and TC-Stat output (no header, different version number)
    
    * rename constant values used in tests to be capitalized
    
    * Fix requests/auth.py:36: DeprecationWarning: Non-string usernames will no longer be supported in Requests 3.0.0. Please convert the object you've passed in (None) to a string or bytes object in the near future to avoid problems
    
    * replace duplicate lines
    
    * Per #3098, add back logic to log the header value for columns that have a header value instead of just using the column index
    
    ---------
    
    Co-authored-by: George McCabe <23407799+georgemccabe@users.noreply.github.com>

--- internal/tests/pytests/util/diff_util/test_diff_util.py
+++ internal/tests/pytests/util/diff_util/test_diff_util.py
@@ -9,15 +9,18 @@ from metplus.util import diff_util as du
 from metplus.util import mkdir_p
 
 
-stat_header = 'VERSION MODEL DESC     FCST_LEAD FCST_VALID_BEG  FCST_VALID_END  OBS_LEAD OBS_VALID_BEG   OBS_VALID_END   FCST_VAR FCST_UNITS FCST_LEV OBS_VAR OBS_UNITS OBS_LEV OBTYPE VX_MASK INTERP_MTHD INTERP_PNTS FCST_THRESH OBS_THRESH COV_THRESH ALPHA LINE_TYPE'
-mpr_line_1 = 'V11.1.0 HRRR  ALL_1.25 120000    20220701_200000 20220701_200000 000000   20220701_200000 20220701_200000 HPBL     m          L0       HPBL    m         L0      ADPSFC DENVER  BILIN       4           NA          NA         NA         NA    MPR       5    4       DENVER            39.78616    -104.41425       0         0       2160.80324 1498.06763 AMDAR NA NA NA'
-mpr_line_2 = 'V11.1.0 HRRR  ALL_1.25 120000    20220701_200000 20220701_200000 000000   20220701_200000 20220701_200000 HPBL     m          L0       HPBL    m         L0      ADPSFC DENVER  BILIN       4           NA          NA         NA         NA    MPR       5    4       DENVER            39.78616    -104.41425       0         0       2160.80324 1498.05994 AMDAR NA NA NA'
-file_path_1 = '/some/path/of/fake/file/one'
-file_path_2 = '/some/path/of/fake/file/two'
-file_path_3 = '/some/path/of/fake/file/three'
-csv_header = 'Last Name, First Name, Progress'
-csv_val_1 = 'Mackenzie, Stu, 0.9999'
-csv_val_2 = 'Kenny-Smith, Ambrose, 0.8977'
+STAT_HEADER = 'VERSION MODEL DESC     FCST_LEAD FCST_VALID_BEG  FCST_VALID_END  OBS_LEAD OBS_VALID_BEG   OBS_VALID_END   FCST_VAR FCST_UNITS FCST_LEV OBS_VAR OBS_UNITS OBS_LEV OBTYPE VX_MASK INTERP_MTHD INTERP_PNTS FCST_THRESH OBS_THRESH COV_THRESH ALPHA LINE_TYPE'
+MPR_LINE_1 = 'V11.1.0 HRRR  ALL_1.25 120000    20220701_200000 20220701_200000 000000   20220701_200000 20220701_200000 HPBL     m          L0       HPBL    m         L0      ADPSFC DENVER  BILIN       4           NA          NA         NA         NA    MPR       5    4       DENVER            39.78616    -104.41425       0         0       2160.80324 1498.06763 AMDAR NA NA NA'
+MPR_LINE_2 = 'V11.1.0 HRRR  ALL_1.25 120000    20220701_200000 20220701_200000 000000   20220701_200000 20220701_200000 HPBL     m          L0       HPBL    m         L0      ADPSFC DENVER  BILIN       4           NA          NA         NA         NA    MPR       5    4       DENVER            39.78616    -104.41425       0         0       2160.80324 1498.05994 AMDAR NA NA NA'
+REFORMATTER_HEADER = 'Idx	version	model	desc	fcst_lead	fcst_valid_beg	fcst_valid_end	fcst_init_beg	obs_lead	obs_valid_beg	obs_valid_end	fcst_var	fcst_units	fcst_lev	obs_var	obs_units	obs_lev	obtype	vx_mask	interp_mthd	interp_pnts	fcst_thresh	obs_thresh	cov_thresh	alpha	line_type	total	stat_name	stat_value	stat_ncl	stat_ncu	stat_bcl	stat_bcu'
+REFORMATTER_LINE_1 = '17	V12.1.0	SFS-GSL	NA	60000	1991-06-01	1991-06-01	1991-05-31 18:00:00	0	1991-06-01	1991-06-01	Soil_moisture	mm	0-1m	soilm1m	mm	19910601_000000,*,*	ERA5	FULL	NEAREST	1	NA	NA	-9999	0.05	CNT	21510	FBAR	504.27735	499.27867	509.27604	NA	NA'
+TC_STAT_LINE_1 = 'V12.2.0 GPMI BEST EVENT_EQUAL AL012015 AL 01 ANA 20150508_120000 240000 20150509_120000 NA NA PROBRIRW 31.6 -77.7 32.5 -77.8 NA 54.23894 5.08551 -54 135.63956 80.31028 0 24 24 44 40 50 10 10 TS TS 5 -30 0 -10 0 0 100 10 0 30 0'
+FILE_PATH_1 = '/some/path/of/fake/file/one'
+FILE_PATH_2 = '/some/path/of/fake/file/two'
+FILE_PATH_3 = '/some/path/of/fake/file/three'
+CVS_HEADER = 'Last Name, First Name, Progress'
+CSV_VAL_1 = 'Mackenzie, Stu, 0.9999'
+CSV_VAL_2 = 'Kenny-Smith, Ambrose, 0.8977'
 
 
 DEFAULT_NC = [
@@ -30,6 +33,18 @@ DEFAULT_NC = [
         [[30, 31], [33, 32], [34, 39]],
     ],
     "Temp",  # variable
+]
+
+DEFAULT_NC_WITH_NAN = [
+    DEFAULT_NC[0],
+    DEFAULT_NC[1],
+    DEFAULT_NC[2],
+    [
+        [[1, 2], [3, 4], [5, 6]],
+        [[2, 3], [4, 5], [6, 7]],
+        [[30, 31], [33, 32], [34, np.nan]],
+    ],
+    DEFAULT_NC[4],
 ]
 
 
@@ -93,85 +108,101 @@ def write_test_files(dirname, files):
         # txt B empty file
         ({'filename.txt': ['some', 'text']}, {'filename.txt': []}, None, False),
         # stat header columns
-        ({'filename.stat': [stat_header, mpr_line_1]},
-         {'filename.stat': [f'{stat_header} NEW_COLUMN', mpr_line_1]},
+        ({'filename.stat': [STAT_HEADER, MPR_LINE_1]},
+         {'filename.stat': [f'{STAT_HEADER} NEW_COLUMN', MPR_LINE_1]},
          None, False),
         # stat number of lines
-        ({'filename.stat': [stat_header, mpr_line_1]},
-         {'filename.stat': [stat_header, mpr_line_1, mpr_line_2]},
+        ({'filename.stat': [STAT_HEADER, MPR_LINE_1]},
+         {'filename.stat': [STAT_HEADER, MPR_LINE_1, MPR_LINE_2]},
          None, False),
         # stat number of columns
-        ({'filename.stat': [stat_header, mpr_line_1]},
-         {'filename.stat': [stat_header, f'{mpr_line_1} extra_value']},
+        ({'filename.stat': [STAT_HEADER, MPR_LINE_1]},
+         {'filename.stat': [STAT_HEADER, f'{MPR_LINE_1} extra_value']},
          None, False),
         # stat string
-        ({'filename.stat': [stat_header, mpr_line_1]},
-         {'filename.stat': [stat_header, mpr_line_1.replace('L0', 'Z0')]},
+        ({'filename.stat': [STAT_HEADER, MPR_LINE_1]},
+         {'filename.stat': [STAT_HEADER, MPR_LINE_1.replace('L0', 'Z0')]},
          None, False),
         # stat default precision
-        ({'filename.stat': [stat_header, mpr_line_1]},
-         {'filename.stat': [stat_header, mpr_line_1.replace('39.78616', '39.78615')]},
+        ({'filename.stat': [STAT_HEADER, MPR_LINE_1]},
+         {'filename.stat': [STAT_HEADER, MPR_LINE_1.replace('39.78616', '39.78615')]},
          None, False),
         # stat float override precision
-        ({'filename.stat': [stat_header, mpr_line_1]},
-         {'filename.stat': [stat_header, mpr_line_1.replace('39.78616', '39.78615')]},
+        ({'filename.stat': [STAT_HEADER, MPR_LINE_1]},
+         {'filename.stat': [STAT_HEADER, MPR_LINE_1.replace('39.78616', '39.78615')]},
          4, True),
         # stat out of order
-        ({'filename.stat': [stat_header, mpr_line_1, mpr_line_2]},
-         {'filename.stat': [stat_header, mpr_line_2, mpr_line_1]},
+        ({'filename.stat': [STAT_HEADER, MPR_LINE_1, MPR_LINE_2]},
+         {'filename.stat': [STAT_HEADER, MPR_LINE_2, MPR_LINE_1]},
          4, True),
         # stat version differs
-        ({'filename.stat': [stat_header, mpr_line_1]},
-         {'filename.stat': [stat_header, mpr_line_1.replace('V11.1.0', 'V12.0.0')]},
+        ({'filename.stat': [STAT_HEADER, MPR_LINE_1]},
+         {'filename.stat': [STAT_HEADER, MPR_LINE_1.replace('V11.1.0', 'V12.0.0')]},
          None, True),
         # file_list A without file_list line
-        ({'file_list.txt': [file_path_1, file_path_2, file_path_3]},
-         {'file_list.txt': ['file_list', file_path_1, file_path_2, file_path_3]},
+        ({'file_list.txt': [FILE_PATH_1, FILE_PATH_2, FILE_PATH_3]},
+         {'file_list.txt': ['file_list', FILE_PATH_1, FILE_PATH_2, FILE_PATH_3]},
          None, True),
         # file_list B without file_list line
-        ({'file_list.txt': ['file_list', file_path_1, file_path_2, file_path_3]},
-         {'file_list.txt': [file_path_1, file_path_2, file_path_3]},
+        ({'file_list.txt': ['file_list', FILE_PATH_1, FILE_PATH_2, FILE_PATH_3]},
+         {'file_list.txt': [FILE_PATH_1, FILE_PATH_2, FILE_PATH_3]},
          None, True),
         # file_list out of order
-        ({'file_list.txt': ['file_list', file_path_1, file_path_2, file_path_3]},
-         {'file_list.txt': ['file_list', file_path_2, file_path_3, file_path_1]},
+        ({'file_list.txt': ['file_list', FILE_PATH_1, FILE_PATH_2, FILE_PATH_3]},
+         {'file_list.txt': ['file_list', FILE_PATH_2, FILE_PATH_3, FILE_PATH_1]},
          None, True),
         # csv equal
-        ({'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
-         {'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
+        ({'file_list.csv': [CVS_HEADER, CSV_VAL_1, CSV_VAL_2]},
+         {'file_list.csv': [CVS_HEADER, CSV_VAL_1, CSV_VAL_2]},
          None, True),
         # csv number of columns A
-        ({'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
-         {'file_list.csv': [f'{csv_header}, Position', f'{csv_val_1}, flute', f'{csv_val_2}, harmonica']},
+        ({'file_list.csv': [CVS_HEADER, CSV_VAL_1, CSV_VAL_2]},
+         {'file_list.csv': [f'{CVS_HEADER}, Position', f'{CSV_VAL_1}, flute', f'{CSV_VAL_2}, harmonica']},
          None, False),
         # csv number of columns B
-        ({'file_list.csv': [f'{csv_header}, Position', f'{csv_val_1}, flute', f'{csv_val_2}, harmonica']},
-         {'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
+        ({'file_list.csv': [f'{CVS_HEADER}, Position', f'{CSV_VAL_1}, flute', f'{CSV_VAL_2}, harmonica']},
+         {'file_list.csv': [CVS_HEADER, CSV_VAL_1, CSV_VAL_2]},
          None, False),
         # csv number of lines A
-        ({'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
-         {'file_list.csv': [csv_header, csv_val_1]},
+        ({'file_list.csv': [CVS_HEADER, CSV_VAL_1, CSV_VAL_2]},
+         {'file_list.csv': [CVS_HEADER, CSV_VAL_1]},
          None, False),
         # csv number of lines B
-        ({'file_list.csv': [csv_header, csv_val_1]},
-         {'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
+        ({'file_list.csv': [CVS_HEADER, CSV_VAL_1]},
+         {'file_list.csv': [CVS_HEADER, CSV_VAL_1, CSV_VAL_2]},
          None, False),
         # csv diff default precision
-        ({'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
-         {'file_list.csv': [csv_header, csv_val_1.replace('0.9999', '0.9998'), csv_val_2]},
+        ({'file_list.csv': [CVS_HEADER, CSV_VAL_1, CSV_VAL_2]},
+         {'file_list.csv': [CVS_HEADER, CSV_VAL_1.replace('0.9999', '0.9998'), CSV_VAL_2]},
          None, False),
         # csv diff default precision
-        ({'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
-         {'file_list.csv': [csv_header, csv_val_1.replace('0.9999', '0.9998'), csv_val_2]},
+        ({'file_list.csv': [CVS_HEADER, CSV_VAL_1, CSV_VAL_2]},
+         {'file_list.csv': [CVS_HEADER, CSV_VAL_1.replace('0.9999', '0.9998'), CSV_VAL_2]},
          3, True),
         # csv diff first item
-        ({'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
-         {'file_list.csv': [csv_header, csv_val_1.replace('Mackenzie', 'Art'), csv_val_2]},
+        ({'file_list.csv': [CVS_HEADER, CSV_VAL_1, CSV_VAL_2]},
+         {'file_list.csv': [CVS_HEADER, CSV_VAL_1.replace('Mackenzie', 'Art'), CSV_VAL_2]},
          None, False),
         # csv diff trunc not equal round
-        ({'file_list.csv': [csv_header, csv_val_1, csv_val_2]},
-         {'file_list.csv': [csv_header, csv_val_1.replace('0.9999', '1.0001'), csv_val_2,]},
+        ({'file_list.csv': [CVS_HEADER, CSV_VAL_1, CSV_VAL_2]},
+         {'file_list.csv': [CVS_HEADER, CSV_VAL_1.replace('0.9999', '1.0001'), CSV_VAL_2, ]},
          3, True),
+        # METdataio reformatter output - equal
+        ({'reformatted.txt': [REFORMATTER_HEADER, REFORMATTER_LINE_1]},
+         {'reformatted.txt': [REFORMATTER_HEADER, REFORMATTER_LINE_1]},
+         None, True),
+        # METdataio reformatter output - version differs
+        ({'reformatted.txt': [REFORMATTER_HEADER, REFORMATTER_LINE_1]},
+         {'reformatted.txt': [REFORMATTER_HEADER, REFORMATTER_LINE_1.replace('V12.1.0', 'V12.1.1')]},
+         None, True),
+        # TC-Stat output - equal
+        ({'PROBRIRW_filter_ee.tcst': [TC_STAT_LINE_1]},
+         {'PROBRIRW_filter_ee.tcst': [TC_STAT_LINE_1]},
+         None, True),
+        # TC-Stat output - version differs
+        ({'PROBRIRW_filter_ee.tcst': [TC_STAT_LINE_1]},
+         {'PROBRIRW_filter_ee.tcst': [TC_STAT_LINE_1.replace('V12.1.0', 'V12.2.0')]},
+         None, True),
     ],
 )
 @pytest.mark.diff
@@ -311,17 +342,7 @@ def test_get_file_type_extensions():
         ),
         # Contains nan difference
         (
-            [
-                DEFAULT_NC[0],
-                DEFAULT_NC[1],
-                DEFAULT_NC[2],
-                [
-                    [[1, 2], [3, 4], [5, 6]],
-                    [[2, 3], [4, 5], [6, 7]],
-                    [[30, 31], [33, 32], [34, np.nan]],
-                ],
-                DEFAULT_NC[4],
-            ],
+            DEFAULT_NC_WITH_NAN,
             None,
             False,
             ["Variable Temp contains NaN. Comparing each value"],
@@ -356,17 +377,7 @@ def test_nc_is_equal(
     "nc_data,fields,expected,check_print",
     [
         (
-            [
-                DEFAULT_NC[0],
-                DEFAULT_NC[1],
-                DEFAULT_NC[2],
-                [
-                    [[1, 2], [3, 4], [5, 6]],
-                    [[2, 3], [4, 5], [6, 7]],
-                    [[30, 31], [33, 32], [34, np.nan]],
-                ],
-                DEFAULT_NC[4],
-            ],
+            DEFAULT_NC_WITH_NAN,
             None,
             True,
             ["Variable Temp contains NaN. Comparing each value"],
