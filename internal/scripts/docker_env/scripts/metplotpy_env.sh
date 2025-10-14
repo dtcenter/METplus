79e4ff3f9a5d617dee3c939d7834b913c9293596
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2025-10-14 14:00:52 -0600

    update plotly and kaleido versions for metplotpy env

--- internal/scripts/docker_env/scripts/metplotpy_env.sh
+++ internal/scripts/docker_env/scripts/metplotpy_env.sh
@@ -1,18 +1,18 @@
 #! /bin/sh
 
 ################################################################################
-# Environment: metplotpy.v5.1
-# Last Updated: 2023-01-30 (mccabe@ucar.edu)
+# Environment: metplotpy.v6.0
+# Last Updated: 2025-10-14 (mccabe@ucar.edu)
 # Notes: Adds Python packages needed to run METplotpy and METcalcpy
 # Python Packages:
 #   matplotlib==3.5.2
 #   scipy==1.8.1
-#   plotly==5.9.0
+#   plotly==6.1.1
 #   xarray==2022.3.0
 #   netcdf4==1.6.2
 #   pyyaml==6.0
 #   statsmodels==0.13.2
-#   python-kaleido==0.2.1
+#   python-kaleido==1.0.0
 #   imageio==2.19.3
 #   imutils==0.5.4
 #   scikit-image==0.19.3
@@ -33,4 +33,4 @@ BASE_ENV=metplus_base.${METPLUS_VERSION}
 
 mamba create -y --clone ${BASE_ENV} --name ${ENV_NAME}
 
-mamba install -y --name ${ENV_NAME} -c conda-forge matplotlib==3.6.3 scipy>=1.11.1 plotly==5.13.0 xarray==2023.1.0 netcdf4==1.6.2 pyyaml==6.0 python-kaleido>=0.2.1 imageio==2.25.0 imutils==0.5.4 scikit-image==0.19.3 pint==0.20.1 metpy==1.4.0 cartopy==0.21.1
+mamba install -y --name ${ENV_NAME} -c conda-forge matplotlib==3.6.3 scipy>=1.11.1 plotly==6.1.1 xarray==2023.1.0 netcdf4==1.6.2 pyyaml==6.0 python-kaleido==1.0.0 imageio==2.25.0 imutils==0.5.4 scikit-image==0.19.3 pint==0.20.1 metpy==1.4.0 cartopy==0.21.1
