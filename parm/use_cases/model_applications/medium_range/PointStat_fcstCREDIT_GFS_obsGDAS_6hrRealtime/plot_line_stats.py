fab47e83ff345b70e02c00c1e4fd03eb5eb7d255
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2025-10-24 13:40:43 -0600

    reduce duplicate code by consolidating scripts into single script to be called with an argument

--- parm/use_cases/model_applications/medium_range/PointStat_fcstCREDIT_GFS_obsGDAS_6hrRealtime/plot_line_stats.py
+++ parm/use_cases/model_applications/medium_range/PointStat_fcstCREDIT_GFS_obsGDAS_6hrRealtime/plot_line_stats.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 from time import perf_counter
 import logging
 import yaml
 import metcalcpy.util.read_env_vars_in_config as readconfig
 from metplotpy.plots.line import line
 
-def main():
+def main(plot_type):
 
     # Read the input data, input files, and output files
-    ploting_stat_list_str = os.environ['PLOTTING_CNT_FCST_STAT_LIST'].split(',')
-    plotting_stat_list = [ps.lstrip() for ps in ploting_stat_list_str]
-    plotting_vars_str = os.environ['PLOTTING_CNT_FCST_VAR_LIST'].split(',')
+    plotting_stat_list_str = os.environ[f'PLOTTING_{plot_type}_FCST_STAT_LIST'].split(',')
+    plotting_stat_list = [ps.lstrip() for ps in plotting_stat_list_str]
+    plotting_vars_str = os.environ[f'PLOTTING_{plot_type}_FCST_VAR_LIST'].split(',')
     plotting_vars = [pv.lstrip() for pv in plotting_vars_str]
-    var_longnames_str = os.environ['PLOTTING_CNT_FCST_VAR_NAME_LIST'].split(',')
+    var_longnames_str = os.environ[f'PLOTTING_{plot_type}_FCST_VAR_NAME_LIST'].split(',')
     var_longnames = [vl.lstrip() for vl in var_longnames_str]
-    var_units_str = os.environ['PLOTTING_CNT_FCST_VAR_UNITS_LIST'].split(',')
+    var_units_str = os.environ[f'PLOTTING_{plot_type}_FCST_VAR_UNITS_LIST'].split(',')
     var_units = [vu.lstrip() for vu in var_units_str]
-    plotting_masks_str = os.environ['PLOTTING_CNT_VX_MASK_LIST'].split(',')
+    plotting_masks_str = os.environ[f'PLOTTING_{plot_type}_VX_MASK_LIST'].split(',')
     plotting_masks = [pm.lstrip() for pm in plotting_masks_str]
-    yaml_files_str = os.environ['PLOTTING_CNT_YAML_CONFIG_FILE_LIST'].split(',')
+    yaml_files_str = os.environ[f'PLOTTING_{plot_type}_YAML_CONFIG_FILE_LIST'].split(',')
     yaml_files = [yf.lstrip() for yf in yaml_files_str]
-    yaml_file_dir = os.environ['PLOTTING_CNT_YAML_CONFIG_DIR']
-    plot_output_dir = os.environ['PLOTTING_CNT_OUTPUT_DIR']
+    yaml_file_dir = os.environ[f'PLOTTING_{plot_type}_YAML_CONFIG_DIR']
+    plot_output_dir = os.environ[f'PLOTTING_{plot_type}_OUTPUT_DIR']
 
     # Make output plot directory if if doesn't exist
     if not os.path.exists(plot_output_dir):
@@ -31,7 +32,7 @@ def main():
 
     # Check to make sure that the lists are the same size
     if not len(plotting_vars) == len(var_longnames) == len(var_units):
-        raise RuntimeError('The length of PLOTTING_CNT_FCST_VAR_LIST must be equal to the lengths of PLOTTING_CNT_FCST_VAR_NAME_LIST and PLOTTING_CNT_FCST_VAR_UNITS_LIST')
+        raise RuntimeError(f'The length of PLOTTING_{plot_type}_FCST_VAR_LIST must be equal to the lengths of PLOTTING_{plot_type}_FCST_VAR_NAME_LIST and PLOTTING_{plot_type}_FCST_VAR_UNITS_LIST')
 
     # Loop through the stats
     for s in plotting_stat_list:
@@ -43,6 +44,10 @@ def main():
             os.environ['PLOTTING_STAT_LONG'] = 'Mean Error'
         elif s == 'RMSE':
             os.environ['PLOTTING_STAT_LONG'] = 'Root Mean Squared Error'
+        elif s == 'CSI':
+            os.environ['PLOTTING_STAT_LONG'] = 'Critical Success Index'
+        elif s == 'FBIAS':
+            os.environ['PLOTTING_STAT_LONG'] = 'Frequency Bias'
         else:
             os.environ['PLOTTING_STAT_LONG'] = s
 
@@ -50,25 +55,25 @@ def main():
         for v,n,u in zip(plotting_vars,var_longnames,var_units):
 
             os.environ['FCST_VAR_VAL1'] = v
-            os.environ['PLOTTING_CNT_LONG_VAR'] = n
-            os.environ['PLOTTING_CNT_VAR_UNITS'] = u
+            os.environ[f'PLOTTING_{plot_type}_LONG_VAR'] = n
+            os.environ[f'PLOTTING_{plot_type}_VAR_UNITS'] = u
 
             # Loop throuth masks
             for m in plotting_masks:
 
-                os.environ['PLOTTING_CNT_OUTPUT_FILENAME'] = os.path.join(plot_output_dir,v+'_'+m)
-                os.environ['PLOTTING_CNT_MASK'] = m
+                os.environ[f'PLOTTING_{plot_type}_OUTPUT_FILENAME'] = os.path.join(plot_output_dir,v+'_'+m)
+                os.environ[f'PLOTTING_{plot_type}_MASK'] = m
 
                 # Loop through data
                 for i in yaml_files:
 
                     # Build yaml config file name
-                    os.environ['PLOTTING_CNT_YAML_CONFIG_NAME'] = os.path.join(yaml_file_dir,i)
+                    os.environ[f'PLOTTING_{plot_type}_YAML_CONFIG_NAME'] = os.path.join(yaml_file_dir,i)
 
                     # Read in the YAML configuration file.  Environment variables in
                     # the configuration file are supported.
                     try:
-                        input_config_file = os.getenv("PLOTTING_CNT_YAML_CONFIG_NAME", "custom_line.yaml")
+                        input_config_file = os.getenv(f"PLOTTING_{plot_type}_YAML_CONFIG_NAME", "custom_line.yaml")
                         settings = readconfig.parse_config(input_config_file)
                         logging.info(settings)
                     except yaml.YAMLError as exc:
@@ -87,4 +92,8 @@ def main():
                         print(val_er)
 
 if __name__ == "__main__":
-  main()
+    if len(sys.argv) < 2:
+        print(f"ERROR: {__file__} - Must provide at least 1 input file argument")
+        sys.exit(1)
+    plot_type_id = sys.argv[1].upper()
+    main(plot_type_id)
