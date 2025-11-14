7ea6465849bbf645e0affc31d4317093fd828d79
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2025-11-14 12:24:54 -0700

    update METexpress version to latest bugfix release

--- metplus/component_versions.py
+++ metplus/component_versions.py
@@ -31,7 +31,7 @@ VERSION_LOOKUP = {
         'metcalcpy': '3.2.0',
         'metdataio': '3.2.0',
         'metviewer': '6.2.0',
-        'metexpress': '6.2.0',
+        'metexpress': '6.2.1',
         'metbaseimage': '3.4.6',
     },
     '6.1': {
