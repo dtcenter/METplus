0e145ca24d8a3db8b41cb8b33053ac69927d5aca
Christina Kalb <kalb@ucar.edu>
2025-11-20 13:19:14 -0700

    Ai documentation update (#3160)
    * Adding AIWP as a feature to search
    
    * Update GridStat_fcstCREDIT_GFS_obsGFS_6hrRealtime.py
    
    * adding AIWP to Point-Stat use case
    
    * Update GridStat_fcstCREDIT_GFS_obsGFS_6hrRealtime.py
    
    * Update PointStat_fcstCREDIT_GFS_obsGDAS_6hrRealtime.py
    
    * Update quicksearch.rst
    
    * Update PointStat_fcstCREDIT_GFS_obsGDAS_6hrRealtime.py
    
    * Update GridStat_fcstCREDIT_GFS_obsGFS_6hrRealtime.py
    
    * Update GridStat_fcstCREDIT_GFS_obsGFS_6hrRealtime.py
    
    * Update PointStat_fcstCREDIT_GFS_obsGDAS_6hrRealtime.py

--- docs/use_cases/model_applications/medium_range/PointStat_fcstCREDIT_GFS_obsGDAS_6hrRealtime.py
+++ docs/use_cases/model_applications/medium_range/PointStat_fcstCREDIT_GFS_obsGDAS_6hrRealtime.py
@@ -272,7 +272,7 @@ model_applications/medium_range/PointStat_fcstCREDIT_GFS_obsGDAS_6hrRealtime.con
 #
 # .. note::
 #
-#   * DataIngestToolUseCase
+#   * DataIngestUseCase
 #   * PB2NCToolUseCase
 #   * PointStatToolUseCase
 #   * StatAnalysisToolUseCase
@@ -284,6 +284,7 @@ model_applications/medium_range/PointStat_fcstCREDIT_GFS_obsGDAS_6hrRealtime.con
 #   * METdbLoadUseCase
 #   * METcalcpyUseCase
 #   * METplotpyUseCase
+#   * AIWPUseCase
 #
 #   Navigate to the :ref:`quick-search` page to discover other similar use cases.
 #
