3271523da872986b9f954b579cc0d57024336f31
John Halley Gotway <johnhg@ucar.edu>
2026-02-06 18:44:47 +0000

    Per #3220, using len() doesn't work for int64 variables. Easier just to remove the Instance type from the log message.

--- metplus/util/diff_util.py
+++ metplus/util/diff_util.py
@@ -867,8 +867,6 @@ def _nc_fields_are_equal(field, nc_a, nc_b):
         return False, f"ERROR: Field {field} not found"
 
     msg = f"Field: {field}\nVar_A:{var_a}\nVar_B:{var_b}"
-    if len(var_a) > 0:
-      msg += f"\nInstance type: {type(var_a[0])}"
 
     values_a = var_a[:]
     values_b = var_b[:]
