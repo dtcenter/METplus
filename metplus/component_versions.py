82ee5352c4b750fe3e1884dcd94cad1e898bc72f
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2026-02-25 15:56:23 -0700

    update METviewer for bugfix release

--- metplus/component_versions.py
+++ metplus/component_versions.py
@@ -30,7 +30,7 @@ VERSION_LOOKUP = {
         'metplotpy': '3.2.0',
         'metcalcpy': '3.2.0',
         'metdataio': '3.2.0',
-        'metviewer': '6.2.2',
+        'metviewer': '6.2.3',
         'metexpress': '6.2.3',
         'metbaseimage': '3.4.9',
     },
