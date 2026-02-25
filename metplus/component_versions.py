b5297779ef2c72be2269fbe2d40596bd5a9bc57c
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2026-02-25 14:57:37 -0700

    update versions of base image v3.4

--- metplus/component_versions.py
+++ metplus/component_versions.py
@@ -32,7 +32,7 @@ VERSION_LOOKUP = {
         'metdataio': '3.2.0',
         'metviewer': '6.2.2',
         'metexpress': '6.2.3',
-        'metbaseimage': '3.4.8',
+        'metbaseimage': '3.4.9',
     },
     '6.1': {
         'metplus': '6.1.0',
@@ -42,7 +42,7 @@ VERSION_LOOKUP = {
         'metdataio': '3.1.0',
         'metviewer': '6.1.0',
         'metexpress': '6.1.4',
-        'metbaseimage': '3.4.8',
+        'metbaseimage': '3.4.9',
     },
     '6.0': {
         'metplus': '6.0.0',
