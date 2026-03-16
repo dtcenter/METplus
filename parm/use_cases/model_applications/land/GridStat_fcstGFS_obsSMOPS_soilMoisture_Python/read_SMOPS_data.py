f35af4903a96320cacbe3a57e0761d7f08cdb1bb
j-opatz <jopatz@ucar.edu>
2026-03-16 22:29:49 +0000

    adding use case material

---
+++ parm/use_cases/model_applications/land/GridStat_fcstGFS_obsSMOPS_soilMoisture_Python/read_SMOPS_data.py
@@ -0,0 +1,43 @@
+import sys
+from netCDF4 import Dataset
+import numpy as np
+
+filename, vari_name = sys.argv[1:]
+
+f_in = Dataset(filename, 'r')
+
+#field = f_in[vari_name][:]
+met_data = f_in[vari_name][:]
+#met_data = field[:]
+date_info = f_in.Date_Start
+
+
+attrs = {
+
+        'valid': str(date_info)+'_000000',
+        'init': str(date_info)+'_000000',
+        'name': vari_name,
+        'long_name': f_in[vari_name].long_name,
+        'lead': '00',
+        'accum': '00',
+        'level': 'UNKNOWN',
+        'units': f_in[vari_name].units,
+
+        'grid': {
+            'name': 'Global 0.25 degree',
+            'type': 'LatLon',
+            'lat_ll': -90.0,
+            'lon_ll': -180.0,
+            'delta_lat': 0.25,
+            'delta_lon': 0.25,
+
+            'Nlon': f_in.dimensions['Longitude'].size,
+            'Nlat': f_in.dimensions['Latitude'].size,
+            }
+        }
+
+#print some output to show script ran successfully
+print("Input file: " + repr(filename))
+print("Variable name: " + repr(vari_name))
+print("Attributes:\t" + repr(attrs))
+f_in.close()
